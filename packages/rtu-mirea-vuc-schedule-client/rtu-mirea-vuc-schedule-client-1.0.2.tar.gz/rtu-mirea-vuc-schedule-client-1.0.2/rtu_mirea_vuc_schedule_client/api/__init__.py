eb0e2f9937e9b129c1de8a1340ba10ce48aaacdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:14:51 +0000

    Add package rtu-mirea-vuc-schedule-client/1.0.2/rtu-mirea-vuc-schedule-client-1.0.2.tar.gz

---
+++ packages/rtu-mirea-vuc-schedule-client/rtu-mirea-vuc-schedule-client-1.0.2.tar.gz/rtu-mirea-vuc-schedule-client-1.0.2/rtu_mirea_vuc_schedule_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from rtu_mirea_vuc_schedule_client.api.healthcheck_api import HealthcheckApi
+from rtu_mirea_vuc_schedule_client.api.schedule_api import ScheduleApi
+from rtu_mirea_vuc_schedule_client.api.workbook_api import WorkbookApi
+
