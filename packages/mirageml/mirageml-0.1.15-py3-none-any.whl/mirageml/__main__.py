c41804351a5abafe1e29eb5ab2da6df8f80a1b3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 14:05:53 +0000

    Add package mirageml/0.1.15/mirageml-0.1.15-py3-none-any.whl

---
+++ packages/mirageml/mirageml-0.1.15-py3-none-any.whl/mirageml/__main__.py
@@ -0,0 +1,205 @@
+import sys
+import time
+from typing import List
+
+import typer
+
+app = typer.Typer(
+    no_args_is_help=True,
+    add_completion=False,
+    rich_markup_mode="markdown",
+    help="""
+    MirageML CLI
+
+    **You can use 'mirage' or 'mml' to call the package.**
+
+
+
+    See the website at https://mirageml.com/ for documentation and more information
+    about running code on MirageML.
+    """,
+    # pretty_exceptions_short=False, pretty_exceptions_enable=False # Needed for debugging
+)
+
+config_app = typer.Typer(name="config", help="Manage the config", no_args_is_help=True)
+add_app = typer.Typer(name="add", help="Add a new resource", no_args_is_help=True)
+list_app = typer.Typer(name="list", help="List resources", no_args_is_help=True)
+sync_app = typer.Typer(name="sync", help="Sync resources", no_args_is_help=True)
+delete_app = typer.Typer(name="delete", help="Delete resources", no_args_is_help=True)
+
+app.add_typer(config_app, rich_help_panel="Utils and Configs")
+app.add_typer(add_app, rich_help_panel="Manage Resources")
+app.add_typer(list_app, rich_help_panel="Manage Resources")
+app.add_typer(sync_app, rich_help_panel="Manage Resources")
+app.add_typer(delete_app, rich_help_panel="Manage Resources")
+
+
+@app.callback()
+def main(ctx: typer.Context):
+    import keyring
+    import segment.analytics as analytics
+
+    from .constants import ANALYTICS_WRITE_KEY, SERVICE_ID, fetch_new_access_token
+
+    analytics.write_key = ANALYTICS_WRITE_KEY
+
+    user_id = keyring.get_password(SERVICE_ID, "user_id")
+    expires_at = keyring.get_password(SERVICE_ID, "expires_at")
+    if not user_id and ctx.invoked_subcommand != "login":
+        typer.echo("Please login first. Run `mirageml login`")
+        raise typer.Exit()
+    elif expires_at and float(expires_at) < time.time() and ctx.invoked_subcommand != "login":
+        try:
+            fetch_new_access_token()
+            analytics.identify(user_id)
+        except Exception:
+            typer.echo("Please login again. Run `mirageml login`")
+            raise typer.Exit()
+
+    if user_id:
+        full_command = " ".join(sys.argv[1:])
+        analytics.track(user_id, "command", {"command": full_command})
+
+
+@app.command(name="help", hidden=True)
+def custom_help():
+    # Your custom help message
+    import os
+
+    os.system("mml --help")
+
+
+@app.command(name="login")
+def login_command():
+    """Login to Mirage ML"""
+    from .commands import login
+
+    login()
+
+
+def generate_help_text():
+    from .constants import help_list_sources
+
+    return help_list_sources()
+
+
+@app.command(name="chat")
+def chat_command(
+    filepaths: List[str] = typer.Option(
+        None,
+        "--files",
+        "-f",
+        help="Path to files/directories to use as context. \n\n\n**"
+        + sys.argv[0].split("/")[-1]
+        + " chat -f {filepath} -f {directory}**",
+    ),
+    urls: List[str] = typer.Option(
+        None,
+        "--urls",
+        "-u",
+        help="URLs to use as context. \n\n\n**" + sys.argv[0].split("/")[-1] + " chat -u {url1} -u {url2}**",
+    ),
+    sources: List[str] = typer.Option([], "--sources", "-s", help=generate_help_text()),
+):
+    """Chat with MirageML"""
+    for url in urls:
+        if not url.startswith("https://"):
+            typer.echo("Every url must start with https://")
+            raise typer.Exit()
+
+    typer.secho(
+        "Starting chat. Type 'exit' to end the chat.",
+        fg=typer.colors.BRIGHT_GREEN,
+        bold=True,
+    )
+    from .commands import chat
+
+    chat(files=filepaths, urls=urls, sources=sources)
+
+
+@config_app.command(name="show")
+def show_config_command():
+    """Show the current config"""
+    from .commands import show_config
+
+    show_config()
+
+
+@config_app.command(name="set")
+def set_config_command():
+    """
+    Set the config file for MirageML
+    """
+    from .commands import set_config
+
+    set_config()
+
+
+# List Commands
+@list_app.command(name="plugins")
+def list_plugins_command():
+    """List connected plugins"""
+    from .commands import list_plugins
+
+    list_plugins()
+
+
+@list_app.command(name="sources")
+def list_sources_command():
+    """List created sources"""
+    from .commands import list_sources
+
+    list_sources()
+
+
+# Add Commands
+@add_app.command(name="plugin")
+def add_plugin_command(name: str):
+    """Add a plugin by name"""
+    from .commands import add_plugin
+
+    add_plugin({"plugin": name})
+
+
+@add_app.command(name="source")
+def add_source_command(link: str = typer.Argument(default="", help="Link to the source")):
+    """Add a new source"""
+    from .commands import add_source
+
+    if not link:
+        while True:
+            link = input("Link for the source: ")
+            if not link.startswith("https://"):
+                typer.echo("Please enter a valid link starting with https://")
+                continue
+            break
+    from urllib.parse import urlparse
+
+    parsed_url = urlparse(link)
+    name = parsed_url.netloc.split(".")[0]
+    if name in ["docs", "www", "en", "platform", "blog"]:
+        name = parsed_url.netloc.split(".")[1]
+    name = input(f"Name for the source [default: {name}]: ") or name
+    add_source(name, link)
+
+
+# Delete Commands
+@delete_app.command(name="source", no_args_is_help=True)
+def delete_source_command(names: List[str] = typer.Argument(help="Names of the sources to delete")):
+    """Delete a source"""
+    from .commands import delete_source
+
+    delete_source(names)
+
+
+# Sync Commands
+@sync_app.command(name="plugin")
+def sync_plugin_command(name: str):
+    """Sync a plugin"""
+    from .commands import sync_plugin
+
+    sync_plugin({"plugin": name})
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/mirageml/mirageml-0.1.15-py3-none-any.whl/mirageml/commands/utils/brain.py
@@ -0,0 +1,86 @@
+import os
+import sys
+from io import StringIO
+
+import requests
+
+from ...constants import (
+    LLM_GPT_ENDPOINT,
+    VECTORDB_EMBED_ENDPOINT,
+    get_headers,
+)
+
+PACKAGE_DIR = os.path.dirname(__file__)
+os.environ["TRANSFORMERS_CACHE"] = os.path.join(PACKAGE_DIR, "models")
+
+
+def local_get_embedding(text_list, embedding_model_id="BAAI/bge-small-en-v1.5"):
+    from sentence_transformers import SentenceTransformer
+
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    model_dir = os.path.join(PACKAGE_DIR, "models", embedding_model_id)
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir, exist_ok=True)
+        print("Downloading model to:", model_dir)
+        print("This will take a few minutes and only happen once!")
+
+    # Suppress stdout/stderr
+    original_stdout, original_stderr = sys.stdout, sys.stderr
+    sys.stdout, sys.stderr = StringIO(), StringIO()
+
+    model = SentenceTransformer(embedding_model_id, cache_folder=model_dir)
+    embeddings = model.encode(text_list, normalize_embeddings=False)
+
+    # Restore stdout/stderr
+    sys.stdout, sys.stderr = original_stdout, original_stderr
+
+    # Convert the embeddings to a list
+    embeddings = embeddings.tolist()
+    return embeddings
+
+
+def local_llm_call(messages, llm_model_id="TheBloke/Llama-2-7b-Chat-GGUF", stream=False):
+    from ctransformers import AutoModelForCausalLM
+
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    model_dir = os.path.join(PACKAGE_DIR, "models", llm_model_id)
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir, exist_ok=True)
+        print("Downloading model to:", model_dir)
+        print("This will take a few minutes and only happen once!")
+
+    # Suppress stdout/stderr
+    original_stdout, original_stderr = sys.stdout, sys.stderr
+    sys.stdout, sys.stderr = StringIO(), StringIO()
+
+    llm = AutoModelForCausalLM.from_pretrained(
+        "TheBloke/Llama-2-7b-Chat-GGUF",
+        model_file="llama-2-7b-chat.Q4_K_M.gguf",
+        model_type="llama",
+    )
+
+    # Restore stdout/stderr
+    sys.stdout, sys.stderr = original_stdout, original_stderr
+    formatted_messages = "\n".join([x["content"] for x in messages])
+
+    if stream:
+        return llm(formatted_messages, stream=True)
+    else:
+        return llm(formatted_messages)
+
+
+def get_embedding(text_list, model="BAAI/bge-small-en-v1.5", local=False):
+    if local:
+        return local_get_embedding(text_list, embedding_model_id=model)
+    response = requests.post(VECTORDB_EMBED_ENDPOINT, json={"data": text_list}, headers=get_headers())
+    response.raise_for_status()  # Raise an exception if the request failed
+    return response.json()["embedding"]
+
+
+def llm_call(messages, model="gpt-3.5-turbo", stream=False, local=False):
+    if local:
+        return local_llm_call(messages, stream=stream)
+    json_data = {"model": model, "messages": messages, "stream": stream}
+    return requests.post(LLM_GPT_ENDPOINT, json=json_data, headers=get_headers(), stream=stream)
