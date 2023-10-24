ea18ae1e1dce4f7e7afd89c6b28144ca3fb7631f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:06:55 +0000

    Add package farewelltyson/0.0.3/farewelltyson-0.0.3.tar.gz

---
+++ packages/farewelltyson/farewelltyson-0.0.3.tar.gz/farewelltyson-0.0.3/farewelltyson/get_the_data.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def get_the_data():
+    return pd.read_csv('data/all_data.csv')
---
+++ packages/farewelltyson/farewelltyson-0.0.3.tar.gz/farewelltyson-0.0.3/farewelltyson/take_action.py
@@ -0,0 +1,5 @@
+def take_action(a):
+    if a == 'abc':
+        print('yes')
+    else:
+        print('no')
