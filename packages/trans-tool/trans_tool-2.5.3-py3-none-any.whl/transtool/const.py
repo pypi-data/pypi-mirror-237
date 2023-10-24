2b77a5db7bc4c0977ae78e5fedfc9e4e479f4ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 14:04:58 +0000

    Add package trans-tool/2.5.3/trans_tool-2.5.3-py3-none-any.whl

---
+++ packages/trans-tool/trans_tool-2.5.3-py3-none-any.whl/transtool/const.py
@@ -0,0 +1,32 @@
+"""
+#
+# trans-tool
+# The translation files checker and syncing tool.
+#
+# Copyright ©2021-2023 Marcin Orlowski <MarcinOrlowski.com>
+# https://github.com/MarcinOrlowski/trans-tool/
+#
+"""
+
+from typing import List
+
+
+class Const(object):
+    APP_NAME: str = 'trans-tool'
+    APP_VERSION: str = '2.5.3'
+    APP_URL: str = 'https://github.com/MarcinOrlowski/trans-tool/'
+    APP_YEARS: str = '2021-2023'
+
+    APP_DESCRIPTION: List[str] = [
+        f'{APP_NAME} v{APP_VERSION} * Copyright {APP_YEARS} by Marcin Orlowski.',
+        'The translation files checker and syncing tool.',
+        f'{APP_URL}',
+    ]
+
+    class RC(object):  # noqa: WPS431
+        """
+        Application return codes.
+        """
+
+        OK: int = 0
+        TRANSLATION_SYNTAX_ERROR = 200
