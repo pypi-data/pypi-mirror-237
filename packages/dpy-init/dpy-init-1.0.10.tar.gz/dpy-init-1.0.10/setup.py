b618abfefdf3ff571fff2ace38526655d6010208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 14:06:03 +0000

    Add package dpy-init/1.0.10/dpy-init-1.0.10.tar.gz

---
+++ packages/dpy-init/dpy-init-1.0.10.tar.gz/dpy-init-1.0.10/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='dpy-init',
+    version='1.0.10',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dpy-init = dpy_init.dpy_init:dpy_init',
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
