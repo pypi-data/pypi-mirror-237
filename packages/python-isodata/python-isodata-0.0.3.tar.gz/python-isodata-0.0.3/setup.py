835c401cabfca652fe103f3682ee69f85f7f528b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:07:16 +0000

    Add package python-isodata/0.0.3/python-isodata-0.0.3.tar.gz

---
+++ packages/python-isodata/python-isodata-0.0.3.tar.gz/python-isodata-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='python-isodata',
+    version='0.0.3',
+    packages=['src'],
+    url='https://github.com/CaffeineLab/isodata',
+    license='GNU General Public License v3.0',
+    author='Glenn',
+    author_email='glenn@caffeinelab.com',
+    description='Energy Market downloading tools'
+)
