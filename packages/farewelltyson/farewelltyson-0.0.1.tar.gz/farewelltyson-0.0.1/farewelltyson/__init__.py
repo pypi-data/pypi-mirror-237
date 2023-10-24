fbbdf2252b5ae91f8a73f50eaf1850fd254d06de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 15:06:32 +0000

    Add package farewelltyson/0.0.1/farewelltyson-0.0.1.tar.gz

---
+++ packages/farewelltyson/farewelltyson-0.0.1.tar.gz/farewelltyson-0.0.1/farewelltyson/__init__.py
@@ -0,0 +1,5 @@
+print("You have been challenged to a Pokemon card battle by your rival, but you have no cards. To start with you should check your balance, then you should buy packs and finally you should open packs. When you are ready, start the battle.")
+
+from buy_packs import buy_packs
+from check_your_balance import check_your_balance
+from open_packs import open_packs
