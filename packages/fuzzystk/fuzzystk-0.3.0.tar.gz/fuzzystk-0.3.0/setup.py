989ae9960e0dfec27f35086162071cf3b0b7017a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:14:49 +0000

    Add package fuzzystk/0.3.0/fuzzystk-0.3.0.tar.gz

---
+++ packages/fuzzystk/fuzzystk-0.3.0.tar.gz/fuzzystk-0.3.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages 
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='fuzzystk',
+    version='0.3.0',
+    license='MIT License',
+    author='Henrique de Lemos Braga',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='henriquelemosbraga@gmail.com',
+    keywords='fuzzystk',
+    description=u'Biblioteca fuzzystk',
+    packages=find_packages('fuzzystk'),
+    install_requires=['numpy', 'matplotlib'],)
