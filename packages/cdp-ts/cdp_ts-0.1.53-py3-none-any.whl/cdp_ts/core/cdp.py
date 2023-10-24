71324b499ed4e0b57c47e36ba7af8e71767a7ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:13:39 +0000

    Add package cdp-ts/0.1.53/cdp_ts-0.1.53-py3-none-any.whl

---
+++ packages/cdp-ts/cdp_ts-0.1.53-py3-none-any.whl/cdp_ts/core/cdp.py
@@ -0,0 +1,158 @@
+""" Concatenated Decision Paths (CDP) main class"""
+
+import os.path
+import pickle
+import csv
+from collections import defaultdict
+from cdp_ts.utils.utils import try_except, similarity_coefficient
+from cdp_ts.utils.logger import logger
+from cdp_ts.utils.dataset import Dataset
+from cdp_ts.core.shapelet_classifier import ShapeletClassifier
+
+# Filename of trained model - contains sequence of decision trees
+MODEL_FILENAME = 'cdp_model.pickle'
+# Filename of csv file that contains predicted class indexes
+PATTERNS_FILE_NAME = 'patterns.csv'
+
+
+class CDP:
+    """ Concatenated Decision Paths (CDP) method implementation"""
+
+    def __init__(self
+                 , model_folder: str
+                 , num_classes_per_tree: int
+                 , num_trees: int
+                 ):
+
+        self.model_folder = model_folder
+        self.num_classes_per_tree = num_classes_per_tree
+        self.num_trees = num_trees
+        self.patterns = []
+        self.classification_trees = {}
+
+    def _load_patterns(self, csv_file_path: str):
+        """ Load class indexes along with their representative patterns (LLR..)"""
+
+        # Initialize an empty list to store the loaded data
+        self.patterns = []
+
+        # Read the data from the CSV file
+        with open(csv_file_path, mode='r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+
+            # Assuming the column headers are 'class_index' and 'class_pattern'
+            for row in reader:
+                class_index = int(row['class_index'])
+                class_pattern = row['class_pattern']
+                self.patterns.append((class_index, class_pattern))
+
+    def _save_patterns(self, csv_file_path: str):
+        """ Save class indexes along with their representative patterns (LLR..) """
+
+        # Load patterns from file
+        headers = ['class_index', 'class_pattern']
+
+        # Write the data to the CSV file
+        with open(csv_file_path, mode='w', encoding='utf-8', newline='') as file:
+            writer = csv.writer(file)
+
+            # Write the header row
+            writer.writerow(headers)
+
+            # Write the data rows
+            for row in self.patterns:
+                writer.writerow(row)
+
+    def _save_classification_trees(self, model_folder_path: str):
+        """ Save model with classification tree sequence """
+
+        with open(os.path.join(model_folder_path, MODEL_FILENAME), 'wb') as file:
+            pickle.dump(self.classification_trees, file)
+
+    def _load_classification_trees(self, model_folder_path: str) -> dict:
+        """ Read model file with decision trees sequence """
+
+        try:
+            with open(os.path.join(model_folder_path, MODEL_FILENAME), 'rb') as file:
+                self.classification_trees = pickle.load(file)
+        except Exception as e:
+            self.classification_trees = dict()
+
+    @try_except
+    def load_model(self):
+        """ Load model- classification trees, along with decision patterns """
+        # Load model
+        self._load_classification_trees(self.model_folder)
+
+        # Load saved patterns
+        self._load_patterns(os.path.join(self.model_folder, PATTERNS_FILE_NAME))
+
+
+    @try_except
+    def fit(self, train_dataset: Dataset):
+
+        """ Fills the dictionary with classification trees. If model exists, tries to reuse
+        trees if compatible with input arguments requirements"""
+
+        logger.info("Training...")
+
+        shapelet_classifier = ShapeletClassifier(dataset=train_dataset
+                                                 , classifiers_folder=self.model_folder
+                                                 , num_classes_per_tree=self.num_classes_per_tree
+                                                 , num_trees=self.num_trees)
+
+        # Load existing classifiers
+        self._load_classification_trees(self.model_folder)
+
+        # Add new classifiers, if required
+        shapelet_classifier.create_and_train_classifiers(self.classification_trees)
+
+        # Save all classifiers
+        self._save_classification_trees(self.model_folder)
+
+        # Create patterns in format:
+        #  [(0, 'LLRLL...LLRLLL')
+        # ....
+        # , (1, 'RRRRL...RRRRLL')]
+        for class_index, time_series in train_dataset.iterrows():
+            decision_pattern = ''.join([classification_tree.build_classification_pattern(time_series)
+                                       for classification_tree in self.classification_trees.values()]
+                                       )
+            self.patterns.append((class_index, decision_pattern))
+
+        # Save patterns
+        self._save_patterns(os.path.join(self.model_folder, PATTERNS_FILE_NAME))
+
+    @try_except
+    def predict(self
+                , dataset: Dataset  # pd.DataFrame
+                ) -> list:
+
+        """ Predict indexes of given time series dataset"""
+
+        logger.info("Predicting...")
+
+        predicted_class_indexes = []
+
+        # Classify by comparing decision patterns
+        for _, time_series in dataset.iterrows():
+
+            # Find the pattern for given time series
+            pattern = ''.join([classification_tree.build_classification_pattern(time_series)
+                               for classification_tree in self.classification_trees.values()])
+
+            # Find similarity between found pattern and saved during training
+            similarities = [(i, similarity_coefficient(pattern, s)) for i, s in self.patterns]
+
+            # Sort tuples by closest distance and take 10 closest
+            biggest_similarities = sorted(similarities, key=lambda x: x[1], reverse=True)[:10]
+
+            # Find most popular index among the closest distances
+            freq_dict = defaultdict(int)
+            for tup in biggest_similarities:
+                freq_dict[tup[0]] += 1
+
+            # Finding index with maximum frequency
+            predicted_class_indexes.append(max(freq_dict, key=freq_dict.get))
+
+        return predicted_class_indexes
---
+++ packages/cdp-ts/cdp_ts-0.1.53-py3-none-any.whl/cdp_ts/core/pso.py
@@ -0,0 +1,153 @@
+""" Particle Swarm Optimization (PSO) approach for finding
+    shapelet that mostly separate two classes of time series"""
+import sys
+import numpy as np
+from cdp_ts.utils.utils import assess_candidate_position
+from cdp_ts.utils.dataset import Dataset
+import numba
+from numba import NumbaWarning
+import warnings
+warnings.filterwarnings("ignore", category=NumbaWarning)
+
+
+class CandidateShapelet:
+    """ Shapelet candidate implementation"""
+    def __init__(self
+                 , length: int
+                 , min_velocity: float, max_velocity: float
+                 , min_position: float, max_position: float):
+        self.optimal_split_distance: float = 0.0
+        self.best_information_gain: float = sys.float_info.min
+        self.length: int = length
+        self.position: np.array = np.random.uniform(min_position, max_position, length)
+        self.velocity: np.array = np.random.uniform(min_velocity, max_velocity, length)
+        self.best_position: np.array = self.position
+
+    def copy(self, candidate):
+        """ Copy so found the best parameters to given candidate"""
+
+        if isinstance(candidate, CandidateShapelet):
+            self.optimal_split_distance = candidate.optimal_split_distance
+            self.best_information_gain = candidate.best_information_gain
+            self.length = candidate.length
+            self.position[:len(candidate.position)] = candidate.position
+            self.velocity[:len(candidate.velocity)] = candidate.velocity
+            self.best_position = candidate.best_position
+
+
+class ShapeletsPso:
+    """ Implementation of Particle Swarm Optimization (PSO) algorithm
+        compares several candidate shapelets in a swarm. As result select the one which
+        maximally separate two classes."""
+
+    # Constants of the process (empirically found)
+    W = 0.729  # inertia weight
+    C1 = 1.49445  # cognitive / local weight
+    C2 = 1.49445  # social / global weight
+
+    # Stop optimization condition
+    MAX_ITERATIONS = 20
+    ITERATION_EPSILON = 0.0000001
+
+    def __init__(self
+                 , min_length: int, max_length: int
+                 , step: int
+                 , min_position: float, max_position: float
+                 , min_velocity: float, max_velocity: float
+                 , train_dataframe: Dataset
+                 ):
+
+        self.swarm: list = []
+        self.min_position: float = min_position
+        self.max_position: float = max_position
+        self.min_velocity: float = min_velocity
+        self.max_velocity: float = max_velocity
+        self.min_particle_length: int = min_length
+        self.max_particle_length: int = max_length
+        self.step: int = step
+        self.train_dataframe = train_dataframe
+
+        # Init best particle
+        self.best_particle: CandidateShapelet = CandidateShapelet(max_length
+                                               , min_velocity, max_velocity
+                                               , min_position, max_position)
+        # Init swarm
+        self._init_swarm()
+
+    @numba.jit()
+    def _init_swarm(self):
+        """ Initialize candidate shapelets' parameters """
+
+        for length in range(self.min_particle_length, self.max_particle_length+1, self.step):
+
+            # Create candidate shapelet
+            candidate = CandidateShapelet(length
+                                          , self.min_velocity, self.max_velocity
+                                          , self.min_position, self.max_position)
+
+            # Assess candidate position
+            information_gain, split_point = \
+                assess_candidate_position(candidate.position
+                                          , self.train_dataframe)
+
+            # Move candidate towards best position
+            if candidate.best_information_gain < information_gain:
+                candidate.best_information_gain = information_gain
+                candidate.best_position = candidate.position
+                candidate.optimal_split_distance = split_point
+                candidate.length = length
+
+            # Create swarm of candidates
+            self.swarm.append(candidate)
+
+            # Initialize best candidate
+            if candidate.best_information_gain > self.best_particle.best_information_gain:
+                self.best_particle.copy(candidate)
+
+    @numba.jit()
+    def start_pso(self):
+        """ Find the best shapelet that distinguishes time series from two classes"""
+
+        new_best_gain = 0.0
+        iteration = 0
+
+        while True:
+
+            iteration += 1
+            for candidate in self.swarm:
+
+                # Update candidate velocity
+                for i in range(candidate.length):
+
+                    r_1 = np.random.rand()
+                    r_2 = np.random.rand()
+
+                    candidate.velocity[i] = self.W * candidate.velocity[i] + \
+                        self.C1*r_1*(candidate.best_position[i] - candidate.position[i]) + \
+                        self.C2*r_2*(self.best_particle.position[i] - candidate.position[i])
+
+                # Update candidate position
+                candidate.position += candidate.velocity
+
+                # Check the fitness of current candidate
+                information_gain, split_point = \
+                    assess_candidate_position(candidate.position
+                                              , self.train_dataframe)
+
+                # Move candidate towards best position
+                if candidate.best_information_gain < information_gain:
+                    candidate.best_information_gain = information_gain
+                    candidate.best_position = candidate.position
+                    candidate.optimal_split_distance = split_point
+
+                # Update best particle
+                if candidate.best_information_gain > self.best_particle.best_information_gain:
+                    self.best_particle.copy(candidate)
+
+            old_best_gain = new_best_gain
+            new_best_gain = self.best_particle.best_information_gain
+
+            if abs(old_best_gain - new_best_gain) <= ShapeletsPso.ITERATION_EPSILON:
+                break
+
+        self.best_particle.position = self.best_particle.position[:self.best_particle.length]
