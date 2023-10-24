c1fc4a6d8023afc1fa778bfd7a87090146877ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:08:37 +0000

    Add package zjbs-file-client/0.5.0/zjbs_file_client-0.5.0-py3-none-any.whl

---
+++ packages/zjbs-file-client/zjbs_file_client-0.5.0-py3-none-any.whl/zjbs_file_client/__init__.py
@@ -0,0 +1,11 @@
+from .async_client import (
+    close_client,
+    delete,
+    download_directory,
+    download_file,
+    init_client,
+    list_directory,
+    upload,
+    upload_directory,
+)
+from .model import CompressMethod, FileSystemInfo, FileType
