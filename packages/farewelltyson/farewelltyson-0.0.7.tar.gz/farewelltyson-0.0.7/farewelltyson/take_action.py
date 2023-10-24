18a8ed8579c359f75f83aebc43686d80130ca65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 17:05:53 +0000

    Add package farewelltyson/0.0.7/farewelltyson-0.0.7.tar.gz

---
+++ packages/farewelltyson/farewelltyson-0.0.7.tar.gz/farewelltyson-0.0.7/farewelltyson/take_action.py
@@ -0,0 +1,5 @@
+def take_action(a):
+    if a == 'createnewemailaddress':
+        print('New email address created. A new name and other details were used. This will not be able to be traced to your prior email addresses.')
+    else:
+        print('no')
