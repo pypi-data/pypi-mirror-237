191b55701d27bad007b3331103e1ef53e73ec9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 17:07:01 +0000

    Add package phyfum/0.0.6/phyfum-0.0.6.tar.gz

---
+++ packages/phyfum/phyfum-0.0.6.tar.gz/phyfum-0.0.6/README.md
@@ -0,0 +1,21 @@
+# phyfum
+
+[![PyPI - Version](https://img.shields.io/pypi/v/phyfum.svg)](https://pypi.org/project/phyfum)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/phyfum.svg)](https://pypi.org/project/phyfum)
+
+-----
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [License](#license)
+
+## Installation
+
+```console
+pip install phyfum
+```
+
+## License
+
+`phyfum` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ packages/phyfum/phyfum-0.0.6.tar.gz/phyfum-0.0.6/src/phyfum/workflow/aux/checkConfig.py
@@ -0,0 +1,19 @@
+import yaml
+
+
+def readDefaults(yaml: str) -> dict:
+    with open(yaml, 'r') as f:
+        config = yaml.safe_load(f)
+
+    default_values = {key: value["default"] for key, value in config.get("properties", {}).items() if "default" in value}
+    return default_values
+
+
+def makeConfig(config: dict, defaults: dict) -> dict:
+    """
+    Remove Nones from config and add missing variables from the defaults
+    """
+    config = {key: value for key, value in config.items() if value}
+
+    # Merge 'config' with 'defaults' to add missing variables
+    return {**defaults, **config}
