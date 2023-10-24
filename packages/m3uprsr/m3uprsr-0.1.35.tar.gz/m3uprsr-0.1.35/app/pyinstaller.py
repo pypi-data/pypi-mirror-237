67a2eac90b34f26ff818ada19e17bacaed80923b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 15:05:59 +0000

    Add package m3uprsr/0.1.35/m3uprsr-0.1.35.tar.gz

---
+++ packages/m3uprsr/m3uprsr-0.1.35.tar.gz/m3uprsr-0.1.35/app/pyinstaller.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import PyInstaller.__main__
+from pathlib import Path
+
+here = Path(__file__).parent.absolute()
+path_to_main = str(here / "main.py")
+
+
+def install():
+    PyInstaller.__main__.run([
+        path_to_main,
+        '--name=m3u-prsr',
+        '--onefile',
+        '--nowindow',
+        '--add-data=README.md:.',
+        '--workpath=/tmp',
+    ])
