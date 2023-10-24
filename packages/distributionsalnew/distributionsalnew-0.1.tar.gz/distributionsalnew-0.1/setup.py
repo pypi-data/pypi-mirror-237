519556f227e11e19f741231bcb607a5abd9e3d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:07:55 +0000

    Add package distributionsalnew/0.1/distributionsalnew-0.1.tar.gz

---
+++ packages/distributionsalnew/distributionsalnew-0.1.tar.gz/distributionsalnew-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributionsalnew',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author="Raghuraj",
+      zip_safe=False)
