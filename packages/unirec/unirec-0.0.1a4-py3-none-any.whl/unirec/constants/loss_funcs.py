977b4fb331ec9411c661cc2264979e67f0534d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:09:16 +0000

    Add package unirec/0.0.1a4/unirec-0.0.1a4-py3-none-any.whl

---
+++ packages/unirec/unirec-0.0.1a4-py3-none-any.whl/unirec/constants/loss_funcs.py
@@ -0,0 +1,16 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+from enum import Enum
+
+class LossFuncType(Enum):
+    BCE = 'bce'
+    BPR = 'bpr'
+    SOFTMAX = 'softmax'
+    CCL = 'ccl'
+    FULLSOFTMAX = 'fullsoftmax'
+
+class DistanceType(Enum):
+    DOT = 'dot'
+    COSINE = 'cosine'
+    MLP = 'mlp'
