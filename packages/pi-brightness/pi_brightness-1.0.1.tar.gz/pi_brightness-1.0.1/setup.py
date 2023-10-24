89b12dcce75b652189586b0ce6c5682435442db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 15:07:02 +0000

    Add package pi-brightness/1.0.1/pi_brightness-1.0.1.tar.gz

---
+++ packages/pi-brightness/pi_brightness-1.0.1.tar.gz/pi_brightness-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pi_brightness',
+    version='1.0.1',
+    description='Controls Raspberry Pi screen brightness (or other Linux devices)',
+    author='Joseph McMahon',
+    packages=['module'],
+)
