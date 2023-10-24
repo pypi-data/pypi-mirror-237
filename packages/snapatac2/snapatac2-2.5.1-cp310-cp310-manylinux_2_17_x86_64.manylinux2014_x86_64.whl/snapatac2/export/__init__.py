c5e49e8a3976abe71bb7ba6f6fba5f76c2aa0945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 14:05:50 +0000

    Add package snapatac2/2.5.1/snapatac2-2.5.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/snapatac2/snapatac2-2.5.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/snapatac2/export/__init__.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+from typing_extensions import Literal
+
+from pathlib import Path
+import numpy as np
+
+import snapatac2._snapatac2 as internal
+
+def export_fragments(
+    adata: internal.AnnData | internal.AnnDataSet,
+    groupby: str | list[str],
+    selections: list[str] | None = None,
+    ids: str | list[str] | None = None,
+    out_dir: Path = "./",
+    prefix: str = "",
+    suffix: str = ".bed.zst",
+    compression: Literal["gzip", "zstandard"] | None = None,
+    compression_level: int | None = None,
+) -> dict[str, str]:
+    """Export and save fragments in a BED format file.
+
+    Parameters
+    ----------
+    adata
+        The (annotated) data matrix of shape `n_obs` x `n_vars`.
+        Rows correspond to cells and columns to regions.
+    groupby
+        Group the cells. If a `str`, groups are obtained from
+        `.obs[groupby]`.
+    selections
+        Export only the selected groups.
+    ids
+        Cell ids add to the bed records. If `None`, `.obs_names` is used.
+    out_dir
+        Directory for saving the outputs.
+    prefix
+        Text added to the output file name.
+    suffix
+        Text added to the output file name.
+    compression
+        Compression type. If `None`, it is inferred from the suffix.
+    compression_level
+        Compression level. 1-9 for gzip, 1-22 for zstandard.
+        If `None`, it is set to 6 for gzip and 3 for zstandard.
+
+    Returns
+    -------
+    dict[str, str]
+        A dictionary contains `(groupname, filename)` pairs. The file names are
+        formatted as `{prefix}{groupname}{suffix}`.
+    """
+    if isinstance(groupby, str):
+        groupby = adata.obs[groupby]
+    if selections is not None:
+        selections = set(selections)
+    
+    if ids is None:
+        ids = adata.obs_names
+    elif isinstance(ids, str):
+        ids = adata.obs[ids]
+
+    if compression is None:
+        if suffix.endswith(".gz"):
+            compression = "gzip"
+        elif suffix.endswith(".zst"):
+            compression = "zstandard"
+
+    return internal.export_fragments(
+        adata, list(ids), list(groupby), out_dir, prefix, suffix, selections, compression, compression_level,
+    )
+
+
+def export_bigwig(
+    adata: internal.AnnData | internal.AnnDataSet,
+    groupby: str | list[str],
+    selections: list[str] | None = None,
+    resolution: int = 1,
+    out_dir: Path = "./",
+    prefix: str = "",
+    suffix: str = ".bw",
+) -> dict[str, str]:
+    """
+    Export and create BigWig format files.
+
+    Parameters
+    ----------
+    adata
+        The (annotated) data matrix of shape `n_obs` x `n_vars`.
+        Rows correspond to cells and columns to regions.
+    groupby
+        Group the cells. If a `str`, groups are obtained from
+        `.obs[groupby]`.
+    selections
+        Export only the selected groups.
+    resolution
+        resolution.
+    out_dir
+        Directory for saving the outputs.
+    prefix
+        Text added to the output file name.
+    suffix
+        Text added to the output file name.
+
+    Returns
+    -------
+    dict[str, str]
+        A dictionary contains `(groupname, filename)` pairs. The file names are
+        formatted as `{prefix}{groupname}{suffix}`.
+    """
+    if isinstance(groupby, str):
+        groupby = adata.obs[groupby]
+    if selections is not None:
+        selections = set(selections)
+    
+    return internal.export_bigwig(
+        adata, list(groupby), selections, resolution, out_dir, prefix, suffix,
+    )
