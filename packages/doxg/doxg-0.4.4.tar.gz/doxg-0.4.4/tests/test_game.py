cbda7b0793f9ec3b2d63163970e87a809db086a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:07:53 +0000

    Add package doxg/0.4.4/doxg-0.4.4.tar.gz

---
+++ packages/doxg/doxg-0.4.4.tar.gz/doxg-0.4.4/tests/test_game.py
@@ -0,0 +1,5 @@
+from doxg.game import coin_game
+
+def test_game():
+    coin_game(3)
+    assert True
---
+++ packages/doxg/doxg-0.4.4.tar.gz/doxg-0.4.4/tests/test_init.py
@@ -0,0 +1,14 @@
+import doxg
+
+# 나머지 테스트 코드
+def test_pic():
+    doxg.pic()
+    assert True
+
+def test_who():
+    doxg.who()
+    assert True
+
+def test_imp():
+    doxg.imp()
+    assert True
