d07d18fe6a6fb84b2c22512bef53710bcc041e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:14:03 +0000

    Add package pyva-toolbox/1.2.1/pyva-toolbox-1.2.1.tar.gz

---
+++ packages/pyva-toolbox/pyva-toolbox-1.2.1.tar.gz/pyva-toolbox-1.2.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  6 12:01:53 2022
+
+"""
+
+from setuptools import setup,find_packages
+
+setup(
+    name = "pyva-toolbox",
+    version = "1.2.1",
+    author = 'Dr. Alexander Peiffer',
+    author_email = "author@alexanderpeiffer.de",
+    py_modules = ["models","useful"],
+    packages = find_packages(),
+    install_requires = ['pint>=0.17','numpy>=1.20.2','matplotlib>=3.3.4','scipy>=1.6.2','pandas>=1.2.5']
+    )
+
