59c6fb7e539be75dbbff43429a404ac8b359f683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:09:14 +0000

    Add package wetterdienst/0.65.0/wetterdienst-0.65.0.tar.gz

---
+++ packages/wetterdienst/wetterdienst-0.65.0.tar.gz/wetterdienst-0.65.0/wetterdienst/core/timeseries/result.py
@@ -0,0 +1,256 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2021, earthobservations developers.
+# Distributed under the MIT License. See LICENSE for more info.
+from dataclasses import dataclass
+from datetime import datetime
+from typing import TYPE_CHECKING, Optional, Union
+
+import polars as pl
+
+from wetterdienst.core.process import filter_by_date
+from wetterdienst.core.timeseries.export import ExportMixin
+from wetterdienst.metadata.columns import Columns
+from wetterdienst.metadata.period import Period
+from wetterdienst.metadata.resolution import Frequency, FrequencyPolars, Resolution
+
+if TYPE_CHECKING:
+    from wetterdienst.core.timeseries.request import TimeseriesRequest
+    from wetterdienst.core.timeseries.values import TimeseriesValues
+    from wetterdienst.provider.dwd.dmo import DwdDmoRequest
+    from wetterdienst.provider.dwd.mosmix import DwdMosmixRequest
+
+
+class StationsFilter:
+    ALL = "all"
+    BY_STATION_ID = "by_station_id"
+    BY_NAME = "by_name"
+    BY_RANK = "by_rank"
+    BY_DISTANCE = "by_distance"
+    BY_BBOX = "by_bbox"
+    BY_SQL = "by_sql"
+
+
+class StationsResult(ExportMixin):
+    def __init__(
+        self,
+        stations: Union["TimeseriesRequest", "DwdMosmixRequest", "DwdDmoRequest"],
+        df: pl.DataFrame,
+        df_all: pl.DataFrame,
+        stations_filter: StationsFilter,
+        rank: Optional[int] = None,
+        **kwargs
+    ) -> None:
+        # TODO: add more attributes from ScalarStations class
+        self.stations = stations
+        self.df = df
+        self.df_all = df_all
+        self.stations_filter = stations_filter
+        self.rank = rank
+        self._kwargs = kwargs
+
+    def __eq__(self, other):
+        return (self.stations == other.stations) and self.df.frame_equal(other.df)
+
+    @property
+    def settings(self):
+        return self.stations.settings
+
+    @property
+    def provider(self):
+        return self.stations._provider
+
+    @property
+    def now(self):
+        return self.stations._now
+
+    @property
+    def station_id(self) -> pl.Series:
+        return self.df.get_column(Columns.STATION_ID.value)
+
+    @property
+    def parameter(self):
+        return self.stations.parameter
+
+    @property
+    def _resolution_type(self):
+        return self.stations._resolution_type
+
+    @property
+    def values(self) -> "TimeseriesValues":
+        return self.stations._values.from_stations(self)
+
+    @property
+    def resolution(self) -> Resolution:
+        return self.stations.resolution
+
+    @property
+    def frequency(self) -> Frequency:
+        return self.stations.frequency
+
+    @property
+    def frequency_polars(self) -> FrequencyPolars:
+        return self.stations.frequency_polars
+
+    @property
+    def period(self) -> Period:
+        return self.stations.period
+
+    @property
+    def start_date(self) -> datetime:
+        return self.stations.start_date
+
+    @property
+    def end_date(self) -> datetime:
+        return self.stations.end_date
+
+    @property
+    def start_issue(self) -> datetime:
+        return self.stations.start_issue
+
+    @property
+    def end_issue(self) -> datetime:
+        return self.stations.end_issue
+
+    @property
+    def tidy(self) -> bool:
+        return self.stations.tidy
+
+    @property
+    def humanize(self) -> bool:
+        return self.stations.humanize
+
+    @property
+    def si_units(self) -> bool:
+        return self.stations.si_units
+
+    @property
+    def skip_empty(self) -> bool:
+        return self.stations.skip_empty
+
+    @property
+    def skip_threshold(self) -> float:
+        return self.stations.skip_threshold
+
+    @property
+    def dropna(self) -> float:
+        return self.stations.dropna
+
+    @property
+    def _has_tidy_data(self) -> bool:
+        return self.stations._has_tidy_data
+
+    @property
+    def _dataset_accessor(self) -> bool:
+        return self.stations._dataset_accessor
+
+    @property
+    def _unique_dataset(self) -> bool:
+        return self.stations._unique_dataset
+
+    @property
+    def _has_datasets(self) -> bool:
+        return self.stations._has_datasets
+
+    @property
+    def _unit_base(self) -> bool:
+        return self.stations._unit_base
+
+    @property
+    def _parameter_base(self) -> bool:
+        return self.stations._parameter_base
+
+    def to_ogc_feature_collection(self) -> dict:
+        """
+        Format station information as OGC feature collection.
+        Will be used by ``.to_geojson()``.
+
+        Return:
+             Dictionary in GeoJSON FeatureCollection format.
+        """
+        features = []
+        for station in self.df.iter_rows(named=True):
+            features.append(
+                {
+                    "type": "Feature",
+                    "properties": {
+                        "id": station["station_id"],
+                        "name": station["name"],
+                        "state": station["state"],
+                        "from_date": station["from_date"].isoformat() if station["from_date"] else None,
+                        "to_date": station["to_date"].isoformat() if station["to_date"] else None,
+                    },
+                    "geometry": {
+                        # WGS84 is implied and coordinates represent decimal degrees
+                        # ordered as "longitude, latitude [,elevation]" with z expressed
+                        # as metres above mean sea level per WGS84.
+                        # -- http://wiki.geojson.org/RFC-001
+                        "type": "Point",
+                        "coordinates": [
+                            station["longitude"],
+                            station["latitude"],
+                            station["height"],
+                        ],
+                    },
+                }
+            )
+
+        return {
+            "type": "FeatureCollection",
+            "features": features,
+        }
+
+
+@dataclass
+class ValuesResult(ExportMixin):
+    # TODO: add more meaningful metadata e.g. describe()
+
+    stations: StationsResult
+    values: "TimeseriesValues"
+    df: pl.DataFrame
+
+    def to_ogc_feature_collection(self):
+        raise NotImplementedError()
+
+    def filter_by_date(self, date: str) -> pl.DataFrame:
+        self.df = filter_by_date(self.df, date=date, resolution=self.stations.resolution)
+        return self.df
+
+    @property
+    def df_stations(self):
+        return self.stations.df.filter(pl.col("station_id").is_in(self.values.stations_collected))
+
+
+@dataclass
+class InterpolatedValuesResult(ExportMixin):
+    stations: StationsResult
+    df: pl.DataFrame
+
+    def __init__(self, df: pl.DataFrame, stations: StationsResult = None, **kwargs) -> None:
+        self.stations = stations
+        self.df = df
+        self._kwargs = kwargs
+
+    def to_ogc_feature_collection(self):
+        raise NotImplementedError()
+
+    def filter_by_date(self, date: str) -> pl.DataFrame:
+        self.df = filter_by_date(self.df, date=date)
+        return self.df
+
+
+@dataclass
+class SummarizedValuesResult(ExportMixin):
+    stations: StationsResult
+    df: pl.DataFrame
+
+    def __init__(self, df: pl.DataFrame, stations: StationsResult = None, **kwargs) -> None:
+        self.stations = stations
+        self.df = df
+        self._kwargs = kwargs
+
+    def to_ogc_feature_collection(self):
+        raise NotImplementedError()
+
+    def filter_by_date(self, date: str) -> pl.DataFrame:
+        self.df = filter_by_date(self.df, date=date, resolution=self.stations.resolution)
+        return self.df
---
+++ packages/wetterdienst/wetterdienst-0.65.0.tar.gz/wetterdienst-0.65.0/wetterdienst/ui/restapi.py
@@ -0,0 +1,621 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2021, earthobservations developers.
+# Distributed under the MIT License. See LICENSE for more info.
+import json
+import logging
+from typing import Literal, Optional
+
+import polars as pl
+from click_params import StringListParamType
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.responses import HTMLResponse, PlainTextResponse, Response
+
+from wetterdienst import Provider, Wetterdienst, __appname__, __version__
+from wetterdienst.core.timeseries.request import TimeseriesRequest
+from wetterdienst.exceptions import ProviderNotFoundError
+from wetterdienst.metadata.columns import Columns
+from wetterdienst.ui.cli import get_api
+from wetterdienst.ui.core import (
+    get_interpolate,
+    get_stations,
+    get_summarize,
+    get_values,
+    set_logging_level,
+)
+from wetterdienst.util.cli import read_list, setup_logging
+
+app = FastAPI(debug=False)
+
+log = logging.getLogger(__name__)
+
+PRODUCER_NAME = "Wetterdienst"
+PRODUCER_LINK = "https://github.com/earthobservations/wetterdienst"
+
+CommaSeparator = StringListParamType(",")
+
+
+@app.get("/", response_class=HTMLResponse)
+def index():
+    appname = f"{__appname__} {__version__}"
+
+    about = "Wetterdienst - Open weather data for humans."
+
+    sources = ""
+    for provider in Provider:
+        shortname = provider.name
+        _, name, country, copyright_, url = provider.value
+        sources += f"<li><a href={url}>{shortname}</a> ({name}, {country}) - {copyright_}</li>"
+
+    return f"""
+    <html>
+        <head>
+            <title>{appname}</title>
+        </head>
+        <body>
+            <h3>About</h3>
+            <h3>{about}</h3>
+            <h4>Providers</h4>
+            <ul>
+                {sources}
+            </ul>
+            <h4>Providers</h4>
+            <ul>
+                <li><a href=restapi/coverage>coverage</a></li>
+                <li><a href=restapi/stations>stations</a></li>
+                <li><a href=restapi/values>values</a></li>
+                <li><a href=restapi/interpolate>interpolation</a></li>
+                <li><a href=restapi/summarize>summary</a></li>
+            </ul>
+            <h4>Producer</h4>
+            {PRODUCER_NAME} - <a href="{PRODUCER_LINK}">{PRODUCER_LINK}</a></li>
+            <h4>Examples</h4>
+            <ul>
+            <li><a href="restapi/stations?provider=dwd&network=observation&parameter=kl&resolution=daily&period=recent&all=true">DWD observation stations</a></li>
+            <li><a href="restapi/values?provider=dwd&network=observation&parameter=kl&resolution=daily&period=recent&station=00011">DWD observation values</a></li>
+            <li><a href="restapi/interpolate?provider=dwd&network=observation&parameter=kl&resolution=daily&station=00071&date=1986-10-31/1986-11-01">DWD observation interpolation</a></li>
+            <li><a href="restapi/summarize?provider=dwd&network=observation&parameter=kl&resolution=daily&station=00071&date=1986-10-31/1986-11-01">DWD observation summary</a></li>
+            </ul>
+        </body>
+    </html>
+    """  # noqa:B950,E501
+
+
+@app.get("/robots.txt", response_class=PlainTextResponse)
+def robots():
+    return """
+User-agent: *
+Disallow: /api/
+    """.strip()
+
+
+@app.get("/restapi/coverage")
+def coverage(
+    provider: str = Query(default=None),
+    network: str = Query(default=None),
+    debug: bool = Query(default=False),
+    dataset=Query(alias="dataset", default=None),
+    resolution=Query(alias="resolution", default=None),
+):
+    set_logging_level(debug)
+
+    if not provider or not network:
+        cov = Wetterdienst.discover()
+        return Response(content=json.dumps(cov, indent=4), media_type="application/json")
+
+    api = get_api(provider=provider, network=network)
+
+    cov = api.discover(
+        dataset=dataset,
+        resolution=resolution,
+        flatten=False,
+    )
+
+    return Response(content=json.dumps(cov, indent=4), media_type="application/json")
+
+
+@app.get("/restapi/stations")
+def stations(
+    provider: str = Query(default=None),
+    network: str = Query(default=None),
+    parameter: str = Query(default=None),
+    resolution: str = Query(default=None),
+    period: str = Query(default=None),
+    all_: bool = Query(alias="all", default=False),
+    station_id: str = Query(default=None),
+    name: str = Query(default=None),
+    coordinates: str = Query(default=None),
+    rank: int = Query(default=None),
+    distance: float = Query(default=None),
+    bbox: str = Query(default=None),
+    sql: str = Query(default=None),
+    fmt: str = Query(alias="format", default="json"),
+    debug: bool = Query(default=False),
+    pretty: bool = Query(default=False),
+):
+    if provider is None or network is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Query arguments 'provider' and 'network' are required",
+        )
+
+    if parameter is None or resolution is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Query arguments 'parameter', 'resolution' and 'period' are required",
+        )
+
+    if fmt not in ("json", "geojson"):
+        raise HTTPException(
+            status_code=400,
+            detail="'format' argument must be one of 'json', 'geojson'",
+        )
+
+    set_logging_level(debug)
+
+    try:
+        api = Wetterdienst(provider, network)
+    except ProviderNotFoundError as e:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Choose provider and network from {app.url_path_for('coverage')}",
+        ) from e
+
+    parameter = read_list(parameter)
+    if period:
+        period = read_list(period)
+    if station_id:
+        station_id = read_list(station_id)
+
+    try:
+        stations_ = get_stations(
+            api=api,
+            parameter=parameter,
+            resolution=resolution,
+            period=period,
+            date=None,
+            issue=None,
+            all_=all_,
+            station_id=station_id,
+            name=name,
+            coordinates=coordinates,
+            rank=rank,
+            distance=distance,
+            bbox=bbox,
+            sql=sql,
+            shape="long",
+            si_units=False,
+            humanize=False,
+            skip_empty=False,
+            skip_threshold=0.95,
+            skip_criteria="min",
+            dropna=False,
+        )
+    except (KeyError, ValueError) as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
+
+    if not stations_.parameter or not stations_.resolution:
+        raise HTTPException(
+            status_code=400,
+            detail=f"No parameter found for provider {provider}, network {network}, "
+            f"parameter(s) {parameter} and resolution {resolution}.",
+        )
+
+    indent = None
+    if pretty:
+        indent = 4
+
+    output = None
+    if fmt == "json":
+        output = stations_.to_dict()
+    elif fmt == "geojson":
+        output = stations_.to_ogc_feature_collection()
+
+    output = make_json_response(output, api._provider)
+
+    output = json.dumps(output, indent=indent, ensure_ascii=False)
+
+    return Response(content=output, media_type="application/json")
+
+
+@app.get("/restapi/values")
+def values(
+    provider: str = Query(default=None),
+    network: str = Query(default=None),
+    parameter: str = Query(default=None),
+    resolution: str = Query(default=None),
+    period: str = Query(default=None),
+    date: str = Query(default=None),
+    issue: str = Query(default="latest"),
+    all_: str = Query(alias="all", default=False),
+    station: str = Query(default=None),
+    name: str = Query(default=None),
+    coordinates: str = Query(default=None),
+    rank: int = Query(default=None),
+    distance: float = Query(default=None),
+    bbox: str = Query(default=None),
+    sql: str = Query(default=None),
+    sql_values: str = Query(alias="sql-values", default=None),
+    humanize: bool = Query(default=True),
+    shape: Literal["long", "wide"] = Query(default="long"),
+    si_units: bool = Query(alias="si-units", default=True),
+    skip_empty: bool = Query(alias="skip-empty", default=False),
+    skip_threshold: float = Query(alias="skip-threshold", default=0.95, gt=0, le=1),
+    skip_criteria: Literal["min", "mean", "max"] = Query(alias="skip-criteria", default="min"),
+    dropna: bool = Query(alias="dropna", default=False),
+    pretty: bool = Query(default=False),
+    debug: bool = Query(default=False),
+):
+    """
+    Acquire data from DWD.
+
+    :param provider:
+    :param network:        string for network of provider
+    :param parameter:   Observation measure
+    :param resolution:  Frequency/granularity of measurement interval
+    :param period:      Recent or historical files
+    :param date:        Date or date range
+    :param issue:
+    :param all_:
+    :param station:
+    :param name:
+    :param coordinates:
+    :param rank:
+    :param distance:
+    :param bbox:
+    :param sql:         SQL expression
+    :param sql_values:
+    :param fmt:
+    :param humanize:
+    :param shape:        Whether to return data in "long" or "wide" format. Default: "long".
+    :param si_units:
+    :param skip_empty:
+    :param skip_criteria:
+    :param skip_threshold:
+    :param dropna:
+    :param pretty:
+    :param debug:
+    :return:
+    """
+    # TODO: Add geojson support
+    fmt = "json"
+
+    if provider is None or network is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Query arguments 'provider' and 'network' are required",
+        )
+
+    if parameter is None or resolution is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Query arguments 'parameter', 'resolution' and 'date' are required",
+        )
+
+    if fmt not in ("json", "geojson"):
+        raise HTTPException(
+            status_code=400,
+            detail="'format' argument must be one of 'json', 'geojson'",
+        )
+
+    set_logging_level(debug)
+
+    try:
+        api: TimeseriesRequest = Wetterdienst(provider, network)
+    except ProviderNotFoundError as e:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Given combination of provider and network not available. "
+            f"Choose provider and network from {Wetterdienst.discover()}",
+        ) from e
+
+    parameter = read_list(parameter)
+    if period:
+        period = read_list(period)
+    if station:
+        station = read_list(station)
+
+    try:
+        values_ = get_values(
+            api=api,
+            parameter=parameter,
+            resolution=resolution,
+            date=date,
+            issue=issue,
+            period=period,
+            all_=all_,
+            station_id=station,
+            name=name,
+            coordinates=coordinates,
+            rank=rank,
+            distance=distance,
+            bbox=bbox,
+            sql=sql,
+            sql_values=sql_values,
+            si_units=si_units,
+            skip_empty=skip_empty,
+            skip_threshold=skip_threshold,
+            skip_criteria=skip_criteria,
+            dropna=dropna,
+            shape=shape,
+            humanize=humanize,
+        )
+    except Exception as e:
+        log.exception(e)
+        raise HTTPException(status_code=400, detail=str(e)) from e
+
+    indent = None
+    if pretty:
+        indent = 4
+
+    output = values_.to_dict()
+
+    output = make_json_response(output, api._provider)
+
+    output = json.dumps(output, indent=indent, ensure_ascii=False)
+
+    return Response(content=output, media_type="application/json")
+
+
+@app.get("/restapi/interpolate")
+def interpolate(
+    provider: str = Query(default=None),
+    network: str = Query(default=None),
+    parameter: str = Query(default=None),
+    resolution: str = Query(default=None),
+    period: str = Query(default=None),
+    date: str = Query(default=None),
+    issue: str = Query(default="latest"),
+    station: str = Query(default=None),
+    coordinates: str = Query(default=None),
+    sql_values: str = Query(alias="sql-values", default=None),
+    humanize: bool = Query(default=True),
+    si_units: bool = Query(alias="si-units", default=True),
+    use_nearby_station_distance: float = Query(default=1.0),
+    pretty: bool = Query(default=False),
+    debug: bool = Query(default=False),
+):
+    """
+    Wrapper around get_interpolate to provide results via restapi
+
+    :param provider:
+    :param network:        string for network of provider
+    :param parameter:   Observation measure
+    :param resolution:  Frequency/granularity of measurement interval
+    :param period:      Recent or historical files
+    :param date:        Date or date range
+    :param issue:
+    :param station:
+    :param coordinates:
+    :param sql_values:
+    :param humanize:
+    :param si_units:
+    :param use_nearby_station_distance:
+    :param pretty:
+    :param debug:
+    :return:
+    """
+    # TODO: Add geojson support
+    fmt = "json"
+
+    if provider is None or network is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Query arguments 'provider' and 'network' are required",
+        )
+
+    if parameter is None or resolution is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Query arguments 'parameter', 'resolution' and 'date' are required",
+        )
+
+    if fmt not in ("json", "geojson"):
+        raise HTTPException(
+            status_code=400,
+            detail="format argument must be one of json, geojson",
+        )
+
+    set_logging_level(debug)
+
+    try:
+        api: TimeseriesRequest = Wetterdienst(provider, network)
+    except ProviderNotFoundError as e:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Given combination of provider and network not available. "
+            f"Choose provider and network from {Wetterdienst.discover()}",
+        ) from e
+
+    parameter = read_list(parameter)
+    if period:
+        period = read_list(period)
+    if station:
+        station = read_list(station)
+
+    try:
+        values_ = get_interpolate(
+            api=api,
+            parameter=parameter,
+            resolution=resolution,
+            date=date,
+            issue=issue,
+            period=period,
+            station_id=station,
+            coordinates=coordinates,
+            sql_values=sql_values,
+            si_units=si_units,
+            humanize=humanize,
+            use_nearby_station_distance=use_nearby_station_distance,
+        )
+    except Exception as err:
+        log.exception(err)
+        raise HTTPException(status_code=404, detail=str(err)) from err
+
+    indent = None
+    if pretty:
+        indent = 4
+
+    output = values_.df
+
+    output = output.with_columns(pl.col(Columns.DATE.value).map_elements(lambda d: d.isoformat()))
+
+    output = output.to_dicts()
+
+    output = make_json_response(output, api._provider)
+
+    output = json.dumps(output, indent=indent, ensure_ascii=False)
+
+    return Response(content=output, media_type="application/json")
+
+
+@app.get("/restapi/summarize")
+def summarize(
+    provider: str = Query(default=None),
+    network: str = Query(default=None),
+    parameter: str = Query(default=None),
+    resolution: str = Query(default=None),
+    period: str = Query(default=None),
+    date: str = Query(default=None),
+    issue: str = Query(default="latest"),
+    station: str = Query(default=None),
+    coordinates: str = Query(default=None),
+    sql_values: str = Query(alias="sql-values", default=None),
+    humanize: bool = Query(default=True),
+    si_units: bool = Query(alias="si-units", default=True),
+    pretty: bool = Query(default=False),
+    debug: bool = Query(default=False),
+):
+    """
+    Wrapper around get_summarize to provide results via restapi
+
+    :param provider:
+    :param network:        string for network of provider
+    :param parameter:   Observation measure
+    :param resolution:  Frequency/granularity of measurement interval
+    :param period:      Recent or historical files
+    :param date:        Date or date range
+    :param issue:
+    :param station:
+    :param coordinates:
+    :param sql_values:
+    :param humanize:
+    :param si_units:
+    :param pretty:
+    :param debug:
+    :return:
+    """
+    # TODO: Add geojson support
+    fmt = "json"
+
+    if provider is None or network is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Query arguments 'provider' and 'network' are required",
+        )
+
+    if parameter is None or resolution is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Query arguments 'parameter', 'resolution' " "and 'date' are required",
+        )
+
+    if fmt not in ("json", "geojson"):
+        raise HTTPException(
+            status_code=400,
+            detail="format argument must be one of json, geojson",
+        )
+
+    set_logging_level(debug)
+
+    try:
+        api: TimeseriesRequest = Wetterdienst(provider, network)
+    except ProviderNotFoundError as e:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Given combination of provider and network not available. "
+            f"Choose provider and network from {Wetterdienst.discover()}",
+        ) from e
+
+    parameter = read_list(parameter)
+    if period:
+        period = read_list(period)
+    if station:
+        station = read_list(station)
+
+    try:
+        values_ = get_summarize(
+            api=api,
+            parameter=parameter,
+            resolution=resolution,
+            date=date,
+            issue=issue,
+            period=period,
+            station_id=station,
+            coordinates=coordinates,
+            sql_values=sql_values,
+            si_units=si_units,
+            humanize=humanize,
+        )
+    except Exception as err:
+        log.exception(err)
+        raise HTTPException(status_code=404, detail=str(err)) from err
+
+    indent = None
+    if pretty:
+        indent = 4
+
+    output = values_.df
+
+    output = output.with_columns(pl.col(Columns.DATE.value).map_elements(lambda d: d.isoformat()))
+
+    output = output.to_dicts()
+
+    output = make_json_response(output, api._provider)
+
+    output = json.dumps(output, indent=indent, ensure_ascii=False)
+
+    return Response(content=output, media_type="application/json")
+
+
+def make_json_response(data, provider):
+    """
+    Function to add wetterdienst metadata information, citation, etc as well as
+    information about the data provider
+
+    :param data:
+    :param provider:
+    :return:
+    """
+    name_local, name_english, country, copyright_, url = provider.value
+
+    return {
+        "meta": {
+            "provider": {
+                "name_local": name_local,
+                "name_english": name_english,
+                "country": country,
+                "copyright": copyright_,
+                "url": url,
+            },
+            "producer": {
+                "name": PRODUCER_NAME,
+                "url": PRODUCER_LINK,
+                "doi": "10.5281/zenodo.3960624",
+            },
+        },
+        "data": data,
+    }
+
+
+def start_service(listen_address: Optional[str] = None, reload: Optional[bool] = False):  # pragma: no cover
+    from uvicorn.main import run
+
+    setup_logging()
+
+    if listen_address is None:
+        listen_address = "127.0.0.1:7890"
+
+    host, port = listen_address.split(":")
+    port = int(port)
+
+    run(app="wetterdienst.ui.restapi:app", host=host, port=port, reload=reload)
