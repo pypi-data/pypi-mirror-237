19b172890d4ea3b00b9577ef1491b1136cfb67c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:14:38 +0000

    Add package pynequa/0.0.1/pynequa-0.0.1-py3-none-any.whl

---
+++ packages/pynequa/pynequa-0.0.1-py3-none-any.whl/pynequa/__init__.py
@@ -0,0 +1,3 @@
+# from .core import Sinequa
+# from .models import *
+from .core import *
---
+++ packages/pynequa/pynequa-0.0.1-py3-none-any.whl/sinequa_py/api.py
@@ -0,0 +1,11 @@
+class API:
+    def __init__(self, access_token) -> None:
+        self.access_token=access_token
+        pass
+    
+    def get(self):
+        pass 
+
+    def post(self):
+        pass 
+
