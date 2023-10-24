02c40df96f177b695c2a3d52a21411f8a1a08fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:14:58 +0000

    Add package mini-kyber/0.0.4/mini-kyber-0.0.4.tar.gz

---
+++ packages/mini-kyber/mini-kyber-0.0.4.tar.gz/mini-kyber-0.0.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools 
+
+with open("README.md", "r") as fh: 
+	description = fh.read() 
+
+setuptools.setup( 
+	name="mini-kyber", 
+	version="0.0.4", 
+	author="Garvit Prasad", 
+	author_email="garvitpd789@gmail.com", 
+	packages=["Kyber"], 
+	description="Implementation of Kyber a post quantum encryption algorithim ", 
+	long_description=description, 
+	long_description_content_type="text/markdown", 
+	url="https://github.com/garvitxx18/Kyber", 
+	license='MIT', 
+	python_requires='>=3.8', 
+	install_requires=['numpy'] 
+) 
