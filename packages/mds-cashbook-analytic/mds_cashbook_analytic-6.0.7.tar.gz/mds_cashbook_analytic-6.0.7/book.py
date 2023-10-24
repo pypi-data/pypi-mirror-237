0bf8d4e0090f1e4fea5f733378bdb50c795bcaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:09:38 +0000

    Add package mds-cashbook-analytic/6.0.7/mds_cashbook_analytic-6.0.7.tar.gz

---
+++ packages/mds-cashbook-analytic/mds_cashbook_analytic-6.0.7.tar.gz/mds_cashbook_analytic-6.0.7/book.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# This file is part of the cashbook-module from m-ds for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the
+# full copyright notices and license terms.
+
+from trytond.model import fields
+from trytond.pool import PoolMeta
+from trytond.pyson import Eval
+
+
+class Book(metaclass=PoolMeta):
+    __name__ = 'cashbook.book'
+
+    analytic = fields.Many2One(
+        string='Analytic Account', ondelete='RESTRICT',
+        model_name='analytic_account.account', select=True,
+        domain=[
+            ('company.id', '=', Eval('company', -1)),
+            ('type', 'in', ['normal', 'distribution']),
+            ],
+        depends=['company'])
+
+# end Book
