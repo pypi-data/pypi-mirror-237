3a63e4f72dedca6529e8c124b1758855aed4ca32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:07:53 +0000

    Add package doxg/0.4.4/doxg-0.4.4-py3-none-any.whl

---
+++ packages/doxg/doxg-0.4.4-py3-none-any.whl/doxg/__init__.py
@@ -0,0 +1,67 @@
+
+def imp():
+    text = """
+    # basic
+    import numpy as np
+    import pandas as pd
+    import math
+
+    # visualize
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import plotly.express as px
+    from plotly.subplots import make_subplots
+    import plotly.figure_factory as ff
+    import plotly.graph_objs as go
+
+    # crawling
+    import requests
+    from bs4 import BeautifulSoup
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.chrome.options import Options                                                                                      
+    
+    import pickle
+    """
+    print(text)
+
+def who():
+    print("I am simon")
+
+def pic():
+    art = """                                                                                      
+                               :!????7~                         :^~^.                               
+                             .YY!:..:^?5!   .:^~~!!7777!!~^:. !555Y555~                             
+                             P?        .Y5?J??7!~~^^~JYYYY5555PJ77777J#~                            
+                            .#:          ^:          !?77777??7?????77#!                            
+                             JP:                     :7???????????77JGJ                             
+                              ~YJ7.          .::::    .~7?????YYYYJ7PG                              
+                                !#.       .~!~^^^^       :^!77???????#~                             
+                                YJ        ~^                 .:^~~~!!PY                             
+                                B~            ^77~.          ^YPGP?. ?P                             
+                               :#:          .P#YP@&?        ~@B^?@@G !P?^                           
+                               :#.          !@#7Y@@#        ^#@#&@@P   ~P?                          
+                               .#:     ..... ?B@@&P^   ....  .755J~ :!!~!B!                         
+                                G!    :!7777!. ::.   .YYYYYJ        :~~~:YY                         
+                                J5     .:^^^:.        5YYY5Y             G!                         
+                                :#:                   ~PYYP^            YY                          
+                                 J5                    ~55~           ^5?                           
+                                  B~                    .:::^~~!!!!~~5G~                            
+                                 !G.      ::.     :^!?JJJJJ#J!!!!!!!7???JJ?7~:                      
+                                :B^       !??????GPJ7~^^:::PJ   ..     ...:~7JY?^                   
+                                P?            ..^~7JJJ7!7~:!B:..............  .PY                   
+                               7G                    :!J7G!:PJ ...............^B:                   
+                              .B^                 ..::^^~G7:~#^.............. 5J                    
+                              J5        .!!77???YPJJJ??77~:::JG............. ~B.                    
+                             .B^        .~^^:.. :#~::::^YYY?::PY ............G7                     
+                             ?P                  Y5::::!B^!&~:^G? ......... ?P                      
+                             G!                  .B!::::^:~7:::^G? ....... ~B:                      
+                            ~B                    7B^::?7:::::::^GJ...... ~B~                       
+                            ?P                     Y5:^#?^:::::::^55.... !B~                        
+                            7G         :?JJ!        PJ:~J~:::::::::JP^ :JP:                         
+                             PJ        ?7.^#:       .GJ^^^^^^^~~~~~!Y#Y57                           
+                              ?57:        .#:  ......^5JJJJJJ????Y#7~^~.                            
+                               .!JJ?!~^:. ?#??????????7??????????J^                                 
+                                  .:~!77??5^                  ..                                                                                                                                          
+    """
+    print(art)
