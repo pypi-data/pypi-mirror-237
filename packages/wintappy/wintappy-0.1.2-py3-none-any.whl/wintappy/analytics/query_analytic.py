2dc4a02c3e5642ffe37d43013f1a246cc2c2cb94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:13:57 +0000

    Add package wintappy/0.1.2/wintappy-0.1.2-py3-none-any.whl

---
+++ packages/wintappy/wintappy-0.1.2-py3-none-any.whl/wintappy/analytics/query_analytic.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional
+
+from pandas import DataFrame
+
+MITRE_CAR_TYPE = "MITRE_CAR"
+
+
+@dataclass
+class MitreAttackCoverage:
+    coverage: str
+    tactics: List[str]
+    technique: str
+    subtechniques: Optional[List[str]] = None
+
+
+@dataclass
+class QueryAnalytic:
+    analytic_id: str
+    analytic_template: str
+    query_type: str
+    metadata: Dict[str, Any]
+    coverage: List[MitreAttackCoverage]
+
+    def get_tactics(self) -> List[str]:
+        """Helper function to quickly return all tactics in coverage"""
+        tactics = []
+        for c in self.coverage:
+            tactics.extend(c.tactics)
+        return tactics
+
+    def get_techniques(self) -> List[str]:
+        """Helper function to quickly return all techniques in coverage"""
+        techniques = []
+        for c in self.coverage:
+            techniques.append(c.technique)
+        return techniques
---
+++ packages/wintappy/wintappy-0.1.2-py3-none-any.whl/wintappy/database/constants.py
@@ -0,0 +1,13 @@
+# constants
+ANALYTICS_TABLE = "mitre_car"
+ANALYTICS_RESULTS_TABLE = "mitre_labels"
+TACTICS_TABLE = "mitre_tactics"
+TECHNIQUES_TABLE = "mitre_techniques"
+TEMPLATE_DIR = "./templates"
+LOOKUPS_DIR = "lookups"
+CREATE_ANALYTICS_RESULTS_TEMPLATE = "create_analytics_results.sql"
+INSERT_ANALYTICS_RESULTS_TEMPLATE = "insert_analytics_results.sql"
+CREATE_ANALYTICS_TEMPLATE = "create_analytics.sql"
+INSERT_ANALYTICS_TEMPLATE = "insert_analytics.sql"
+# column names
+PID_HASH = "pid_hash"
