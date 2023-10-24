d250948f34270e5bf91ca9ac0ee63fa4a8ec6266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:07:00 +0000

    Add package swahilipro/2.1/swahilipro-2.1.tar.gz

---
+++ packages/swahilipro/swahilipro-2.1.tar.gz/swahilipro-2.1/swahilipro/swahilipro.py
@@ -0,0 +1,512 @@
+
+from . import strings_with_arrows
+
+import string
+import os
+import math
+
+
+# CONSTANTS
+
+
+DIGITS = '0123456789'
+LETTERS = string.ascii_letters
+LETTERS_DIGITS = LETTERS + DIGITS
+
+
+# ERRORS
+
+
+class Error:
+  def __init__(self, pos_start, pos_end, error_name, details):
+    self.pos_start = pos_start
+    self.pos_end = pos_end
+    self.error_name = error_name
+    self.details = details
+  
+  def as_string(self):
+    result  = f'{self.error_name}: {self.details}\n'
+    result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
+    result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+    return result
+
+class IllegalCharError(Error):
+  def __init__(self, pos_start, pos_end, details):
+    super().__init__(pos_start, pos_end, 'Illegal Character', details)
+
+class ExpectedCharError(Error):
+  def __init__(self, pos_start, pos_end, details):
+    super().__init__(pos_start, pos_end, 'Expected Character', details)
+
+class InvalidSyntaxError(Error):
+  def __init__(self, pos_start, pos_end, details=''):
+    super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+
+class RTError(Error):
+  def __init__(self, pos_start, pos_end, details, context):
+    super().__init__(pos_start, pos_end, 'Runtime Error', details)
+    self.context = context
+
+  def as_string(self):
+    result  = self.generate_traceback()
+    result += f'{self.error_name}: {self.details}'
+    result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+    return result
+
+  def generate_traceback(self):
+    result = ''
+    pos = self.pos_start
+    ctx = self.context
+
+    while ctx:
+      result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+      pos = ctx.parent_entry_pos
+      ctx = ctx.parent
+
+    return 'Traceback (most recent call last):\n' + result
+
+
+# POSITION
+
+
+class Position:
+  def __init__(self, idx, ln, col, fn, ftxt):
+    self.idx = idx
+    self.ln = ln
+    self.col = col
+    self.fn = fn
+    self.ftxt = ftxt
+
+  def advance(self, current_char=None):
+    self.idx += 1
+    self.col += 1
+
+    if current_char == '\n':
+      self.ln += 1
+      self.col = 0
+
+    return self
+
+  def copy(self):
+    return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+
+
+# TOKENS
+
+
+TT_INT		= 'NAMBARI'
+TT_FLOAT    = 'KIBONYEZO'
+TT_PLUS     = 'ONGEZA'
+TT_MINUS    = 'TOA'
+TT_MUL      = 'MARA'
+TT_DIV      = 'GAWA'
+TT_LPAREN   = 'MABANO_KUSHOTO'
+TT_RPAREN   = 'MABANO_KULIA'
+TT_LSQUARE  = 'NUKTA_KUSHOTO'
+TT_RSQUARE  = 'NUKTA_KULIA'
+TT_EE		= 'NI_SAWA'
+TT_NE		= 'SI_SAWA'
+TT_LT		= 'NDOGO_ZAIDI'
+TT_GT		= 'KUBWA_ZAIDI'
+TT_LTE		= 'NDOGO_ZAIDI_AU_SAWA'
+TT_GTE		= 'KUBWA_ZAIDI_AU_SAWA'
+TT_COMMA	= 'KOMA'
+TT_ARROW	= 'MSHALE'
+TT_NEWLINE  = 'MSTARIMPYA'
+TT_EOF		= 'MWISHO'
+TT_POW		= 'KARISIMU'
+TT_KEYWORD  = 'NENO_MUHIMU'
+TT_IDENTIFIER = 'KITAMBULISHO'
+TT_EQ	= 'SAWA'
+TT_STRING = 'NENO'
+
+
+
+KEYWORDS = [
+  'VAR',
+  'NA',
+  'AU',
+  'NOT',
+  'IKIWA',
+  'AU_IKIWA',
+  'IKIWA_NYINGINE',
+  'KWA',
+  'HADI',
+  'STEP',
+  'WAKATI',
+  'NJIA',
+  'BASI',
+  'FUNGA',
+  'RUDISHA',
+  'CONTINUE',
+  'VUNJA',
+]
+
+class Token:
+  def __init__(self, type_, value=None, pos_start=None, pos_end=None):
+    self.type = type_
+    self.value = value
+
+    if pos_start:
+      self.pos_start = pos_start.copy()
+      self.pos_end = pos_start.copy()
+      self.pos_end.advance()
+
+    if pos_end:
+      self.pos_end = pos_end.copy()
+
+  def matches(self, type_, value):
+    return self.type == type_ and self.value == value
+  
+  def __repr__(self):
+    if self.value: return f'{self.type}:{self.value}'
+    return f'{self.type}'
+
+
+# LEXER
+
+
+class Lexer:
+  def __init__(self, fn, text):
+    self.fn = fn
+    self.text = text
+    self.pos = Position(-1, 0, -1, fn, text)
+    self.current_char = None
+    self.advance()
+  
+  def advance(self):
+    self.pos.advance(self.current_char)
+    self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+
+  def make_tokens(self):
+    tokens = []
+
+    while self.current_char != None:
+      if self.current_char in ' \t':
+        self.advance()
+      elif self.current_char == '#':
+        self.skip_comment()
+      elif self.current_char in ';\n':
+        tokens.append(Token(TT_NEWLINE, pos_start=self.pos))
+        self.advance()
+      elif self.current_char in DIGITS:
+        tokens.append(self.make_number())
+      elif self.current_char in LETTERS:
+        tokens.append(self.make_identifier())
+      elif self.current_char == '"':
+        tokens.append(self.make_string())
+      elif self.current_char == '+':
+        tokens.append(Token(TT_PLUS, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '-':
+        tokens.append(self.make_minus_or_arrow())
+      elif self.current_char == '*':
+        tokens.append(Token(TT_MUL, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '/':
+        tokens.append(Token(TT_DIV, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '^':
+        tokens.append(Token(TT_POW, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '(':
+        tokens.append(Token(TT_LPAREN, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == ')':
+        tokens.append(Token(TT_RPAREN, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '[':
+        tokens.append(Token(TT_LSQUARE, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == ']':
+        tokens.append(Token(TT_RSQUARE, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '!':
+        token, error = self.make_not_equals()
+        if error: return [], error
+        tokens.append(token)
+      elif self.current_char == '=':
+        tokens.append(self.make_equals())
+      elif self.current_char == '<':
+        tokens.append(self.make_less_than())
+      elif self.current_char == '>':
+        tokens.append(self.make_greater_than())
+      elif self.current_char == ',':
+        tokens.append(Token(TT_COMMA, pos_start=self.pos))
+        self.advance()
+      else:
+        pos_start = self.pos.copy()
+        char = self.current_char
+        self.advance()
+        return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
+
+    tokens.append(Token(TT_EOF, pos_start=self.pos))
+    return tokens, None
+
+  def make_number(self):
+    num_str = ''
+    dot_count = 0
+    pos_start = self.pos.copy()
+
+    while self.current_char != None and self.current_char in DIGITS + '.':
+      if self.current_char == '.':
+        if dot_count == 1: break
+        dot_count += 1
+      num_str += self.current_char
+      self.advance()
+
+    if dot_count == 0:
+      return Token(TT_INT, int(num_str), pos_start, self.pos)
+    else:
+      return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
+
+  def make_string(self):
+    string = ''
+    pos_start = self.pos.copy()
+    escape_character = False
+    self.advance()
+
+    escape_characters = {
+      'n': '\n',
+      't': '\t'
+    }
+
+    while self.current_char != None and (self.current_char != '"' or escape_character):
+      if escape_character:
+        string += escape_characters.get(self.current_char, self.current_char)
+      else:
+        if self.current_char == '\\':
+          escape_character = True
+        else:
+          string += self.current_char
+      self.advance()
+      escape_character = False
+    
+    self.advance()
+    return Token(TT_STRING, string, pos_start, self.pos)
+
+  def make_identifier(self):
+    id_str = ''
+    pos_start = self.pos.copy()
+
+    while self.current_char != None and self.current_char in LETTERS_DIGITS + '_':
+      id_str += self.current_char
+      self.advance()
+
+    tok_type = TT_KEYWORD if id_str in KEYWORDS else TT_IDENTIFIER
+    return Token(tok_type, id_str, pos_start, self.pos)
+
+  def make_minus_or_arrow(self):
+    tok_type = TT_MINUS
+    pos_start = self.pos.copy()
+    self.advance()
+
+    if self.current_char == '>':
+      self.advance()
+      tok_type = TT_ARROW
+
+    return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+  def make_not_equals(self):
+    pos_start = self.pos.copy()
+    self.advance()
+
+    if self.current_char == '=':
+      self.advance()
+      return Token(TT_NE, pos_start=pos_start, pos_end=self.pos), None
+
+    self.advance()
+    return None, ExpectedCharError(pos_start, self.pos, "'=' (after '!')")
+  
+  def make_equals(self):
+    tok_type = TT_EQ
+    pos_start = self.pos.copy()
+    self.advance()
+
+    if self.current_char == '=':
+      self.advance()
+      tok_type = TT_EE
+
+    return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+  def make_less_than(self):
+    tok_type = TT_LT
+    pos_start = self.pos.copy()
+    self.advance()
+
+    if self.current_char == '=':
+      self.advance()
+      tok_type = TT_LTE
+
+    return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+  def make_greater_than(self):
+    tok_type = TT_GT
+    pos_start = self.pos.copy()
+    self.advance()
+
+    if self.current_char == '=':
+      self.advance()
+      tok_type = TT_GTE
+
+    return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+  def skip_comment(self):
+    self.advance()
+
+    while self.current_char != '\n':
+      self.advance()
+
+    self.advance()
+
+# NODES
+
+class NumberNode:
+  def __init__(self, tok):
+    self.tok = tok
+
+    self.pos_start = self.tok.pos_start
+    self.pos_end = self.tok.pos_end
+
+  def __repr__(self):
+    return f'{self.tok}'
+
+class StringNode:
+  def __init__(self, tok):
+    self.tok = tok
+
+    self.pos_start = self.tok.pos_start
+    self.pos_end = self.tok.pos_end
+
+  def __repr__(self):
+    return f'{self.tok}'
+
+class ListNode:
+  def __init__(self, element_nodes, pos_start, pos_end):
+    self.element_nodes = element_nodes
+
+    self.pos_start = pos_start
+    self.pos_end = pos_end
+
+class VarAccessNode:
+  def __init__(self, var_name_tok):
+    self.var_name_tok = var_name_tok
+
+    self.pos_start = self.var_name_tok.pos_start
+    self.pos_end = self.var_name_tok.pos_end
+
+class VarAssignNode:
+  def __init__(self, var_name_tok, value_node):
+    self.var_name_tok = var_name_tok
+    self.value_node = value_node
+
+    self.pos_start = self.var_name_tok.pos_start
+    self.pos_end = self.value_node.pos_end
+
+class BinOpNode:
+  def __init__(self, left_node, op_tok, right_node):
+    self.left_node = left_node
+    self.op_tok = op_tok
+    self.right_node = right_node
+
+    self.pos_start = self.left_node.pos_start
+    self.pos_end = self.right_node.pos_end
+
+  def __repr__(self):
+    return f'({self.left_node}, {self.op_tok}, {self.right_node})'
+
+class UnaryOpNode:
+  def __init__(self, op_tok, node):
+    self.op_tok = op_tok
+    self.node = node
+
+    self.pos_start = self.op_tok.pos_start
+    self.pos_end = node.pos_end
+
+  def __repr__(self):
+    return f'({self.op_tok}, {self.node})'
+
+class IfNode:
+  def __init__(self, cases, else_case):
+    self.cases = cases
+    self.else_case = else_case
+
+    self.pos_start = self.cases[0][0].pos_start
+    self.pos_end = (self.else_case or self.cases[len(self.cases) - 1])[0].pos_end
+
+class ForNode:
+  def __init__(self, var_name_tok, start_value_node, end_value_node, step_value_node, body_node, should_return_null):
+    self.var_name_tok = var_name_tok
+    self.start_value_node = start_value_node
+    self.end_value_node = end_value_node
+    self.step_value_node = step_value_node
+    self.body_node = body_node
+    self.should_return_null = should_return_null
+
+    self.pos_start = self.var_name_tok.pos_start
+    self.pos_end = self.body_node.pos_end
+
+class WhileNode:
+  def __init__(self, condition_node, body_node, should_return_null):
+    self.condition_node = condition_node
+    self.body_node = body_node
+    self.should_return_null = should_return_null
+
+    self.pos_start = self.condition_node.pos_start
+    self.pos_end = self.body_node.pos_end
+
+class FuncDefNode:
+  def __init__(self, var_name_tok, arg_name_toks, body_node, should_auto_return):
+    self.var_name_tok = var_name_tok
+    self.arg_name_toks = arg_name_toks
+    self.body_node = body_node
+    self.should_auto_return = should_auto_return
+
+    if self.var_name_tok:
+      self.pos_start = self.var_name_tok.pos_start
+    elif len(self.arg_name_toks) > 0:
+      self.pos_start = self.arg_name_toks[0].pos_start
+    else:
+      self.pos_start = self.body_node.pos_start
+
+    self.pos_end = self.body_node.pos_end
+
+class CallNode:
+  def __init__(self, node_to_call, arg_nodes):
+    self.node_to_call = node_to_call
+    self.arg_nodes = arg_nodes
+
+    self.pos_start = self.node_to_call.pos_start
+
+    if len(self.arg_nodes) > 0:
+      self.pos_end = self.arg_nodes[len(self.arg_nodes) - 1].pos_end
+    else:
+      self.pos_end = self.node_to_call.pos_end
+
+class ReturnNode:
+  def __init__(self, node_to_return, pos_start, pos_end):
+    self.node_to_return = node_to_return
+
+    self.pos_start = pos_start
+    self.pos_end = pos_end
+
+class ContinueNode:
+  def __init__(self, pos_start, pos_end):
+    self.pos_start = pos_start
+    self.pos_end = pos_end
+
+class BreakNode:
+  def __init__(self, pos_start, pos_end):
+    self.pos_start = pos_start
+    self.pos_end = pos_end
+
+
+
+# RUN
+
+
+def run(fn, text):
+    lexer = Lexer(fn, text)
+    tokens, error = lexer.make_tokens()
+
+    return tokens, error
