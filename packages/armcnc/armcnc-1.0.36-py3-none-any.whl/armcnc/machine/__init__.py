d72c6aa21f88a25ddeaa3252b229f7b427f373ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:09:25 +0000

    Add package armcnc/1.0.36/armcnc-1.0.36-py3-none-any.whl

---
+++ packages/armcnc/armcnc-1.0.36-py3-none-any.whl/armcnc/machine/__init__.py
@@ -0,0 +1,13 @@
+"""
+******************************************************************************
+* @author  ARMCNC site:www.armcnc.net github:armcnc.github.io
+******************************************************************************
+"""
+
+class Machine:
+
+    def __init__(self, framework):
+        self.framework = framework
+        self.user = "armcnc"
+        self.is_alive = False
+        self.stat = None
