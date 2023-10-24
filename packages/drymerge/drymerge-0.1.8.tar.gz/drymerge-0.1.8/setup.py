cf01ccf6ef6709af5b89dec90436d14b62da862c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 15:07:06 +0000

    Add package drymerge/0.1.8/drymerge-0.1.8.tar.gz

---
+++ packages/drymerge/drymerge-0.1.8.tar.gz/drymerge-0.1.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='drymerge',
+    version='0.1.8',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='Samuel Brashears',
+    author_email='sam@drymerge.com',
+    description='DryMerge Client SDK.',
+)
+
