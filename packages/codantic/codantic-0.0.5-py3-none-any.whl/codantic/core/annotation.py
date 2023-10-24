a936a01c225180ad768c7684fdca840d9f4cf8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 15:05:47 +0000

    Add package codantic/0.0.5/codantic-0.0.5-py3-none-any.whl

---
+++ packages/codantic/codantic-0.0.5-py3-none-any.whl/codantic/core/annotation.py
@@ -0,0 +1,27 @@
+from typing import *
+
+from mousse import Dataclass, Field
+
+from .bbox import BBox, BBoxType, bbox_fields, convert_bbox
+
+__all__ = ["Annotation"]
+
+
+class Annotation(Dataclass, dynamic=True):
+    annotation_id: int = Field(alias="id")
+
+    image_id: int
+    video_id: int
+
+    category_id: int = -1
+    bbox: BBox = None
+
+    def __build__(self):
+        if self.bbox is None:
+            for btype, fields in bbox_fields.items():
+                if all(hasattr(self, field) for field in fields):
+                    self.bbox = convert_bbox(
+                        tuple(getattr(self, field) for field in fields),
+                        btype,
+                        BBoxType.ltwh,
+                    )
---
+++ packages/codantic/codantic-0.0.5-py3-none-any.whl/codantic/models/face_detection.py
@@ -0,0 +1,14 @@
+from typing import *
+
+from .object_detection import ODAnnotation, ODCocoDataset
+
+__all__ = ["FaceAnnotation", "FaceCocoDataset"]
+
+
+class FaceAnnotation(ODAnnotation):
+    keypoints: Sequence[float] = []
+    landmarks: Sequence[Tuple[float, float]] = []
+
+
+class FaceCocoDataset(ODCocoDataset):
+    annotations: List[FaceAnnotation] = []
