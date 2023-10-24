9cfa8aabe83936a6599ed911b73e38863f385873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:07:57 +0000

    Add package abusify-id/0.8/abusify-id-0.8.tar.gz

---
+++ packages/abusify-id/abusify-id-0.8.tar.gz/abusify-id-0.8/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='abusify-id',
+    version='0.8',
+    packages=find_packages(),
+    package_data={
+        'abusify_id': ['.env', 'model.pkl', 'tfidf_vectorizer.pkl'],
+    },
+    install_requires=[
+        'scikit-learn',
+        'pandas',
+        'nltk',
+        'pymysql',
+        'python-decouple',
+    ],
+)
