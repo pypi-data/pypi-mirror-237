6718d8c785d5e524a1836fc6b476c1778c90763a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:07:09 +0000

    Add package esmf-aspect-model-loader/0.0.3/esmf_aspect_model_loader-0.0.3-py3-none-any.whl

---
+++ packages/esmf-aspect-model-loader/esmf_aspect_model_loader-0.0.3-py3-none-any.whl/esmf_aspect_meta_model_python/base/characteristics/either.py
@@ -0,0 +1,31 @@
+#  Copyright (c) 2023 Robert Bosch Manufacturing Solutions GmbH
+#
+#  See the AUTHORS file(s) distributed with this work for additional
+#  information regarding authorship.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+#   SPDX-License-Identifier: MPL-2.0
+
+from abc import ABC, abstractmethod
+
+from esmf_aspect_meta_model_python.base.characteristics.characteristic import Characteristic
+
+
+class Either(Characteristic, ABC):
+    """Either interface class.
+
+    Describes a property that has exactly one of two possible values.
+    """
+
+    @property
+    @abstractmethod
+    def left(self) -> Characteristic:
+        """Left."""
+
+    @property
+    @abstractmethod
+    def right(self) -> Characteristic:
+        """Right."""
