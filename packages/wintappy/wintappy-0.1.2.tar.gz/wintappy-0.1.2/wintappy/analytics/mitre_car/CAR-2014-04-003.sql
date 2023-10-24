5b34292db9f8aaa4c7cd288529e6832839560cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:13:58 +0000

    Add package wintappy/0.1.2/wintappy-0.1.2.tar.gz

---
+++ packages/wintappy/wintappy-0.1.2.tar.gz/wintappy-0.1.2/wintappy/analytics/mitre_car/CAR-2014-04-003.sql
@@ -0,0 +1,12 @@
+-- Powershell Execution
+SELECT
+    child.pid_hash AS pid_hash,
+    COALESCE(child.first_seen, child.daypk) AS first_seen
+FROM process AS child,
+    process AS parent
+WHERE
+    parent.pid_hash = child.parent_pid_hash
+    AND parent.process_name != 'explorer.exe'
+    AND child.process_name = 'powershell.exe'
+    AND child.daypk = {{ search_day_pk|default(20230501, true) }}
+    AND parent.daypk = {{ search_day_pk|default(20230501, true) }}
---
+++ packages/wintappy/wintappy-0.1.2.tar.gz/wintappy-0.1.2/wintappy/database/templates/insert_analytics.sql
@@ -0,0 +1,14 @@
+INSERT INTO mitre_car(
+    analytic_id,
+    technique_id,
+    technique_stix_type,
+    tactic_id,
+    tactic_stix_type
+)
+VALUES (
+    '{{analytic_id}}',
+    '{{technique_id}}',
+    '{{technique_stix_type}}',
+    '{{tactic_id}}',
+    '{{tactic_stix_type}}'
+)
