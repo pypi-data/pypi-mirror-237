bce6ae5deacb0cd29367d3d766a17c0589a54688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:07:56 +0000

    Add package atmosphyre/0.0.5/atmosphyre-0.0.5.tar.gz

---
+++ packages/atmosphyre/atmosphyre-0.0.5.tar.gz/atmosphyre-0.0.5/docs/source/index.rst
@@ -0,0 +1,23 @@
+Atmosphyre Documentation
+===================================
+
+**Atmosphyre** is a Python package to characterise the impact of atmospheric dispersion on a spectrograph's transmission.
+
+It can be installed with pip:
+
+.. code-block:: console
+
+   (.venv) $ pip install astrophyre
+
+.. note::
+
+   This project is under active development.
+
+Contents
+--------
+
+.. toctree::
+
+   introduction
+   functions
+   examples
---
+++ packages/atmosphyre/atmosphyre-0.0.5.tar.gz/atmosphyre-0.0.5/setup.py
@@ -0,0 +1,18 @@
+import setuptools 
+  
+with open("README.md", "r") as fh: 
+    description = fh.read() 
+  
+setuptools.setup( 
+    name="atmosphyre", 
+    version="0.0.5", 
+    author="Jay Stephan", 
+    author_email="Jay.Stephan@stfc.ac.uk", 
+    packages=["atmosphyre"],  
+    description="A Python package to characterise the impact of atmospheric dispersion on a spectrographs throughput.", 
+    long_description=description, 
+    long_description_content_type="text/markdown", 
+    license='MIT', 
+    install_requires=['numpy','astropy','matplotlib','scipy'] 
+) 
+
