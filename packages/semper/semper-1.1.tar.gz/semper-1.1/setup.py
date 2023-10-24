b24f0dee81346e14275b903f5039dfbccb477c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 14:06:00 +0000

    Add package semper/1.1/semper-1.1.tar.gz

---
+++ packages/semper/semper-1.1.tar.gz/semper-1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup,find_namespace_packages
+from os import listdir
+setup(name='semper',
+
+      version='1.1',
+
+      url='https://github.com/parlorsky/sempaiper',
+
+      license='MIT',
+
+      author='Levap Vobayr',
+
+      author_email='tffriend015@gmail.com',
+
+      description='',
+      packages=find_namespace_packages(where="src"),
+      package_dir={"": "src"},
+      package_data={},
+
+
+      zip_safe=False)
