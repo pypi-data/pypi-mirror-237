2a3b05f1fcf0b46dfd5c9f69a6c43e5842141274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 14:05:21 +0000

    Add package pbu/1.1.5/pbu-1.1.5-py3-none-any.whl

---
+++ packages/pbu/pbu-1.1.5-py3-none-any.whl/pbu/performance_logger.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+from logging import Logger
+
+
+class PerformanceLogger:
+    def __init__(self, logger=None):
+        self.start_time = datetime.now()
+        self.last_checkpoint = None
+        self.logger = logger
+
+    def start(self):
+        self.start_time = datetime.now()
+
+    def checkpoint(self, message: str = None, logger: Logger = None):
+        start_time = self.last_checkpoint if self.last_checkpoint is not None else self.start_time
+        now = datetime.now()
+        duration = now - start_time
+        PerformanceLogger._log_message(duration, message, logger if self.logger is None else self.logger)
+        self.last_checkpoint = now
+
+    def finish(self, message: str = None, logger: Logger = None):
+        duration = datetime.now() - self.start_time
+        PerformanceLogger._log_message(duration, message, logger if self.logger is None else self.logger)
+
+    @staticmethod
+    def _log_message(duration: timedelta, message: str = None, logger: Logger = None):
+        if message is None:
+            return
+
+        print_string = f"{message} took {duration}"
+        if logger is not None:
+            logger.info(print_string)
+        else:
+            print(print_string)
+
+    def get_total_runtime(self) -> timedelta:
+        return datetime.now() - self.start_time
+
+    def get_runtime(self) -> timedelta:
+        return datetime.now() - self.last_checkpoint if self.last_checkpoint is not None else self.start_time
