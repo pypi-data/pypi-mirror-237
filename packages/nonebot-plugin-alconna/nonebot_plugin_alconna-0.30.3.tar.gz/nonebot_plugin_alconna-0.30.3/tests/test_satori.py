521fa613b341e53663e1eedafda6cf45c21a80d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 15:06:32 +0000

    Add package nonebot-plugin-alconna/0.30.3/nonebot_plugin_alconna-0.30.3.tar.gz

---
+++ packages/nonebot-plugin-alconna/nonebot_plugin_alconna-0.30.3.tar.gz/nonebot_plugin_alconna-0.30.3/tests/test_satori.py
@@ -0,0 +1,51 @@
+import pytest
+from nonebug import App
+from nonebot import get_adapter
+from arclet.alconna import Args, Alconna
+from nonebot.adapters.satori import Bot, Adapter, Message
+
+from tests.fake import fake_message_event_satori
+
+
+def test_satori():
+    from nonebot.adapters.satori import Message
+
+    from nonebot_plugin_alconna.adapters.satori import Bold, Underline
+
+    msg = Message("/com<b>mand some_arg</b> <u>some_arg</u> some_arg")
+
+    alc = Alconna("/command", Args["some_arg", Bold]["some_arg1", Underline]["some_arg2", str])
+
+    res = alc.parse(msg)
+    assert res.matched
+    assert res.some_arg.type == "bold"
+    assert res.some_arg1.type == "underline"
+    assert isinstance(res.some_arg2, str)
+
+    msg1 = "/command " + Bold("foo bar baz")
+
+    alc1 = Alconna("/command", Args["foo", str]["bar", Bold]["baz", Bold])
+
+    res1 = alc1.parse(msg1)
+    assert res1.matched
+    assert isinstance(res1.foo, str)
+    assert res1.bar.type == "bold"
+    assert res1.baz.data["text"] == "baz"
+
+
+@pytest.mark.asyncio()
+async def test_send(app: App):
+    from nonebot_plugin_alconna import Text, Image, on_alconna
+
+    test_cmd = on_alconna(Alconna("test"))
+
+    @test_cmd.handle()
+    async def tt_h():
+        await test_cmd.send(Text("ok\n") + Image(raw={"data": b"123", "mimetype": "image/png"}))
+
+    async with app.test_matcher(test_cmd) as ctx:
+        adapter = get_adapter(Adapter)
+        bot = ctx.create_bot(base=Bot, adapter=adapter, platform="satori", info=None)
+        event = fake_message_event_satori(message=Message("test"), id=123)
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, Message('ok\n<img src="data:image/png;base64,MTIz" />'))
