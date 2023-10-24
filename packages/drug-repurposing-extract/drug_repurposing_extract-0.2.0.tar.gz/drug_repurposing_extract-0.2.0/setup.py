372760148051497530b98b6b8304a73c80de490b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:14:32 +0000

    Add package drug-repurposing-extract/0.2.0/drug_repurposing_extract-0.2.0.tar.gz

---
+++ packages/drug-repurposing-extract/drug_repurposing_extract-0.2.0.tar.gz/drug_repurposing_extract-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='drug_repurposing_extract',
+    packages=find_packages(include=["drug_repurposing"]),
+    version='0.2.0',
+    description='Library for automatic generation of drug repurposing data',
+    author='Shiva Aryal',
+    include_package_data=True,
+    package_data={'': ['rf_model.pkl']}
+)
