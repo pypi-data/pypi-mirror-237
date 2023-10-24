c9314335f30ef4779559946613a158a7b483ab4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 15:07:12 +0000

    Add package bm-video-tools/0.0.2/bm_video_tools-0.0.2.tar.gz

---
+++ packages/bm-video-tools/bm_video_tools-0.0.2.tar.gz/bm_video_tools-0.0.2/bm_video_tools/audio.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from bm_video_tools.media import Media
+from bm_video_tools.operate import Operate
+from bm_video_tools.utils import backslash2slash, subprocess_exec
+
+
+class Audio(Media):
+    async def run(self, op: Operate, output_path: str) -> Audio:
+        """
+        执行操作
+        :param op: Operation实例对象
+        :param output_path: 输出路径
+        :return: 媒体对象
+        """
+        output_path = backslash2slash(output_path)
+
+        cmd = op.exec(o_a=True)
+        await subprocess_exec(cmd, self.input_path, output_path)
+
+        return self.__class__(output_path)
---
+++ packages/bm-video-tools/bm_video_tools-0.0.2.tar.gz/bm_video_tools-0.0.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as stream:
+    long_description = stream.read()
+
+setuptools.setup(
+    name="bm_video_tools",
+    version="0.0.2",
+    author="galaxyeye",
+    author_email="xieshengpeng@galaxyeye-tech.com",
+    description="音频处理工具",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        r"torch~=2.1.0",
+        r"numpy~=1.24.4",
+        r"scikit-image~=0.21.0",
+        r"pillow~=10.0.1",
+        r"torchvision~=0.16.0",
+        r"moviepy~=1.0.3",
+        r"opencv-python~=4.8.1.78"
+    ],
+)
