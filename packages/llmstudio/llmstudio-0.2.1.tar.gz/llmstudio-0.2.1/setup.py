77e8613c5519226db058a78fed0e0493406c16dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 16:07:29 +0000

    Add package llmstudio/0.2.1/llmstudio-0.2.1.tar.gz

---
+++ packages/llmstudio/llmstudio-0.2.1.tar.gz/llmstudio-0.2.1/setup.py
@@ -0,0 +1,28 @@
+import os
+from importlib.machinery import SourceFileLoader
+from setuptools import setup, find_packages
+from llmstudio import __version__ as SDK_VERSION, __requirements__ as REQUIREMENTS
+
+setup(
+    name="llmstudio",
+    author="TensorOps",
+    url="https://llmstudio.ai/",
+    project_urls={
+        "Source Code": "https://github.com/tensoropsai/llmstudio",
+        "Bug Tracker": "https://github.com/tensoropsai/llmstudio/issues",
+        "Documentation": "https://docs.llmstudio.ai",
+    },
+    author_email="contact@tensorops.ai",
+    description="Prompt Perfection at Your Fingertips",
+    keywords="ml ai llm llmops openai langchain chatgpt llmstudio tensorops",
+    version=SDK_VERSION,
+    packages=["llmstudio", "llmstudio.models"],
+    package_dir={
+        "llmstudio": "llmstudio",
+        "llmstudio.models": "llmstudio/models",
+    },
+    install_requires=REQUIREMENTS,
+    include_package_data=True,
+    entry_points={"console_scripts": ["llmstudio = llmstudio.cli:main"]},
+    python_requires="~=3.9",
+)
