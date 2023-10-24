77ba1902816a3d3ebd7ba6d6dbd3435a785179b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 15:05:57 +0000

    Add package marcuslion/0.2.16/marcuslion-0.2.16.tar.gz

---
+++ packages/marcuslion/marcuslion-0.2.16.tar.gz/marcuslion-0.2.16/src/marcuslion/providers.py
@@ -0,0 +1,46 @@
+import builtins
+import io
+import urllib
+import urllib3
+
+import pandas as pd
+
+import src.marcuslion as ml
+
+class Providers:
+    """
+    MarcusLion Providers class
+    """
+
+    @staticmethod
+    def list() -> pd.DataFrame:
+        """
+        Providers.list()
+        """
+        # $ curl 'https://qa1.marcuslion.com/core/datasets/search?providers=kaggle,usgov&title=bike'
+        url = ml.base_url + "/" +'core/datasets/providers?'
+        params = {"apiKey": ml.ml_api_key }
+        full_url = url + urllib.parse.urlencode(params)
+
+        # Sending a GET request and getting back response as HTTPResponse object.
+        http = urllib3.PoolManager()
+        resp = http.request("GET", full_url)
+        if resp.status != 200:
+            raise ValueError("status: " + url + " -> " + str(resp.status))
+
+        # converting
+        string_io = io.StringIO(resp.data.decode())
+        return pd.read_json(string_io)
+
+    def query(self):
+        """
+        Providers.query()
+        """
+        pass
+
+    def search(self):
+        """
+        Providers.search()
+        """
+        pass
+
