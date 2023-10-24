384ac2e2cb01467d8e51aeffa6f8282b2b75afa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-24 03:14:08 +0000

    Add package olscheck/0.1.7/olscheck-0.1.7.tar.gz

---
+++ packages/olscheck/olscheck-0.1.7.tar.gz/olscheck-0.1.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="olscheck",
+    version="0.1.7",
+    author="Jan Rathfelder",
+    author_email="jan_rathfelder@yahoo.de",
+    description="A library to check OLS assumptions.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/janrth/olscheck", 
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License", # or another license
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "statsmodels",
+        "scienceplot"
+    ],
+)
+
