355331f275bd55569b83399c6e22ac4b401a5eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 14:06:17 +0000

    Add package marcuslion/0.2.17/marcuslion-0.2.17.tar.gz

---
+++ packages/marcuslion/marcuslion-0.2.17.tar.gz/marcuslion-0.2.17/tests/ml_lib_test.py
@@ -0,0 +1,18 @@
+import src.marcuslion as ml
+
+
+if __name__ == '__main__':
+    try:
+        help(ml)
+        help(ml.providers)
+        # help(ml.datasets)
+
+        df = ml.providers.list()
+        print(df.head(10))
+
+        df = ml.datasets.search("bike", "kaggle,usgov")
+        print(df.head(3))
+        print(df.tail(3))
+
+    except Exception as e:
+        print("Exception ", e)
