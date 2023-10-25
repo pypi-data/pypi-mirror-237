d02fd1433cb35245d66dc0d76461f4924f63b11a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 04:06:47 +0000

    Add package conda-poetry-liaison/0.0.2/conda_poetry_liaison-0.0.2.tar.gz

---
+++ packages/conda-poetry-liaison/conda_poetry_liaison-0.0.2.tar.gz/conda_poetry_liaison-0.0.2/conda_poetry_liaison/clean.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+
+from .utils import get_files
+
+
+def run_cleaning(env_path):
+    if not isinstance(env_path, str):
+        raise TypeError(f"env_path should be a string, not {type(env_path)}")
+    direct_url_paths = get_files(env_path, "direct_url.json")
+    for path in direct_url_paths:
+        try:
+            os.remove(path)
+        except FileNotFoundError:
+            # Due to symlinks we often have repeated files.
+            pass
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Notify poetry of conda Python packages"
+    )
+    parser.add_argument(
+        "env_path",
+        type=str,
+        nargs="?",
+        help="Path of conda environment to clean",
+    )
+    args = parser.parse_args()
+    run_cleaning(args.env_path)
+
+
+if __name__ == "__main__":
+    main()
