4dda8e6d16ffb0d15a22955d2b2c3223fbcd217d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:07:37 +0000

    Add package herta-villa-sdk/0.8.4/herta_villa_sdk-0.8.4-py3-none-any.whl

---
+++ packages/herta-villa-sdk/herta_villa_sdk-0.8.4-py3-none-any.whl/hertavilla/apis/img.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from hertavilla.apis.internal import _BaseAPIMixin
+
+
+class ImgAPIMixin(_BaseAPIMixin):
+    async def transfer_image(
+        self,
+        villa_id: int,
+        url: str,
+    ) -> str:
+        """将非米游社的三方图床图片转存到米游社官方图床
+
+        Args:
+            villa_id (int): 大别野 id
+            url (str): 三方图床的图片链接
+
+        Returns:
+            str: 新的米游社官方图床的图片链接
+        """
+        return (
+            await self.base_request(
+                "/transferImage",
+                "POST",
+                villa_id,
+                data={"url": url},
+            )
+        )["new_url"]
---
+++ packages/herta-villa-sdk/herta_villa_sdk-0.8.4-py3-none-any.whl/hertavilla/utils.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import json
+
+
+class MsgEncoder(json.JSONEncoder):
+    def default(self, obj):
+        from hertavilla.message.internal import MsgContent
+
+        if isinstance(obj, MsgContent):
+            data = obj.dict()
+            for k in data.copy().keys():
+                if k.startswith("_"):
+                    data.pop(k)
+            return data
+        return json.JSONEncoder.default(self, obj)
+
+
+def _c(text: str) -> int:
+    return (len(text.encode("utf-16")) // 2) - 1
+
+
+def _rc(length: int) -> int:
+    return (length + 1) * 2
