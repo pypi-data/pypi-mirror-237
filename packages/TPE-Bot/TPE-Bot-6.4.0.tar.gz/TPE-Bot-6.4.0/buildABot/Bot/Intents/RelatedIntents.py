6e5d9fc80fee2f37ab94d64e3665e08cfb23994c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:08:00 +0000

    Add package TPE-Bot/6.4.0/TPE-Bot-6.4.0.tar.gz

---
+++ packages/TPE-Bot/TPE-Bot-6.4.0.tar.gz/TPE-Bot-6.4.0/buildABot/Bot/Intents/RelatedIntents.py
@@ -0,0 +1,106 @@
+import pandas as pd
+
+class RelatedIntents():
+    def getYouMayAlsoLikeWeb():
+        """
+            Dialogflow's text template for "You may also like:"
+            :return: JSON of text response of "you may also like"
+        """
+        return {
+                "type": "0",
+                "title": "",
+                "textToSpeech": "",
+                "lang": "en",
+                "speech": [
+                "👇🏼 You may also like:"  
+                ],
+                "condition": ""
+        }
+    
+    def getRelatedIntentsWeb():
+        """
+        Dialogflow's chips template for related intents to be recommended to user
+        :return: JSON of chips response consisting all related intents
+        """
+        return {
+        "type": "4",
+        "title": "",
+        "payload": {
+            "richContent": [
+                [
+                    {
+                        "options": [ # top neighbour intent button payload here ["payload"]["richContent"][0][0]
+                        ],
+                        "type": "chips"
+                    }
+                ]
+            ]
+        },
+        "textToSpeech": "",
+        "lang": "en",
+        "condition": ""
+        }
+
+    def getRelatedIntentName():
+        """
+            Dialogflow's text template as placeholder for related intent's name
+            :return: JSON of text response consisting all related intent's Q1 as name
+        """
+        return {
+        "text": ""  # Intent Q1 as text
+        }
+    
+    def getRelatedIntentsTele():
+        """
+        Dialogflow's chips template for related intents to be recommended to user
+        :return: JSON of chips response consisting all related intents
+        """
+        return {
+            "type": "1",
+            "platform": "telegram",
+            "title": "👇🏼 You may also like:",
+            "buttons": [],
+            "textToSpeech": "",
+            "lang": "en",
+            "condition": ""
+        }
+    
+    def getTeleButtons(names):
+        return {
+            "postback": names,
+            "text": names
+        }
+    
+    def createRelatedTextWeb():
+        return RelatedIntents.getYouMayAlsoLikeWeb()  # Add in 'You may also like'
+        
+        
+    def createRelatedPayload(relatedIntent, df2):
+        recResponseWeb = RelatedIntents.getRelatedIntentsWeb()
+        recResponseTele = RelatedIntents.getRelatedIntentsTele()
+
+        for i in range(1, len(relatedIntent.columns)+1):
+            recIntentNames = RelatedIntents.getRelatedIntentName()
+
+            intent_key = "Related Intent {}".format(str(i)) # Search for related intent's columns
+            pd.set_option("display.max_colwidth", None) # Settings to show full text for chips
+            
+            if(relatedIntent[intent_key].item()):
+                related = df2[df2['S/N'] == relatedIntent[intent_key].item()]
+                related_str = related['Q1'].to_string(index=False) #Get related intent's Q1 as display text for chips
+
+                # Pull data into the text payload and append to list
+                recIntentNames["text"] = related_str
+                
+                #recIntentNames_copy = recIntentNames.copy()
+                recResponseWeb["payload"]["richContent"][0][0]["options"].append(recIntentNames)
+
+                recResponseTele["buttons"].append(RelatedIntents.getTeleButtons(related_str))
+    
+        
+        return recResponseWeb, recResponseTele
+    
+
+
+
+
