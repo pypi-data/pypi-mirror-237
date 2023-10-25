1f50f8282c7d4cfcfe5a0f8c450efb00299176a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 06:07:28 +0000

    Add package smartystreets-python-sdk/4.12.1/smartystreets_python_sdk-4.12.1.tar.gz

---
+++ packages/smartystreets-python-sdk/smartystreets_python_sdk-4.12.1.tar.gz/smartystreets_python_sdk-4.12.1/smartystreets_python_sdk/international_autocomplete/candidate.py
@@ -0,0 +1,9 @@
+class Candidate:
+    def __init__(self, obj=None):
+        self.street = obj.get('street', None)
+        self.locality = obj.get('locality', None)
+        self.administrative_area = obj.get('administrative_area', None)
+        self.super_administrative_area = obj.get('super_administrative_area', None)
+        self.sub_administrative_area = obj.get('sub_administrative_area', None)
+        self.postal_code = obj.get('postal_code', None)
+        self.country_iso3 = obj.get('country_iso3', None)
