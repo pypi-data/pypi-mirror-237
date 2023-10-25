93ace3d513c4d9976d98caf7a00e4c3e1a207033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:05:31 +0000

    Add package opentelemetry-instrumentation-cohere/0.0.5/opentelemetry_instrumentation_cohere-0.0.5.tar.gz

---
+++ packages/opentelemetry-instrumentation-cohere/opentelemetry_instrumentation_cohere-0.0.5.tar.gz/opentelemetry_instrumentation_cohere-0.0.5/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cohere']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['opentelemetry-api>=1.19.0,<2.0.0',
+ 'opentelemetry-instrumentation>=0.40b0,<0.41',
+ 'opentelemetry-semantic-conventions-ai>=0.0.7,<0.0.8']
+
+setup_kwargs = {
+    'name': 'opentelemetry-instrumentation-cohere',
+    'version': '0.0.5',
+    'description': 'OpenTelemetry Cohere instrumentation',
+    'long_description': '# opentelemetry-instrumentation-cohere\n\nProject description here.\n',
+    'author': 'Gal Kleinman',
+    'author_email': 'gal@traceloop.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.1,<4',
+}
+
+
+setup(**setup_kwargs)
