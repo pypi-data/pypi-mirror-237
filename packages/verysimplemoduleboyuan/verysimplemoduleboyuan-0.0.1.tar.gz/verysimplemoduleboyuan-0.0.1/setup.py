cd7482494b3489b8671d7093f79b38ab4641d1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 14:07:33 +0000

    Add package verysimplemoduleboyuan/0.0.1/verysimplemoduleboyuan-0.0.1.tar.gz

---
+++ packages/verysimplemoduleboyuan/verysimplemoduleboyuan-0.0.1.tar.gz/verysimplemoduleboyuan-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with long description'
+
+setup(
+    name="verysimplemoduleboyuan",
+    version=VERSION,
+    author="Happy Learner",
+    author_email="",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'first package'],
+    classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
