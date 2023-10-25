cc19573a3dce7f610c939f67445f2bda0cb31cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 04:06:17 +0000

    Add package nonebot-plugin-scheduled-broadcast/0.1.1/nonebot_plugin_scheduled_broadcast-0.1.1.tar.gz

---
+++ packages/nonebot-plugin-scheduled-broadcast/nonebot_plugin_scheduled_broadcast-0.1.1.tar.gz/nonebot_plugin_scheduled_broadcast-0.1.1/nonebot_plugin_scheduled_broadcast/__init__.py
@@ -0,0 +1,69 @@
+"""The initialization file."""
+
+from nonebot import on_command
+from nonebot.adapters import Bot, Event, Message
+from nonebot.params import CommandArg
+from nonebot.permission import SUPERUSER
+from nonebot.plugin import PluginMetadata
+
+from .config import Config
+from .core import load_broadcast_db, save_broadcast_db, dump_event, pause_target_jobs, resume_target_jobs
+
+__plugin_meta__ = PluginMetadata(
+    name="定时广播插件",
+    description="一款可配置的, 不依赖具体适配器的, 基于事件的定时广播插件.",
+    usage="超级用户指令: 启动广播/enablebc 广播ID, 关闭广播/disablebc 广播ID\\装饰器: broadcast.",
+    type="library",
+    homepage="https://github.com/T0nyX1ang/nonebot-plugin-scheduled-broadcast",
+    config=Config,
+)
+
+anchor_enable = on_command(cmd='启动广播', aliases={'enablebc'}, permission=SUPERUSER)
+anchor_disable = on_command(cmd='关闭广播', aliases={'disablebc'}, permission=SUPERUSER)
+
+
+@anchor_enable.handle()
+async def handle_anchor_enable(bot: Bot, event: Event, arg: Message = CommandArg()):
+    """Handle the anchor_enable command."""
+    self_id = str(bot.self_id)
+    broadcast_id = arg.extract_plain_text().strip()
+    if not broadcast_id:
+        await anchor_enable.finish("广播ID不能为空.")
+
+    event_data, event_hash = dump_event(event)
+
+    broadcast_db = load_broadcast_db()
+    if self_id not in broadcast_db:
+        broadcast_db[self_id] = {}
+
+    if broadcast_id not in broadcast_db[self_id]:
+        broadcast_db[self_id][broadcast_id] = {}
+        broadcast_db[self_id][broadcast_id]["config"] = {}
+        broadcast_db[self_id][broadcast_id]["data"] = event_data
+        broadcast_db[self_id][broadcast_id]["hash"] = event_hash
+
+    broadcast_db[self_id][broadcast_id]["enable"] = True  # enable the broadcast
+    save_broadcast_db(broadcast_db)
+    resume_target_jobs(self_id, broadcast_id)
+
+    await anchor_enable.finish(f"已启动广播, 广播ID为{broadcast_id}, 请进行广播配置, 需要删除广播时请使用关闭广播+广播ID")
+
+
+@anchor_disable.handle()
+async def handle_anchor_disable(bot: Bot, arg: Message = CommandArg()):
+    """Handle the anchor_disable command."""
+    self_id = str(bot.self_id)
+    broadcast_id = arg.extract_plain_text().strip()
+
+    broadcast_db = load_broadcast_db()
+    if self_id not in broadcast_db:
+        await anchor_disable.finish("该机器人没有启动任何广播.")
+
+    if broadcast_id not in broadcast_db[self_id]:
+        await anchor_disable.finish("该广播ID不存在, 请检查输入是否正确.")
+
+    broadcast_db[self_id][broadcast_id]["enable"] = False  # disable the broadcast
+    save_broadcast_db(broadcast_db)
+    pause_target_jobs(self_id, broadcast_id)
+
+    await anchor_disable.finish("已关闭广播.")
---
+++ packages/nonebot-plugin-scheduled-broadcast/nonebot_plugin_scheduled_broadcast-0.1.1.tar.gz/nonebot_plugin_scheduled_broadcast-0.1.1/nonebot_plugin_scheduled_broadcast/core.py
@@ -0,0 +1,103 @@
+"""Core functions for scheduled broadcast plugin."""
+
+import base64
+import hashlib
+import pickle
+import json
+import sys
+
+import nonebot
+from nonebot.adapters import Event
+from nonebot.log import logger
+
+from .config import Config
+
+scheduler = nonebot.require("nonebot_plugin_apscheduler").scheduler
+global_config = nonebot.get_driver().config
+config = Config.parse_obj(global_config)
+config.broadcast_policy_location.touch(exist_ok=True)  # create the policy file if not exists
+
+try:
+    with open(config.broadcast_policy_location, 'r', encoding='utf-8') as fi:
+        fin = fi.read()
+        broadcast_db: dict[str, dict[str, dict[str, dict]]] = json.loads(fin) if fin else {}
+except Exception:
+    logger.error("Failed to load broadcast policy database, please check it.")
+    sys.exit(1)
+
+
+def load_broadcast_db() -> dict[str, dict[str, dict[str, dict]]]:
+    """Load the broadcast policy database."""
+    return broadcast_db
+
+
+def save_broadcast_db(content: dict[str, dict[str, dict[str, dict]]]) -> None:
+    """Save the broadcast policy database."""
+    with open(config.broadcast_policy_location, 'w', encoding='utf-8') as fo:
+        fout = json.dumps(content, indent=4, ensure_ascii=False, sort_keys=True)
+        fo.write(fout)
+
+
+def load_event(event_str: str, event_hash: str) -> Event:
+    """Load an event from a base64 string and validate its hash."""
+    event_dump = base64.b64decode(event_str.encode())
+    if hashlib.sha256(event_dump).hexdigest() != event_hash:
+        raise ValueError('Event hash mismatch, the data may be corrupted or tampered.')
+    return pickle.loads(event_dump)
+
+
+def dump_event(event: Event) -> tuple[str, str]:
+    """Dump an event into a base64 string with a hash."""
+    event_dump = pickle.dumps(event)
+    event_hash = hashlib.sha256(event_dump).hexdigest()
+    event_b64 = base64.b64encode(event_dump).decode()
+    return (event_b64, event_hash)
+
+
+def valid(cmd_name: str) -> list[tuple[str, str]]:
+    """Get all valid [self_id, broadcast_id] tuple for broadcast policy."""
+    return [(self_id, broadcast_id)
+            for self_id in broadcast_db.keys()
+            for broadcast_id in broadcast_db[self_id].keys()
+            if cmd_name in broadcast_db[self_id][broadcast_id]["config"]]
+
+
+def pause_target_jobs(self_id: str, broadcast_id: str) -> None:
+    """Pause the target broadcast jobs."""
+    for cmd_name in broadcast_db[self_id][broadcast_id]["config"].keys():
+        scheduler.pause_job(f'broadcast_{broadcast_id}_bot_{self_id}_command_{cmd_name}')
+        logger.debug(f'Paused broadcast [{broadcast_id}] with bot [{self_id}] for command [{cmd_name}].')
+
+
+def resume_target_jobs(self_id: str, broadcast_id: str) -> None:
+    """Resume the target broadcast jobs."""
+    for cmd_name in broadcast_db[self_id][broadcast_id]["config"].keys():
+        scheduler.resume_job(f'broadcast_{broadcast_id}_bot_{self_id}_command_{cmd_name}')
+        logger.debug(f'Resumed broadcast [{broadcast_id}] with bot [{self_id}] for command [{cmd_name}].')
+
+
+def broadcast(cmd_name: str):
+    """Check the policy of each broadcast by name."""
+    _name = cmd_name
+
+    def _broadcast(func):
+        """Rule wrapper for "broadcast" item in the policy control."""
+        logger.debug(f'Checking broadcast: [{_name}].')
+        for self_id, broadcast_id in valid(_name):
+            event_data = broadcast_db[self_id][broadcast_id]['data']
+            event_hash = broadcast_db[self_id][broadcast_id]['hash']
+            event = load_event(event_data, event_hash)
+            scheduler.add_job(func=func,
+                              args=(self_id, event),
+                              trigger='cron',
+                              id=f'broadcast_{broadcast_id}_bot_{self_id}_command_{_name}',
+                              misfire_grace_time=30,
+                              replace_existing=True,
+                              **broadcast_db[self_id][broadcast_id]["config"][_name])
+            logger.debug(f'Created broadcast [{broadcast_id}] with bot [{self_id}] for command [{_name}].')
+
+            if not broadcast_db[self_id][broadcast_id]["enable"]:
+                scheduler.pause_job(f'broadcast_{broadcast_id}_bot_{self_id}_command_{_name}')
+                logger.debug(f'Paused broadcast [{broadcast_id}] with bot [{self_id}] for command [{_name}].')
+
+    return _broadcast
