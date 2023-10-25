63031ef2de5269cfdc895feac6929370fbbbafbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 04:05:58 +0000

    Add package mnnlib/0.3.0/mnnlib-0.3.0.tar.gz

---
+++ packages/mnnlib/mnnlib-0.3.0.tar.gz/mnnlib-0.3.0/mnn/__init__.py
@@ -0,0 +1,7 @@
+from mnn.network import network
+from mnn.layer import layer
+import mnn.activations
+from mnn.dataset import dataset
+from mnn.trainer import trainer
+from mnn.save import save
+from mnn.load import load
