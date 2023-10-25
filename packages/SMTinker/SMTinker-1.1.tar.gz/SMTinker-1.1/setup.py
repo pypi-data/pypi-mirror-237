7d505b7758f1b1d89cb647d94b6ef9e710a2ae2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 15:05:22 +0000

    Add package SMTinker/1.1/SMTinker-1.1.tar.gz

---
+++ packages/SMTinker/SMTinker-1.1.tar.gz/SMTinker-1.1/setup.py
@@ -0,0 +1,12 @@
+import os
+import easyvenv as ev
+
+project_dir = os.path.dirname(__file__)
+publish = True
+
+if not publish:
+    ev.easyvenv(project_dir)
+else:
+    # package_dir = ev.create_package("SMTinker", "1.1", project_dir)
+    package_dir = "G:\\SMS\\LIBRARY\\Repositories\\SMTinker\\SMTinker"
+    ev.publish_package(package_dir, "pypi-AgEIcHlwaS5vcmcCJDJmZTBjOWNmLWE5NzAtNGM2ZS1hMjg2LTVmNjFhMGZmZjRhYgACKlszLCI0YjgwNzdmYi04ZGU5LTQyZGItOGZjZC1mZTcwYmJlMDUyNDkiXQAABiD2yesyTn5-IoDMGWrQNNL47K97CGHGhjz_3p5NNc6KSA")
