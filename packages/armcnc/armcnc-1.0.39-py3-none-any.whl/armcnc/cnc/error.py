5abd195d584c02c629b5bd8ca128f39f8d9bfc17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:07:41 +0000

    Add package armcnc/1.0.39/armcnc-1.0.39-py3-none-any.whl

---
+++ packages/armcnc/armcnc-1.0.39-py3-none-any.whl/armcnc/cnc/error.py
@@ -0,0 +1,28 @@
+"""
+******************************************************************************
+* @author  ARMCNC site:www.armcnc.net github:armcnc.github.io
+******************************************************************************
+"""
+
+import threading
+import linuxcnc
+
+class Error:
+
+    def __init__(self, framework):
+        self.framework = framework
+        self.linuxcnc = linuxcnc
+        self.api = self.linuxcnc.error_channel()
+        self.task = threading.Thread(name="error_task", target=self.task)
+        self.task.daemon = True
+        self.task.start()
+
+    def task(self):
+        while True:
+            if self.framework.machine.is_alive:
+                error = self.api.poll()
+                if error:
+                    kind, text = error
+                    if kind in (linuxcnc.NML_ERROR, linuxcnc.OPERATOR_ERROR):
+                        self.framework.utils.service.service_write({"command": "launch:machine:error", "message": text, "data": False})
+            self.framework.utils.set_sleep(0.1)
