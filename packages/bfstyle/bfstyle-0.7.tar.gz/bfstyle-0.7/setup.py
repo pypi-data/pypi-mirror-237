a2587d4074de843b29550574ee410e76d8e969dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 05:06:26 +0000

    Add package bfstyle/0.7/bfstyle-0.7.tar.gz

---
+++ packages/bfstyle/bfstyle-0.7.tar.gz/bfstyle-0.7/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+readme = open("./README.md", "r")
+
+
+setup(
+    name='bfstyle',
+    packages=['bfstyle'],  # this must be the same as the name above
+    version='0.7',
+    description='Librería de estilo plots Brain Food',
+    long_description=readme.read(),
+    long_description_content_type='text/markdown',
+    author='J. Ignacio del Rio',
+    author_email='jorge.delrio@ug.uchile.cl',
+    # use the URL to the github repo
+    url='https://github.com/ignacio365/BF_plots',
+    download_url='https://github.com/ignacio365/BF_plots/tarball/0.5',
+    keywords=['brainfood', 'style', 'plot'],
+    classifiers=[ ],
+    license='MIT',
+    include_package_data=True,
+    package_data={
+        # If any package contains *.ttf , include them:
+        '': ['.ttf'],
+    },
+)
