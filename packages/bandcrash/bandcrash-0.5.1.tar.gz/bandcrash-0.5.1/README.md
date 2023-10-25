d1442ecb0b5cd27139a4179c9bb5f67e94f57780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:05:35 +0000

    Add package bandcrash/0.5.1/bandcrash-0.5.1.tar.gz

---
+++ packages/bandcrash/bandcrash-0.5.1.tar.gz/bandcrash-0.5.1/README.md
@@ -0,0 +1,261 @@
+# Bandcrash (formerly pyBlamscamp)
+
+Bandcrash is a standalone program that automatically encodes an album of songs into a bunch of different formats for distribution on various platforms, such as [itch.io](https://itch.io/), or for hosting on your own website. The embedded player is originally based on the one from [blamscamp](https://github.com/blackle/blamscamp) by [@blackle](https://github.com/blackle), although it has been pretty thoroughly modified at this point.
+
+[See it in action](https://fluffy.itch.io/novembeat-2021)!
+
+## Features
+
+* Output as mp3, ogg, FLAC, and web preview (HTML5+mp3 at a lower bitrate)
+* Optionally upload everything to your page on itch using [butler](https://itch.io/docs/butler/)
+* High-quality encoding and metadata, with support for cover songs, per-track artwork, embedded lyrics, and more
+* Web player also supports per-track artwork
+
+## Usage
+
+Bandcrash comes with everything it needs to operate out of the box.
+
+### GUI
+
+The easiest way to install the GUI is to download it from [my itch.io page](https://fluffy.itch.io/bandcrash). Better yet, use the [itch app](https://itch.io/app) to keep it updated!
+
+Alternately, you can install [Python](https://python.org/) and run
+
+```
+pip install bandcrash -e gui
+```
+
+and then launch it with `bandcrash-gui`.
+
+### Command-line
+
+For the command-line version, the suggested approach is to install [Python](https://python.org), after which you can install Bandcrash with:
+
+```
+pip install bandcrash
+```
+
+`bandcrash --help` will provide a lot more detailed information. For the most basic usage you can run:
+
+```
+bandcrash --init input_dir output_dir
+```
+
+which will try to guess the track order, titles, and artwork from the files in that directory, and store the resulting album configuration as `input_dir/album.json`. It will then process the album and put its results into `output_dir`.
+
+You can also specify the input as a path to the JSON file, e.g.:
+
+```
+bandcrash input_dir/my_album.json output_dir
+```
+
+or if you want to keep the JSON file in a different directory or use a name other than `album.json`, you can use a relative path like:
+
+```
+bandcrash input_dir -j ../data/my_album.json
+```
+
+Note that the `-j` path is relative to `input_dir`, *not* the current working directory.
+
+The entry point for the CLI is in `bandcrash/cli.py`.
+
+## Use as a library
+
+Bandcrash can also be used as a Python library in order to embed it into a web service or application. Here is a minimal example of how to use it:
+
+```python
+import bandcrash
+import bandcrash.options
+import collections.defaultdict
+import concurrent.futures
+
+config = bandcrash.options.Options()
+album = {
+    'artist': 'My Band',
+    'title': 'My album',
+    'tracks': [
+        {'filename': 'track 1.wav', 'title': 'The First Track'},
+    ],
+}
+
+pool = concurrent.futures.ThreadPoolExecutor()
+futures = collections.defaultdict(list)
+
+bandcrash.process(config, album, pool, futures)
+for task in concurrent.futures.as_completed(list(itertools.chain(*futures.values()))):
+    try:
+        task.result()
+    except Exception:
+        # handle the exception in some helpful way
+```
+
+1. Load your album metadata into a Python `dict` structure (e.g. by doing `json.loads` on a JSON file or the like)
+2. Create a `concurrent.futures.ThreadPoolExecutor` and a `collections.defaultdict(list)` to store its futures
+3. Create a `bandcrash.options.Options` and set its options accordingly
+4. Call `bandcrash.process(options, album_data, threadpool, futures_dict`
+5. Wait for all of the futures in the `futures_dict` to complete
+
+The `futures_dict` is a mapping from process step &rarr; list of futures, and is fully populated after `process()` returns. You can use something like `list(itertools.chain(*futures_dict.values()))` to collapse it into a single list of futures if you don't care about tracking individual stages in the pipeline.
+
+It is also fine (and, in fact, preferable) to share a `ThreadPoolExecutor` across multiple concurrent batches.
+
+## Album format
+
+For the CLI and GUI, album information is stored in a JSON file (normally named `album.json`) that looks something like this:
+
+```json
+{
+    "artist": "The artist of the album",
+    "title": "The title of the album",
+    "bg_color": "black",
+    "fg_color": "white",
+    "highlight_color": "#cc00ff",
+    "artwork": "album_cover.jpg",
+    "tracks": [{
+        "filename": "the first track.wav",
+        "title": "The First Track",
+        "artwork": "track1_cover.jpg",
+        "lyrics": ["This is the first line",
+            "This is the second line",
+            "This is the third line",
+            "",
+            "This is the second verse",
+            "This song just keeps getting worse"],
+        "hidden": false,
+        "preview": true
+    }]
+}
+```
+
+You can also automatically generate a stub `album.json` file with the `--init` option. Here are a few examples of its use:
+
+```
+# stored as input_dir/album.json
+bandcrash --init input_dir output_dir
+
+# stored as input_dir/filename.json
+bandcrash --init input_dir/filename.json output_dir
+
+# stored as input_dir/data/filename.json
+bandcrash --init input_dir -j data/filename.json output_dir
+
+```
+
+which will try to guess the track order and titles from the audio files in `input_dir`.
+
+Basically, the top-level album contains the following properties (all optional):
+
+* `artist`: The artist for the album as a whole
+* `title`: The album's title
+* `year`: The release year
+* `composer`: The album's composer
+* `artwork`: an image file to use for the album's cover art (relative or absolute paths okay)
+* `bg_color`, `fg_color`, `highlight_color`: The color theme for the player
+* `genre`: The default genre for all tracks
+* `tracks`: an array of track descriptions, in album order
+* `do_mp3`: Whether to build the album in MP3 format
+* `do_ogg`: Whether to build the album in Ogg Vorbis format
+* `do_flac`: Whether to build the album in FLAC format
+* `do_preview`: Whether to build the web preview
+* `do_zip`: Whether to build a zip file of each output format
+* `do_butler`: Whether to upload the builds to itch.io
+* `butler_target`: The itch.io Butler target (e.g. `"fluffy/songs-of-substance"`)
+* `butler_prefix`: Any prefix to add to the Butler channel name (e.g. `"bonus-"` will upload it as `"fluffy/songs-of-substance:bonus-mp3"`)
+
+And each track contains (all optional except `filename`):
+
+* `filename`: The audio file to encode into the final output track (ideally wav or aif)
+* `group`: The title of the track's grouping (i.e. a work with multiple movements)
+* `title`: The title of the track
+* `artist`: The performing artist of this track
+* `composer`: The composer of this track
+* `cover_of`: The original artist that this track is a cover of, if any
+* `genre`: The genre of this track
+* `artwork`: Track-specific cover art
+* `lyrics`: An array of strings, one line of lyrics per string; alternately, the name of a text file to read the lyrics from (relative or absolute paths okay)
+* `hidden`: A boolean for whether to hide this track from the web player entirely (e.g. a purchase bonus); defaults to `false`
+* `preview`: A boolen for whether to generate a preview for the web player; defaults to `true`
+* `about`: Detailed commentary about the track
+
+See the [sample album JSON file](https://github.com/fluffy-critter/Bandcrash/blob/main/tests/album/album.json) for a rough example.
+
+## Publishing to Itch
+
+Here's the process for publishing an album to [itch.io](https://itch.io):
+
+1. Install [butler](https://itch.io/docs/butler/) and log in with `butler login`
+1. [Create a new project](https://itch.io/game/new)
+2. Set it as a "soundtrack," with the kind of project being "HTML"
+3. Set your pricing, add preview artwork, etc., and save. Don't do any uploading from this interface.
+4. Run `bandcrash` with a `-b user/project` flag; for example:
+
+    ```sh
+    bandcrash novembeat-2021/wav novembeat-2021/out -b fluffy/novembeat-2021
+    ```
+5. Wait a moment for itch to finish processing; you can use `butler status user/project` (e.g. `butler status fluffy/novembeat-2021`)
+6. Reload your project edit page; you should now have a few targets, such as `preview`, `mp3`, etc.
+7. Set the `preview` target to "This file will be played in the browser". Set all the other targets to "soundtrack" and, optionally, change the display name.
+8. View the project page, and when you're ready to publish, publish!
+
+### Recommended "embed options"
+
+* Set it to "embed in page" with "manually set size"
+* Enable "mobile friendly" (with orientation "default") and "automatically start on page load"
+* Disable "fullscreen button" and "enable scrollbars"
+
+## Publishing to other sites
+
+If you have a website of your own, upload the `preview` directory to your site somewhere, and then embed it using an iframe, e.g.
+
+```html
+<iframe src="/path/to/album/" width=640 height=360></iframe>
+```
+
+## Contributing
+
+Pull requests are welcome! But please note the following:
+
+The generated web player must not receive any added dependencies. The generator must stay as a single, self-contained file that is as small as reasonably possible. The point is for the generated file to be lightweight. Stick to Vanilla JS.
+
+### Development environment notes
+
+If you are developing under Windows, you will probably need to use a POSIX environment under Windows (such as [msys](https://www.msys2.org) or [Git Bash](https://git-scm.com)) rather than WSL.
+
+If you are developing under macOS, there are special considerations in terms of the Python environment you run, especially if you're building the GUI bundle. First, you need a `universal2` build of Python (such as the ones installable from [python.org](https://python.org)) and you need to ensure that you've created your environment against that (e.g. `poetry env use /usr/local/bin/python3.11`). You also need to build a local `universal2` wheel of some packages; to do that, you can run:
+
+```
+./make-universal2.sh
+```
+
+### Roadmap
+
+See the [github issues](https://github.com/fluffy-critter/bandcrash/issues) for details, but roughly:
+
+* Local GUI and/or web UI to make setting up the `album.json` easier (and easier installation, especially on Windows!)
+* Various player improvements
+* Easy embedding into websites (opengraph et al)
+
+## FAQ
+
+### How is this different from blamscamp, scritch, etc.?
+
+[Blamscamp](https://suricrasia.online/blamscamp/) and [scritch](https://torcado.itch.io/scritch-editor) are both great programs for publishing album previews on itch.io and other websites! However, their functionality is only to bundle already-encoded audio files into a web-based player. They don't do the difficult work of encoding and tagging your files, which can be an extremely tedious and error-prone process. Bandcrash's intention is to make the process of encoding and uploading your albums easier to as many stores as possible.
+
+### Why was it renamed?
+
+Back when this project started, it was named pyBlamscamp as the intention was to be basically a Python version of the blamscamp GUI which would also handle encoding steps for you, but it very quickly drifted away from that and became something else. Unfortunately, the similarity of the names was incredibly confusing.
+
+Currently the only connection between Bandcrash and blamscamp is that Bandcrash uses a highly-modified version of blamscamp's web player. They serve different goals.
+
+### Why run it locally instead of as a web app?
+
+You already have your large .wav files on your local hard drive. Your local drive is also a good place to keep your previous encoding results. Your local computer also has a lot more space available than a typical cloud server, doesn't have to juggle cloud storage credentials, doesn't have to worry about the security of the server running the encoder app, the cost of running servers or paying for cloud storage, and so on.
+
+Basically, it's easier for everyone.
+
+Sometimes local apps are just Better™.
+
+## Credits
+
+* Main code: [@fluffy-critter](https://github.com/fluffy-critter)
+* Original player code and this project's name: [@blackle](https://github.com/blackle)
---
+++ packages/bandcrash/bandcrash-0.5.1.tar.gz/bandcrash-0.5.1/bandcrash/gui/track_editor.py
@@ -0,0 +1,383 @@
+""" Track editing widgets """
+
+import logging
+import os
+import os.path
+import typing
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import (QAbstractItemView, QAbstractScrollArea,
+                               QCheckBox, QFileDialog, QFormLayout,
+                               QHBoxLayout, QLineEdit, QListWidget,
+                               QListWidgetItem, QPlainTextEdit, QPushButton,
+                               QScrollArea, QSizePolicy, QSplitter,
+                               QVBoxLayout, QWidget)
+
+from .. import util
+from . import datatypes, file_utils
+from .file_utils import FileRole
+from .widgets import FileSelector, wrap_layout
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TrackEditor(QWidget):
+    """ A track editor pane """
+    # pylint:disable=too-many-instance-attributes
+
+    def __init__(self, album_editor, data: datatypes.TrackData):
+        """ edit an individual track
+
+        :param dict data: The metadata blob
+        """
+        super().__init__()
+        self.setMinimumSize(400, 0)
+
+        self.album_editor = album_editor
+        self.data = data
+
+        layout = QFormLayout()
+        layout.setFieldGrowthPolicy(
+            QFormLayout.FieldGrowthPolicy.AllNonFixedFieldsGrow)
+        self.setLayout(layout)
+
+        self.filename = FileSelector(FileRole.AUDIO, album_editor)
+        self.group = QLineEdit()
+        self.title = QLineEdit()
+        self.genre = QLineEdit()
+        self.artist = QLineEdit()
+        self.composer = QLineEdit()
+        self.cover_of = QLineEdit()
+        self.artwork = FileSelector(FileRole.IMAGE, album_editor)
+        self.lyrics = QPlainTextEdit()
+        self.about = QLineEdit()
+
+        self.preview = QCheckBox("Generate preview")
+        self.hidden = QCheckBox("Hidden track")
+
+        layout.addRow("Audio file", self.filename)
+        layout.addRow("Title", self.title)
+
+        player_options = QHBoxLayout()
+        player_options.setSpacing(0)
+        player_options.setContentsMargins(0, 0, 0, 0)
+        player_options.addWidget(self.preview)
+        player_options.addWidget(self.hidden)
+        layout.addRow("Player options", player_options)
+
+        layout.addRow("Track artist", self.artist)
+        layout.addRow("Cover of", self.cover_of)
+        layout.addRow("Artwork", self.artwork)
+        layout.addRow("Lyrics", self.lyrics)
+        layout.addRow("Genre", self.genre)
+        layout.addRow("Grouping", self.group)
+        layout.addRow("Track comment", self.about)
+
+        self.reset(data)
+
+    def reset(self, data: datatypes.TrackData):
+        """ Reset to the specified backing data """
+        self.data = data
+
+        for key, widget in (
+            ('filename', self.filename.file_path),
+            ('title', self.title),
+            ('genre', self.genre),
+            ('artist', self.artist),
+            ('composer', self.composer),
+            ('cover_of', self.cover_of),
+            ('artwork', self.artwork.file_path),
+            ('group', self.group),
+            ('about', self.about),
+        ):
+            widget.setText(self.data.get(key, ''))
+
+        lyrics = self.data.get('lyrics', '')
+        if isinstance(lyrics, str):
+            self.lyrics.document().setPlainText(lyrics)
+        else:
+            self.lyrics.document().setPlainText('\n'.join(lyrics))
+
+        self.preview.setCheckState(
+            datatypes.to_checkstate(self.data.get('preview', True)))
+        self.hidden.setCheckState(
+            datatypes.to_checkstate(self.data.get('hidden', False)))
+
+    def apply(self):
+        """ Apply our data to the backing data """
+        # pylint:disable=too-many-branches
+
+        if not self.data:
+            return
+
+        LOGGER.debug("TrackEditor.apply %s", self.data.get('filename'))
+
+        relpath = util.make_relative_path(self.album_editor.filename)
+
+        datatypes.apply_text_fields(self.data, (
+            ('filename', self.filename.file_path),
+            ('artwork', self.artwork.file_path),
+        ), relpath)
+
+        datatypes.apply_text_fields(self.data, (
+            ('title', self.title),
+            ('genre', self.genre),
+            ('artist', self.artist),
+            ('composer', self.composer),
+            ('cover_of', self.cover_of),
+            ('group', self.group),
+            ('about', self.about),
+        ))
+
+        def split_lyrics(text):
+            lines = text.split('\n')
+            return lines if len(lines) != 1 else text
+
+        lyrics = split_lyrics(self.lyrics.document().toPlainText())
+        if lyrics:
+            self.data['lyrics'] = lyrics
+        elif 'lyrics' in self.data:
+            del self.data['lyrics']
+
+        datatypes.apply_checkbox_fields(self.data, (
+            ('preview', self.preview, True),
+            ('hidden', self.hidden, False),
+        ))
+
+
+class TrackListEditor(QSplitter):
+    """ The track listing panel and editor """
+    # pylint:disable=too-many-instance-attributes
+
+    class TrackItem(QListWidgetItem):
+        """ an item in the track listing """
+
+        def __init__(self, album_editor, data: datatypes.TrackData):
+            super().__init__()
+            self.editor = TrackEditor(album_editor, data)
+            self.setText(self.display_name)
+
+            self.editor.title.textChanged.connect(self.update_name)
+            self.editor.filename.file_path.textChanged.connect(
+                self.update_name)
+
+        def reset(self, data: datatypes.TrackData):
+            """ Reset the track listing from a new tracklist
+
+            :param list data: album['data']
+            """
+            LOGGER.debug("TrackItem.reset %s", self.display_name)
+            self.editor.reset(data)
+            self.setText(self.display_name)
+
+        def apply(self):
+            """ Apply the GUI values to the backing store """
+            LOGGER.debug("TrackItem.apply %s", self.display_name)
+            self.editor.apply()
+            self.update_name()
+
+        def update_name(self):
+            """ Update the display name """
+            self.setText(self.display_name)
+
+        @property
+        def display_name(self):
+            """ Get the display name of this track """
+            info = self.editor.data
+            if info and 'title' in info:
+                return info['title']
+            if info and 'filename' in info:
+                return f"({os.path.basename(info['filename'])})"
+            return "(unknown)"
+
+    class TrackList(QListWidget):
+        """ The actual track listing panel """
+
+        def __init__(self, parent):
+            super().__init__(parent)
+            self.album = parent
+            self.setDragDropMode(QAbstractItemView.DragDropMode.InternalMove)
+            self.setAcceptDrops(True)
+
+        def dragEnterEvent(self, event):
+            LOGGER.debug("dragEnterEvent %s %s", event, event.proposedAction())
+            LOGGER.debug("hasurls: %s", event.mimeData().hasUrls())
+            if event.proposedAction() == Qt.DropAction.CopyAction and event.mimeData().hasUrls():
+                if files := file_utils.filter_audio_urls(event.mimeData().urls()):
+                    LOGGER.debug("accepted files: %s", files)
+                    event.acceptProposedAction()
+            else:
+                super().dragEnterEvent(event)
+
+        def dropEvent(self, event):
+            if event.proposedAction() == Qt.DropAction.CopyAction and event.mimeData().hasUrls():
+                if files := file_utils.filter_audio_urls(event.mimeData().urls()):
+                    LOGGER.debug("adding files: %s", files)
+                    self.album.add_files(files)
+                    event.acceptProposedAction()
+            else:
+                super().dropEvent(event)
+
+            self.album.apply()
+
+    def __init__(self, album_editor):
+        super().__init__()
+        LOGGER.debug("TrackListEditor.__init__")
+
+        self.data: datatypes.TrackList = []
+        self.album_editor = album_editor
+
+        left_panel = QVBoxLayout(self)
+        left_panel.setSpacing(0)
+        left_panel.setContentsMargins(0, 0, 0, 0)
+        self.addWidget(wrap_layout(self, left_panel))
+
+        self.track_listing = TrackListEditor.TrackList(self)
+        left_panel.addWidget(self.track_listing)
+
+        self.button_add = QPushButton("+")
+        self.button_add.clicked.connect(self.add_track_button)
+        self.button_delete = QPushButton("-")
+        self.button_delete.clicked.connect(self.delete_track)
+        self.button_move_up = QPushButton("^")
+        self.button_move_up.clicked.connect(self.move_up)
+        self.button_move_down = QPushButton("v")
+        self.button_move_down.clicked.connect(self.move_down)
+
+        buttons = QHBoxLayout(self)
+        buttons.setSpacing(0)
+        buttons.setContentsMargins(0, 0, 0, 0)
+        buttons.addWidget(self.button_add)
+        buttons.addWidget(self.button_delete)
+        buttons.addStretch(1000)
+        buttons.addWidget(self.button_move_up)
+        buttons.addWidget(self.button_move_down)
+        left_panel.addWidget(wrap_layout(self, buttons))
+
+        self.slug = TrackEditor(album_editor, {})
+        self.slug.setEnabled(False)
+
+        self.editpanel = QScrollArea()
+        self.editpanel.setMinimumSize(450, 0)
+        self.editpanel.setWidgetResizable(True)
+        self.editpanel.setSizeAdjustPolicy(
+            QAbstractScrollArea.SizeAdjustPolicy.AdjustToContents)
+        self.editpanel.setWidget(self.slug)
+        self.addWidget(self.editpanel)
+
+        self.track_listing.currentRowChanged.connect(self.set_item)
+
+        for widget in (self, self.track_listing):
+            policy = widget.sizePolicy()
+            policy.setVerticalPolicy(QSizePolicy.Policy.Expanding)
+            widget.setSizePolicy(policy)
+
+        self.setSizes([1, 10])
+
+    def reset(self, data: datatypes.TrackList):
+        """ Reset to the backing storage """
+        LOGGER.debug("TrackListEditor.reset")
+
+        current_row = self.track_listing.currentRow()
+
+        if self.track_listing.count() and self.track_listing.count() != len(data):
+            LOGGER.warning("Sync error: Track listing had %d, expected %d",
+                           self.track_listing.count(), len(data))
+
+        for idx, track in enumerate(data):
+            item = typing.cast(TrackListEditor.TrackItem,
+                               self.track_listing.item(idx))
+            if item:
+                item.reset(track)
+            else:
+                self.track_listing.addItem(
+                    TrackListEditor.TrackItem(self.album_editor, track))
+
+        while self.track_listing.count() > len(data):
+            self.track_listing.takeItem(self.track_listing.count() - 1)
+
+        self.data = data
+
+        if current_row != self.track_listing.currentRow():
+            LOGGER.warning("Sync error: list position changed from %d to %d",
+                           self.track_listing.currentRow(), current_row)
+            self.track_listing.setCurrentRow(current_row)
+
+    def apply(self):
+        """ Save any currently-edited track """
+        LOGGER.debug("TrackListEditor.apply")
+        self.data.clear()
+        for row in range(self.track_listing.count()):
+            item = typing.cast(TrackListEditor.TrackItem,
+                               self.track_listing.item(row))
+            item.apply()
+            LOGGER.debug("  -- append %s", item.display_name)
+            self.data.append(item.editor.data)
+
+    def set_item(self, row):
+        """ Signal handler for row change """
+        LOGGER.debug("TrackListEditor.set_item")
+        self.apply()
+        self.editpanel.takeWidget()  # necessary to prevent Qt from GCing it on replacement
+        item = typing.cast(TrackListEditor.TrackItem,
+                           self.track_listing.item(row))
+        if item:
+            self.editpanel.setWidget(item.editor)
+        else:
+            self.editpanel.setWidget(self.slug)
+
+    def add_track_button(self):
+        """ Prompt to add some tracks """
+        LOGGER.debug("TrackListEditor.add_tracks")
+        role = FileRole.AUDIO
+        LOGGER.debug("filter: %s", role.file_filter)
+        filenames, _ = QFileDialog.getOpenFileNames(
+            self,
+            "Select audio files",
+            dir=self.album_editor.get_last_directory(role),
+            filter=role.file_filter)
+
+        if filenames:
+            # update the audio role selection path
+            ref_file = filenames[0]
+            LOGGER.debug("Audio role: using filename %s", ref_file)
+            role.default_directory = os.path.dirname(ref_file)
+            self.album_editor.set_last_directory(
+                role, os.path.dirname(ref_file))
+
+        self.add_files(filenames)
+
+    def add_files(self, filenames):
+        """ Accepts files into the track listing """
+        LOGGER.debug("TrackListEditor.add_files")
+        for filename in filenames:
+            _, title = util.guess_track_title(filename)
+            track = {'filename': filename, 'title': title}
+            self.data.append(track)
+            self.track_listing.addItem(
+                TrackListEditor.TrackItem(self.album_editor, track))
+
+    def delete_track(self):
+        """ Remove a track """
+        LOGGER.debug("TrackListEditor.delete_track")
+        self.track_listing.takeItem(self.track_listing.currentRow())
+
+    def move_up(self):
+        """ Move the currently-selected track up in the track listing """
+        LOGGER.debug("TrackListEditor.move_up")
+        row = self.track_listing.currentRow()
+        if row > 0:
+            dest = row - 1
+            item = self.track_listing.takeItem(row)
+            self.track_listing.insertItem(dest, item)
+            self.track_listing.setCurrentRow(dest)
+
+    def move_down(self):
+        """ Move the currently-selected track up in the track listing """
+        LOGGER.debug("TrackListEditor.move_down")
+        row = self.track_listing.currentRow()
+        if row < self.track_listing.count() - 1:
+            dest = row + 1
+            item = self.track_listing.takeItem(row)
+            self.track_listing.insertItem(dest, item)
+            self.track_listing.setCurrentRow(dest)
