99ba3b86d74df87938ff8a9fff51dfd727a2c8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:08:10 +0000

    Add package docker-remote-sync/0.2.2/docker_remote_sync-0.2.2.tar.gz

---
+++ packages/docker-remote-sync/docker_remote_sync-0.2.2.tar.gz/docker_remote_sync-0.2.2/drsync/sync.py
@@ -0,0 +1,50 @@
+import sys
+import tarfile
+from drsync.io_util import print_header
+from fabric import Connection
+
+from drsync.subprocess_utils import check_subprocess_errors, start_subprocess
+
+
+def sync_folders(
+    source_folder: str, remote: str, port: int | None, destination_folder: str
+):
+    print_header("Syncing local changes with remote")
+    rsync_executable = "rsync"
+    ssh_cmd_with_port = ()
+    if port is not None:
+        ssh_cmd_with_port = ("-e", f"'ssh -p {port}'")
+    try:
+        process = start_subprocess(
+            rsync_executable,
+            "--times",
+            "--delete",
+            "--recursive",
+            "--verbose",
+            *ssh_cmd_with_port,
+            f"{source_folder}/",
+            f"{remote}:{destination_folder}",
+        )
+        while line := process.stdout.readline():
+            print(line.rstrip())
+        check_subprocess_errors(process.stderr.readlines())
+    except FileNotFoundError as e:
+        if e.filename == rsync_executable:
+            print(f"Could not find {rsync_executable} executable", file=sys.stderr)
+            sys.exit(1)
+        else:
+            raise e
+
+
+def extract_tar_file(file, output_folder):
+    print_header("Extracting the layers of image")
+    with tarfile.open(fileobj=file, mode="r:") as tf:
+        tf.extractall(path=output_folder)
+
+
+def build_remote_tar(remote: str, port: int | None, folder: str):
+    print_header("Building remote tar file")
+    tar_file_loc = f"{folder}.tar"
+    conn = Connection(host=remote, port=port)
+    output = conn.run(f"tar cf {tar_file_loc} {folder}/*")
+    print(output)
