f141e34fa228bbdf8fe67a18ce8af80a400888d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:05:53 +0000

    Add package sudeep/0.1.0/sudeep-0.1.0.tar.gz

---
+++ packages/sudeep/sudeep-0.1.0.tar.gz/sudeep-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="sudeep",
+    version="0.1.0",
+    packages=["sudeep"],
+    url="https://github.com/amalrajan/sudeep",
+    license="MIT",
+    author="Amal Rajan",
+    author_email="amalrajan@outlook.com",
+    description="About Sudeep",
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
---
+++ packages/sudeep/sudeep-0.1.0.tar.gz/sudeep-0.1.0/sudeep/__init__.py
@@ -0,0 +1,9 @@
+# amal/__init__.py
+
+
+def about():
+    return "Hello! I'm Sudeep. I am very passionate."
+
+
+def is_gay():
+    return "Nope nada zilch. Definitely not gay. I am only into rahul."
