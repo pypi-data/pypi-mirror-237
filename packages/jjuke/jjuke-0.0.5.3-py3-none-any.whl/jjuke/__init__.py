d58e22173878fdc1316d2af1b3e0171e2539565c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:07:48 +0000

    Add package jjuke/0.0.5.3/jjuke-0.0.5.3-py3-none-any.whl

---
+++ packages/jjuke/jjuke-0.0.5.3-py3-none-any.whl/jjuke/__init__.py
@@ -0,0 +1,9 @@
+from . import datasets, metrics, models, net_utils, utils
+# from jjuke import *
+from .net_utils import logger, options
+from .models import trainer, ema_trainer, optimizer, scheduler
+
+__all__ = ["datasets", "metrics", "models", "net_utils", "utils",
+           "logger", "options", "trainer", "ema_trainer", "optimizer", "scheduler"]
+
+__version__ = "0.0.5.3"
