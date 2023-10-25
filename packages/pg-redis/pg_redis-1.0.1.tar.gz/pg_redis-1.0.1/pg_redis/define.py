83dd798c2c0af8daff63f481205f6902d6ee35f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:07:26 +0000

    Add package pg-redis/1.0.1/pg_redis-1.0.1.tar.gz

---
+++ packages/pg-redis/pg_redis-1.0.1.tar.gz/pg_redis-1.0.1/pg_redis/define.py
@@ -0,0 +1,21 @@
+KEY_REDIS = "redis"
+KEY_REDIS_HOST = "host"
+KEY_REDIS_PORT = "port"
+KEY_REDIS_DB = "db"
+KEY_REDIS_PASSWORD = "password"
+KEY_REDIS_POOL_SIZE = "pool_size"
+"""
+redis configuration format
+====
+{
+  "redis": {
+    "default": {
+      "host": "127.0.0.1",
+      "port": 6379,
+      "db": 0,
+      "password": "",
+      "pool_size": 1
+    }
+  }
+}
+"""
