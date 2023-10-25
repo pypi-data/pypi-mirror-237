25df93d8082dc51cf16fd9918820996c6bfc46ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 05:05:23 +0000

    Add package arcae/0.2.1/arcae-0.2.1-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/arcae/arcae-0.2.1-cp310-cp310-manylinux_2_28_x86_64.whl/arcae/__init__.py
@@ -0,0 +1,14 @@
+# Needed to load in the arrow c++ shared libraries
+import pyarrow as pa  # noqa
+from typing import TYPE_CHECKING
+
+__version__ = "0.2.1"
+
+if TYPE_CHECKING:
+    from arcae.lib.arrow_tables import Table
+
+def table(filename: str) -> "Table":
+    # Defer cython module import, to avoid conflicts between arcae casacore libraries
+    # and python-casacore casacore libraries
+    from arcae.lib.arrow_tables import Table
+    return Table.from_filename(filename)
