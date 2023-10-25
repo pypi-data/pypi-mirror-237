df4da0bbd914ce16b591c0183846a26924adbbc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 04:06:19 +0000

    Add package bandwidth-sdk/14.5.3/bandwidth-sdk-14.5.3.tar.gz

---
+++ packages/bandwidth-sdk/bandwidth-sdk-14.5.3.tar.gz/bandwidth-sdk-14.5.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+VERSION = os.environ['RELEASE_VERSION']
+
+if sys.version_info[0] < 3:
+    with open('README.md', 'r') as fh:
+        long_description = fh.read()
+else:
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = fh.read()
+
+with open("requirements.txt", "r") as fp:
+    requirements = fp.readlines()
+
+setup(
+    name='bandwidth-sdk',
+    version=VERSION,
+    description='Bandwidth\'s set of APIs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='APIMatic SDK Generator',
+    author_email='support@apimatic.io',
+    url='https://apimatic.io',
+    packages=find_packages(),
+    install_requires=requirements
+)
