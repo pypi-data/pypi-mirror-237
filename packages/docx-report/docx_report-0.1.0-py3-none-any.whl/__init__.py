59bb3a8111eac14e956bc2d5f62fc4c76eb2b714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 06:06:20 +0000

    Add package docx-report/0.1.0/docx_report-0.1.0-py3-none-any.whl

---
+++ packages/docx-report/docx_report-0.1.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+docx_report
+===========
+
+This module is a wrapper for the python-docx library to make it easier to create
+reports in Microsoft Word.
+
+Modules:
+    docx_report:
+        The main module for the docx_report package.
+"""
+
+from .docx_report import DocxReport
