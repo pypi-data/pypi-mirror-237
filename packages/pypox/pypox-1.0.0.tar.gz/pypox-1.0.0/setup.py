4cec8e6c192a6765900fe79874819ee4865c6645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:08:02 +0000

    Add package pypox/1.0.0/pypox-1.0.0.tar.gz

---
+++ packages/pypox/pypox-1.0.0.tar.gz/pypox-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='pypox',
+    version='1.0.0',
+    author='Karl Robeck Alferez',
+    author_email='karlalferezfx@gmail.com',
+    description='Python to javascript transpiler',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11.4',
+)
