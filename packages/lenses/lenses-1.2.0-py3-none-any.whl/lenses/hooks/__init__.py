e0ffe05910dcf9a216bb755a6c51d7c0d2ccc9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 05:05:21 +0000

    Add package lenses/1.2.0/lenses-1.2.0-py3-none-any.whl

---
+++ packages/lenses/lenses-1.2.0-py3-none-any.whl/lenses/hooks/__init__.py
@@ -0,0 +1,30 @@
+from .hook_funcs import (
+    __doc__,
+    contains_add,
+    contains_remove,
+    from_iter,
+    setattr,
+    setitem,
+    to_iter,
+)
+
+__all__ = [
+    "__doc__",
+    "contains_add",
+    "contains_remove",
+    "from_iter",
+    "setattr",
+    "setitem",
+    "to_iter",
+]
+
+supported_modules = ["pyrsistent"]
+
+for _hook in supported_modules:
+    try:
+        __import__(_hook)
+    except ImportError:
+        pass
+    else:
+        _subname = "{}.{}".format(__name__, _hook)
+        __import__(_subname)
