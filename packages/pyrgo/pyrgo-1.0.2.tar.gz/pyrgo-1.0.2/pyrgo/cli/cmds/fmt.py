d73dff7461206b16db79bcfee6b4cde7bdb81c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 06:08:19 +0000

    Add package pyrgo/1.0.2/pyrgo-1.0.2.tar.gz

---
+++ packages/pyrgo/pyrgo-1.0.2.tar.gz/pyrgo-1.0.2/pyrgo/cli/cmds/fmt.py
@@ -0,0 +1,26 @@
+"""Format command."""
+from __future__ import annotations
+
+import sys
+
+import click
+from result import Ok
+
+from pyrgo.cli.cmds._utils import inform_and_run_program
+from pyrgo.command_exec import PythonCommandExec
+from pyrgo.conf import PyrgoConf
+
+
+@click.command("fmt")
+def fmt() -> None:
+    """Format code with `ruff` and `black`."""
+    configuration = PyrgoConf.new()
+    ruff_command = PythonCommandExec.new(program="ruff").add_args(args=["--fix-only"])
+    fmt_command = PythonCommandExec.new(program="ruff").add_args(args=["format", "."])
+    for command in [ruff_command, fmt_command]:
+        command.add_args(args=configuration.relevant_paths)
+
+    program_execution = inform_and_run_program(commands=[ruff_command, fmt_command])
+    if not isinstance(program_execution, Ok):
+        sys.exit(1)
+    sys.exit(0)
