b7610d9cc3544f89edba7846d083b2c31e9c0a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:06:33 +0000

    Add package UniLoaders/0.0.2/UniLoaders-0.0.2-py3-none-any.whl

---
+++ packages/UniLoaders/UniLoaders-0.0.2-py3-none-any.whl/UniLoaders/main.py
@@ -0,0 +1,37 @@
+
+
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.document_loaders import Docx2txtLoader
+from langchain.document_loaders import UnstructuredPowerPointLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders.image import UnstructuredImageLoader
+
+
+def all_loaders(path_or_url: str):
+    filters = ["https", "http", "docx", "ppt", "pdf", "jpg", "png"]
+    if "https" or "http" in path_or_url:
+        url = [path_or_url]
+        loader = UnstructuredURLLoader(urls=url)
+        data = loader.load()
+        return data
+    
+    if "docx" in path_or_url:
+        loader = Docx2txtLoader(path_or_url)
+        data = loader.load()
+        return data
+
+    if "ppt" in path_or_url:
+        loader = UnstructuredPowerPointLoader(path_or_url)
+        data = loader.load()
+        return data
+    
+    if "pdf" in path_or_url:
+        loader = PyPDFLoader(path_or_url)
+        data = loader.load()
+        return data
+    
+    if "jpg" or "png" in path_or_url:
+        loader = UnstructuredImageLoader(path_or_url, mode="elements")
+        data = loader.load()
+        return data
+    return "TypeError"
