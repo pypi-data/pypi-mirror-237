9d0674a3213c692f3f28f516173d91e40b6d7ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:06:13 +0000

    Add package openbrain/0.22.0/openbrain-0.22.0-py3-none-any.whl

---
+++ packages/openbrain/openbrain-0.22.0-py3-none-any.whl/openbrain/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.22.0"
+
+# import os
+# from pathlib import Path
+#
+# cwd = Path(os.getcwd()) or '.'
+#
+# env_file_path = cwd / ".env"
+#
+# if not os.path.exists(".env") and os.environ.get("MODE") is None:
+#     os.environ["MODE"] = "LOCAL"
+#
+#     print(f"Did not find .env file or env vars, setting MODE=LOCAL in env var.")
