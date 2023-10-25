ae6eb000dc758bcdb9b814a6c8684dffc907b182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 05:06:02 +0000

    Add package nurnpy/1.0/nurnpy-1.0.tar.gz

---
+++ packages/nurnpy/nurnpy-1.0.tar.gz/nurnpy-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='nurnpy',
+      version='1.0',
+      description='none',
+      packages=['nurnpy'],
+      author_email='zuszusov676@gmail.com',
+      zip_safe=False)
