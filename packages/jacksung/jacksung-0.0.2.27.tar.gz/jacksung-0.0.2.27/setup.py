b2dec9d0f7c0d44be64bc6f78f526cae95dd4b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:05:59 +0000

    Add package jacksung/0.0.2.27/jacksung-0.0.2.27.tar.gz

---
+++ packages/jacksung/jacksung-0.0.2.27.tar.gz/jacksung-0.0.2.27/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jacksung',
+    version='0.0.2.27',
+    author='Zijiang Song',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm',
+        'pymysql',
+        'pytz',
+        'selenium',
+        'termcolor'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ecnu_login = jacksung.utils.login:main',
+            'watch_gpu = jacksung.utils.nvidia:main'
+        ]
+    }
+)
