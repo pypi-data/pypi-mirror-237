43b44de181b9fbe60837c7e60af9d4c2145b6ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 05:06:04 +0000

    Add package focus-spec-validator/0.5.2.dev1/focus_spec_validator-0.5.2.dev1-py3-none-any.whl

---
+++ packages/focus-spec-validator/focus_spec_validator-0.5.2.dev1-py3-none-any.whl/focus_validator/outputter/outputter_console.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from tabulate import tabulate
+
+from focus_validator.config_objects import Rule
+from focus_validator.rules.spec_rules import ValidationResult
+
+
+class ConsoleOutputter:
+    def __init__(self, output_destination):
+        self.output_destination = output_destination
+        self.result_set = None
+
+    @staticmethod
+    def __restructure_check_list__(result_set: ValidationResult):
+        rows = []
+        for value in result_set.checklist.values():
+            if isinstance(value.rule_ref, Rule):
+                check_type = value.rule_ref.check_type_friendly_name
+            else:
+                check_type = "ERRORED"
+
+            row_obj = value.model_dump()
+            row_obj.update(
+                {
+                    "check_type": check_type,
+                    "status": row_obj["status"].value.title(),
+                }
+            )
+            rows.append(row_obj)
+        df = pd.DataFrame(rows)
+        df.rename(
+            columns={
+                "check_name": "Check Name",
+                "check_type": "Check Type",
+                "column_id": "Column",
+                "friendly_name": "Friendly Name",
+                "error": "Error",
+                "status": "Status",
+            },
+            inplace=True,
+        )
+        df = df.reindex(
+            columns=[
+                "Check Name",
+                "Check Type",
+                "Column",
+                "Friendly Name",
+                "Error",
+                "Status",
+            ]
+        )
+        return df
+
+    def write(self, result_set: ValidationResult):
+        self.result_set = result_set
+
+        checklist = self.__restructure_check_list__(result_set)
+        print("Checklist:")
+        print(tabulate(checklist, headers="keys", tablefmt="psql"))
+
+        if result_set.failure_cases is not None:
+            print("Checks summary:")
+            print(
+                tabulate(
+                    tabular_data=result_set.failure_cases,  # type: ignore
+                    headers="keys",
+                    tablefmt="psql",
+                )
+            )
---
+++ packages/focus-spec-validator/focus_spec_validator-0.5.2.dev1-py3-none-any.whl/focus_validator/rules/checks.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from typing import Union
+
+import pandas as pd
+from pandera import extensions
+
+from focus_validator.utils.download_currency_codes import get_currency_codes
+
+
+def is_camel_case(column_name):
+    return (
+        column_name != column_name.lower()
+        and column_name != column_name.upper()
+        and "_" not in column_name
+    )
+
+
+@extensions.register_check_method()
+def check_not_null(pandas_obj: pd.Series, allow_nulls: bool):
+    # TODO: works for string type, need to verify for other data types
+    check_values = pandas_obj.isnull() | (pandas_obj == "")
+    if not allow_nulls:
+        check_values = check_values | (pandas_obj == "NULL")
+    return ~check_values
+
+
+@extensions.register_check_method()
+def check_unique(pandas_obj: pd.Series):
+    return ~pandas_obj.duplicated()
+
+
+@extensions.register_check_method()
+def check_value_in(pandas_obj: pd.Series, allowed_values):
+    return pandas_obj.isin(allowed_values)
+
+
+@extensions.register_check_method()
+def check_datetime_dtype(pandas_obj: pd.Series):
+    def __validate_date_obj__(value: Union[str, datetime]):
+        if isinstance(value, str):
+            # fix of python 3.10 and lower, strings ending with Z are not parsed automatically
+            if value.endswith("Z"):
+                value = value.replace("Z", "+00:00")
+
+            try:
+                value = datetime.fromisoformat(value)
+            except ValueError:
+                # failed to parse iso string
+                return False
+
+        if not isinstance(value, datetime):
+            return False
+
+        # match timezone to ensure datetime is in UTC
+        return value.tzname() == "UTC"
+
+    return pd.Series(map(__validate_date_obj__, pandas_obj.values))
+
+
+@extensions.register_check_method()
+def check_currency_code_dtype(pandas_obj: pd.Series):
+    currency_codes = set(get_currency_codes())
+    return pd.Series(
+        map(lambda v: isinstance(v, str) and v in currency_codes, pandas_obj.values)
+    )
