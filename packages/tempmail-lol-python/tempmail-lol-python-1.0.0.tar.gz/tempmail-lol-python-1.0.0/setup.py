c6d883a81bc9965ad9c355c90f88a5b4f9b65774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 06:06:37 +0000

    Add package tempmail-lol-python/1.0.0/tempmail-lol-python-1.0.0.tar.gz

---
+++ packages/tempmail-lol-python/tempmail-lol-python-1.0.0.tar.gz/tempmail-lol-python-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tempmail-lol-python',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+)
