aad208b0e5379b1d58ac1d4804fadb92896fff07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:05:34 +0000

    Add package bandcrash/0.5.1/bandcrash-0.5.1-py3-none-any.whl

---
+++ packages/bandcrash/bandcrash-0.5.1-py3-none-any.whl/bandcrash/gui/datatypes.py
@@ -0,0 +1,40 @@
+""" Data type conversion functions """
+
+import typing
+
+from PySide6.QtCore import Qt
+
+TrackData = dict[str, typing.Any]
+TrackList = list[TrackData]
+
+
+def to_checkstate(val):
+    """ Convert a bool to a qt CheckState """
+    return Qt.CheckState.Checked if val else Qt.CheckState.Unchecked
+
+
+def apply_text_fields(data, fields, xform=lambda x: x):
+    """ Apply textbox controls to backing storage
+
+    :param dict data: Target dictionary
+    :param list fields: List of (dict_key, widget)
+    """
+    for key, widget in fields:
+        if value := widget.text():
+            data[key] = xform(value)
+        elif key in data:
+            del data[key]
+
+
+def apply_checkbox_fields(data, fields):
+    """ Apply checkbox controls to backing storage
+
+    :param dict data: Target dictionary
+    :param list fields: List of (dict_key, widget, default)
+    """
+    for key, widget, dfl in fields:
+        value = widget.checkState() == Qt.CheckState.Checked
+        if value != dfl:
+            data[key] = value
+        elif key in data:
+            del data[key]
