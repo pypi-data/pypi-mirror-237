615379319dea2236e1fd964029392282136df263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 06:06:40 +0000

    Add package chain_harvester/0.15.2/chain_harvester-0.15.2.tar.gz

---
+++ packages/chain_harvester/chain_harvester-0.15.2.tar.gz/chain_harvester-0.15.2/chain_harvester/networks/ethereum/mainnet.py
@@ -0,0 +1,46 @@
+import json
+import logging
+
+import requests
+
+from chain_harvester.chain import Chain, ChainException
+from chain_harvester.constants import CHAINS
+
+log = logging.getLogger(__name__)
+
+
+class EthereumMainnetChain(Chain):
+    def __init__(
+        self, rpc=None, rpc_nodes=None, api_key=None, api_keys=None, abis_path=None, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.chain = "ethereum"
+        self.network = "mainnet"
+        self.rpc = rpc or rpc_nodes[self.chain][self.network]
+        self.chain_id = CHAINS[self.chain][self.network]
+        self.abis_path = abis_path or "abis/"
+        self.api_key = api_key or api_keys[self.chain][self.network]
+
+    def get_abi_from_source(self, contract_address):
+        try:
+            response = requests.get(
+                "https://api.etherscan.io/api?module=contract&action=getabi&address="
+                + contract_address
+                + "&apikey="
+                + self.api_key,
+                timeout=5,
+            )
+        except requests.exceptions.Timeout:
+            log.exception(
+                "Timeout when get abi from etherscan", extra={"contract_address": contract_address}
+            )
+            raise
+
+        response.raise_for_status()
+        data = response.json()
+
+        if data["status"] != "1":
+            raise ChainException("Request to etherscan failed: {}".format(data["result"]))
+
+        abi = json.loads(data["result"])
+        return abi
