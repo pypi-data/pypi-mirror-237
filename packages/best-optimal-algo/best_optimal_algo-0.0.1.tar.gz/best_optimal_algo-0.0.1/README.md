36f5f04d2556a952345b99175389abc90325d8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:06:58 +0000

    Add package best-optimal-algo/0.0.1/best_optimal_algo-0.0.1.tar.gz

---
+++ packages/best-optimal-algo/best_optimal_algo-0.0.1.tar.gz/best_optimal_algo-0.0.1/README.md
@@ -0,0 +1,22 @@
+# Best Optimal Algorithm
+
+--> Install package in your machine
+
+'''
+ pip install best_optimal_algo
+'''
+
+--> Create object and pass the parameters
+
+'''
+algo = BestOptimalAlgo(X_train=X_tr,X_test=X_t, y_test=y_t, y_train=y_tr)
+'''
+
+--> Call the method "best_optimal_algo"
+
+'''
+algo.best_optimal_algo()
+
+'''
+
+--> Wait for results
---
+++ packages/best-optimal-algo/best_optimal_algo-0.0.1.tar.gz/best_optimal_algo-0.0.1/src/best_optimal_algo/__init__.py
@@ -0,0 +1,24 @@
+from src.utils import evaluate_models
+
+class BestOptimalAlgo:
+
+    def __init__(self,  X_train , X_test, y_train, y_test):
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+
+    
+    def best_optimal_algo(self):
+        
+        model, params = evaluate_models(X_train=self.X_train,y_train=self.y_train,X_test=self.X_test,y_test=self.y_test)
+        
+        # best_model_score = max(sorted(model_report.values()))
+
+        ## To get best model name from dict
+        # best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+        
+        print(model)
+        for p in params['Parameters']:
+            #print(list(zip(p.keys(),p.values())))
+            print(p)
