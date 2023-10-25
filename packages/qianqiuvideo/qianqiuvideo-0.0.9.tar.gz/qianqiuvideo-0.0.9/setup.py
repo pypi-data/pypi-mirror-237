4d637bec4fba581b6613eb9fce00e7d89299287d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:06:32 +0000

    Add package qianqiuvideo/0.0.9/qianqiuvideo-0.0.9.tar.gz

---
+++ packages/qianqiuvideo/qianqiuvideo-0.0.9.tar.gz/qianqiuvideo-0.0.9/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.9'
+DESCRIPTION = 'Easily cut the video by moviepy'
+
+setup(
+    name="qianqiuvideo",
+    version=VERSION,
+    author="chunlei li",
+    author_email="li_cl@foxmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    license="MIT",
+    url="https://github.com/chunleili/cut_video",
+    scripts=['qianqiuvideo/test.py', 'qianqiuvideo/mysql.py'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
