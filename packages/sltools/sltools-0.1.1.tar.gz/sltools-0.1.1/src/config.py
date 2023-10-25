269380e0932d06f1a6c1d153f10e5ce567cb18d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 06:07:55 +0000

    Add package sltools/0.1.1/sltools-0.1.1.tar.gz

---
+++ packages/sltools/sltools-0.1.1.tar.gz/sltools-0.1.1/src/config.py
@@ -0,0 +1,29 @@
+PRIMARY_ENCODING = 'windows-1251'
+
+# Constants for command names
+VALIDATE_ENCODING = 'validate-encoding'
+FIX_ENCODING = 'fix-encoding'
+VALIDATE_XML = 'validate-xml'
+FORMAT_XML = 'format-xml'
+CHECK_PRIMARY_LANG = 'check-primary-lang'
+TRANSLATE = 'translate'
+ANALYZE_PATTERNS = 'analyze-patterns'
+FIX_KNOWN_BROKEN_PATTERNS = 'fix-known-broken-patterns'
+
+CMD_TO_ALIASES = {
+    VALIDATE_ENCODING: ['ve'],
+    FIX_ENCODING: ['fe'],
+    VALIDATE_XML: ['vx'],
+    FORMAT_XML: ['fx'],
+    CHECK_PRIMARY_LANG: ['cpl'],
+    TRANSLATE: ['tr'],
+    ANALYZE_PATTERNS: ['ap'],
+    FIX_KNOWN_BROKEN_PATTERNS: ['fbp'],
+}
+
+min_recognizable_text_length = 30
+
+UNKNOWN_LANG = "Unknown"
+TOO_LITTLE_DATA = "Too little data"
+
+
