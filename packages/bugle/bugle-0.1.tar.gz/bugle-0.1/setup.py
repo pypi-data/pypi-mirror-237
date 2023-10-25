06477a283968c861bb403e5880a0b8dd1de9dbfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 06:07:59 +0000

    Add package bugle/0.1/bugle-0.1.tar.gz

---
+++ packages/bugle/bugle-0.1.tar.gz/bugle-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bugle",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="htm",
+    author_email="hari2608@gmail.com",
+    description="A simple investing package",
+    long_description="A simple investing package with basic operations.",
+    long_description_content_type="text/markdown"
+)
