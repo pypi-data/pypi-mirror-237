209282240ff87a63acdc25273c6e0fe610089111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:06:58 +0000

    Add package hyperon-das/0.1.6/hyperon_das-0.1.6-py3-none-any.whl

---
+++ packages/hyperon-das/hyperon_das-0.1.6-py3-none-any.whl/hyperon_das/factory.py
@@ -0,0 +1,47 @@
+from abc import abstractmethod
+from enum import Enum
+from typing import Protocol
+
+from hyperon_das_atomdb import IAtomDB
+from hyperon_das_atomdb.adapters import InMemoryDB, RedisMongoDB
+
+
+class DatabaseType(Enum):
+    REDIS_MONGO = 'redis_mongo'
+    RAM_ONLY = 'ram_only'
+
+    @classmethod
+    def values(cls):
+        return [choices.value for choices in cls]
+
+
+class IDatabaseFactory(Protocol):
+    @abstractmethod
+    def create_redis_mongo_database(self):
+        ...  # pragma no cover
+
+    @abstractmethod
+    def create_ram_only_database(self):
+        ...  # pragma no cover
+
+
+class DatabaseFactory:
+    def __init__(self, database_name: DatabaseType) -> None:
+        self.name = database_name
+
+    def create_redis_mongo_database(self):
+        return RedisMongoDB
+
+    def create_ram_only_database(self):
+        return InMemoryDB
+
+
+def database_factory(factory: IDatabaseFactory) -> IAtomDB:
+    redis_mongo_database = factory.create_redis_mongo_database()
+    ram_only_database = factory.create_ram_only_database()
+
+    if factory.name == DatabaseType.REDIS_MONGO.value:
+        return redis_mongo_database()
+
+    if factory.name == DatabaseType.RAM_ONLY.value:
+        return ram_only_database()
