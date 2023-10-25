9edfb043d581dbf107d0d23e79372f992da8b286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:08:54 +0000

    Add package opentelemetry-instrumentation-anthropic/0.0.8/opentelemetry_instrumentation_anthropic-0.0.8.tar.gz

---
+++ packages/opentelemetry-instrumentation-anthropic/opentelemetry_instrumentation_anthropic-0.0.8.tar.gz/opentelemetry_instrumentation_anthropic-0.0.8/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['anthropic']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['opentelemetry-api>=1.19.0,<2.0.0',
+ 'opentelemetry-instrumentation>=0.40b0,<0.41',
+ 'opentelemetry-semantic-conventions-ai>=0.0.6,<0.0.7']
+
+setup_kwargs = {
+    'name': 'opentelemetry-instrumentation-anthropic',
+    'version': '0.0.8',
+    'description': 'OpenTelemetry Anthropic instrumentation',
+    'long_description': '# opentelemetry-instrumentation-anthropic\n\nProject description here.\n',
+    'author': 'Gal Kleinman',
+    'author_email': 'gal@traceloop.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.1,<4',
+}
+
+
+setup(**setup_kwargs)
