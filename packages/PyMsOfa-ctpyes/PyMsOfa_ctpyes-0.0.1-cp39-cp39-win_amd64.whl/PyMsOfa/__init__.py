9df10d07152806152ca46b1d8d7b03c38f4519b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:05:48 +0000

    Add package PyMsOfa-ctpyes/0.0.1/PyMsOfa_ctpyes-0.0.1-cp39-cp39-win_amd64.whl

---
+++ packages/PyMsOfa-ctpyes/PyMsOfa_ctpyes-0.0.1-cp39-cp39-win_amd64.whl/PyMsOfa/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from PyMsOfa.PyMsOfa import *
+from PyMsOfa.PyMsOfa_astrometry import *
+from PyMsOfa.PyMsOfa_basic import *
+from PyMsOfa.PyMsOfa_earth_attitude import *
+from PyMsOfa.PyMsOfa_time import *
