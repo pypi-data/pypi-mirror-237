6b2463b5caf47f4553b2d7249ecb279ac291c037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 05:06:14 +0000

    Add package osism/0.20231024.0/osism-0.20231024.0-py3-none-any.whl

---
+++ packages/osism/osism-0.20231024.0-py3-none-any.whl/osism/tasks/ansible.py
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: Apache-2.0
+
+from celery import Celery
+
+from osism import settings
+from osism.tasks import Config, run_ansible_in_environment
+
+app = Celery("ansible")
+app.config_from_object(Config)
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        settings.GATHER_FACTS_SCHEDULE, gather_facts.s(), expires=10
+    )
+
+
+@app.task(bind=True, name="osism.tasks.ansible.gather_facts")
+def gather_facts(self, publish=True):
+    return run_ansible_in_environment(
+        self.request.id, "osism-ansible", "generic", "facts", [], publish
+    )
+
+
+@app.task(bind=True, name="osism.tasks.ansible.run")
+def run(self, environment, playbook, arguments, publish=True, locking=True):
+    return run_ansible_in_environment(
+        self.request.id,
+        "osism-ansible",
+        environment,
+        playbook,
+        arguments,
+        publish,
+        locking,
+    )
