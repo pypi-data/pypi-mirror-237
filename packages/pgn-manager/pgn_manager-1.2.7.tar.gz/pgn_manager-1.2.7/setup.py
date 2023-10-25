9e3af606a3be99bb36b2c7543211cbe295f84c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:09:48 +0000

    Add package pgn-manager/1.2.7/pgn_manager-1.2.7.tar.gz

---
+++ packages/pgn-manager/pgn_manager-1.2.7.tar.gz/pgn_manager-1.2.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="pgn_manager",
+    version="1.2.7",
+    description="library for managing pgn files",
+    author="Luca Canali",
+    author_email="lucacanali.dev@gmail.com",
+    packages= ["pgn_manager"],
+    install_requires=[
+        "python-chess",
+    ],
+)
