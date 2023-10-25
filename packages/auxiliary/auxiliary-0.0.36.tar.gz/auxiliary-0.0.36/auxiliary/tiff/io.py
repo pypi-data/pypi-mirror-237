6a899811e4abf06031c565c0dc94d14ac6c618e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 06:07:15 +0000

    Add package auxiliary/0.0.36/auxiliary-0.0.36.tar.gz

---
+++ packages/auxiliary/auxiliary-0.0.36.tar.gz/auxiliary-0.0.36/auxiliary/tiff/io.py
@@ -0,0 +1,25 @@
+from tifffile import imread, imwrite
+import os
+
+from auxiliary.turbopath import turbopath
+
+
+def read_tiff(tiff_path: str):
+    data = imread(tiff_path)
+    return data
+
+
+def write_tiff(
+    numpy_array,
+    output_tiff_path: str,
+    create_parent_directory: bool = False,
+    transpose: bool = False,
+):
+    if transpose == True:
+        numpy_array = numpy_array.T
+
+    if create_parent_directory == True:
+        output_tiff_path = turbopath(output_tiff_path)
+        parent_dir = output_tiff_path.parent
+        os.makedirs(parent_dir, exist_ok=True)
+    imwrite(output_tiff_path, numpy_array)
