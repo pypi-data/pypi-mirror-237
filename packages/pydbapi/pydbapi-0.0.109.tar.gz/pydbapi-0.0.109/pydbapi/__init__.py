a6988f7846378c84572927bd3e79a027ad4cdbfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 15:06:15 +0000

    Add package pydbapi/0.0.109/pydbapi-0.0.109.tar.gz

---
+++ packages/pydbapi/pydbapi-0.0.109.tar.gz/pydbapi-0.0.109/pydbapi/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author: chunyang.xu
+# @Date:   2023-06-02 15:27:41
+# @Last Modified by:   longfengpili
+# @Last Modified time: 2023-08-11 11:13:58
+# @github: https://github.com/longfengpili
+
+
+import os
+import logging.config
+from pydbapi.conf import LOGGING_CONFIG
+logging.config.dictConfig(LOGGING_CONFIG)
+
+os.environ['NUMEXPR_MAX_THREADS'] = '16'
+
+# from pydbapi.api import SqliteDB, RedshiftDB, MysqlDB, SnowflakeDB
+# from pydbapi.sql import SqlParse, SqlCompile, SqlFileParse, ColumnModel, ColumnsModel
+
+# __all__ = ['SqliteDB', 'RedshiftDB', 'MysqlDB', 'SnowflakeDB',
+#            'SqlParse', 'SqlCompile', 'SqlFileParse', 'ColumnModel', 'ColumnsModel']
