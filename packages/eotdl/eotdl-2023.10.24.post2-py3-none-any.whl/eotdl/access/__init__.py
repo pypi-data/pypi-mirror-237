ac20b01da03b2cc59daa42b8aa2ddc4c25039d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:08:50 +0000

    Add package eotdl/2023.10.24.post2/eotdl-2023.10.24.post2-py3-none-any.whl

---
+++ packages/eotdl/eotdl-2023.10.24.post2-py3-none-any.whl/eotdl/access/__init__.py
@@ -0,0 +1,8 @@
+from .sentinelhub import (SHClient, 
+                          SHParametersFeature, 
+                          EvalScripts, 
+                          sentinel_1_download_parameters, 
+                          sentinel_2_l2a_download_parameters,
+                          dem_download_parameters)
+from .airbus import (AirbusClient,
+                     get_airbus_access_token)
