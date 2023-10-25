8c5337c4fc0ca52dc74521c8e34a5c6dc6d12857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 15:05:52 +0000

    Add package xConfigparser/1.0.0/xConfigparser-1.0.0.tar.gz

---
+++ packages/xConfigparser/xConfigparser-1.0.0.tar.gz/xConfigparser-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='xConfigparser',
+    version='1.0.0',
+    description='A simple library for getting the value from config.ini',
+    author='ks lim',
+    author_email='kianseng.lim@sony.com',
+    packages=['python'],
+)
