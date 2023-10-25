a18a34d810f3caf67556ad31a49a96398d758bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:06:04 +0000

    Add package bluemoss/0.1.2/bluemoss-0.1.2-py3-none-any.whl

---
+++ packages/bluemoss/bluemoss-0.1.2-py3-none-any.whl/bluemoss/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'bluemoss'
+__author__ = 'LucaTabone'
+__license__ = 'APACHE'
+__copyright__ = 'Copyright 2023-2023 LucaTabone'
+__version__ = '0.1.2'
+
+from .classes import (
+    BlueMoss, Node, Root, Range, Ex, InvalidTargetTypeException, InvalidKeysForTargetException,
+    InvalidXpathException, PartialKeysException, EqualIndicesException, Jsonify, JsonifyWithTag
+)
+from .extract import extract
