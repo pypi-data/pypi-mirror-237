dba301386b756ac02165d76f78e5b815635ffa42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:05:47 +0000

    Add package Strawberry-PI/0.0.64/Strawberry-PI-0.0.64.tar.gz

---
+++ packages/Strawberry-PI/Strawberry-PI-0.0.64.tar.gz/Strawberry-PI-0.0.64/src/LoRa/boards/board_rpi.py
@@ -0,0 +1,53 @@
+import RPi.GPIO as GPIO  # type:ignore
+import spidev  # type:ignore
+
+from .board import Board as IBoard
+
+
+class Board(IBoard):
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)  # type: ignore -> Così posso utilizzare i numeri
+
+        super().__init__(
+            8,
+            10,
+            9,
+            11,
+            22,
+            4,
+            17,
+            18,
+            27,
+            None,
+            None,
+        )
+
+    # -------------------------------------------------------------------------------------------- #
+    def pin(self, id: int | None, in_out: object = GPIO.OUT) -> None:
+        if id is not None:
+            GPIO.setup(id, in_out, pull_up_down=GPIO.PUD_DOWN)
+
+    def spi(self) -> object:
+        spi = spidev.SpiDev()
+        spi.open(0, 0)  # type: ignore
+        spi.max_speed_hz = 5000000
+
+        return spi  # type: ignore
+
+    def add_event(self, pin: int | None, callback: object) -> None:
+        if pin is not None:
+            self.pin(pin, GPIO.IN)
+            GPIO.add_event_detect(pin, GPIO.RISING, callback=callback)
+
+    # -------------------------------------------------------------------------------------------- #
+    def _write(self, reg: int, value: list[int]) -> None:
+        self._spi.xfer([reg | 0x80] + value)
+
+    def read(self, reg: int) -> int:
+        return self._spi.xfer([reg] + [0])[1]
+
+    def reads(self, reg: int, length: int) -> list[int]:
+        return self._spi.xfer([reg] + [0] * length)[1:]
+
+
+Any = object()
