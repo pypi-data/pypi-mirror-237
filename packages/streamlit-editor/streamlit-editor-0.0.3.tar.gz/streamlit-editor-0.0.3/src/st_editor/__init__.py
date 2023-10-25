e17c7bf14cebe859d22530a16386ad6a57f3f206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:06:48 +0000

    Add package streamlit-editor/0.0.3/streamlit-editor-0.0.3.tar.gz

---
+++ packages/streamlit-editor/streamlit-editor-0.0.3.tar.gz/streamlit-editor-0.0.3/src/st_editor/__init__.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from typing import Optional
+
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Tell streamlit that there is a component called st_editor,
+# and that the code to display that component is in the "frontend" folder
+frontend_dir = (Path(__file__).parent / "frontend").absolute()
+_component_func = components.declare_component(
+	"st_editor", path=str(frontend_dir)
+)
+
+# Create the python function that will be called
+def st_editor(
+    key: Optional[str] = None,
+    innerHtml = '',
+    height = 260
+):
+    """
+    Add a descriptive docstring
+    """
+    component_value = _component_func(
+        key=key,
+        innerHtml = innerHtml,
+        height = height
+    )
+
+    return component_value
+
+
+def main():
+    value = st_editor(innerHtml="",height=160)
+
+    st.write(value)
+
+
+if __name__ == "__main__":
+    main()
