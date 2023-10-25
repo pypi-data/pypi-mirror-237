0d5b0227bd6dfdae66b96037ddb842dbbd90e061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 15:06:12 +0000

    Add package PyMsOfa-python/0.0.1/PyMsOfa-python-0.0.1.tar.gz

---
+++ packages/PyMsOfa-python/PyMsOfa-python-0.0.1.tar.gz/PyMsOfa-python-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+from setuptools import Extension
+
+
+long_desc ='''
+*PyMsOfa* is a `Python <http://www.python.org/>`_ module for accessing `International Astronomical Union <http://www.iau.org/>`_'s `SOFA library <http://www.iausofa.org/>`_ from Python. SOFA (Standards of Fundamental Astronomy) is a set of algorithms and procedures that implement standard models used in fundamental astronomy.
+
+*PyMsOfa* is not a part of SOFA routines but a Python package for the SOFA C library. 
+
+*PyMsOfa* is neither distributed, supported nor endorsed by the International Astronomical Union. In addition to *PyMsOfa*’s license, any use of this module should comply with `SOFA’s license and terms of use <http://www.iausofa.org/tandc.html>`_. Especially, but not exclusively, any published work or commercial products including results achieved by using *PyMsOfa* shall acknowledge that the SOFA software was used to obtain those results.
+'''
+
+setup(
+    name='PyMsOfa-python',
+    version='0.0.1',
+    author='Tan,dongjie',
+    author_email="djtan@pmo.ac.cn",
+    description='a Python package for the Standards of Fundamental Astronomy (SOFA) service',
+    long_description=long_desc,
+    packages=['PyMsOfa'],
+    license='MIT',   
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.0",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Scientific/Engineering :: Astronomy"
+    ]
+)
