e934d4fc3f6eb89e2ba542ea2230c85f9d175e2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:06:39 +0000

    Add package jy_lib/0.1.8/jy_lib-0.1.8.tar.gz

---
+++ packages/jy_lib/jy_lib-0.1.8.tar.gz/jy_lib-0.1.8/jy_lib/__init__.py
@@ -0,0 +1,30 @@
+"""
+Python SDK for jyfund
+"""
+# 客户端类
+from .auth_client import AuthClient
+from .smb_client import SmbClient
+
+# 枚举类
+from .bank import BankSC
+from .country import CountryA2, CountryA3
+from .currency import CurrencyAlphabeticCode
+from .exchange import Exchange
+from .symbol_em import EMParser
+from .symbol import (
+    SymbolSubType,
+    StockType,
+    FundType,
+    BondType,
+    OptionsType,
+    DrType,
+    IndexType,
+    FuturesType,
+    WarrantsType,
+    BlockType,
+    SpotType,
+    SymbolType,
+    SymbolFlag,
+)
+
+__version__ = "0.1.8"
