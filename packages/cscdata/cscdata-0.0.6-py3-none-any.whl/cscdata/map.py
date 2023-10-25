128681c17c560c6083a2e67f27afd18e3a8c5bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 15:06:11 +0000

    Add package cscdata/0.0.6/cscdata-0.0.6-py3-none-any.whl

---
+++ packages/cscdata/cscdata-0.0.6-py3-none-any.whl/cscdata/map.py
@@ -0,0 +1,16 @@
+df2ck = {
+    "int8": "Int8",
+    "int16": "Int16",
+    "int32": "Int32",
+    "int64": "Int64",
+    "float32": "Float32",
+    "float64": "Float64",
+    "uint8": "UInt8",
+    "uint16": "UInt16",
+    "uint32": "UInt32",
+    "uint64": "UInt64",
+    "bool": "UInt8",
+    "object": "String",
+    "datetime64[ns]": "DateTime",
+    "timedelta[ns]": "Int64"
+}
