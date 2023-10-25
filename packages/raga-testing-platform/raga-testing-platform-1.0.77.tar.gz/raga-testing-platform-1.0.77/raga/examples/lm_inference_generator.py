a799d5c37ea96fe6f4759fd40541a32850399845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:07:56 +0000

    Add package raga-testing-platform/1.0.77/raga-testing-platform-1.0.77.tar.gz

---
+++ packages/raga-testing-platform/raga-testing-platform-1.0.77.tar.gz/raga-testing-platform-1.0.77/raga/examples/lm_inference_generator.py
@@ -0,0 +1,25 @@
+import io
+import pathlib
+import random
+from raga import *
+import pandas as pd
+import datetime
+
+
+run_name = f"lm-inference-test-{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
+
+
+# create test_session object of TestSession instance
+test_session = TestSession(project_name="testingProject", run_name= run_name, access_key="LGXJjQFD899MtVSrNHGH", secret_key="TC466Qu9PhpOjTuLu5aGkXyGbM7SSBeAzYH6HpcP", host="http://3.111.106.226:8080")
+
+
+filters = Filter()
+filters.add(TimestampFilter(gte="2022-03-15T00:00:00Z", lte="2030-03-15T00:00:00Z"))
+
+
+lightmetrics_inference_generator(test_session=test_session, 
+                   dataset_name="test-lm-loader-20-oct-v1", 
+                   filter=filters, 
+                   model_name="Complex-America-Stop", 
+                   event_inference_col_name="Complex-America-Stop-Event", 
+                   model_inference_col_name="Complex-America-Stop-Model")
