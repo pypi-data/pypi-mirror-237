272b6c4eee02921d001d4f2358731abebdc78f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:05:50 +0000

    Add package nikoperations/0.1/nikoperations-0.1.tar.gz

---
+++ packages/nikoperations/nikoperations-0.1.tar.gz/nikoperations-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='nikoperations',
+    version='0.1',
+    description='Basic math operations module',
+    author='Nikhil Patil',
+    author_email='nikhilpatil18012004@gmail.com',
+    url='https://github.com/Nikhil18Patil/nikoperations.git',
+    packages=['nikoperations'],
+)
