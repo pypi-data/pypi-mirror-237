56cd832e0d4a0a27c930e65f45211b1ceddb6fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 14:06:57 +0000

    Add package spetlr/3.0.35/spetlr-3.0.35.tar.gz

---
+++ packages/spetlr/spetlr-3.0.35.tar.gz/spetlr-3.0.35/src/spetlr/etl/extractors/__init__.py
@@ -0,0 +1,9 @@
+from .incremental_extractor import IncrementalExtractor
+from .simple_extractor import SimpleExtractor
+from .stream_extractor import StreamExtractor
+
+__all__ = [
+    "IncrementalExtractor",
+    "SimpleExtractor",
+    "StreamExtractor",
+]
