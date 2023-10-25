e8ceb69b23b7266b9ac532bf0687c02189d802c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 05:05:06 +0000

    Add package django-seeding/1.0.3/django-seeding-1.0.3.tar.gz

---
+++ packages/django-seeding/django-seeding-1.0.3.tar.gz/django-seeding-1.0.3/README.md
@@ -0,0 +1,3 @@
+This is the documentation page of this package
+
+it is comming soon
---
+++ packages/django-seeding/django-seeding-1.0.3.tar.gz/django-seeding-1.0.3/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from pathlib import Path
+
+
+setuptools.setup(
+    name='django-seeding',
+    version='1.0.3',
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(exclude=['tests']),
+    install_requires=('django', 'djangorestframework', 'pandas')
+)
