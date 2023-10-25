61382709ed5797224502ce8ac6a0912c7afc21b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:08:59 +0000

    Add package opentelemetry-instrumentation-transformers/0.0.2/opentelemetry_instrumentation_transformers-0.0.2.tar.gz

---
+++ packages/opentelemetry-instrumentation-transformers/opentelemetry_instrumentation_transformers-0.0.2.tar.gz/opentelemetry_instrumentation_transformers-0.0.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['transformers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['opentelemetry-api>=1.19.0,<2.0.0',
+ 'opentelemetry-instrumentation>=0.40b0,<0.41',
+ 'opentelemetry-semantic-conventions-ai>=0.0.6,<0.0.7']
+
+setup_kwargs = {
+    'name': 'opentelemetry-instrumentation-transformers',
+    'version': '0.0.2',
+    'description': 'OpenTelemetry transformers instrumentation',
+    'long_description': '# opentelemetry-instrumentation-transformers\n\nProject description here.\n',
+    'author': 'Gal Kleinman',
+    'author_email': 'gal@traceloop.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.1,<4',
+}
+
+
+setup(**setup_kwargs)
