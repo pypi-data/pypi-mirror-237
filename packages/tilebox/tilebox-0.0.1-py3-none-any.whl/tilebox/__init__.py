97d7a15c3c371ba56f3b768b412e0ba3713cb95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:05:49 +0000

    Add package tilebox/0.0.1/tilebox-0.0.1-py3-none-any.whl

---
+++ packages/tilebox/tilebox-0.0.1-py3-none-any.whl/tilebox/__init__.py
@@ -0,0 +1,7 @@
+raise ImportError("""
+Ups - It seems like you installed the wrong version of the tilebox python package
+
+Currently our package is not available on the official pypi repository, instead
+we provide private pypi instances. Head over to docs.tilebox.com to find out
+how to install and use the tilebox python package.
+""".strip())
