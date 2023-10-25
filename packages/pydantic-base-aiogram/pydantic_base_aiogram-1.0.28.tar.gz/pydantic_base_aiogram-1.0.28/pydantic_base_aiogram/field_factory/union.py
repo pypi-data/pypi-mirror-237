f60a2c3bac1f48ab77a9f16ece1126ae9e6c42ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 04:05:55 +0000

    Add package pydantic-base-aiogram/1.0.28/pydantic_base_aiogram-1.0.28.tar.gz

---
+++ packages/pydantic-base-aiogram/pydantic_base_aiogram-1.0.28.tar.gz/pydantic_base_aiogram-1.0.28/pydantic_base_aiogram/field_factory/union.py
@@ -0,0 +1,55 @@
+from abc import abstractmethod
+from enum import Enum
+from itertools import chain
+from typing import Type, Union
+from pydantic import BaseModel
+from pydantic.fields import ModelField
+from .base import BaseFieldFactory
+
+
+class UnionFieldFactory(BaseFieldFactory):
+    def _countup_things(self, field, things: list):
+        strs_count = 0
+        enums = []
+        models = []
+
+        for item in things:
+            if issubclass(item, Enum):
+                enums.append(item)
+            elif issubclass(item, BaseModel):
+                models.append(item)
+            elif issubclass(item, Union[str, float, int]):
+                strs_count += 1
+            else:
+                raise NotImplementedError(f'`{field.type_}` is too comple type for `{self.__class__.__name__}`')
+
+        return strs_count, enums, models
+
+    @abstractmethod
+    def create4models(self, field: ModelField, models: list[Type[BaseModel]], kwargs: dict):
+        raise NotImplementedError
+
+    def create4uniontype(self, field: ModelField, parents, **kwargs):
+        return self.create4_uniongenericalias(field, parents, **kwargs)
+
+    def create4_uniongenericalias(self, field: ModelField, parents, **kwargs):
+        args = field.type_.__args__
+        all_count = len(args)
+        strs_count, enums, models = self._countup_things(field, args)
+
+        if strs_count == all_count:
+            return self.create4type(field, parents, force_type=str, **kwargs)
+
+        if all_count == (len(enums) + strs_count):
+            combined_name = ''.join(map(lambda x: x.__name__, enums))
+            type_ = Enum(combined_name, [(x.name, x.value) for x in chain(*enums)])
+            return self._create(field, type_=type_, parents=parents, is_string_allowed=strs_count > 0, **kwargs)
+
+        elif strs_count or len(enums):
+            raise NotImplementedError(f'`{field.type_}` is too comple type for `{self.__class__.__name__}`')
+
+        kwargs['parents'] = (*parents, field.name)
+        kwargs['states'] = getattr(kwargs['states'], field.name)
+        
+        return self.create4models(field, models=models, kwargs=kwargs) 
+
