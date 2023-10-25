1df9ed3a6d1b1147793eadf074a8fe46a50828a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:08:06 +0000

    Add package haupt/2.0.0rc54/haupt-2.0.0rc54-py3-none-any.whl

---
+++ packages/haupt/haupt-2.0.0rc54-py3-none-any.whl/haupt/managers/sandbox.py
@@ -0,0 +1,28 @@
+import os
+
+from haupt.schemas.sandbox_config import SandboxConfig
+from polyaxon._config.manager import ConfigManager
+
+
+class SandboxConfigManager(ConfigManager):
+    """Manages sandbox configuration .sandbox file."""
+
+    VISIBILITY = ConfigManager.Visibility.ALL
+    CONFIG_FILE_NAME = ".sandbox"
+    CONFIG = SandboxConfig
+
+    @classmethod
+    def get_config_from_env(cls):
+        config_paths = [os.environ, {"dummy": "dummy"}]
+
+        config = cls._CONFIG_READER.read_configs(config_paths)
+        config = cls.CONFIG.from_dict(config.data)
+        config.set_default_artifacts_store()
+        return config
+
+    @classmethod
+    def get_config_or_default(cls) -> SandboxConfig:
+        if not cls.is_initialized():
+            return cls.get_config_from_env()
+
+        return cls.get_config()
