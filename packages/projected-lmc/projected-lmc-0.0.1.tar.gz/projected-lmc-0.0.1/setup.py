d0abdfdfb92bc3b972882da96cd56cd9cdbfc9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:05:21 +0000

    Add package projected-lmc/0.0.1/projected-lmc-0.0.1.tar.gz

---
+++ packages/projected-lmc/projected-lmc-0.0.1.tar.gz/projected-lmc-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+# Load the long_description from README.md
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# with open('requirements.txt', 'r') as f:
+    # required = f.read().splitlines()
+
+setuptools.setup(
+    name="projected-lmc",
+    version="0.0.1",
+    author="",
+    author_email="",
+    description="A short package based on gpytorch implementing the Projected LMC model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://qwerty6191.github.io/projected-lmc/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=['gpytorch', 'linear_operator', 'matplotlib', 'pandas', 'scikit_learn', 'seaborn'],
+    # install_requires=required,
+)
