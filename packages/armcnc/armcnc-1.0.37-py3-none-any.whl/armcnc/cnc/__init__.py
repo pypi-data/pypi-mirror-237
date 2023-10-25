79d3fd56f8070a7ea25504d21bd0e1a140cb4881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 15:06:17 +0000

    Add package armcnc/1.0.37/armcnc-1.0.37-py3-none-any.whl

---
+++ packages/armcnc/armcnc-1.0.37-py3-none-any.whl/armcnc/cnc/__init__.py
@@ -0,0 +1,65 @@
+"""
+******************************************************************************
+* @author  ARMCNC site:www.armcnc.net github:armcnc.github.io
+******************************************************************************
+"""
+
+import sys
+import subprocess
+import linuxcnc
+from .status import Status
+from .command import Command
+from .error import Error
+
+class CNC:
+
+    def __init__(self, framework):
+        self.framework = framework
+        self.status = Status(self.framework)
+        self.command = Command(self.framework)
+        self.error = Error(self.framework)
+
+    def start(self):
+        linuxcnc_pid = subprocess.Popen(["pidof", "-x", "linuxcnc"], stdout=subprocess.PIPE)
+        linuxcnc_pid_result = linuxcnc_pid.communicate()[0]
+        if len(linuxcnc_pid_result) == 0:
+            self.framework.utils.service.service_write({"command": "launch:restart", "message": "", "data": False})
+            sys.exit()
+        self.framework.machine.is_alive = True
+        self.framework.utils.service.service_write({"command": "launch:restart", "message": "", "data": True})
+
+    def message_callback(self, message):
+        if message and message["command"] and message["command"] != "":
+            if message["command"] == "desktop:control:device:estop":
+                self.status.api.poll()
+                if self.status.api.task_state == linuxcnc.STATE_ESTOP:
+                    self.command.api.state(linuxcnc.STATE_ESTOP_RESET)
+                else:
+                    if self.status.api.task_state == linuxcnc.STATE_ESTOP_RESET or self.status.api.task_state == linuxcnc.STATE_ON or self.status.api.task_state == linuxcnc.STATE_OFF:
+                        self.command.api.state(linuxcnc.STATE_ESTOP)
+                self.command.api.wait_complete(0.5)
+
+            if message["command"] == "desktop:control:device:start":
+                self.status.api.poll()
+                if self.status.api.task_state == linuxcnc.STATE_ESTOP:
+                    return False
+                if self.status.api.task_state == linuxcnc.STATE_ON:
+                    self.command.api.state(linuxcnc.STATE_OFF)
+                else:
+                    if self.status.api.task_state == linuxcnc.STATE_OFF or self.status.api.task_state == linuxcnc.STATE_ESTOP_RESET:
+                        self.command.api.state(linuxcnc.STATE_ON)
+                self.command.api.wait_complete(0.5)
+
+                print("read_line", self.status.api.read_line)
+                print("linear_units", self.status.api.linear_units)
+                print("paused", self.status.api.paused)
+                print("estop", self.status.api.estop)
+                print("enabled", self.status.api.enabled)
+                print("state", self.status.api.state)
+                print("interp_state", self.status.api.interp_state)
+                print("task_state", self.status.api.task_state)
+                print("homed", self.status.api.homed)
+                print("--------------------")
+
+
+
---
+++ packages/armcnc/armcnc-1.0.37-py3-none-any.whl/armcnc/cnc/status.py
@@ -0,0 +1,47 @@
+"""
+******************************************************************************
+* @author  ARMCNC site:www.armcnc.net github:armcnc.github.io
+******************************************************************************
+"""
+
+import threading
+import linuxcnc
+
+
+class Status:
+
+    def __init__(self, framework):
+        self.framework = framework
+        self.linuxcnc = linuxcnc
+        self.api = linuxcnc.stat()
+        self.task = threading.Thread(name="status_task", target=self.task)
+        self.task.daemon = True
+        self.task.start()
+
+    def task(self):
+        while True:
+            if self.framework.machine.is_alive:
+                try:
+                    self.api.poll()
+                except linuxcnc.error as detail:
+                    self.framework.utils.service.service_write(
+                        {"command": "launch:machine:error", "message": detail, "data": False})
+
+                self.framework.machine.info = {}
+                for x in dir(self.api):
+                    if not x.startswith("_") and not callable(getattr(self.api, x)):
+                        self.framework.machine.info[x] = getattr(self.api, x)
+
+                if self.framework.machine.info["ini_filename"]:
+                    inifile = linuxcnc.ini(self.framework.machine.stat["ini_filename"])
+                    user_data = {
+                        "status": self.framework.machine.stat,
+                        "user": self.framework.machine.user,
+                        "increments": inifile.find("DISPLAY", "INCREMENTS") or [],
+                        "coordinates": inifile.find("TRAJ", "COORDINATES") or "unknown",
+                        "linear_units": inifile.find("TRAJ", "LINEAR_UNITS") or "mm",
+                        "angular_units": inifile.find("TRAJ", "ANGULAR_UNITS") or "degree",
+                    }
+
+                    self.framework.utils.service.service_write({"command": "launch:machine:info", "message": "", "data": user_data})
+            self.framework.utils.set_sleep(0.05)
