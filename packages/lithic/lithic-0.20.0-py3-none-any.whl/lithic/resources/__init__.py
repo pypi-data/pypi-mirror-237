acf82b7010aadfc39cf0684dfabdc20c8572be1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:09:46 +0000

    Add package lithic/0.20.0/lithic-0.20.0-py3-none-any.whl

---
+++ packages/lithic/lithic-0.20.0-py3-none-any.whl/lithic/resources/__init__.py
@@ -0,0 +1,71 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from .cards import Cards, AsyncCards
+from .events import Events, AsyncEvents
+from .reports import Reports, AsyncReports
+from .accounts import Accounts, AsyncAccounts
+from .balances import Balances, AsyncBalances
+from .disputes import Disputes, AsyncDisputes
+from .payments import Payments, AsyncPayments
+from .three_ds import ThreeDS, AsyncThreeDS
+from .webhooks import Webhooks, AsyncWebhooks
+from .auth_rules import AuthRules, AsyncAuthRules
+from .card_product import CardProduct, AsyncCardProduct
+from .transactions import Transactions, AsyncTransactions
+from .tokenizations import Tokenizations, AsyncTokenizations
+from .account_holders import AccountHolders, AsyncAccountHolders
+from .aggregate_balances import AggregateBalances, AsyncAggregateBalances
+from .financial_accounts import FinancialAccounts, AsyncFinancialAccounts
+from .responder_endpoints import ResponderEndpoints, AsyncResponderEndpoints
+from .auth_stream_enrollment import (
+    AuthStreamEnrollmentResource,
+    AsyncAuthStreamEnrollmentResource,
+)
+from .external_bank_accounts import ExternalBankAccounts, AsyncExternalBankAccounts
+from .tokenization_decisioning import (
+    TokenizationDecisioning,
+    AsyncTokenizationDecisioning,
+)
+
+__all__ = [
+    "Accounts",
+    "AsyncAccounts",
+    "AccountHolders",
+    "AsyncAccountHolders",
+    "AuthRules",
+    "AsyncAuthRules",
+    "AuthStreamEnrollmentResource",
+    "AsyncAuthStreamEnrollmentResource",
+    "TokenizationDecisioning",
+    "AsyncTokenizationDecisioning",
+    "Tokenizations",
+    "AsyncTokenizations",
+    "Cards",
+    "AsyncCards",
+    "Balances",
+    "AsyncBalances",
+    "AggregateBalances",
+    "AsyncAggregateBalances",
+    "Disputes",
+    "AsyncDisputes",
+    "Events",
+    "AsyncEvents",
+    "FinancialAccounts",
+    "AsyncFinancialAccounts",
+    "Transactions",
+    "AsyncTransactions",
+    "ResponderEndpoints",
+    "AsyncResponderEndpoints",
+    "Webhooks",
+    "AsyncWebhooks",
+    "ExternalBankAccounts",
+    "AsyncExternalBankAccounts",
+    "Payments",
+    "AsyncPayments",
+    "ThreeDS",
+    "AsyncThreeDS",
+    "Reports",
+    "AsyncReports",
+    "CardProduct",
+    "AsyncCardProduct",
+]
---
+++ packages/lithic/lithic-0.20.0-py3-none-any.whl/lithic/resources/events/events.py
@@ -0,0 +1,449 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List, Union
+from datetime import datetime
+from typing_extensions import Literal
+
+from ...types import (
+    Event,
+    MessageAttempt,
+    event_list_params,
+    event_list_attempts_params,
+)
+from ..._types import NOT_GIVEN, Body, Query, Headers, NoneType, NotGiven
+from ..._utils import maybe_transform
+from ..._resource import SyncAPIResource, AsyncAPIResource
+from ...pagination import SyncCursorPage, AsyncCursorPage
+from .subscriptions import Subscriptions, AsyncSubscriptions
+from ..._base_client import AsyncPaginator, make_request_options
+
+if TYPE_CHECKING:
+    from ..._client import Lithic, AsyncLithic
+
+__all__ = ["Events", "AsyncEvents"]
+
+
+class Events(SyncAPIResource):
+    subscriptions: Subscriptions
+
+    def __init__(self, client: Lithic) -> None:
+        super().__init__(client)
+        self.subscriptions = Subscriptions(client)
+
+    def retrieve(
+        self,
+        event_token: str,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | None | NotGiven = NOT_GIVEN,
+    ) -> Event:
+        """
+        Get an event.
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._get(
+            f"/events/{event_token}",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=Event,
+        )
+
+    def list(
+        self,
+        *,
+        begin: Union[str, datetime] | NotGiven = NOT_GIVEN,
+        end: Union[str, datetime] | NotGiven = NOT_GIVEN,
+        ending_before: str | NotGiven = NOT_GIVEN,
+        event_types: List[
+            Literal[
+                "account_holder.created",
+                "account_holder.updated",
+                "account_holder.verification",
+                "card.created",
+                "card.shipped",
+                "card_transaction.updated",
+                "digital_wallet.tokenization_approval_request",
+                "digital_wallet.tokenization_result",
+                "digital_wallet.tokenization_two_factor_authentication_code",
+                "dispute.updated",
+                "dispute_evidence.upload_failed",
+                "three_ds_authentication.created",
+                "payment_transaction.created",
+                "payment_transaction.updated",
+                "transfer_transaction.created",
+            ]
+        ]
+        | NotGiven = NOT_GIVEN,
+        page_size: int | NotGiven = NOT_GIVEN,
+        starting_after: str | NotGiven = NOT_GIVEN,
+        with_content: bool | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | None | NotGiven = NOT_GIVEN,
+    ) -> SyncCursorPage[Event]:
+        """List all events.
+
+        Args:
+          begin: Date string in RFC 3339 format.
+
+        Only entries created after the specified time
+              will be included. UTC time zone.
+
+          end: Date string in RFC 3339 format. Only entries created before the specified time
+              will be included. UTC time zone.
+
+          ending_before: A cursor representing an item's token before which a page of results should end.
+              Used to retrieve the previous page of results before this item.
+
+          event_types: Event types to filter events by.
+
+          page_size: Page size (for pagination).
+
+          starting_after: A cursor representing an item's token after which a page of results should
+              begin. Used to retrieve the next page of results after this item.
+
+          with_content: Whether to include the event payload content in the response.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._get_api_list(
+            "/events",
+            page=SyncCursorPage[Event],
+            options=make_request_options(
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+                query=maybe_transform(
+                    {
+                        "begin": begin,
+                        "end": end,
+                        "ending_before": ending_before,
+                        "event_types": event_types,
+                        "page_size": page_size,
+                        "starting_after": starting_after,
+                        "with_content": with_content,
+                    },
+                    event_list_params.EventListParams,
+                ),
+            ),
+            model=Event,
+        )
+
+    def list_attempts(
+        self,
+        event_token: str,
+        *,
+        begin: Union[str, datetime] | NotGiven = NOT_GIVEN,
+        end: Union[str, datetime] | NotGiven = NOT_GIVEN,
+        ending_before: str | NotGiven = NOT_GIVEN,
+        page_size: int | NotGiven = NOT_GIVEN,
+        starting_after: str | NotGiven = NOT_GIVEN,
+        status: Literal["FAILED", "PENDING", "SENDING", "SUCCESS"] | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | None | NotGiven = NOT_GIVEN,
+    ) -> SyncCursorPage[MessageAttempt]:
+        """
+        List all the message attempts for a given event.
+
+        Args:
+          begin: Date string in RFC 3339 format. Only entries created after the specified time
+              will be included. UTC time zone.
+
+          end: Date string in RFC 3339 format. Only entries created before the specified time
+              will be included. UTC time zone.
+
+          ending_before: A cursor representing an item's token before which a page of results should end.
+              Used to retrieve the previous page of results before this item.
+
+          page_size: Page size (for pagination).
+
+          starting_after: A cursor representing an item's token after which a page of results should
+              begin. Used to retrieve the next page of results after this item.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._get_api_list(
+            f"/events/{event_token}/attempts",
+            page=SyncCursorPage[MessageAttempt],
+            options=make_request_options(
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+                query=maybe_transform(
+                    {
+                        "begin": begin,
+                        "end": end,
+                        "ending_before": ending_before,
+                        "page_size": page_size,
+                        "starting_after": starting_after,
+                        "status": status,
+                    },
+                    event_list_attempts_params.EventListAttemptsParams,
+                ),
+            ),
+            model=MessageAttempt,
+        )
+
+    def resend(
+        self,
+        event_token: str,
+        *,
+        event_subscription_token: str,
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+    ) -> None:
+        """Resend an event to an event subscription."""
+        self._post(
+            f"/events/{event_token}/event_subscriptions/{event_subscription_token}/resend",
+            options=make_request_options(extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body),
+            cast_to=NoneType,
+        )
+
+
+class AsyncEvents(AsyncAPIResource):
+    subscriptions: AsyncSubscriptions
+
+    def __init__(self, client: AsyncLithic) -> None:
+        super().__init__(client)
+        self.subscriptions = AsyncSubscriptions(client)
+
+    async def retrieve(
+        self,
+        event_token: str,
+        *,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | None | NotGiven = NOT_GIVEN,
+    ) -> Event:
+        """
+        Get an event.
+
+        Args:
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return await self._get(
+            f"/events/{event_token}",
+            options=make_request_options(
+                extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body, timeout=timeout
+            ),
+            cast_to=Event,
+        )
+
+    def list(
+        self,
+        *,
+        begin: Union[str, datetime] | NotGiven = NOT_GIVEN,
+        end: Union[str, datetime] | NotGiven = NOT_GIVEN,
+        ending_before: str | NotGiven = NOT_GIVEN,
+        event_types: List[
+            Literal[
+                "account_holder.created",
+                "account_holder.updated",
+                "account_holder.verification",
+                "card.created",
+                "card.shipped",
+                "card_transaction.updated",
+                "digital_wallet.tokenization_approval_request",
+                "digital_wallet.tokenization_result",
+                "digital_wallet.tokenization_two_factor_authentication_code",
+                "dispute.updated",
+                "dispute_evidence.upload_failed",
+                "three_ds_authentication.created",
+                "payment_transaction.created",
+                "payment_transaction.updated",
+                "transfer_transaction.created",
+            ]
+        ]
+        | NotGiven = NOT_GIVEN,
+        page_size: int | NotGiven = NOT_GIVEN,
+        starting_after: str | NotGiven = NOT_GIVEN,
+        with_content: bool | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | None | NotGiven = NOT_GIVEN,
+    ) -> AsyncPaginator[Event, AsyncCursorPage[Event]]:
+        """List all events.
+
+        Args:
+          begin: Date string in RFC 3339 format.
+
+        Only entries created after the specified time
+              will be included. UTC time zone.
+
+          end: Date string in RFC 3339 format. Only entries created before the specified time
+              will be included. UTC time zone.
+
+          ending_before: A cursor representing an item's token before which a page of results should end.
+              Used to retrieve the previous page of results before this item.
+
+          event_types: Event types to filter events by.
+
+          page_size: Page size (for pagination).
+
+          starting_after: A cursor representing an item's token after which a page of results should
+              begin. Used to retrieve the next page of results after this item.
+
+          with_content: Whether to include the event payload content in the response.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._get_api_list(
+            "/events",
+            page=AsyncCursorPage[Event],
+            options=make_request_options(
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+                query=maybe_transform(
+                    {
+                        "begin": begin,
+                        "end": end,
+                        "ending_before": ending_before,
+                        "event_types": event_types,
+                        "page_size": page_size,
+                        "starting_after": starting_after,
+                        "with_content": with_content,
+                    },
+                    event_list_params.EventListParams,
+                ),
+            ),
+            model=Event,
+        )
+
+    def list_attempts(
+        self,
+        event_token: str,
+        *,
+        begin: Union[str, datetime] | NotGiven = NOT_GIVEN,
+        end: Union[str, datetime] | NotGiven = NOT_GIVEN,
+        ending_before: str | NotGiven = NOT_GIVEN,
+        page_size: int | NotGiven = NOT_GIVEN,
+        starting_after: str | NotGiven = NOT_GIVEN,
+        status: Literal["FAILED", "PENDING", "SENDING", "SUCCESS"] | NotGiven = NOT_GIVEN,
+        # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
+        # The extra values given here take precedence over values defined on the client or passed to this method.
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+        timeout: float | None | NotGiven = NOT_GIVEN,
+    ) -> AsyncPaginator[MessageAttempt, AsyncCursorPage[MessageAttempt]]:
+        """
+        List all the message attempts for a given event.
+
+        Args:
+          begin: Date string in RFC 3339 format. Only entries created after the specified time
+              will be included. UTC time zone.
+
+          end: Date string in RFC 3339 format. Only entries created before the specified time
+              will be included. UTC time zone.
+
+          ending_before: A cursor representing an item's token before which a page of results should end.
+              Used to retrieve the previous page of results before this item.
+
+          page_size: Page size (for pagination).
+
+          starting_after: A cursor representing an item's token after which a page of results should
+              begin. Used to retrieve the next page of results after this item.
+
+          extra_headers: Send extra headers
+
+          extra_query: Add additional query parameters to the request
+
+          extra_body: Add additional JSON properties to the request
+
+          timeout: Override the client-level default timeout for this request, in seconds
+        """
+        return self._get_api_list(
+            f"/events/{event_token}/attempts",
+            page=AsyncCursorPage[MessageAttempt],
+            options=make_request_options(
+                extra_headers=extra_headers,
+                extra_query=extra_query,
+                extra_body=extra_body,
+                timeout=timeout,
+                query=maybe_transform(
+                    {
+                        "begin": begin,
+                        "end": end,
+                        "ending_before": ending_before,
+                        "page_size": page_size,
+                        "starting_after": starting_after,
+                        "status": status,
+                    },
+                    event_list_attempts_params.EventListAttemptsParams,
+                ),
+            ),
+            model=MessageAttempt,
+        )
+
+    async def resend(
+        self,
+        event_token: str,
+        *,
+        event_subscription_token: str,
+        extra_headers: Headers | None = None,
+        extra_query: Query | None = None,
+        extra_body: Body | None = None,
+    ) -> None:
+        """Resend an event to an event subscription."""
+        await self._post(
+            f"/events/{event_token}/event_subscriptions/{event_subscription_token}/resend",
+            options=make_request_options(extra_headers=extra_headers, extra_query=extra_query, extra_body=extra_body),
+            cast_to=NoneType,
+        )
