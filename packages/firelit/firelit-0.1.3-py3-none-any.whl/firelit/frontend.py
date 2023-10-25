d8fd104ad4f78e97c0794e155f6dcab4df1f3d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:05:51 +0000

    Add package firelit/0.1.3/firelit-0.1.3-py3-none-any.whl

---
+++ packages/firelit/firelit-0.1.3-py3-none-any.whl/firelit/frontend.py
@@ -0,0 +1,55 @@
+import streamlit as st
+
+from firelit.admin import FirebaseAdmin
+
+
+def firelit_login_form(
+    firelit_config: str | dict | None = None,
+    sidebar: bool = False,
+    admin: FirebaseAdmin | None = None,
+    email: str = "",
+    password: str = "",
+) -> FirebaseAdmin:
+    if sidebar:
+        with st.sidebar:
+            return firelit_login_form(firelit_config=firelit_config, sidebar=False)
+
+    if admin is None:
+        if "firelit_admin" not in st.session_state:
+            admin = FirebaseAdmin(config=firelit_config)
+            st.session_state["firelit_admin"] = admin
+        else:
+            admin = st.session_state["firelit_admin"]
+
+    if not admin.authentication_status:
+        with st.form("**Login**"):
+            st.subheader("Login")
+            email = st.text_input("Email", value=email)
+            password = st.text_input("Password", type="password", value=password)
+
+            if st.form_submit_button(
+                "🔐 Login", type="primary", use_container_width=True
+            ):
+                admin.login(email, password)
+
+                if admin.authentication_status:
+                    st.success(
+                        "Login successful 🎉 Welcome %s" % admin.user_info["name"]
+                    )
+                    st.rerun()
+
+                else:
+                    st.error("Invalid credentials")
+                    st.stop()
+
+    else:
+        with st.expander("Logged", expanded=False):
+            st.subheader(f"Welcome {admin.user_info['name']}")
+
+            st.info("Licence expiration date: %s" % admin.licence["expiration_date"])
+
+            if st.button("🔒 Logout", type="primary", use_container_width=True):
+                admin.reset_connection()
+                st.rerun()
+
+    return admin
