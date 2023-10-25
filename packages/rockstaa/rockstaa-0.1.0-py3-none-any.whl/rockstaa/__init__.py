e60c87be8d4ba1cb2ff946fc68f9650677693c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:05:55 +0000

    Add package rockstaa/0.1.0/rockstaa-0.1.0-py3-none-any.whl

---
+++ packages/rockstaa/rockstaa-0.1.0-py3-none-any.whl/rockstaa/__init__.py
@@ -0,0 +1,9 @@
+# amal/__init__.py
+
+
+def about():
+    return "Hello! I'm Rahul. I am a software developer and a huge javascript fan!"
+
+
+def is_gay():
+    return "Nope nada zilch. Definitely not gay. I am only into sudeep."
