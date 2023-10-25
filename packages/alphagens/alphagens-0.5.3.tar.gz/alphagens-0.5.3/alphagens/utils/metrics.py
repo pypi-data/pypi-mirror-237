658f5d185b9f4c797a231bd1d4027c9760ed5991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:10:14 +0000

    Add package alphagens/0.5.3/alphagens-0.5.3.tar.gz

---
+++ packages/alphagens/alphagens-0.5.3.tar.gz/alphagens-0.5.3/alphagens/utils/metrics.py
@@ -0,0 +1,176 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import typing
+from ..utils.visualization import ColorGenerator
+
+class StrategyMetrics:
+
+    """用staticmethod好一些吗? 目前数据格式不是很统一, 还是一个一个算比较好
+    """
+    annulized_factor: int = 252
+
+    @staticmethod
+    def annulized_mean(strategy_returns, annulized_factor=252):
+        return np.mean(strategy_returns) * annulized_factor
+    
+    @staticmethod
+    def annulized_std(strategy_returns, annulized_factor=252):
+        return np.std(strategy_returns) * np.sqrt(annulized_factor)
+
+    @staticmethod
+    def annulized_sharpe_ratio(strategy_returns, rfr=0, annulized_factor=252):
+        return (np.mean(strategy_returns- rfr)) / np.std(strategy_returns) * np.sqrt(annulized_factor)
+    
+    @staticmethod
+    def drawdown(strategy_returns: pd.Series) -> (pd.Series, float, int):
+        """
+        Returns
+        -------
+        (max drawdown, longest drawdown duration, drawdown series,)
+        """
+        net_curve = (1+strategy_returns).cumprod()
+        cmax = net_curve.cummax()
+        rlt = (cmax - net_curve) / cmax
+        longest_drawdown = cmax.value_counts().max() - 1
+        if isinstance(strategy_returns, pd.Series):
+            rlt = pd.Series(rlt, index=strategy_returns.index)
+        elif isinstance(strategy_returns, np.ndarray):
+            rlt = np.array(rlt)
+        return rlt.max(), longest_drawdown, rlt
+    
+    @staticmethod
+    def evaluate(strategy_returns, benchmark_returns, rfr=0, annulized_factor=252):
+        result = {}
+        result["mean"] = StrategyMetrics.annulized_mean(strategy_returns, annulized_factor)
+        result["std"] = StrategyMetrics.annulized_std(strategy_returns, annulized_factor)
+        result["sharpe_ratio"] = StrategyMetrics.annulized_sharpe_ratio(strategy_returns, rfr, annulized_factor)
+        result["max_drawdown"], result['longest_drawdown'], _ = StrategyMetrics.drawdown(strategy_returns)
+        result["alpha"], result["beta"], _ = StrategyMetrics.reg_stats(strategy_returns, benchmark_returns, rfr, annulized_factor)
+        return result
+    
+    def evaluate_by_year(strategy_returns, benchmark_returns, rfr=0, annulized_factor=252):
+        result = strategy_returns.resample('Y').apply(lambda x: pd.Series(StrategyMetrics.evaluate(x, benchmark_returns.loc[x.index])))
+        return result.unstack()
+
+    @staticmethod
+    def reg_stats(strategy_returns, benchmark_returns, rfr=0, annulize_factor=252):
+        strategy_returns = np.array(strategy_returns)
+        benchmark_returns = np.array(benchmark_returns)
+        rfr = np.array(rfr)
+        y = strategy_returns - rfr
+        X = benchmark_returns.reshape(-1, 1) - rfr
+        model = LinearRegression()
+        model.fit(X, y)
+        annulized_alpha = model.intercept_ * annulize_factor
+        beta = model.coef_[0]
+        residuals = y - model.predict(X)
+        annulized_residual_std = np.std(residuals) * np.sqrt(annulize_factor)
+        return annulized_alpha, beta, annulized_residual_std
+
+    @staticmethod
+    def plot_heatmap_of_monthly_returns(
+        strategy_returns: pd.Series, 
+        figsize: tuple[float] = (12, 10),
+        title: str = 'Monthly Returns Heatmap',
+        vlim: tuple[float] = (-0.3, 0.3),
+        dpi: int = 100,
+    ):
+        tmp = strategy_returns.resample("M").sum()
+        tmp.name = "monthly returns"
+        tmp = pd.DataFrame(tmp)
+        tmp['year'] = tmp.index.year
+        tmp['month'] = tmp.index.month
+        bb = tmp.pivot_table(tmp, index=tmp["year"], columns=tmp["month"])
+        bb.columns.name = None
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=figsize, dpi=dpi)
+        ax = sns.heatmap(bb.values, annot=True, cmap="RdYlGn_r", fmt=".2%", linewidths=0.5, linecolor='white', 
+                         yticklabels=bb.index, xticklabels=bb.columns, vmin=vlim[0], vmax=vlim[1], ax=ax)
+        ax.set_title(title)
+        ax.set_xlabel('Month')
+        ax.set_ylabel('Year')
+        fig.tight_layout()
+        return fig, ax
+
+    @staticmethod
+    def integrated_informations_by_year(
+        strategy_returns,
+    ):
+        pass
+
+    @staticmethod
+    def beta():
+        raise Warning("to get beta, call reg_stats")
+    
+    @staticmethod
+    def alpha():
+        raise Warning("to get alpha, call reg_stats")
+    
+
+class FactorMetrics:
+    
+    @staticmethod
+    def ic_series(factor_scores, forward_returns, method="pearson") -> pd.Series:
+        """_summary_
+        caculate the 
+        - `mean-IC`
+        - `ICIR`
+
+        Parameters
+        ----------
+        factor_scores : _type_
+            _description_
+        forward_returns : _type_
+            _description_
+        method : str, optional
+            _description_, by default "pearson"
+
+        Returns
+        -------
+        pd.Series
+            _description_
+        """
+        pass
+    
+
+    @staticmethod
+    def plot_pnl_by_groups(strategies_pnls: pd.DataFrame, dpi=100):
+        colors = ColorGenerator(5).data
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,8), dpi=dpi)
+        for idx, name in enumerate(strategies_pnls.columns):
+            pnl = strategies_pnls[name]
+            ax.plot(pnl, label=f"group_{idx}", color=colors[idx])
+        ax.legend()
+        ax.set_xlabel("Date")
+        ax.set_ylabel("pnl")
+        ax.grid()
+        fig.tight_layout()
+        return fig, ax
+
+    @staticmethod
+    def cum_ic_series():
+        raise NotImplementedError
+
+    @staticmethod
+    def long_short_returns(integrated_factors) -> pd.Series:
+        pass
+
+    @staticmethod
+    def group_returns(integrated_factors) -> pd.DataFrame:
+        pass
+
+    @staticmethod
+    def turnover():
+        raise NotImplementedError
+    
+    @staticmethod
+    def fitness():
+        pass
+
+    staticmethod
+    def margin():
+        pass
+    
+
---
+++ packages/alphagens/alphagens-0.5.3.tar.gz/alphagens-0.5.3/setup.py
@@ -0,0 +1,77 @@
+from distutils.core import setup
+from setuptools import find_packages
+from Cython.Build import cythonize
+import numpy
+import glob
+import platform
+
+PLATFORM = platform.system()
+from alphagens import __version__
+# extensions = glob.glob("torchqtm/_C/*.pyx")
+# extensions.extend(glob.glob("torchqtm/window/*.pyx"))
+# extensions.extend(glob.glob("torchqtm/finance/*.pyx"))
+
+install_requires = [
+    'numpy',
+    'pandas',
+    'pandas_market_calendars',
+    "joblib>=1.2.0",
+    "pyperclip",
+    "tushare"
+]
+
+
+def main():
+    setup(
+        # ext_modules=cythonize(extensions, annotate=True),
+        include_dirs=[numpy.get_include()],
+        name="alphagens",
+        version=__version__,
+        author="ny",
+        author_email="nymath@163.com",
+        install_requires=install_requires,
+        description="An event-driven backtesting system",
+        long_description=open('README.md', 'r').read(),
+        long_description_content_type="text/markdown",
+        download_url="https://github.com/nymath/alphagens/releases/tag/v1.0",
+        packages=find_packages(),
+        classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python :: 3',
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: Unix",
+        ],
+        project_urls={
+            "Homepage": "https://github.com/nymath/alphagens",
+            'Documentation': 'https://github.com/nymath/alphagens',
+            # 'Funding': 'https://donate.pypi.org',
+            "Download": "https://github.com/nymath/alphagens/releases/tag/v1.0",
+            'Source': 'https://github.com/nymath/alphagens',
+            # 'Tracker': 'https://github.com/pypa/sampleproject/issues',
+        },
+        python_requires='>=3.9',
+        entry_points={
+            "console_scripts": [
+                "texgen=alphagens.cli.texgen:main",
+            ]
+        }
+    )
+
+
+if __name__ == "__main__":
+    main()
+
+## setup cython
+# python setup.py build_ext --inplace
+## setup package
+# pip install twine
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+# twine upload --skip-existing dist/*
+
+# ny.math
+# xxx
