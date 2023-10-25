e9bacf99fceb7a6b9b3a6a9d69c4c6f58a64acdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 15:05:43 +0000

    Add package marcuslion/0.2.20/marcuslion-0.2.20.tar.gz

---
+++ packages/marcuslion/marcuslion-0.2.20.tar.gz/marcuslion-0.2.20/marcuslion/documents.py
@@ -0,0 +1,36 @@
+import io
+import urllib
+import urllib3
+import json
+
+import pandas as pd
+
+from marcuslion.config import base_url, api_key
+
+
+class Documents:
+    """
+    MarcusLion Documents class
+    """
+
+    @staticmethod
+    def list() -> pd.DataFrame:
+        """
+        Documents.list()
+        """
+        pass
+
+    @staticmethod
+    def query(self):
+        """
+        Documents.query()
+        """
+        pass
+
+    @staticmethod
+    def search(self):
+        """
+        Documents.search()
+        """
+        pass
+
