c9819d344eea08c86592edcd128bbe06b462b665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 04:05:59 +0000

    Add package strangeworks-core/0.3.4/strangeworks_core-0.3.4.tar.gz

---
+++ packages/strangeworks-core/strangeworks_core-0.3.4.tar.gz/strangeworks_core-0.3.4/strangeworks_core/types/func.py
@@ -0,0 +1,28 @@
+"""func.py."""
+from dataclasses import dataclass
+from typing import Any, Callable, Optional, Tuple
+
+
+# using dataclass instead of pydantic as the latter has limitations with validating
+# Callable types.
+@dataclass
+class Func:
+    """
+    Func represents a function to be executed as a batch job.
+
+    Attributes
+    ----------
+    func : Callable[..., Any]
+        The function to be executed.
+    fargs : Tuple[Any]
+        The function's arguments.
+    fkwargs : dict[str, Any]
+        The function's keyword arguments.
+    requirements_path : Optional[str]
+        A path to the function's requirements file.
+    """
+
+    func: Callable[[...], Any]
+    fargs: Tuple[Any]
+    fkwargs: dict[str, Any]
+    requirements_path: Optional[str] = None
