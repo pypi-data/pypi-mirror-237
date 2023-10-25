ca2906a10608159de49f23f576164bf6842a2dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:06:40 +0000

    Add package tlcpack-nightly-cu113/2.15.0/tlcpack-nightly-cu113-2.15.0.tar.gz

---
+++ packages/tlcpack-nightly-cu113/tlcpack-nightly-cu113-2.15.0.tar.gz/tlcpack-nightly-cu113-2.15.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+
+
+setup(name="tlcpack-nightly-cu113",
+      version='2.15.0',
+      description='test',
+      author='test',
+      license='MIT',
+      zip_safe=False
+)
+
