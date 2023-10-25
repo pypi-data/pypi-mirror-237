75c48035f48c5a1eb9231f60e847df485d00e1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 03:06:16 +0000

    Add package imio.smartweb.core/1.1.30/imio.smartweb.core-1.1.30.tar.gz

---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.1.30.tar.gz/imio.smartweb.core-1.1.30/src/imio/smartweb/core/contents/rest/view.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.core.interfaces import IViewWithoutLeadImage
+from plone import api
+from Products.Five import BrowserView
+from zope.interface import implementer
+
+
+@implementer(IViewWithoutLeadImage)
+class BaseRestView(BrowserView):
+    @property
+    def batch_size(self):
+        return self.context.nb_results
+
+    @property
+    def local_query_url(self):
+        base_url = self.context.absolute_url()
+        return f"{base_url}/@results"
+
+    @property
+    def local_filters_query_url(self):
+        base_url = self.context.absolute_url()
+        return f"{base_url}/@results-filters"
+
+    def get_current_language(self):
+        return api.portal.get_current_language()[:2]
---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.1.30.tar.gz/imio.smartweb.core-1.1.30/src/imio/smartweb/core/subscribers.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.common.faceted.utils import configure_faceted
+from imio.smartweb.core.behaviors.minisite import IImioSmartwebMinisite
+from imio.smartweb.core.utils import safe_html
+from imio.smartweb.locales import SmartwebMessageFactory as _
+from plone import api
+from plone.app.layout.navigation.interfaces import INavigationRoot
+from zope.component import getMultiAdapter
+from zope.lifecycleevent import ObjectRemovedEvent
+from zope.globalrequest import getRequest
+
+import os
+
+
+def moved_folder(obj, event):
+    """We use IObjectMovedEvent instead of IObjectAddedEvent because Minisite interface is not yet
+    provided when we use IObjectAddedEvent"""
+    if not IImioSmartwebMinisite.providedBy(obj):
+        return
+    if type(event) is ObjectRemovedEvent:
+        # We don't have anything to do if minisite is being removed
+        return
+    parent = event.newParent
+    if INavigationRoot.providedBy(parent):
+        # Minisites must always be under navigation root
+        if not IImioSmartwebMinisite.providedBy(parent):
+            # We are not in another minisite, everything is good
+            return
+    request = getRequest()
+    minisite_settings = getMultiAdapter((obj, request), name="minisite_settings")
+    minisite_settings.disable()
+    api.portal.show_message(
+        _(
+            "Your Folder was a minisite but this behaviour has been disabled with this action"
+        ),
+        request,
+        type="warning",
+    )
+
+
+def added_collection(obj, event):
+    faceted_config_path = "{}/faceted/config/collection.xml".format(
+        os.path.dirname(__file__)
+    )
+    configure_faceted(obj, faceted_config_path)
+
+
+def added_sectionhtml(obj, event):
+    obj.html = safe_html(obj.html)
+
+
+def modified_sectionhtml(obj, event):
+    obj.html = safe_html(obj.html)
