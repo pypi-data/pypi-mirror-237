7b047e21e611c2f8b3cbf8817ec4c8279d631348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 04:05:57 +0000

    Add package dgisim/0.3.2/dgisim-0.3.2-py3-none-any.whl

---
+++ packages/dgisim/dgisim-0.3.2-py3-none-any.whl/dgisim/_core/character/characters_set.py
@@ -0,0 +1,47 @@
+from typing import FrozenSet, Type
+
+from .character import *
+
+__all__ = [
+    "default_characters",
+]
+
+_DEFAULT_CHARACTERS: list[type[Character]] = [
+    Albedo,
+    AratakiItto,
+    Bennett,
+    Collei,
+    ElectroHypostasis,
+    FatuiPyroAgent,
+    Fischl,
+    Ganyu,
+    JadeplumeTerrorshroom,
+    Jean,
+    KaedeharaKazuha,
+    Keqing,
+    Kaeya,
+    Klee,
+    MaguuKenki,
+    Mona,
+    Nahida,
+    Ningguang,
+    Noelle,
+    Qiqi,
+    RhodeiaOfLoch,
+    SangonomiyaKokomi,
+    Shenhe,
+    Tighnari,
+    Venti,
+    Xingqiu,
+    YaeMiko,
+    Yoimiya,
+]
+
+_DEFAULT_CHARACTER_FSET = None
+
+
+def default_characters() -> frozenset[type[Character]]:
+    global _DEFAULT_CHARACTER_FSET
+    if _DEFAULT_CHARACTER_FSET is None:
+        _DEFAULT_CHARACTER_FSET = frozenset(_DEFAULT_CHARACTERS)
+    return _DEFAULT_CHARACTER_FSET
