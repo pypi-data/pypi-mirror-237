79e17842031541323964ebe366f5f16190a3c521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:06:34 +0000

    Add package llm-autoeval/0.1.0/llm_autoeval-0.1.0.tar.gz

---
+++ packages/llm-autoeval/llm_autoeval-0.1.0.tar.gz/llm_autoeval-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="llm_autoeval",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Bernardo Garcia del Rio",
+    author_email="bernardo@flowrite.com",
+    description="A placeholder package for llm_autoeval",
+    long_description="A placeholder package for llm_autoeval.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/flowritecom/llm_autoeval",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
+
