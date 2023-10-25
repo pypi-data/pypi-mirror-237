fd0581d4d342811ab430b858bc834dfc5a6f5aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:07:49 +0000

    Add package my-task-manager/1.0/my_task_manager-1.0.tar.gz

---
+++ packages/my-task-manager/my_task_manager-1.0.tar.gz/my_task_manager-1.0/setup.py
@@ -0,0 +1,36 @@
+"""
+Configuration du Projet - setup.py
+
+Ce fichier de configuration définit les métadonnées et les informations nécessaires à la construction et à l'installation du projet "my_task_manager".
+
+Informations du Projet:
+    - Nom: my_task_manager
+    - Version: 1.0
+    - Description: Un projet de gestion de liste de tâches (To-do List).
+    - Auteurs: Alban Pereira, Yuchen Xia, Aurélien Raulo, Salimatou Traore
+    - Emails des Auteurs: alban.pereira98@gmail.com, xiayuchen35@gmail.com, aurelien0raulo@gmail.com, tra.salimatou@gmail.com
+    - Packages: Recherche de tous les packages nécessaires pour le projet.
+    - Dépendances: datetime, logging
+
+Ce fichier est utilisé pour configurer le projet, y compris le nom, la version, la description, les auteurs, les dépendances et les packages nécessaires pour l'installation. Il permet également de spécifier les informations clés nécessaires à la distribution du projet.
+
+Pour installer le projet, utilisez 'pip install .' dans le répertoire racine du projet.
+"""
+
+
+from setuptools import setup, find_packages
+
+# Les données de projet
+setup(
+    name='my_task_manager',
+    version='1.0',
+    description='Métadonnées et informations sur la construction de projet To-do List',
+    author='Alban Pereira, Yuchen Xia, Aurélien Raulo, Salimatou Traore',
+    author_email='alban.pereira98@gmail.com, xiayuchen35@gmail.com, aurelien0raulo@gmail.com, tra.salimatou@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'datetime',
+        'logging',
+    ],
+    zip_safe=False,
+)
