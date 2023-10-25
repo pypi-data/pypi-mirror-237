77e4cee3a1e3fbd07ef8ec041416471fca5cae29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 05:06:32 +0000

    Add package wlr-layout-ui/1.2.0/wlr_layout_ui-1.2.0-py3-none-any.whl

---
+++ packages/wlr-layout-ui/wlr_layout_ui-1.2.0-py3-none-any.whl/wlr_layout_ui/__init__.py
@@ -0,0 +1,51 @@
+import pyglet
+from .gui import UI
+from .settings import UI_RATIO, PROG_NAME
+from .screens import displayInfo, load
+
+try:
+    import setproctitle
+
+    setproctitle.setproctitle(PROG_NAME)
+except ImportError:
+    pass
+
+
+def main():
+    load()
+    max_width = int(
+        sum(
+            max(screen.available, key=lambda mode: mode.width).width
+            for screen in displayInfo
+        )
+        // UI_RATIO
+    )
+    max_height = int(
+        sum(
+            max(screen.available, key=lambda mode: mode.height).height
+            for screen in displayInfo
+        )
+        // UI_RATIO
+    )
+    average_width = int(
+        sum(
+            max(screen.available, key=lambda mode: mode.width).width
+            for screen in displayInfo
+        )
+        / len(displayInfo)
+        // UI_RATIO
+    )
+    average_height = int(
+        sum(
+            max(screen.available, key=lambda mode: mode.height).height
+            for screen in displayInfo
+        )
+        / len(displayInfo)
+        // UI_RATIO
+    )
+
+    width = max_width + average_width * 2
+    height = max_height + average_height * 2
+    window = UI(width, height)
+    window.set_wm_class(PROG_NAME)
+    pyglet.app.run()
