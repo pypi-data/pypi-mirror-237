9c2578de7e982e0e1e206e175ac39682435c0f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 04:07:10 +0000

    Add package gapper/0.2.0/gapper-0.2.0-py3-none-any.whl

---
+++ packages/gapper/gapper-0.2.0-py3-none-any.whl/gapper/__init__.py
@@ -0,0 +1,4 @@
+from .core.problem import problem
+from .core.test_parameter import (param, test_case, test_cases,
+                                  test_cases_params, test_cases_product,
+                                  test_cases_singular_params, test_cases_zip)
