692c113dff0e5330b26991254f45a3fb66cb8c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:05:55 +0000

    Add package rockstaa/0.1.0/rockstaa-0.1.0.tar.gz

---
+++ packages/rockstaa/rockstaa-0.1.0.tar.gz/rockstaa-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="rockstaa",
+    version="0.1.0",
+    packages=["rockstaa"],
+    url="https://github.com/amalrajan/rockstaa",
+    license="MIT",
+    author="Amal Rajan",
+    author_email="amalrajan@outlook.com",
+    description="About Rahul",
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
