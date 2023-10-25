7cb28722174ddcdc3050e3752329d7a8e9524522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 14:06:59 +0000

    Add package covalent-ecs-plugin/0.32.0/covalent-ecs-plugin-0.32.0.tar.gz

---
+++ packages/covalent-ecs-plugin/covalent-ecs-plugin-0.32.0.tar.gz/covalent-ecs-plugin-0.32.0/covalent_ecs_plugin/assets/infra/main.tf
@@ -0,0 +1,66 @@
+# Copyright 2021 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the Apache License 2.0 (the "License"). A copy of the
+# License may be obtained with this software package or at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Use of this file is prohibited except in compliance with the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.name}-bucket"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership_controls" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.ownership_controls]
+  bucket     = aws_s3_bucket.bucket.id
+  acl        = "private"
+}
+
+resource "aws_ecr_repository" "ecr_repository" {
+  name                 = "${var.name}-ecr-repo"
+  image_tag_mutability = "IMMUTABLE"
+
+  force_delete = true
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name = "${var.name}-log-group"
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "${var.name}-ecs-cluster"
+
+  configuration {
+    execute_command_configuration {
+      logging    = "OVERRIDE"
+      log_configuration {
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.log_group.name
+      }
+    }
+  }
+}
