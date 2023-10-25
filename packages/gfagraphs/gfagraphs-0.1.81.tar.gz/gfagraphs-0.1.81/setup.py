6f33b252636f6de02a29988f3cbfe6fe1392353e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:07:39 +0000

    Add package gfagraphs/0.1.81/gfagraphs-0.1.81.tar.gz

---
+++ packages/gfagraphs/gfagraphs-0.1.81.tar.gz/gfagraphs-0.1.81/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from sys import version_info, stderr
+from setuptools import setup, find_packages
+
+NAME = "gfagraphs"
+CURRENT_PYTHON = version_info[:2]
+REQUIRED_PYTHON = (3, 10)
+
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    stderr.write(
+        f"{NAME} requires Python 3.10 or higher and your current version is {CURRENT_PYTHON}.")
+    exit(1)
+
+
+setup(
+    name=NAME,
+    version='0.1.81',
+    description='Abstraction layer for GFA file format',
+    url='https://github.com/Tharos-ux/gfatypes',
+    author='Tharos',
+    author_email='dubois.siegfried@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['networkx', 'tharos-pytools']
+)
