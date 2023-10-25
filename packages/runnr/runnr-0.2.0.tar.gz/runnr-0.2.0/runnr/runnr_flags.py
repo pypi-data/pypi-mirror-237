9977fc2a9f05c8a647672c778bc0efedab0e718e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 04:06:59 +0000

    Add package runnr/0.2.0/runnr-0.2.0.tar.gz

---
+++ packages/runnr/runnr-0.2.0.tar.gz/runnr-0.2.0/runnr/runnr_flags.py
@@ -0,0 +1,23 @@
+class runnr_flags:
+    s_file_name : str
+    s_extension : str
+    s_custom_output_file_name : str
+    s_extra_param : str
+    s_extra_args : str
+    l_file_names : list
+
+    b_run_after_compilatiion : bool
+    b_debug_mode : bool
+
+    def __init__(self):
+        self.b_run_after_compilatiion = True
+        self.b_debug_mode = False
+        self.b_multiple_files = False
+
+        self.l_file_names = []
+
+        self.s_file_name = ''
+        self.s_extension = ''
+        self.s_extra_param = ''
+        self.s_extra_args = ''
+        self.s_custom_output_file_name = ''
