edd4837440afc032b8be4f81d293e1e6a5a8fc0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:09:50 +0000

    Add package plone.app.dexterity/3.1.2/plone.app.dexterity-3.1.2.tar.gz

---
+++ packages/plone.app.dexterity/plone.app.dexterity-3.1.2.tar.gz/plone.app.dexterity-3.1.2/plone/app/dexterity/tests/test_doctests.py
@@ -0,0 +1,33 @@
+from plone.app.dexterity.testing import DEXTERITY_FUNCTIONAL_TESTING
+from plone.testing import layered
+
+import doctest
+import unittest
+
+
+tests = (
+    "discussion.txt",
+    "editing.rst",
+    "namefromtitle.txt",
+    "metadata.txt",
+    "nextprevious.txt",
+    "filename.txt",
+    "schema_events.txt",
+)
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    OPTIONFLAGS = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
+    for testfile in tests:
+        suite.addTest(
+            layered(
+                doctest.DocFileSuite(
+                    testfile,
+                    optionflags=OPTIONFLAGS,
+                    # package='plone.app.dexterity.tests',
+                ),
+                layer=DEXTERITY_FUNCTIONAL_TESTING,
+            )
+        )
+    return suite
