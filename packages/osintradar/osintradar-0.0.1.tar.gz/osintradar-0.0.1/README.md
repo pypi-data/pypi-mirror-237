59f6162cd45c667e0b7c30291aaf2fe44758e240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 06:08:12 +0000

    Add package osintradar/0.0.1/osintradar-0.0.1.tar.gz

---
+++ packages/osintradar/osintradar-0.0.1.tar.gz/osintradar-0.0.1/README.md
@@ -0,0 +1,16 @@
+
+# How to Install
+```shell
+pip install osintradar
+```
+# How to Use It
+```python
+from osintradar import osintradar
+response = osintradar("test.com")
+print(response)
+```
+
+### Allowed Asset Types
+- Url
+- Domain
+- Ipv4
---
+++ packages/osintradar/osintradar-0.0.1.tar.gz/osintradar-0.0.1/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='osintradar',
+    author='makarson',
+    version="0.0.1",
+    author_email='mali@karson.com.tr',
+    description='Osintradar free asset malicious checker',
+    keywords='example, pypi, package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/makarson/osintradar',
+    project_urls={
+        'Documentation': 'https://github.com/makarson/osintradar',
+        'Bug Reports':
+        'https://github.com/makarson/osintradar/issues',
+        'Source Code': 'https://github.com/makarson/osintradar',
+    },
+    package_dir={'': 'osintradar'},
+    packages=setuptools.find_packages(where='osintradar'),
+    classifiers=[
+        # see https://pypi.org/classifiers/
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=['requests','validators'],
+    extras_require={
+        'dev': ['check-manifest'],
+        # 'test': ['coverage'],
+    },
+)
