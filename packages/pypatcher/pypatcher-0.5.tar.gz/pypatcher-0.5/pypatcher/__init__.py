4c6261385410d0c7cebf1f302ce50555dbf418a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 15:06:00 +0000

    Add package pypatcher/0.5/pypatcher-0.5.tar.gz

---
+++ packages/pypatcher/pypatcher-0.5.tar.gz/pypatcher-0.5/pypatcher/__init__.py
@@ -0,0 +1,34 @@
+import click
+from pypatcher.patcher import Patcher
+from pathlib import Path
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.argument('patch_repo_src', type=click.Path(exists=True))
+@click.argument('patch_destination', type=click.Path(exists=True))
+@click.argument('message')
+def new_patch(patch_repo_src, patch_destination, message):
+    patch_repo_src = Path(patch_repo_src)
+    patch_destination = Path(patch_destination)
+
+    patcher = Patcher(patch_repo_src)
+    patcher.new_patch(message, patches_dir=patch_destination)
+
+@click.command()
+@click.argument('target_repo_path', type=click.Path(exists=True))
+@click.argument('patches_directory', type=click.Path(exists=True))
+@click.option('--ignore-detach', is_flag=True)
+@click.option('--no-commit', is_flag=True)
+def apply_patches(target_repo_path, patches_directory, ignore_detach, no_commit):
+    target_repo_path = Path(target_repo_path)
+    patches_directory = Path(patches_directory)
+
+    patcher = Patcher(target_repo_path)
+    patcher.apply_patches(patches_directory, ignore_detach, no_commit)
+
+
+cli.add_command(new_patch)
+cli.add_command(apply_patches)
