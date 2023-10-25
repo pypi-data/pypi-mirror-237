f3d71023cdb8f6b2b93b5eef45f2290e320eee8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:07:58 +0000

    Add package uMAIA/0.2/uMAIA-0.2.tar.gz

---
+++ packages/uMAIA/uMAIA-0.2.tar.gz/uMAIA-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+__version__ = 0.2
+
+# Read requirements.txt
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+    
+setup(
+    name='uMAIA',
+    version= __version__,
+    packages=find_packages(),
+    description='Toolbox for the processing and analysis of MALDI-MSI data',
+    author='@lamanno-epfl',
+    author_email='gioele.lamanno@epfl.ch',
+    install_requires=requirements,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lamanno-epfl/uMAIA"
+)
