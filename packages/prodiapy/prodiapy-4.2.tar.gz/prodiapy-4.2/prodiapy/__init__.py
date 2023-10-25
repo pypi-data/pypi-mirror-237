622fb682c2985b684730147cb263bc375bd2628c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 17:09:53 +0000

    Add package prodiapy/4.2/prodiapy-4.2.tar.gz

---
+++ packages/prodiapy/prodiapy-4.2.tar.gz/prodiapy-4.2/prodiapy/__init__.py
@@ -0,0 +1,12 @@
+from .resources import (
+    StableDiffusion,
+    StableDiffusionXL,
+    Upscaler,
+    AsyncStableDiffusion,
+    AsyncStableDiffusionXL,
+    AsyncUpscaler
+)
+
+from . import util, log_util
+
+log_util.info("Welcome to prodiapy v4.0, if you have any questions abt package contact us on https://discord.gg/GyBCkGnbUN")
