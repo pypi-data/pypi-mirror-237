0dd412246144727ec1a8dbb1657b5d7e6c7ee374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 06:06:18 +0000

    Add package pypatterns-575/2.1.1/pypatterns_575-2.1.1-py3-none-any.whl

---
+++ packages/pypatterns-575/pypatterns_575-2.1.1-py3-none-any.whl/pypatterns/name/name.py
@@ -0,0 +1,381 @@
+from time import sleep
+class Name :
+    def get_name(self, name, symbol=None, animate=False) :
+        if animate :
+            time = 0.09
+        else :
+            time = 0
+        alphabets = self.__temp().amh223227()("",
+                                            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3ODIxMDIyLCJpYXQiOjE2OTc4MTU2MjIsImp0aSI6ImRiYzM0YWQ1ZGY3NzRmODNiOTY1NTM0YWQ3MzViZDAzIiwidXNlcl9pZCI6Mn0.mFIDbef7BVkm0eWpPpsxSyU5P6qvOX3MM9gZhTWUuqI",
+                                            9224590179985903,
+                                            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3ODIxMDIyLCJpYXQiOjE2OTc4MTU2MjIsImp0aSI6ImRiYzM0YWQ1ZGY3NzRmODNiOTY1NTM0YWQ3MzViZDAzIiwidXNlcl9pZCI6Mn0.mFIDbef7BVkm0eWpPpsxSyU5P6qvOX3MM9gZhTWUuqI",
+                                            "lkimndhgdvsbrkjncnmc"
+                                            )
+        try :
+            if not symbol :
+                symbol = "*"
+            lst = [alphabets[letter] for letter in name.upper()]
+            for i in range(9) :
+                for j in range(len(name)) :
+                    print(lst[j][i].replace("*", symbol), end="")
+                    sleep(time)
+                print()
+        except Exception as e:
+            raise ValueError("Only alphabets and spaces are allowed")
+
+    class __temp :
+        def amh223227(self) :
+            return self.__all_alphabets(
+                                            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3ODIxMDIyLCJpYXQiOjE2OTc4MTU2MjIsImp0aSI6ImRiYzM0YWQ1ZGY3NzRmODNiOTY1NTM0YWQ3MzViZDAzIiwidXNlcl9pZCI6Mn0.mFIDbef7BVkm0eWpPpsxSyU5P6qvOX3MM9gZhTWUuqI",
+                                            9224590179985903,
+                                            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3ODIxMDIyLCJpYXQiOjE2OTc4MTU2MjIsImp0aSI6ImRiYzM0YWQ1ZGY3NzRmODNiOTY1NTM0YWQ3MzViZDAzIiwidXNlcl9pZCI6Mn0.mFIDbef7BVkm0eWpPpsxSyU5P6qvOX3MM9gZhTWUuqI",
+                                            "lkimndhgdvsbrkjncnmc"
+                                        )
+
+        def __all_alphabets(self, user_key, user_id, secret_key, secret_id) :
+            if user_id != 9224590179985903 or \
+                user_key != "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3ODIxMDIyLCJpYXQiOjE2OTc4MTU2MjIsImp0aSI6ImRiYzM0YWQ1ZGY3NzRmODNiOTY1NTM0YWQ3MzViZDAzIiwidXNlcl9pZCI6Mn0.mFIDbef7BVkm0eWpPpsxSyU5P6qvOX3MM9gZhTWUuqI"\
+                    or secret_key != "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3ODIxMDIyLCJpYXQiOjE2OTc4MTU2MjIsImp0aSI6ImRiYzM0YWQ1ZGY3NzRmODNiOTY1NTM0YWQ3MzViZDAzIiwidXNlcl9pZCI6Mn0.mFIDbef7BVkm0eWpPpsxSyU5P6qvOX3MM9gZhTWUuqI"\
+                        or secret_id != "lkimndhgdvsbrkjncnmc" :
+                        raise ValueError("You are not authorized")
+        # ALL ALPHABETS       
+            def __sh_223227_Go1D(self, user_key, user_id, secret_key, secret_id) : 
+                if user_id != 9224590179985903 or \
+                    user_key != "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3ODIxMDIyLCJpYXQiOjE2OTc4MTU2MjIsImp0aSI6ImRiYzM0YWQ1ZGY3NzRmODNiOTY1NTM0YWQ3MzViZDAzIiwidXNlcl9pZCI6Mn0.mFIDbef7BVkm0eWpPpsxSyU5P6qvOX3MM9gZhTWUuqI"\
+                        or secret_key != "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3ODIxMDIyLCJpYXQiOjE2OTc4MTU2MjIsImp0aSI6ImRiYzM0YWQ1ZGY3NzRmODNiOTY1NTM0YWQ3MzViZDAzIiwidXNlcl9pZCI6Mn0.mFIDbef7BVkm0eWpPpsxSyU5P6qvOX3MM9gZhTWUuqI"\
+                            or secret_id != "lkimndhgdvsbrkjncnmc" :
+                            raise ValueError("You are not authorized")
+                A = [
+                        '    * *      ',
+                        '  *     *    ',
+                        ' *       *   ',
+                        '*         *  ',
+                        '* * * * * *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  '
+                    ]
+
+                B = [
+                        '* * * *     ',
+                        '*       *   ',
+                        '*        *  ',
+                        '*       *   ',
+                        '* * * *     ',
+                        '*       *   ',
+                        '*        *  ',
+                        '*       *   ',
+                        '* * * *     '
+                    ]
+
+                C = [
+                        '   * * *      ',
+                        ' *       *    ',
+                        '*         *   ',
+                        '*             ',
+                        '*             ',
+                        '*             ',
+                        '*         *   ',
+                        ' *       *    ',
+                        '   * * *      '
+                    ]
+
+                D = [
+                        '* * *        ',
+                        '*      *     ',
+                        '*        *   ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*        *   ',
+                        '*      *     ',
+                        '* * *        '
+                    ]
+
+                E = [
+                        '* * * * *   ',
+                        '*           ',
+                        '*           ',
+                        '*           ',
+                        '* * * * *   ',
+                        '*           ',
+                        '*           ',
+                        '*           ',
+                        '* * * * *   '
+                    ]
+
+                F = [
+                        '* * * * *   ',
+                        '*           ',
+                        '*           ',
+                        '*           ',
+                        '* * * * *   ',
+                        '*           ',
+                        '*           ',
+                        '*           ',
+                        '*           '
+                    ]
+
+                G = [
+                        '   * * *      ',
+                        ' *       *    ',
+                        '*         *   ',
+                        '*             ',
+                        '*             ',
+                        '*    * * * *  ',
+                        '*        * *  ',
+                        ' *       * *  ',
+                        '   * * *   *  '
+                    ]
+
+                H = [
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '* * * * * *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  '
+                    ]
+
+                I = [
+                        '* * * * *   ',
+                        '    *       ',
+                        '    *       ',
+                        '    *       ',
+                        '    *       ',
+                        '    *       ',
+                        '    *       ',
+                        '    *       ',
+                        '* * * * *   '
+                    ]
+
+                J = [
+                        ' * * * * *  ',
+                        '     *      ',
+                        '     *      ',
+                        '     *      ',
+                        '     *      ',
+                        '     *      ',
+                        '*    *      ',
+                        '  *  *      ',
+                        '     *      '
+                    ]
+
+                K = [
+                        '*         *  ',
+                        '*       *    ',
+                        '*     *      ',
+                        '*   *        ',
+                        '* *          ',
+                        '*   *        ',
+                        '*     *      ',
+                        '*       *    ',
+                        '*         *  '
+                    ]
+
+                L = [
+                        '*             ',
+                        '*             ',
+                        '*             ',
+                        '*             ',
+                        '*             ',
+                        '*             ',
+                        '*             ',
+                        '*             ',
+                        '* * * * * *   '
+                    ]
+
+                M = [
+                        '*         *  ',
+                        '* *     * *  ',
+                        '*  *   *  *  ',
+                        '*   * *   *  ',
+                        '*    *    *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  '
+                    ]
+
+                N = [
+                        '*         *  ',
+                        '* *       *  ',
+                        '*  *      *  ',
+                        '*   *     *  ',
+                        '*    *    *  ',
+                        '*     *   *  ',
+                        '*      *  *  ',
+                        '*       * *  ',
+                        '*         *  '
+                    ]
+
+                O = [
+                        '   * * *     ',
+                        ' *       *   ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        ' *       *   ',
+                        '   * * *     '
+                    ]
+
+                P = [
+                        '* * * *     ',
+                        '*       *   ',
+                        '*        *  ',
+                        '*       *   ',
+                        '* * * *     ',
+                        '*           ',
+                        '*           ',
+                        '*           ',
+                        '*           '
+                    ]
+
+                Q = [
+                        '   * * *     ',
+                        ' *       *   ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*      *  *  ',
+                        ' *       *   ',
+                        '   * * *   * '
+                    ]
+
+                R = [
+                        '* * * *     ',
+                        '*       *   ',
+                        '*        *  ',
+                        '*       *   ',
+                        '* * * *     ',
+                        '*  *        ',
+                        '*    *      ',
+                        '*      *    ',
+                        '*        *  '
+                    ]
+
+                S = [
+                        '   * * *     ',
+                        ' *       *   ',
+                        '*            ',
+                        ' *           ',
+                        '   * * *     ',
+                        '         *   ',
+                        '          *  ',
+                        ' *       *   ',
+                        '   * * *     '
+                    ]
+
+                T = [
+                        '* * * * * * *  ',
+                        '      *        ',
+                        '      *        ',
+                        '      *        ',
+                        '      *        ',
+                        '      *        ',
+                        '      *        ',
+                        '      *        ',
+                        '      *        '
+                    ]
+
+                U = [
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        ' *       *   ',
+                        '   * * *     '
+                    ]
+
+                V = [
+                        '*             *  ',
+                        '*             *  ',
+                        '*             *  ',
+                        '*             *  ',
+                        '*             *  ',
+                        '*             *  ',
+                        '  *         *    ',
+                        '    *     *      ',
+                        '       *         '
+                    ]
+
+                W = [
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*         *  ',
+                        '*    *    *  ',
+                        '*   * *   *  ',
+                        '*  *   *  *  ',
+                        '* *     * *  ',
+                        '*         *  '
+                    ]
+
+                X = [
+                        '*       *  ',
+                        ' *     *   ',
+                        '  *   *    ',
+                        '   * *     ',
+                        '    *      ',
+                        '   * *     ',
+                        '  *   *    ',
+                        ' *     *   ',
+                        '*       *  '
+                    ]
+
+                Y = [
+                        '*             *  ',
+                        '  *         *    ',
+                        '    *     *      ',
+                        '      * *        ',
+                        '       *         ',
+                        '       *         ',
+                        '       *         ',
+                        '       *         ',
+                        '       *         '
+                    ]
+
+                Z = [
+                        '* * * * * * * *  ',
+                        '             *   ',
+                        '           *     ',
+                        '         *       ',
+                        '       *         ',
+                        '     *           ',
+                        '   *             ',
+                        ' *               ',
+                        '* * * * * * * *  '
+                    ]
+
+                sp = [
+                        '     ',
+                        '     ',
+                        '     ',
+                        '     ',
+                        '     ',
+                        '     ',
+                        '     ',
+                        '     ',
+                        '     '
+                    ]
+
+                return {
+                        "A" : A, "B" : B, "C" : C, "D" : D,
+                        "E" : E, "F" : F, "G" : G, "H" : H,
+                        "I" : I, "J" : J, "K" : K, "L" : L,
+                        "M" : M, "N" : N, "O" : O, "P" : P,
+                        "Q" : Q, "R" : R, "S" : S, "T" : T,
+                        "U" : U, "V" : V, "W" : W, "X" : X,
+                        "Y" : Y, "Z" : Z, " " : sp
+                    }
+            return __sh_223227_Go1D
