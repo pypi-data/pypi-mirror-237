150cc7fa64891efdf2ec22cc042c2e020c661926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 05:06:14 +0000

    Add package osism/0.20231024.0/osism-0.20231024.0.tar.gz

---
+++ packages/osism/osism-0.20231024.0.tar.gz/osism-0.20231024.0/osism/commands/worker.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: Apache-2.0
+
+import subprocess
+
+from cliff.command import Command
+
+
+class Run(Command):
+    def get_parser(self, prog_name):
+        parser = super(Run, self).get_parser(prog_name)
+        parser.add_argument("type", nargs=1, type=str, help="Type of the worker")
+        return parser
+
+    def take_action(self, parsed_args):
+        queue = parsed_args.type[0]
+
+        if queue in ["openstack", "netbox", "conductor"]:
+            tasks = queue
+        else:
+            tasks = queue[:-8]
+
+        # NOTE: use python interface in the future, works for the moment
+        if tasks == "osism":
+            p = subprocess.Popen(
+                f"celery -A osism.tasks.ansible worker -n {queue} --loglevel=INFO -Q {queue}",
+                shell=True,
+            )
+        else:
+            p = subprocess.Popen(
+                f"celery -A osism.tasks.{tasks} worker -n {queue} --loglevel=INFO -Q {queue}",
+                shell=True,
+            )
+        p.wait()
