7eca2e51770de03a6c55fa3c8d1e6abade821334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 16:05:44 +0000

    Add package amal/0.1.0/amal-0.1.0.tar.gz

---
+++ packages/amal/amal-0.1.0.tar.gz/amal-0.1.0/amal/__init__.py
@@ -0,0 +1,5 @@
+# amal/__init__.py
+
+
+def about():
+    return "Hello! I'm Amal, a 24-year-old developer. I create cool stuff!"
---
+++ packages/amal/amal-0.1.0.tar.gz/amal-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="amal",
+    version="0.1.0",
+    packages=["amal"],
+    url="https://github.com/amalrajan/amal",
+    license="MIT",
+    author="Amal Rajan",
+    author_email="amalrajan@outlook.com",
+    description="About Amal Rajan",
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
