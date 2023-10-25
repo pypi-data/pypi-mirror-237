5f2fe1fe3a1c5034667787cbfb3b5b57f582c3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-25 04:07:15 +0000

    Add package hotsos/1.16.1.post32/hotsos-1.16.1.post32.tar.gz

---
+++ packages/hotsos/hotsos-1.16.1.post32.tar.gz/hotsos-1.16.1.post32/hotsos/core/utils.py
@@ -0,0 +1,64 @@
+import tempfile
+
+from hotsos.core.config import HotSOSConfig
+
+
+def sorted_dict(d, key=None, reverse=False):
+    """
+    Return dictionary sorted using key. If no key provided sorted by dict keys.
+    """
+    if key is None:
+        return dict(sorted(d.items(), key=lambda e: e[0], reverse=reverse))
+
+    return dict(sorted(d.items(), key=key, reverse=reverse))
+
+
+def mktemp_dump(data, prefix=None):
+    """Create a temporary file under the current plugin tmp directory and write
+    data to the file.
+    """
+    if prefix is not None:
+        ftmp = tempfile.mktemp(prefix=prefix, dir=HotSOSConfig.plugin_tmp_dir)
+    else:
+        ftmp = tempfile.mktemp(dir=HotSOSConfig.plugin_tmp_dir)
+
+    with open(ftmp, 'w') as fd:
+        fd.write(data)
+
+    return ftmp
+
+
+def seconds_to_date(secs):
+    days = secs / 86400
+    hours = secs / 3600 % 24
+    mins = secs / 60 % 60
+    secs = secs % 60
+    return '{}d:{}h:{}m:{}s'.format(int(days), int(hours),
+                                    int(mins), int(secs))
+
+
+def sample_set_regressions(samples, ascending=True):
+    """
+    Takes a sample set and determines the number of times the sequence
+    regresses i.e. values go the opposite direction than is expected.
+
+    The default is to expect values to be increasing but this can be reversed
+    by setting ascending=False.
+    """
+    if ascending:
+        prev_reset = min(samples)
+    else:
+        prev_reset = max(samples)
+
+    prev = prev_reset
+    repetitions = 0
+    for s in samples:
+        if ascending and s >= prev:
+            prev = s
+        elif not ascending and s <= prev:
+            prev = s
+        else:
+            repetitions += 1
+            prev = prev_reset
+
+    return repetitions
