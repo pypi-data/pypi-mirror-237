2eb38907ab23650a6cb78769e6a708cc9904766a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:09:02 +0000

    Add package astra-vector/2.15.0/astra-vector-2.15.0.tar.gz

---
+++ packages/astra-vector/astra-vector-2.15.0.tar.gz/astra-vector-2.15.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+
+
+setup(name="astra-vector",
+      version='2.15.0',
+      description='test',
+      author='test',
+      license='MIT',
+      zip_safe=False
+)
+
