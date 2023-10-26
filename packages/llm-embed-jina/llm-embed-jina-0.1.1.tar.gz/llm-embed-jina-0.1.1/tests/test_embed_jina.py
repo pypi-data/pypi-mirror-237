bf5643c2e90a9d822295a7a9ae47b47503754cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 15:07:35 +0000

    Add package llm-embed-jina/0.1.1/llm-embed-jina-0.1.1.tar.gz

---
+++ packages/llm-embed-jina/llm-embed-jina-0.1.1.tar.gz/llm-embed-jina-0.1.1/tests/test_embed_jina.py
@@ -0,0 +1,30 @@
+from click.testing import CliRunner
+from llm.cli import cli
+import llm
+
+
+def test_jina_embed_small():
+    model = llm.get_embedding_model("jina-embeddings-v2-small-en")
+    floats = model.embed("hello world")
+    assert len(floats) == 512
+    assert all(isinstance(f, float) for f in floats)
+
+
+def test_jina_embed_multi(tmpdir):
+    db_path = str(tmpdir / "test.db")
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            "embed-multi",
+            "-m",
+            "jina-embeddings-v2-small-en",
+            "test",
+            "-",
+            "-d",
+            db_path,
+        ],
+        input='[{"id": "a", "text": "abc"}]',
+        catch_exceptions=False,
+    )
+    assert result.exit_code == 0
