cefc26c2b71001f45bc29d5fd1292f22679e2b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 15:07:29 +0000

    Add package dbcontroller/0.1.56/dbcontroller-0.1.56.tar.gz

---
+++ packages/dbcontroller/dbcontroller-0.1.56.tar.gz/dbcontroller-0.1.56/src/dbcontroller/manager/mongo.py
@@ -0,0 +1,274 @@
+"""
+    Mongo Manager
+"""
+
+# import functools
+import math
+
+from .utils import Objects  # clean_form, clean_update_form,
+from .utils import Decode, Response, fixed_id_column, pagination
+from .utils.mongo_where import BinaryExpression as MongoWhere
+
+
+class MongoCrud:
+    """
+    Mongo Manager (CORE)
+    """
+
+    def __init__(self, client=None):
+        self.collection = client
+
+    async def create(self, form: dict | list) -> Response:
+        """CREATE/CREATE-MANY"""
+        if isinstance(form, dict):
+            return await self.create_one(form)
+        return await self.create_many(form)
+
+    async def create_one(self, form: dict) -> Response:
+        """CREATE"""
+        collection = self.collection
+        try:
+            result = await collection.insert_one(form)
+            instance = await collection.find_one({"_id": {"$eq": result.inserted_id}})
+            result = Response(data=Objects.mongo(instance))
+        except Exception as error:
+            result = Response(error=True, error_message=str(error))
+        return result
+
+    async def create_many(self, forms: list) -> Response:
+        """CREATE-MANY"""
+        collection = self.collection
+        try:
+            result = await collection.insert_many(forms)
+            search = {"_id": {"$in": result.inserted_ids}}
+            cursor = collection.find(search)
+            items = [i async for i in cursor]
+            result = Response(data=Objects.mongo(items), count=len(items))
+        except Exception as error:
+            result = Response(error=True, error_message=str(error))
+        return result
+
+    async def update(self, search: dict = None, form: dict = None) -> Response:
+        """UPDATE"""
+        collection = self.collection
+        try:
+            instance = await collection.update_many(search, {"$set": form})
+            count = instance.modified_count
+            result = Response(count=count)
+        except Exception as error:
+            result = Response(error=True, error_message=str(error))
+        return result
+
+    async def delete(self, search: dict) -> Response:
+        """DELETE"""
+        collection = self.collection
+        try:
+            instance = await collection.delete_many(search)
+            count = instance.deleted_count
+            result = Response(count=count)
+        except Exception as error:
+            result = Response(error=True, error_message=str(error))
+        return result
+
+    async def find_one(self, search: dict) -> Response:
+        """DETAIL"""
+        collection = self.collection
+        try:
+            instance = await collection.find_one(search)
+            result = Objects.mongo(instance)
+        except Exception:
+            result = None
+        return result
+
+    async def find(
+        self,
+        search: dict = None,
+        page: int | None = None,
+        limit: int | None = None,
+        sort_by: str = "-id",
+    ) -> Response:
+        """FIND"""
+        collection = self.collection
+        sort_desc = 1
+        # Check Sort By
+        if sort_by.startswith("-"):
+            sort_by = sort_by[1:]
+            sort_desc = -1
+        if sort_by == "id":
+            sort_by = "_id"
+        try:
+            # Add Sort By
+            cursor = collection.find(search).sort(sort_by, sort_desc)
+            # Offset & Limit
+            if page and limit:
+                _page = pagination(page=page, limit=limit)
+                cursor.skip(_page.offset).limit(_page.limit)
+            items = [i async for i in cursor]
+            count = await collection.count_documents(search)
+            pages = int(math.ceil(count / limit))
+            result = Response(data=Objects.mongo(items), count=count, pages=pages)
+        except Exception as error:
+            result = Response(error=True, error_message=str(error))
+        return result
+
+
+    async def find_all(
+        self,
+        search: dict = None,
+        sort_by: str = "-id",
+    ) -> Response:
+        """FIND-ALL"""
+        collection = self.collection
+        sort_desc = 1
+        # Check Sort By
+        if sort_by.startswith("-"):
+            sort_by = sort_by[1:]
+            sort_desc = -1
+        if sort_by == "id":
+            sort_by = "_id"
+        try:
+            # Add Sort By
+            cursor = collection.find(search).sort(sort_by, sort_desc)
+            items = [i async for i in cursor]
+            result = Response(data=Objects.mongo(items), count=len(items), pages=1)
+        except Exception as error:
+            result = Response(error=True, error_message=str(error))
+        return result
+
+    async def all(self):
+        """ALL-Rows"""
+        collection = self.collection
+        cursor = collection.find()
+        items = [i async for i in cursor]
+        return Response(data=Objects.mongo(items), count=len(items), pages=1)
+
+
+class Mongo:
+    """
+    Mongo Manager (CRUD)
+    """
+
+    def __init__(self, objects=None, database=None, keys=None):
+        self.collection = database
+        self.crud = MongoCrud(objects)
+        self.columns = keys
+        self.where = MongoWhere
+
+    @staticmethod
+    def id_decode(unique_id):
+        """ID-DECODER"""
+        return Decode.mongo(unique_id)
+
+    async def create(self, form: dict):
+        """Create Single-Row."""
+        return await self.crud.create(form)
+
+    async def update(self, unique_ids: list[str], form: dict, ignore_return: bool = False):
+        """Update Multiple/Single-Row(s)"""
+        if not isinstance(unique_ids, list):
+            unique_ids = [unique_ids]
+        _ids = [Decode.mongo(i) for i in unique_ids]
+        search = {"_id": {"$in": _ids}}
+        results = await self.crud.update(search, form)
+        if results.count == 1 and not results.error and not ignore_return:
+            results.data = await self.detail(unique_ids[0])
+        return results
+
+    async def delete(self, unique_ids: list[str], all: bool = False):
+        """Delete Multiple/Single-Row(s)"""
+        if not all:
+            if not isinstance(unique_ids, list):
+                unique_ids = [unique_ids]
+            _id = [Decode.mongo(i) for i in unique_ids]
+            search = {"_id": {"$in": _id}}
+        else:
+            search = {}
+        return await self.crud.delete(search)
+
+    async def detail(self, unique_id: str):
+        """Get Single-Row from Database Collection by ID"""
+        _id = Decode.mongo(unique_id)
+        search = {"_id": {"$eq": _id}}
+        return await self.crud.find_one(search)
+
+    async def get_by(self, **kwargs):
+        """Get Single-Row by <Keyword-Arguments>"""
+        kwargs = fixed_id_column(kwargs)
+        search = {key: {"$eq": val} for key, val in kwargs.items()}
+        return await self.crud.find_one(search)
+
+    async def find_one(self, search: dict = None):
+        """Get Single-Row from Database Collection"""
+        if isinstance(search, MongoWhere):
+            search = search.query
+        return await self.crud.find_one(search)
+
+    async def all(self):
+        """Get All-Rows from Database"""
+        return await self.crud.all()
+
+    async def find(
+        self, search: dict = None, page: int = 1, limit: int = 100, sort_by: str = "-id"
+    ):
+        """Get Multiple-Rows from Database Collection"""
+        sort_by = fixed_id_column(sort_by)
+        if isinstance(search, MongoWhere):
+            search = search.query
+        return await self.crud.find(
+            search=search, page=page, limit=limit, sort_by=sort_by
+        )
+
+    async def find_all(
+        self, search: dict = None, sort_by: str = "-id"
+    ):
+        """Get Multiple-Rows from Database Collection"""
+        sort_by = fixed_id_column(sort_by)
+        if isinstance(search, MongoWhere):
+            search = search.query
+        return await self.crud.find_all(
+            search=search, sort_by=sort_by
+        )
+
+    async def filter_by(
+        self, search: dict = None, page: int = 1, limit: int = 100, sort_by: str = "-id"
+    ):
+        """Get Multiple-Rows from Database Collection by <Keyword-Arguments>"""
+        search = fixed_id_column(search)
+        query = {key: {"$eq": val} for key, val in search.items()}
+        return await self.find(search=query, page=page, limit=limit, sort_by=sort_by)
+
+    async def search(
+        self,
+        columns: list | None = None,
+        value: str | None = None,
+        page: int = 1,
+        limit: int = 100,
+        sort_by: str = "-id",
+    ):
+        """Get Multiple-Rows from Database Collection by <Searching-Columns>"""
+        search = [{key: {"$regex": value}} for key in columns]
+        return await self.find({"$or": search}, page=page, limit=limit, sort_by=sort_by)
+
+    def query_list(self, data: list | None = None):
+        """Array of Mongo.where(s)"""
+        query = None
+        operator = None
+
+        for item in data:
+            if isinstance(item, list):
+                column = item[0]
+                op = item[1]
+                value = item[2]
+                expression = self.where(column, op, value)
+                if query is None:
+                    query = expression
+                else:
+                    if operator == "and":
+                        query = query & expression
+                    elif operator == "or":
+                        query = query | expression
+            elif item == "and":
+                operator = "and"
+            elif item == "or":
+                operator = "or"
+        return query
---
+++ packages/dbcontroller/dbcontroller-0.1.56.tar.gz/dbcontroller-0.1.56/tests/test_mongo.py
@@ -0,0 +1,170 @@
+"""Test - Library
+"""
+
+import asyncio
+import os
+import pathlib
+import sys
+
+import pytest
+
+
+def dir_up(depth):
+    """Easy level-up folder(s)."""
+    return sys.path.append(os.path.join(pathlib.Path(__file__).parents[depth], "src"))
+
+
+# Append to (sys.path)
+dir_up(1)
+
+# Test
+import dbcontroller as dbc
+
+# Config
+mongo = dbc.Controller(mongo="mongodb://localhost:63445/test_database")
+
+# Types
+@mongo.model(table_name="main_user")
+class UserMongo:
+    name: str
+    notes: dbc.text
+    meta: dbc.json
+    disabled: bool = False
+
+
+# Init Objects
+dbc.load([UserMongo])
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+
+
+class ID:
+    @classmethod
+    async def one(cls):
+        results = await UserMongo.objects.all()
+        return results.data[0].id
+
+
+@pytest.mark.asyncio
+async def test_create():
+    results = await UserMongo.objects.create({"name": "jane doll"})
+    assert results.error == False and results.data.name == "jane doll"
+
+
+@pytest.mark.asyncio
+async def test_update():
+    selector = await ID.one()
+    form = {
+        "name": "joe doe",
+    }
+    results = await UserMongo.objects.update(selector, form)
+    assert results.error == False and results.data.name == "joe doe"
+
+
+@pytest.mark.asyncio
+async def test_detail():
+    selector = await ID.one()
+    results = await UserMongo.objects.detail(selector)
+    assert results.name == "joe doe"
+
+
+@pytest.mark.asyncio
+async def test_get_by():
+    results = await UserMongo.objects.get_by(name="joe doe")
+    assert results.name == "joe doe"
+
+
+@pytest.mark.asyncio
+async def test_find_one():
+    query = {"name": {"$regex": "joe"}}
+    results = await UserMongo.objects.find_one(query)
+    assert results.name == "joe doe"
+
+
+@pytest.mark.asyncio
+async def test_delete():
+    selector = await ID.one()
+    results = await UserMongo.objects.delete(selector)
+    assert results.error == False and results.count == 1
+
+
+@pytest.mark.asyncio
+async def test_all():
+    await UserMongo.objects.create([{"name": "joe doe"}, {"name": "jane doll"}])
+    results = await UserMongo.objects.all()
+    assert (
+        results.error == False
+        and isinstance(results.data, list) == True
+        and len(results.data) == 2
+    )
+
+
+@pytest.mark.asyncio
+async def test_filter_by():
+    query = {"name": "joe doe"}
+    results = await UserMongo.objects.filter_by(
+        search=query, page=1, limit=100, sort_by="-id"
+    )
+    assert results.error == False and results.count == 1
+
+
+@pytest.mark.asyncio
+async def test_find():
+    search = [{"name": {"$regex": "joe"}}, {"name": {"$regex": "jane"}}]
+    query = {"$or": search}
+    results = await UserMongo.objects.find(query, page=1, limit=100, sort_by="-id")
+    assert results.error == False and results.count == 2
+
+
+@pytest.mark.asyncio
+async def test_search():
+    search_value = "j"
+    columns = ["name", "notes"]
+    results = await UserMongo.objects.search(
+        columns=columns, value=search_value, page=1, limit=100, sort_by="-id"
+    )
+    assert results.error == False and results.count == 2
+
+
+@pytest.mark.asyncio
+async def test_query_list():
+    query = UserMongo.objects.query_list(
+        [["name", "contains", "jane"], "or", ["name", "contains", "joe"]]
+    )
+    results = await UserMongo.objects.find(query, page=1, limit=100, sort_by="-id")
+    assert results.error == False and results.count == 2
+
+@pytest.mark.asyncio
+async def test_find_all():
+    query = UserMongo.objects.query_list(
+        [["name", "contains", "jane"], "or", ["name", "contains", "joe"]]
+    )
+    results = await UserMongo.objects.find_all(query, sort_by="-id")
+    assert results.error == False and results.count == 2
+
+@pytest.mark.asyncio
+async def test_reset():
+    results = await UserMongo.objects.delete(None, all=True)
+    assert results.error == False
+
+
+@pytest.mark.asyncio
+async def demo_example():
+    selector = await ID.one()
+    form = {
+        "name": "joe doe",
+    }
+    # results = await UserMongo.objects.update(selector, form)
+    results = await UserMongo.objects.update(selector, form)
+    print(results)
+
+
+# asyncio.run(demo_example())
