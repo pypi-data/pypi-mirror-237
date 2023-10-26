394d92cdaf57ce76e6fe80a757d046456cfc9e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 06:07:41 +0000

    Add package thds/0.0.1/thds-0.0.1.tar.gz

---
+++ packages/thds/thds-0.0.1.tar.gz/thds-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="thds",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="https://www.trillianthealth.com",
+    author="Trilliant Health Data Science",
+    author_email="datascience@trillianthealth.com",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"],
+)
