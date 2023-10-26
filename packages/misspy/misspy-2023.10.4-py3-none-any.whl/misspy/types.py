5a269ce48ee0d4bb4f3285934e619185f1c87903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 15:09:13 +0000

    Add package misspy/2023.10.4/misspy-2023.10.4-py3-none-any.whl

---
+++ packages/misspy/misspy-2023.10.4-py3-none-any.whl/misspy/types.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+class poll:
+    def __init__(
+        self,
+        choices: list,
+        multiple: bool = False,
+        expiresAt: int = None,
+        expiredAfter: int = None,
+    ):
+        self.choices = choices
+        self.multiple = multiple
+        self.expiresAt = expiresAt
+        self.expiredAfter = expiredAfter
