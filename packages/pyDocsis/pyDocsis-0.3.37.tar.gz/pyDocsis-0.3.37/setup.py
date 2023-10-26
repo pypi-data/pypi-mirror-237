f0fce5253135fe8fe92b52839f0ae71bd2e73ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 14:09:51 +0000

    Add package pyDocsis/0.3.37/pyDocsis-0.3.37.tar.gz

---
+++ packages/pyDocsis/pyDocsis-0.3.37.tar.gz/pyDocsis-0.3.37/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='pyDocsis',
+    version='0.3.37',
+    packages=['pydocsis'],
+    url='https://github.com/botheredtodd/pyDocsis',
+    license='Apache License 2.0',
+    author='Todd Balsley',
+    author_email='todd.balsley@cox.com',
+    description='A library for working with docsis files',
+    install_requires=[
+
+    ],
+    # python_requires='>=3.10',
+    scripts=[
+        'Scripts/cm_decode',
+        'Scripts/mib-json-builder.py',
+    ]
+)
+
