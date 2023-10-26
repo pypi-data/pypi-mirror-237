fe6b21536c27374e7c2a049e08358b72aab2d7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 16:07:24 +0000

    Add package krupy/0.0.1/krupy-0.0.1-py3-none-any.whl

---
+++ packages/krupy/krupy-0.0.1-py3-none-any.whl/krupy/__init__.py
@@ -0,0 +1,9 @@
+"""Krupy.
+
+Docs: https://krupy.readthedocs.io/
+"""
+
+from .main import *  # noqa: F401,F403
+
+# This version is a placeholder autoupdated by poetry-dynamic-versioning
+__version__ = "0.0.0"
