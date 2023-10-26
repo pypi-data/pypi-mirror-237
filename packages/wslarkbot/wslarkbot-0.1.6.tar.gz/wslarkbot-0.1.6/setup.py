27d3e6f2833e8195a6a9bd0277cce5ac548c5753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:07:12 +0000

    Add package wslarkbot/0.1.6/wslarkbot-0.1.6.tar.gz

---
+++ packages/wslarkbot/wslarkbot-0.1.6.tar.gz/wslarkbot-0.1.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+VERSION = '0.1.6'
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name='wslarkbot',  # package name
+    version=VERSION,  # package version
+    description='lark(feishu) websocket client',  # package description
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    project_urls={
+        "Documentation": "https://www.connectai-e.com",
+        "Code": "http://github.com/connectAI-E/Feishu-Webhook-Proxy",
+        "Issue tracker": "http://github.com/connectAI-E/Feishu-Webhook-Proxy/issues",
+    },
+    author="lloydzhou@gmail..com",
+    license="BSD",
+    keywords=["Feishu", "Lark", "Webhook", "Websocket", "Bot"],
+    packages=find_packages(),
+    zip_safe=False,
+)
