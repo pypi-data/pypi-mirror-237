a5785ad5108e8096fcded4a08afc2d4cc0d1176d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:08:14 +0000

    Add package mep-tools/1.1.24/mep_tools-1.1.24.tar.gz

---
+++ packages/mep-tools/mep_tools-1.1.24.tar.gz/mep_tools-1.1.24/setup.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+
+# Function to discover and collect .pyi files
+def discover_pyi_files(root_package):
+    pyi_files = []
+    for package_name in find_packages(root_package):
+        package_path = root_package + '/' + package_name.replace('.', '/')
+        pyi_files.extend([str(p) for p in Path(package_path).rglob('*.pyi')])
+    return pyi_files
+
+
+setup(
+    name="mep_tools",
+    version="1.1.24",
+    author="Khai",
+    author_email="sarraj@marksmen.nl",
+    license="MIT",
+    packages=find_packages(),
+    package_data={'': discover_pyi_files('mep_tools')},
+    description="grpc for rep",
+    long_description="hold the code for the grpc files",
+    install_requires=requirements,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+)
