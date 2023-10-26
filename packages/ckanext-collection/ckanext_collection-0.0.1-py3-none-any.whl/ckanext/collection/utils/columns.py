eabe3b577668f95adc2e9a777bd1f42591529df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:13:32 +0000

    Add package ckanext-collection/0.0.1/ckanext_collection-0.0.1-py3-none-any.whl

---
+++ packages/ckanext-collection/ckanext_collection-0.0.1-py3-none-any.whl/ckanext/collection/utils/columns.py
@@ -0,0 +1,47 @@
+
+from __future__ import annotations
+import dataclasses
+from typing import Any
+from ckanext.collection.types import TDataCollection
+from .shared import AttachTrait, AttrSettingsTrait
+
+
+class Columns(AttachTrait[TDataCollection], AttrSettingsTrait):
+    """Collection of columns details for filtering/rendering.
+
+    Attributes:
+      names: list of all available columns
+      visible: columns that can be viewed
+      sortable: columns that can be sorted
+      labels: UI labels for columns
+    """
+
+    names: list[str] = dataclasses.field(default_factory=list)
+    visible: set[str] = dataclasses.field(default_factory=set)
+    sortable: set[str] = dataclasses.field(default_factory=set)
+    filterable: set[str] = dataclasses.field(default_factory=set)
+    labels: dict[str, str] = dataclasses.field(default_factory=dict)
+
+    def __init__(
+        self,
+        collection: TDataCollection,
+        names: list[str] | None = None,
+        visible: set[str] | None = None,
+        hidden: set[str] | None = None,
+        sortable: set[str] | None = None,
+        filterable: set[str] | None = None,
+        labels: dict[str, str] | None = None,
+        **kwargs: Any,
+    ):
+        self.attach(collection)
+        self.gather_settings(kwargs)
+        self.names = names if names is not None else []
+
+        self.visible = visible if visible is not None else set(self.names)
+        if hidden is not None:
+            self.visible = {c for c in self.visible if c not in hidden}
+
+        self.sortable = sortable if sortable is not None else set(self.names)
+        self.filterable = filterable if filterable is not None else set(self.names)
+
+        self.labels = labels if labels is not None else {c: c for c in self.names}
---
+++ packages/ckanext-collection/ckanext_collection-0.0.1-py3-none-any.whl/ckanext/collection/utils/filter.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+from typing import Any, TypedDict
+from ckanext.collection.types import TDataCollection
+from .shared import AttachTrait, AttrSettingsTrait
+
+
+class Option(TypedDict):
+    """Single option for Filter."""
+
+    value: str
+    label: str
+
+
+class Filter(TypedDict):
+    """Dropdown filter."""
+
+    name: str
+    placeholder: str | None
+    options: list[Option]
+
+
+class DateRange(TypedDict):
+    """After/before datepicker."""
+
+    name: str
+    label: str
+
+
+class Filters(AttachTrait[TDataCollection], AttrSettingsTrait):
+    """Information about UI filters.
+
+    Redefine its methods to produce information for UI filters, date-range
+    field and action buttions(Download, Export, etc.).
+
+    """
+
+    dropdowns: list[Filter]
+    date_range: DateRange | None
+    actions: list[Any]
+
+    _collection: TDataCollection
+
+    def __init__(self, obj: TDataCollection, /, **kwargs: Any):
+        self.attach(obj)
+        self.gather_settings(kwargs)
+
+        self.date_range = kwargs.get("date_range", self.make_date_range())
+        self.dropdowns = kwargs.get("dropdowns", self.make_dropdowns())
+        self.actions = kwargs.get("actions", self.make_actions())
+
+    def make_date_range(self) -> None | DateRange:
+        return
+        # return DateRange(name="date_range", label="Date Range")
+
+    def make_actions(self) -> list[Option]:
+        return []
+
+    def make_dropdowns(self) -> list[Filter]:
+        return []
