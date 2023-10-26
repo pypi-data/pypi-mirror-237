306d972804c657d22dc89b9a19c41f81f2436d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:07:23 +0000

    Add package dbt-iris/1.5.7/dbt-iris-1.5.7.tar.gz

---
+++ packages/dbt-iris/dbt-iris-1.5.7.tar.gz/dbt-iris-1.5.7/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from setuptools import find_namespace_packages, setup
+import os
+
+package_name = "dbt-iris"
+package_version = "1.5.7"
+description = """The InterSystems IRIS adapter plugin for dbt"""
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + "/requirements.txt"
+
+requirements = []
+if os.path.isfile(requirementPath):
+    with open("./requirements.txt") as f:
+        for line in f.read().splitlines():
+            requirements.append(line)
+
+setup(
+    name=package_name,
+    version=package_version,
+    description=description,
+    long_description=description,
+    license="MIT",
+    author="CaretDev",
+    author_email="info@caretdev.com",
+    url="https://github.com/caretdev/dbt-iris",
+    packages=find_namespace_packages(
+        include=["dbt", "dbt.*", "iris", "intersystems_iris", "intersystems_iris.*", "irisnative"]
+    ),
+    include_package_data=True,
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">3.7,<3.12",
+)
