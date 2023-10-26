63bbd34e6e0d71173e51dcaef6f268a23b954672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:09:14 +0000

    Add package api-analytics/1.1.6/api_analytics-1.1.6-py3-none-any.whl

---
+++ packages/api-analytics/api_analytics-1.1.6-py3-none-any.whl/api_analytics/django.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from time import time
+from typing import Callable
+
+from api_analytics.core import log_request
+from django import conf
+from django.core.handlers.wsgi import WSGIRequest
+from django.http.response import HttpResponse
+
+
+class Analytics:
+    def __init__(self, get_response: Callable[[WSGIRequest], HttpResponse]):
+        self.get_response = get_response
+        self.api_key = getattr(conf.settings, "ANALYTICS_API_KEY", None)
+
+    def __call__(self, request: WSGIRequest) -> HttpResponse:
+        start = time()
+        response = self.get_response(request)
+
+        request_data = {
+            'hostname': request.get_host(),
+            'ip_address': request.META.get('REMOTE_ADDR'),
+            'path': request.path,
+            'user_agent': request.headers['user-agent'],
+            'method': request.method,
+            'status': response.status_code,
+            'response_time': int((time() - start) * 1000),
+            'created_at': datetime.now().isoformat()
+        }
+
+        log_request(self.api_key, request_data, 'Django')
+        return response
---
+++ packages/api-analytics/api_analytics-1.1.6-py3-none-any.whl/api_analytics/tornado.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from time import time
+
+from api_analytics.core import log_request
+from tornado.httputil import HTTPServerRequest
+from tornado.web import Application, RequestHandler
+
+
+class Analytics(RequestHandler):
+    def __init__(self, app: Application, res: HTTPServerRequest, api_key: str):
+        super().__init__(app, res)
+        self.api_key = api_key
+        self.start = time()
+
+    def prepare(self):
+        self.start = time()
+
+    def on_finish(self):
+        request_data = {
+            'hostname': self.request.host,
+            'ip_address': self.request.remote_ip,
+            'path': self.request.path,
+            'user_agent': self.request.headers['user-agent'],
+            'method': self.request.method,
+            'status': self.get_status(),
+            'response_time': int((time() - self.start) * 1000),
+            'created_at': datetime.now().isoformat()
+        }
+
+        log_request(self.api_key, request_data, 'Tornado')
+        self.start = None
