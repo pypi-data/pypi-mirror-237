b629a99e377512804d07e2ce25d549db458a6c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:10:50 +0000

    Add package sflkit/0.2.14/sflkit-0.2.14-py3-none-any.whl

---
+++ packages/sflkit/sflkit-0.2.14-py3-none-any.whl/sflkit/instrumentation/__init__.py
@@ -0,0 +1,25 @@
+import json
+from abc import abstractmethod
+from typing import List
+
+from sflkitlib.events import event
+from sflkit.language.visitor import ASTVisitor
+
+
+class Instrumentation:
+    def __init__(self, visitor: ASTVisitor):
+        self.visitor = visitor
+        self.events = list()
+
+    @abstractmethod
+    def instrument(
+        self, src: str, dst: str, suffixes: List[str] = None, file: str = ""
+    ):
+        raise NotImplementedError()
+
+    def dump_events(self, out_file):
+        with open(out_file, "w") as fp:
+            json.dump(self.events, fp, cls=event.EventEncoder)
+
+
+__all__ = ["dir_instrumentation", "file_instrumentation", "Instrumentation"]
