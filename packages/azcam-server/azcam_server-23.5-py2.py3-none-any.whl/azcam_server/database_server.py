36a0058f2d5ebd7a8a63c2ce01b843393a31c6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 14:08:56 +0000

    Add package azcam-server/23.5/azcam_server-23.5-py2.py3-none-any.whl

---
+++ packages/azcam-server/azcam_server-23.5-py2.py3-none-any.whl/azcam_server/database_server.py
@@ -0,0 +1,27 @@
+"""
+Contains the azcam database class for azcamserver.
+
+There is only one instance of this class which is referenced as `azcam.db` and contains
+temporary data for this current process.
+"""
+
+from azcam.database import AzcamDatabase
+from azcam_server.parameters_server import ParametersServer
+from azcam.logger import Logger
+from azcam_server.cmdserver import CommandServer
+from azcam_server.webserver.fastapi_server import WebServer
+
+
+class AzcamDatabaseServer(AzcamDatabase):
+    """
+    The azcam database class.
+    """
+
+    parameters: ParametersServer
+    """parameters object"""
+
+    cmdserver: CommandServer
+    """command server object"""
+
+    webserver: WebServer
+    """webserver object"""
