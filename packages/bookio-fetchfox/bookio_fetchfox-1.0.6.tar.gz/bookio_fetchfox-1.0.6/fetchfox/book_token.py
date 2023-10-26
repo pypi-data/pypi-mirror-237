0f8608d5975ca9cfbc4168ae12fb1148671708eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:07:16 +0000

    Add package bookio-fetchfox/1.0.6/bookio_fetchfox-1.0.6.tar.gz

---
+++ packages/bookio-fetchfox/bookio_fetchfox-1.0.6.tar.gz/bookio_fetchfox-1.0.6/fetchfox/book_token.py
@@ -0,0 +1,28 @@
+from fetchfox.apis import coingeckocom
+from fetchfox.apis.cardano import muesliswap
+from fetchfox.constants.book import (
+    BOOK_TOKEN_ASSET_ID,
+    BOOK_TOKEN_ASSET_NAME,
+    BOOK_TOKEN_POLICY_ID,
+    BOOK_TOKEN_FINGERPRINT,
+    BOOK_TOKEN_SYMBOL,
+)
+symbol = BOOK_TOKEN_SYMBOL
+policy_id = BOOK_TOKEN_POLICY_ID
+asset_name = BOOK_TOKEN_ASSET_NAME
+asset_id = BOOK_TOKEN_ASSET_ID
+fingetprint = BOOK_TOKEN_FINGERPRINT
+
+cardanoscan_url = f"https://cardanoscan.io/token/{asset_id}"
+cexplorer_url = f"https://cexplorer.io/asset/{fingetprint}"
+minswap_url = f"https://app.minswap.org/swap?currencySymbolA={policy_id}&tokenNameA={asset_name}&currencySymbolB=&tokenNameB="
+
+
+def ada() -> float:
+    return muesliswap.price(asset_id)
+
+
+def usd() -> float:
+    from fetchfox.constants.currencies import ADA
+
+    return ada() * coingeckocom.usd(ADA)
---
+++ packages/bookio-fetchfox/bookio_fetchfox-1.0.6.tar.gz/bookio_fetchfox-1.0.6/fetchfox/constants/currencies.py
@@ -0,0 +1,15 @@
+from fetchfox.book_token import BOOK_TOKEN_SYMBOL
+
+ALGO = "ALGO"
+ADA = "ADA"
+ETH = "ETH"
+MATIC = "MATIC"
+
+
+CURRENCIES = [
+    ALGO,
+    ADA,
+    BOOK_TOKEN_SYMBOL,
+    ETH,
+    MATIC,
+]
