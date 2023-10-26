96dc3f0321dc29baa2e886f0858dde1526479746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:13:55 +0000

    Add package lyyddforward/0.9/lyyddforward-0.9.tar.gz

---
+++ packages/lyyddforward/lyyddforward-0.9.tar.gz/lyyddforward-0.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='lyyddforward',
+    version='0.9',
+    author='lyy',
+    author_email='',
+    description='lyyddforwardtools for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+        'psutil',
+    ],
+)
