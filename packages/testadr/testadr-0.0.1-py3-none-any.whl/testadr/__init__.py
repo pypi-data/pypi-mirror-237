b2b31be05a32ba3426be4e8f9ce44ef275d85c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 15:07:18 +0000

    Add package testadr/0.0.1/testadr-0.0.1-py3-none-any.whl

---
+++ packages/testadr/testadr-0.0.1-py3-none-any.whl/testadr/__init__.py
@@ -0,0 +1,11 @@
+from .case import TestCase
+from .page import Page
+from .running.runner import main
+from .core.android import *
+from .utils.config import config
+from .utils.pytest_util import *
+from .utils.allure_util import *
+
+
+__version__ = "0.0.1"
+__description__ = "安卓平台自动化测试框架"
