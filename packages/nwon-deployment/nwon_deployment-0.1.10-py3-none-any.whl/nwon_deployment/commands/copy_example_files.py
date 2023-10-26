949fc6715713c5b42954e7a294737f8d8833c746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:10:28 +0000

    Add package nwon-deployment/0.1.10/nwon_deployment-0.1.10-py3-none-any.whl

---
+++ packages/nwon-deployment/nwon_deployment-0.1.10-py3-none-any.whl/nwon_deployment/commands/copy_example_files.py
@@ -0,0 +1,19 @@
+from os.path import basename, dirname, exists, join
+from shutil import copy
+from typing import List
+
+from nwon_deployment.print_output import print_output
+
+
+def copy_example_files(example_files: List[str]):
+    for file in example_files:
+        if exists(file):
+            name = basename(file).replace(".example", "")
+            target = join(dirname(file), name)
+
+            copy(name, target)
+
+            print_output(f"Copied {file} to {target}")
+
+
+__all__ = ["copy_example_files"]
