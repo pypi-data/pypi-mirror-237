8c667ba57a31bf9328c4a1f5dc7c8ae666ecfe96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:11:09 +0000

    Add package logos-shift-client/0.8.0/logos-shift-client-0.8.0.tar.gz

---
+++ packages/logos-shift-client/logos-shift-client-0.8.0.tar.gz/logos-shift-client-0.8.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="logos-shift-client",
+    version="0.8.0",
+    author="Saurabh Bhatnagar",
+    author_email="saurabh@virevol.com",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "asyncio",
+        "tenacity",
+        "httpx",
+    ],
+    extras_require={"dev": ["pytest", "ruff>=0.1.2", "bump2version==1.0.1"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
