d503aa00df83c539630ebdb8647ebf56704856d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 14:06:50 +0000

    Add package matplotsup/0.0.1/matplotsup-0.0.1.tar.gz

---
+++ packages/matplotsup/matplotsup-0.0.1.tar.gz/matplotsup-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='matplotsup',
+    version='0.0.1',
+    author= 'fireing123',
+    author_email= 'gimd82368@gmail.com',
+    url= 'https://github.com/fireing123/pygame_phyics',
+    install_requires=['matplotlib'],
+    packages=find_packages()
+)
