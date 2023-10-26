f70f541c93c138aee9bc6dfbbdbb129160028da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:08:43 +0000

    Add package tempproj/2.0.1/tempproj-2.0.1-py3-none-any.whl

---
+++ packages/tempproj/tempproj-2.0.1-py3-none-any.whl/tempproj/__init__.py
@@ -0,0 +1,14 @@
+'''
+Date         : 2023-01-30 12:19:11
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2023-10-25 14:47:04
+LastEditors  : BDFD
+Description  : 
+FilePath     : \tempproj\__init__.py
+Copyright (c) 2023 by BDFD, All Rights Reserved. 
+'''
+
+
+from tempproj.test import add_one, add_two, add_three
+from tempproj.supervised_classification import Car_Prediction
