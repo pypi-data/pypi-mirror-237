67bb99b254d3215d5607a03ee061aceecae246e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:07:12 +0000

    Add package SnipeGenius/1.5.0/SnipeGenius-1.5.0.tar.gz

---
+++ packages/SnipeGenius/SnipeGenius-1.5.0.tar.gz/SnipeGenius-1.5.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SnipeGenius',
+    version='1.5.0',
+    description='A sniping bot for detecting and trading new token pairs safely',
+    url='https://github.com/ELHARAKA/SnipeGenius',
+    author='Fahd El Haraka',
+    author_email='fahd@web3dev.ma',
+    license='https://github.com/ELHARAKA/SnipeGenius/blob/main/LICENSE',
+    packages=find_packages(),
+    install_requires=[
+        'pyfiglet',
+        'colorlog',
+        'termcolor',
+        'web3',
+        'requests',
+        'cryptography',
+        'google-api-python-client==2.104.0',
+        'pwinput'
+    ],
+    python_requires='>=3.0',
+    entry_points={
+        'console_scripts': [
+            'snipegenius = snipe:main',
+        ],
+    },
+)
