66de9fb128f47c53d5b24691f9678ed89bdc226b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:10:17 +0000

    Add package RacksDB/0.2.0/RacksDB-0.2.0-py3-none-any.whl

---
+++ packages/RacksDB/RacksDB-0.2.0-py3-none-any.whl/racksdb/drawers/base.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2022-2023 Rackslab
+#
+# This file is part of RacksDB.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import logging
+
+import cairo
+
+logger = logging.getLogger(__name__)
+
+
+class ImagePoint:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Drawer:
+    def __init__(self, db, file, output_format):
+        self.db = db
+        self.file = file
+        self.output_format = output_format
+        self.surface = None
+        self.ctx = None
+
+    def init_ctx(self, width, height):
+        if self.output_format == "png":
+            self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
+        elif self.output_format == "svg":
+            self.surface = cairo.SVGSurface(self.file, width, height)
+        elif self.output_format == "pdf":
+            self.surface = cairo.PDFSurface(self.file, width, height)
+        self.ctx = cairo.Context(self.surface)
+
+    def write(self):
+        if self.output_format == "png":
+            self.surface.write_to_png(self.file)
+        elif self.output_format in ["svg", "pdf"]:
+            self.surface.finish()
+            self.surface.flush()
