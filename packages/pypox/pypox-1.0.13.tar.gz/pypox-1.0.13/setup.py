0360f34f6f81a1f8ceacef22fd3e021af41a4590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:10:31 +0000

    Add package pypox/1.0.13/pypox-1.0.13.tar.gz

---
+++ packages/pypox/pypox-1.0.13.tar.gz/pypox-1.0.13/setup.py
@@ -0,0 +1,22 @@
+from importlib.metadata import entry_points
+from setuptools import setup,find_packages
+import os
+setup(
+    name='pypox',
+    version=str(os.getenv('__package_version__')), #type: ignore
+    author='Karl Robeck Alferez',
+    author_email='karlalferezfx@gmail.com',
+    description='Python to javascript transpiler',
+    packages=find_packages(),
+    entry_points = {
+      'console_scripts':[
+          'pypox = pypox:main'
+      ]  
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11.4',
+)
---
+++ packages/pypox/pypox-1.0.13.tar.gz/pypox-1.0.13/test/test.py
@@ -0,0 +1,8 @@
+from pypox.javascript.window.console import *
+
+__COMPILER__ = "JAVASCRIPT"
+
+x = 1
+
+if x == 1:
+    console.log(f'hello {x}')
