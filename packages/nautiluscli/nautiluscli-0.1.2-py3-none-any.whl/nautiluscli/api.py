ba3bef597d4b8745ec12cbd03f2971661328838e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 14:06:03 +0000

    Add package nautiluscli/0.1.2/nautiluscli-0.1.2-py3-none-any.whl

---
+++ packages/nautiluscli/nautiluscli-0.1.2-py3-none-any.whl/nautiluscli/api.py
@@ -0,0 +1,104 @@
+from urllib import request
+
+import requests
+import os
+import sys
+import json
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from nautiluscli.model import (
+    CreateQACollectionRequest,
+    DeleteQACollectionRequest,
+    AddDocRequest,
+    AskRequest,
+)
+
+
+def create_collection(url: str, name: str):
+    url += "/qacollections/create"
+    req = CreateQACollectionRequest(name=name)
+    resp = requests.post(url=url, data=req.model_dump_json())
+    return("status_code:", resp.status_code, resp.json())
+
+
+def delete_collection(url: str, name: str):
+    url += "/qacollections/delete"
+    req = DeleteQACollectionRequest(name=name)
+    resp = requests.post(url=url, data=req.model_dump_json())
+    return("status_code:", resp.status_code, resp.json())
+
+
+def list_collections(url: str):
+    url += "/qacollections/list"
+    resp = requests.get(url=url)
+    return("status_code:", resp.status_code, resp.json())
+
+
+def add_doc(url: str, clname: str, file_path: str):
+    url += "/qadocs/add"
+    req = AddDocRequest(collection_name=clname)
+    data = {"request": req.model_dump_json()}
+    fname = os.path.basename(file_path)
+    validation_error = validate_file(file_path)
+    if validation_error is not None:
+        return validation_error
+    with open(file_path, 'rb') as f:
+        resp = requests.post(url=url, files={'file': (fname, f)}, data=data)
+        return("status_code:", resp.status_code, resp.json())
+
+
+def add_web_doc(url: str, clname: str, file_path: str):
+    url += "/qadocs/add"
+    req = AddDocRequest(collection_name=clname)
+    data = {"request": req.model_dump_json()}
+    fname = os.path.basename(file_path)
+
+    validation_error = validate_url_file(file_path)
+    if validation_error is not None:
+        return validation_error
+
+    with request.urlopen(file_path) as f:
+        resp = requests.post(url=url, files={'file': (fname, f)}, data=data)
+        return("status_code:", resp.status_code, resp.json())
+
+
+def validate_url_file(url):
+    nbyte = 0
+    try:
+        resp = requests.request('HEAD', url)
+        nbyte = int(resp.headers.get("Content-Length"))
+    except Exception as e:
+        pass
+    if nbyte > 9_500_000:
+        return (
+            f"File {url} exceeds size limit 9.5MB, actual size {nbyte} "
+            f"bytes")
+    return None
+
+
+def validate_file(path: str):
+    nbyte = os.path.getsize(path)
+    if nbyte > 9_500_000:
+        return (f"File {path} exceeds size limit 9.5MB, actual size {nbyte} "
+                f"bytes")
+    return None
+
+def ask(url: str, clname: str, q: str, explain: bool):
+    url += "/qadocs/ask"
+    req = AskRequest(collection_name=clname, question=q)
+    resp = requests.post(url=url, data=req.model_dump_json())
+    answer = resp.json()
+
+    # Pretty-format success cases
+    if not explain and 'answer' in answer:
+        return answer.get("answer")
+    elif resp.status_code == 200:
+        return json.dumps(answer, indent=4)
+    return("status_code:", resp.status_code, resp.json())
+
+def list_collections(url: str):
+    url += "/qacollections/list"
+    resp = requests.get(url=url)
+    print("status_code:", resp.status_code, resp.json())
+
