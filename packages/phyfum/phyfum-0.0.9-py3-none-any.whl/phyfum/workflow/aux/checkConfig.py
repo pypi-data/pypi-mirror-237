9a7a4551ff299bb5b3ca3e0286f7d6261f7b932f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:09:09 +0000

    Add package phyfum/0.0.9/phyfum-0.0.9-py3-none-any.whl

---
+++ packages/phyfum/phyfum-0.0.9-py3-none-any.whl/phyfum/workflow/aux/checkConfig.py
@@ -0,0 +1,23 @@
+import yaml
+
+
+def readDefaults(yaml_path: str) -> dict:
+    with open(yaml_path, 'r') as f:
+        config = yaml.safe_load(f)
+    default_values = {key: value["default"] for key, value in config.get("properties", {}).items() if "default" in value}
+    return default_values
+
+
+def makeConfig(config: dict, defaults: dict) -> dict:
+    """
+    Remove Nones from config and add missing variables from the defaults
+    """
+    config = {key: value for key, value in config.items() if value}
+
+    # Merge 'config' with 'defaults' to add missing variables
+    config = {**defaults, **config}
+    if not config.get("mle_iterations"):
+        config["mle_iterations"] = int(config.get("iterations") / config.get("mle_steps"))
+    if not config.get("mle_sampling"):
+        config["mle_sampling"] = int(config.get("iterations") / 1000)
+    return config
