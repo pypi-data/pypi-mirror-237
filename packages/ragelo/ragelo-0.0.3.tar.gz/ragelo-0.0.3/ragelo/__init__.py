2761029d03671aab77938e1f809967ee82eaecbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:08:21 +0000

    Add package ragelo/0.0.3/ragelo-0.0.3.tar.gz

---
+++ packages/ragelo/ragelo-0.0.3.tar.gz/ragelo-0.0.3/ragelo/__init__.py
@@ -0,0 +1,11 @@
+"""CLI for automatically evaluating Retrieval Augmented Generation (RAG) models."""
+from ragelo.answer_evaluators import AnswerEvaluatorFactory
+from ragelo.doc_evaluators import DocumentEvaluatorFactory
+
+__version__ = "0.1.0"
+__app_name__ = "ragelo"
+__author__ = "Zeta Alpha"
+__email__ = "camara@zeta-alpha.com"
+__description__ = (
+    "CLI for automatically evaluating Retrieval Augmented Generation (RAG) models."
+)
