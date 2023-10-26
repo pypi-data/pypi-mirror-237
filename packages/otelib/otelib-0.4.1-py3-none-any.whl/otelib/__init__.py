b32e49c627f9895cbde06b53829ff71c55e85aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:11:58 +0000

    Add package otelib/0.4.1/otelib-0.4.1-py3-none-any.whl

---
+++ packages/otelib/otelib-0.4.1-py3-none-any.whl/otelib/__init__.py
@@ -0,0 +1,11 @@
+"""OTElib
+
+A package to interact with the Open Translation Environment (OTE) API.
+"""
+from .client import OTEClient
+
+__all__ = ("OTEClient",)
+
+__version__ = "0.4.1"
+__author__ = "SINTEF"
+__author_email__ = "Team4.0@SINTEF.no"
