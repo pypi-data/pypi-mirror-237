d6e1917334743face88e3ae0fc90ada4cf7b1a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 14:06:28 +0000

    Add package simpleadder/0.1/simpleadder-0.1.tar.gz

---
+++ packages/simpleadder/simpleadder-0.1.tar.gz/simpleadder-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simpleadder",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Andrew Hahn",
+    author_email="hahnandy0201@gmail.com",
+    description="A simple package to add two numbers.",
+    keywords="add addition math",
+    url="https://github.com/hahnandrew/simpleadder",   # project home page, if any
+)
