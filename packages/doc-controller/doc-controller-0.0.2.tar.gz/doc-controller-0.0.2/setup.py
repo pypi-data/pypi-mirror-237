3e47d8e1197ab85983dfe9ba4333d4d068372e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:11:37 +0000

    Add package doc-controller/0.0.2/doc-controller-0.0.2.tar.gz

---
+++ packages/doc-controller/doc-controller-0.0.2.tar.gz/doc-controller-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'To update and retrieve documentation data'
+LONG_DESCRIPTION = 'A package that allows to update and retrieve documentation data.'
+
+# Setting up
+setup(
+    name="doc-controller",
+    version=VERSION,
+    author="Bernard Birendra Das",
+    author_email="<bernardbdas@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['rasa'],
+    keywords=['python', 'conversational AI', 'AI', 'Task Automation', 'SimplifyQA', 'Simplify3x'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
