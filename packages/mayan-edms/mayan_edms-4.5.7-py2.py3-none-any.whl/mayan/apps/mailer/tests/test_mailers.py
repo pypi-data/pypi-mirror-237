64edbf1623fd2aa2e0134481885c37ed7da4481f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:07:54 +0000

    Add package mayan-edms/4.5.7/mayan_edms-4.5.7-py2.py3-none-any.whl

---
+++ packages/mayan-edms/mayan_edms-4.5.7-py2.py3-none-any.whl/mayan/apps/mailer/tests/test_mailers.py
@@ -0,0 +1,29 @@
+from mayan.apps.testing.tests.base import BaseTestCase
+
+from .literals import MAILER_BACKEND_DJANGO_PATH
+from .mixins import MailerTestMixin
+
+
+class DjangoMailerBackendTestCase(MailerTestMixin, BaseTestCase):
+    _test_mailer_backend_path = MAILER_BACKEND_DJANGO_PATH
+
+    def test_mailer_connection_kwargs(self):
+        self._create_test_user_mailer(
+            extra_backend_data={
+                'host': '127.0.0.1',
+                'port': '999',
+                'stored_credential_id': self._test_stored_credential.pk
+            }
+        )
+
+        backend_instance = self._test_user_mailer.get_backend_instance()
+        connection_kwargs = backend_instance.get_connection_kwargs()
+
+        self.assertEqual(
+            connection_kwargs['password'],
+            self._test_stored_credential_backend_data['password']
+        )
+        self.assertEqual(
+            connection_kwargs['username'],
+            self._test_stored_credential_backend_data['username']
+        )
