b616ff35245e6f94db8ee29d758b4d25a16252c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:08:19 +0000

    Add package desckgc/0.0.5/desckgc-0.0.5-py3-none-any.whl

---
+++ packages/desckgc/desckgc-0.0.5-py3-none-any.whl/DescKGC/scripts/manage_db.py
@@ -0,0 +1,73 @@
+import argparse
+
+from DescKGC.procedures.build_config import load_config
+from DescKGC.tools.db_manager import DBManager
+
+
+def add_arguments(parser):
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "--delete-by-type",
+        action="store_true",
+        help="Delete nodes by type. You will be asked to enter the type.",
+    )
+    group.add_argument(
+        "--delete-all",
+        action="store_true",
+        help="Delete all nodes. You will be asked to confirm.",
+    )
+    group.add_argument(
+        "--show-schema",
+        action="store_true",
+        help="Show the schema of the database.",
+    )
+
+
+def main(args):
+    config = load_config()
+
+    db_manager = DBManager(**config["neo4jdb"], **config["chromadb"])
+    node_labels = db_manager.get_node_labels()
+
+    if args.delete_by_type:
+        delete_by_type(db_manager, node_labels)
+
+    if args.delete_all:
+        delete_all(db_manager, node_labels)
+
+    if args.show_schema:
+        show_schema(db_manager)
+
+
+def show_schema(db_manager):
+    db_manager.show_schema()
+
+
+def delete_all(db_manager, node_labels):
+    if len(node_labels) == 0:
+        print("No data in the database.")
+    else:
+        confirmation = input("Are you sure to delete all data? (y/n): ")
+        if confirmation == "y":
+            for node_label in node_labels:
+                db_manager.delete_by_type(node_label)
+            db_manager.vector_db.client.reset()
+        else:
+            print("Delete aborted.")
+
+
+def delete_by_type(db_manager, node_labels):
+    if len(node_labels) == 0:
+        print("No data in the database.")
+    else:
+        print("The available types are:", end=" ")
+        print(f'{", ".join(node_labels)}.')
+        type_name = input("Please input the type name: \n")
+        db_manager.delete_by_type(type_name)
+        print(f"Delete all nodes with type {type_name}.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    main(args)
---
+++ packages/desckgc/desckgc-0.0.5-py3-none-any.whl/DescKGC/tools/config_manager/base.py
@@ -0,0 +1,50 @@
+from collections import OrderedDict
+
+import oyaml as yaml
+
+from .default_config import DescKGCConfig
+
+
+class ConfigManager:
+    def __init__(self, local_config_path=".kgc_config.yaml", default_config=DescKGCConfig(), verbose=False):
+        self.local_config_path = local_config_path
+        self.default_config = default_config
+
+    def get_config(self):
+        local_config = self._get_local_config(self.local_config_path)
+        merged_config = DescKGCConfig(**local_config)
+        # return ordered dict
+        return merged_config.dict()
+
+    def _convert_to_ordered_dict(self, config):
+        if isinstance(config, dict):
+            return OrderedDict([(key, self._convert_to_ordered_dict(value)) for key, value in config.items()])
+        elif isinstance(config, list):
+            return [self._convert_to_ordered_dict(value) for value in config]
+        else:
+            return config
+
+    def _get_local_config(self, local_config_path):
+        local_config = self._load_local_config(local_config_path)
+        if local_config is None:
+            print(
+                "No local config found, use default config. "
+                "Please run `desckgc manage_config --init` to initialize a local config."
+            )
+            exit(1)
+        return local_config
+
+    def init_local_config(self):
+        local_config = self._load_local_config(self.local_config_path)
+        if local_config is None:
+            yaml.dump(self.default_config.dict(), open(self.local_config_path, "w"))
+        else:
+            print("Local config already exists, skip initialization.")
+
+    def _load_local_config(self, local_config_path):
+        try:
+            with open(local_config_path, "r") as local_config_file:
+                local_config = yaml.safe_load(local_config_file)
+        except FileNotFoundError:
+            local_config = None
+        return local_config
