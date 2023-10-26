26a9990d7df1391c5f56ca51cda95049cf4aa25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 14:09:52 +0000

    Add package kuroco-py/0.0.1/kuroco_py-0.0.1.tar.gz

---
+++ packages/kuroco-py/kuroco_py-0.0.1.tar.gz/kuroco_py-0.0.1/kuroco_api/__init__.py
@@ -0,0 +1,14 @@
+from .kuroco_api import KurocoAPI
+from .kuroco_content import KurocoContent
+# Environment variables
+
+# Kuroco
+KUROCO_ENDPOINT = "KUROCO_ENDPOINT"
+KUROCO_VERSION = "KUROCO_VERSION"
+KUROCO_ACCESS_TOKEN = "KUROCO_ACCESS_TOKEN"
+
+
+__all__ = [
+    "KurocoAPI", 
+    "KurocoContent",
+]
