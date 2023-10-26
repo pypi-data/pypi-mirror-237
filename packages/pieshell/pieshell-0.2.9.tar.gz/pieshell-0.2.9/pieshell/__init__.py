90a03a9bbe23e6a3c056517c63571602207cdddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:10:43 +0000

    Add package pieshell/0.2.9/pieshell-0.2.9.tar.gz

---
+++ packages/pieshell/pieshell-0.2.9.tar.gz/pieshell-0.2.9/pieshell/__init__.py
@@ -0,0 +1,22 @@
+import pkg_resources
+
+from .iterio import *
+from .signalio import *
+from .redir import *
+from .pipeline import *
+from .environ import *
+from .shell import *
+from .module import *
+from .utils import *
+from .ps import *
+from .version import *
+import builtins as __pieshell_builtins
+
+name = "PieShell %s <)" % (version,)
+
+with pkg_resources.resource_stream("pieshell", "README.md") as f:
+    __doc__ = "%s\n\n%s" % (name, f.read().decode("utf-8"))
+
+banner = """%s
+Python %s
+Type help(pieshell) for more information.""" % (name, sys.version.replace("\n", " "),)
