44cf90433d485539927cea4b0f57e793de5419f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 16:09:24 +0000

    Add package JyPyUI/1.1.0/JyPyUI-1.1.0.tar.gz

---
+++ packages/JyPyUI/JyPyUI-1.1.0.tar.gz/JyPyUI-1.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as arq:
+  readme = arq.read()
+
+setup(
+    name='JyPyUI',
+    version='1.1.0',
+    license='MIT License',
+    author='Paulo Pelecer',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='ppelecer@gmail.com',
+    keywords='jypyui',
+    description=
+    u'JyPyUI e uma Interface grafica Baseada no Pygame Focada no android ',
+    packages=["JyPyUI"],
+    install_requires=['requests', 'plyer', 'pygame', 'pyjnius', 'gtts'],
+)
