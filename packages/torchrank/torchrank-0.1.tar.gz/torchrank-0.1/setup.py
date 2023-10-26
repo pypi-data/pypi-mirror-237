84768b6a3bca49a10a23e60dfe1d493ae01b9037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 14:07:05 +0000

    Add package torchrank/0.1/torchrank-0.1.tar.gz

---
+++ packages/torchrank/torchrank-0.1.tar.gz/torchrank-0.1/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Felix Petersen',
+    author_email='ads0740@felix-petersen.de',
+    description='Differentiable Sorting Networks.',
+    install_requires=['diffsort'],
+    long_description='Use `diffsort <https://pypi.python.org/pypi/diffsort/>`_ instead.',
+    maintainer='Felix Petersen',
+    maintainer_email='ads0740@felix-petersen.de',
+    name='torchrank',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/diffsort/',
+    version='0.1',
+    zip_safe=False,
+)
