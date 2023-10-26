80be3005be5191955cc07113e4dfcd17ee5b48ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:11:45 +0000

    Add package protocolinterface/0.4.7/protocolinterface-0.4.7-py3-none-any.whl

---
+++ packages/protocolinterface/protocolinterface-0.4.7-py3-none-any.whl/protocolinterface/__init__.py
@@ -0,0 +1,9 @@
+import protocolinterface.validators as val
+from protocolinterface.protocolinterface import (
+    ProtocolInterface,
+    RunnableProtocol,
+    WebProtocol,
+    weboptions,
+)
+
+__version__ = "0.4.7"
