df144bac613eaf46540f9283df1132680a9025f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:13:24 +0000

    Add package scrapethat/1.0.3/scrapethat-1.0.3.tar.gz

---
+++ packages/scrapethat/scrapethat-1.0.3.tar.gz/scrapethat-1.0.3/README.md
@@ -0,0 +1,11 @@
+# scrapethat
+
+scrapethat makes your scraping python codes faster and more readable
+
+## Installation
+
+You can install your package using pip:
+
+```bash
+pip install scrapethat
+```
---
+++ packages/scrapethat/scrapethat-1.0.3.tar.gz/scrapethat-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scrapethat",
+    version="1.0.3",
+    author="Mihaly Orsos",
+    author_email="ormraat.pte@gmail.com",
+    description="Tools for faster scraping",
+    url="https://github.com/misrori/scrapethat",
+    license="MIT",
+    install_requires=["bs4", "requests", "pandas", "cloudscraper"],
+    packages=find_packages()
+)
+
