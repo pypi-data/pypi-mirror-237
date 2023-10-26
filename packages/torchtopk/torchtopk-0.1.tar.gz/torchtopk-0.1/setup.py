ff1824add6a7c5667954a5360d307a07de0ce99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 14:07:05 +0000

    Add package torchtopk/0.1/torchtopk-0.1.tar.gz

---
+++ packages/torchtopk/torchtopk-0.1.tar.gz/torchtopk-0.1/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Felix Petersen',
+    author_email='ads0600@felix-petersen.de',
+    description='Differentiable Sorting, Ranking, and Top-k.',
+    install_requires=['difftopk'],
+    long_description='Use `difftopk <https://pypi.python.org/pypi/difftopk/>`_ instead.',
+    maintainer='Felix Petersen',
+    maintainer_email='ads0740@felix-petersen.de',
+    name='torchtopk',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/difftopk/',
+    version='0.1',
+    zip_safe=False,
+)
