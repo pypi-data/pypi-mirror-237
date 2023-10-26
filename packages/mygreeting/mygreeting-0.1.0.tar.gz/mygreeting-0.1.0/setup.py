78d1953e1b81a384882b8920855fc2a67f628ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:08:41 +0000

    Add package mygreeting/0.1.0/mygreeting-0.1.0.tar.gz

---
+++ packages/mygreeting/mygreeting-0.1.0.tar.gz/mygreeting-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mygreeting",
+    version="0.1.0",
+    description="A simple greeting library",
+    author="Nikhil Patil",
+    author_email="nikhilpatil18012004@gmail.com",
+    packages=find_packages(),
+)
