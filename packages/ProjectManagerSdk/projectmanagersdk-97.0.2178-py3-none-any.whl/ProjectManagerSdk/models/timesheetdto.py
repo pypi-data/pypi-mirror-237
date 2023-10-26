d9f8e9e52dd2299279b9bd1726a3b207174a3353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:09:44 +0000

    Add package ProjectManagerSdk/97.0.2178/projectmanagersdk-97.0.2178-py3-none-any.whl

---
+++ packages/ProjectManagerSdk/projectmanagersdk-97.0.2178-py3-none-any.whl/ProjectManagerSdk/models/timesheetdto.py
@@ -0,0 +1,68 @@
+#
+# ProjectManager API for Python
+#
+# (c) 2023-2023 ProjectManager.com, Inc.
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @author     ProjectManager.com <support@projectmanager.com>
+# @copyright  2023-2023 ProjectManager.com, Inc.
+# @link       https://github.com/projectmgr/projectmanager-sdk-python
+#
+
+
+from ProjectManagerSdk.models.timesheetprojectdto import TimeSheetProjectDto
+from ProjectManagerSdk.models.timesheetresourcedto import TimesheetResourceDto
+from ProjectManagerSdk.models.timesheettaskdto import TimesheetTaskDto
+from dataclasses import dataclass
+
+@dataclass
+class TimesheetDto:
+    """
+    Created Time entry response data
+    """
+
+    id: str | None = None
+    """
+    TimesheetId
+    """
+
+    date: str | None = None
+    """
+    Date of time entry
+    """
+
+    notes: str | None = None
+    """
+    Notes
+    """
+
+    approved: bool | None = None
+    """
+    Shows if timesheet approved
+    """
+
+    hours: float | None = None
+    """
+    Total Hours
+    """
+
+    task: TimesheetTaskDto | None = None
+    """
+    associated tasks with time sheet
+    """
+
+    project: TimeSheetProjectDto | None = None
+    """
+    associated projects with timesheet
+    """
+
+    resource: TimesheetResourceDto | None = None
+    """
+    The resource associated with this timesheet entry
+    """
+
+
+    def to_dict(self) -> dict:
+        return dataclass.asdict(self)
