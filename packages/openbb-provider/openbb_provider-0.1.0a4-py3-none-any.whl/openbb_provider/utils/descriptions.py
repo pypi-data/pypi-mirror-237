ba2c49a8b62158207fb1c8b566b08d3750d989d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:08:54 +0000

    Add package openbb-provider/0.1.0a4/openbb_provider-0.1.0a4-py3-none-any.whl

---
+++ packages/openbb-provider/openbb_provider-0.1.0a4-py3-none-any.whl/openbb_provider/utils/descriptions.py
@@ -0,0 +1,32 @@
+"""Common descriptions for model fields."""
+
+QUERY_DESCRIPTIONS = {
+    "symbol": "Symbol to get data for.",
+    "start_date": "Start date of the data, in YYYY-MM-DD format.",
+    "end_date": "End date of the data, in YYYY-MM-DD format.",
+    "interval": "Time interval of the data to return.",
+    # "weekly": "Whether to return weekly data.",
+    # "monthly": "Whether to return monthly data.",
+    "period": "Time period of the data to return.",
+    "date": "A specific date to get data for.",
+    "limit": "The number of data entries to return.",
+    "countries": "The country or countries to get data.",
+    "units": """The unit of measurement for the CPI data.
+    Options:
+    - `growth_previous`: growth from the previous period
+    - `growth_same`: growth from the same period in the previous year
+    - `index_2015`: index with base year 2015.""",
+    "frequency": "The frequency of the data points; options include `monthly`, `quarter`, and `annual`",
+}
+
+DATA_DESCRIPTIONS = {
+    "symbol": "Symbol representing the entity requested in the data.",
+    "date": "The date of the data.",
+    "open": "The open price of the symbol.",
+    "high": "The high price of the symbol.",
+    "low": "The low price of the symbol.",
+    "close": "The close price of the symbol.",
+    "volume": "The volume of the symbol.",
+    "adj_close": "The adjusted close price of the symbol.",
+    "vwap": "Volume Weighted Average Price of the symbol.",
+}
