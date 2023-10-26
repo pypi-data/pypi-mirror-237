8bf9882f60d375fd29b669de7b2fe6763d02993e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 06:08:00 +0000

    Add package decode/2.4.0/decode-2.4.0.tar.gz

---
+++ packages/decode/decode-2.4.0.tar.gz/decode-2.4.0/decode/__init__.py
@@ -0,0 +1,20 @@
+__all__ = [
+    "assign",
+    "correct",
+    "fit",
+    "load",
+    "make",
+    "plot",
+    "select",
+]
+__version__ = "2.4.0"
+
+
+# submodules
+from . import assign
+from . import correct
+from . import fit
+from . import load
+from . import make
+from . import plot
+from . import select
