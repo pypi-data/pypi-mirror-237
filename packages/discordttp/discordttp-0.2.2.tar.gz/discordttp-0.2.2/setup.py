aeaaeb173cfa39e6d45cacc56dce486cccc992c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:13:51 +0000

    Add package discordttp/0.2.2/discordttp-0.2.2.tar.gz

---
+++ packages/discordttp/discordttp-0.2.2.tar.gz/discordttp-0.2.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="discordttp",
+    version="0.2.2",
+    author="viie",
+    author_email="viie@femboy.cx",
+    description="A python module used for editing and using discord tokens!",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
