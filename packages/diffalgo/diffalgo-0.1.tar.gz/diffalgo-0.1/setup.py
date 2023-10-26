dbd9c242e009fbba5792750c7c19d38ba448b9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 14:06:54 +0000

    Add package diffalgo/0.1/diffalgo-0.1.tar.gz

---
+++ packages/diffalgo/diffalgo-0.1.tar.gz/diffalgo-0.1/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Felix Petersen',
+    author_email='ads0399@felix-petersen.de',
+    description='AlgoVision - A Framework for Differentiable Algorithms and Algorithmic Supervision',
+    install_requires=['algovision'],
+    long_description='Use `algovision <https://pypi.python.org/pypi/algovision/>`_ instead.',
+    maintainer='Felix Petersen',
+    maintainer_email='ads0740@felix-petersen.de',
+    name='diffalgo',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/algovision/',
+    version='0.1',
+    zip_safe=False,
+)
