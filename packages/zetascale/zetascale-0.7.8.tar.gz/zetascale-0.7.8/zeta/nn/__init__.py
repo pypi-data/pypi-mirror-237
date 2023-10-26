d6e774e69aaffa99ce30b8a8c6cb1ba7b81be672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:08:38 +0000

    Add package zetascale/0.7.8/zetascale-0.7.8.tar.gz

---
+++ packages/zetascale/zetascale-0.7.8.tar.gz/zetascale-0.7.8/zeta/nn/__init__.py
@@ -0,0 +1,20 @@
+# architecture
+# from zeta.structs import *
+
+# Attention
+# from zeta.nn.attention import *
+from zeta.nn import attention
+
+
+# embeddings
+# from zeta.nn.embeddings import *
+from zeta.nn import embeddings
+
+# modules
+# from zeta.nn.modules import *
+from zeta.nn import modules
+
+
+# biases
+# from zeta.nn.biases import *
+from zeta.nn import biases
