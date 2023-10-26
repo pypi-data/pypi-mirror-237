63140bc758f160b1460206522e2b8e247153b91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:13:55 +0000

    Add package llmstudio/0.2.8/llmstudio-0.2.8-py3-none-any.whl

---
+++ packages/llmstudio/llmstudio-0.2.8-py3-none-any.whl/llmstudio/__init__.py
@@ -0,0 +1,20 @@
+name = "version"
+__version__ = "0.2.8"
+
+__requirements__ = [
+    "pydantic",
+    "requests",
+    "numpy",
+    "torch",
+    "sentence-transformers",
+    "fastapi",
+    "uvicorn",
+    "PyYaml",
+    "openai",
+    "tiktoken",
+    "google-auth",
+    "google-cloud-aiplatform",
+    "fastapi",
+    "boto3",
+    "langchain",
+]
