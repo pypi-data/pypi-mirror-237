197fff43f4d6fe856bb507aa0a713b9f18bc1e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:07:58 +0000

    Add package Tdrd/0.0.1/Tdrd-0.0.1.tar.gz

---
+++ packages/Tdrd/Tdrd-0.0.1.tar.gz/Tdrd-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name = "Tdrd",                                                                                                                              
+    version = "0.0.1",
+    author = "ditisrmel",
+    license='MIT license',
+    author_email="kimichen2907@gmail.com",
+    description="Make a to-do record notebook",
+    url="https://github.com/ditisrmel/Tdrn", 
+    python_requires='>=3.11.4'
+    
+
+    
+)
