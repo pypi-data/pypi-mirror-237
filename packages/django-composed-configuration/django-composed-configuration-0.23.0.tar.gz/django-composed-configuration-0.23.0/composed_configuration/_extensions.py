238a2b84e27c2952579c2c69a951e7de9f5b53d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 06:06:24 +0000

    Add package django-composed-configuration/0.23.0/django-composed-configuration-0.23.0.tar.gz

---
+++ packages/django-composed-configuration/django-composed-configuration-0.23.0.tar.gz/django-composed-configuration-0.23.0/composed_configuration/_extensions.py
@@ -0,0 +1,19 @@
+from typing import Type
+
+from ._base import ComposedConfiguration, ConfigMixin
+
+
+class ExtensionsMixin(ConfigMixin):
+    """
+    Configure Django Extensions.
+
+    This requires the `django-extensions` package to be installed.
+    """
+
+    @staticmethod
+    def mutate_configuration(configuration: Type[ComposedConfiguration]) -> None:
+        configuration.INSTALLED_APPS += ['django_extensions']
+
+    SHELL_PLUS_PRINT_SQL = True
+    SHELL_PLUS_PRINT_SQL_TRUNCATE = None
+    RUNSERVER_PLUS_PRINT_SQL_TRUNCATE = None
