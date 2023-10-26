57e24f8700a8c34fa4752633f57c1c491b7e153b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 14:06:56 +0000

    Add package torchrenderer/0.1/torchrenderer-0.1.tar.gz

---
+++ packages/torchrenderer/torchrenderer-0.1.tar.gz/torchrenderer-0.1/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Felix Petersen',
+    author_email='ads0475@felix-petersen.de',
+    description='GenDR - The Generalized Differentiable Renderer',
+    install_requires=['gendr'],
+    long_description='Use `gendr <https://pypi.python.org/pypi/gendr/>`_ instead.',
+    maintainer='Felix Petersen',
+    maintainer_email='ads0740@felix-petersen.de',
+    name='torchrenderer',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/gendr/',
+    version='0.1',
+    zip_safe=False,
+)
