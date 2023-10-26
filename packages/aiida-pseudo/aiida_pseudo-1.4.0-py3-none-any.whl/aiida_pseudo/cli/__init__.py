35ff4083c4e12eb08c6f1bdc7867ae042658df3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 03:10:28 +0000

    Add package aiida-pseudo/1.4.0/aiida_pseudo-1.4.0-py3-none-any.whl

---
+++ packages/aiida-pseudo/aiida_pseudo-1.4.0-py3-none-any.whl/aiida_pseudo/cli/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Module for the command line interface."""
+from .family import cmd_family
+from .install import cmd_install, cmd_install_family, cmd_install_pseudo_dojo, cmd_install_sssp
+from .list import cmd_list
+from .root import cmd_root
