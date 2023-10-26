83c8fcb2df6e139713bf990398696c41a0d66ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:09:14 +0000

    Add package armcnc/1.0.42/armcnc-1.0.42-py3-none-any.whl

---
+++ packages/armcnc/armcnc-1.0.42-py3-none-any.whl/armcnc/cnc/command.py
@@ -0,0 +1,15 @@
+"""
+******************************************************************************
+* @author  ARMCNC site:www.armcnc.net github:armcnc.github.io
+******************************************************************************
+"""
+
+import linuxcnc
+
+class Command:
+
+    def __init__(self, framework):
+        self.framework = framework
+        self.linuxcnc = linuxcnc
+        self.api = self.linuxcnc.command()
+
