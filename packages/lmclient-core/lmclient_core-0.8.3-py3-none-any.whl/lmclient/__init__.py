7f0f4b05a4efed61823c9649bd8db5c46ade73db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 16:06:12 +0000

    Add package lmclient-core/0.8.3/lmclient_core-0.8.3-py3-none-any.whl

---
+++ packages/lmclient-core/lmclient_core-0.8.3-py3-none-any.whl/lmclient/__init__.py
@@ -0,0 +1,54 @@
+from lmclient.chat_engine import ChatEngine
+from lmclient.completion_engine import CompletionEngine
+from lmclient.models import (
+    AzureChat,
+    HunyuanChat,
+    HunyuanChatParameters,
+    MinimaxChat,
+    MinimaxChatParameters,
+    MinimaxProChat,
+    MinimaxProChatParameters,
+    OpenAIChat,
+    OpenAIChatParameters,
+    WenxinChat,
+    WenxinChatParameters,
+    ZhiPuChat,
+    ZhiPuChatParameters,
+)
+from lmclient.types import GeneralParameters, Message, RetryStrategy
+from lmclient.utils import BaseSchema, PydanticVersion, function
+from lmclient.version import __version__
+
+LMClient = CompletionEngine
+
+if PydanticVersion == 1:
+    from pydantic import BaseModel
+
+    BaseModel.model_copy = BaseModel.copy  # type: ignore
+    BaseModel.model_dump = BaseModel.dict  # type: ignore
+    BaseModel.model_dump_json = BaseModel.json  # type: ignore
+
+
+__all__ = [
+    'CompletionEngine',
+    'ChatEngine',
+    'AzureChat',
+    'OpenAIChat',
+    'OpenAIChatParameters',
+    'MinimaxProChat',
+    'MinimaxProChatParameters',
+    'MinimaxChat',
+    'MinimaxChatParameters',
+    'ZhiPuChat',
+    'ZhiPuChatParameters',
+    'WenxinChat',
+    'WenxinChatParameters',
+    'HunyuanChat',
+    'HunyuanChatParameters',
+    'BaseSchema',
+    'function',
+    'GeneralParameters',
+    'Message',
+    'RetryStrategy',
+    '__version__',
+]
