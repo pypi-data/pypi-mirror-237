6672dbd1fe059bedd962409e94618fa3480345ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:09:20 +0000

    Add package dataproc-jupyter-plugin/0.1.51/dataproc_jupyter_plugin-0.1.51.tar.gz

---
+++ packages/dataproc-jupyter-plugin/dataproc_jupyter_plugin-0.1.51.tar.gz/dataproc_jupyter_plugin-0.1.51/src/utils/listRuntimeTemplateInterface.ts
@@ -0,0 +1,40 @@
+export interface ISessionTemplateRoot {
+  sessionTemplates: ISessionTemplate[];
+  nextPageToken: string;
+}
+
+export interface ISessionTemplate {
+  name: string;
+  createTime: string;
+  jupyterSession: IJupyterSession;
+  creator: string;
+  labels: ILabels;
+  environmentConfig: IEnvironmentConfig;
+  description: string;
+  updateTime: string;
+}
+
+export interface IJupyterSession {
+  kernel: string;
+  displayName: string;
+}
+
+export interface ILabels {
+  purpose: string;
+}
+
+export interface IEnvironmentConfig {
+  executionConfig: IExecutionConfig;
+}
+
+export interface IExecutionConfig {
+  subnetworkUri: string;
+}
+
+export interface ISessionTemplateDisplay {
+  name: string;
+  owner: string;
+  description: string;
+  lastModified: string;
+  id: string;
+}
