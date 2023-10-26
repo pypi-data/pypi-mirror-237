508ae29e7d1813cbc084c8173f10606d3182b6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 16:07:25 +0000

    Add package fastapi-init/0.1.0/fastapi_init-0.1.0.tar.gz

---
+++ packages/fastapi-init/fastapi_init-0.1.0.tar.gz/fastapi_init-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="fastapi_init",
+    version="0.1.0",
+    description="A simple Python library",
+    author="Le Dinh Manh",
+    author_email="manhld@rabiloo.com",
+    packages=["fastapi_init"],
+    install_requires=["numpy", "pandas"],
+    python_requires=">=3.6",
+)
