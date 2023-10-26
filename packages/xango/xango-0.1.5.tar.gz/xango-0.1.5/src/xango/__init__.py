c49c4c6bbc646ff682105aeaf9112797f7020e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 06:06:47 +0000

    Add package xango/0.1.5/xango-0.1.5.tar.gz

---
+++ packages/xango/xango-0.1.5.tar.gz/xango-0.1.5/src/xango/__init__.py
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+# -- Xango --
+# ------------------------------------------------------------------------------
+
+from .database import Database as db, Collection, CollectionItem, ActiveCollectionItem, GraphEdgeCollection, GraphEdgeCollectionItem, GraphEdgeCollectionNode
+from .lib_xql import xql_to_aql
+from .dict_mutator import mutate as parse_dict_mutations
+from .lib import gen_xid
+from arango.exceptions import ArangoError
