466ecaedacfc09125ea75abcddf68ce8f7e17b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:08:58 +0000

    Add package pvshepherd/3.0.0/pvshepherd-3.0.0.tar.gz

---
+++ packages/pvshepherd/pvshepherd-3.0.0.tar.gz/pvshepherd-3.0.0/pvshepherd/_language_info.py
@@ -0,0 +1,161 @@
+LANGUAGE_TO_CODE = {
+    "English": "en",
+    "Deutsch": "de",
+    "Español": "es",
+    "Français": "fr",
+    "Italiano": "it",
+    "Nederlands": "nl",
+    "Polski": "pl",
+    "Português": "pt",
+    "Svenska": "sv",
+    "Tiếng Việt": "vn",
+    "Русский": "ru",
+    "العربية": "ar",
+    "فارسی": "fa",
+    "हिंदी": "hi",
+    "한국어": "ko",
+    "日本語": "ja",
+    "普通话": "zh",
+}
+
+DEFAULT_PORCUPINE_MODEL = dict(
+    en={
+        "name": "Hey Computer",
+        "file": "hey computer_en_cortexm.ppn",
+    },
+    de={
+        "name": "Hey Computer",
+        "file": "hey computer_de_cortexm.ppn",
+    },
+    es={
+        "name": "Hola Computadora",
+        "file": "hola computadora_es_cortexm.ppn",
+    },
+    fa={
+        "name": "سلام رایانه",
+        "file": "سلام رایانه_fa_cortexm.ppn",
+    },
+    fr={
+        "name": "Bonjour Ordinateur",
+        "file": "bonjour ordinateur_fr_cortexm.ppn",
+    },
+    it={
+        "name": "Ciao Computer",
+        "file": "ciao computer_it_cortexm.ppn",
+    },
+    ja={
+        "name": "こんにちはコンピュータ",
+        "file": "こんにちは コンピューター_ja_cortexm.ppn",
+    },
+    ko={
+        "name": "안녕 컴퓨터",
+        "file": "안녕 컴퓨터_ko_cortexm.ppn",
+    },
+    pt={
+        "name": "Olá computador",
+        "file": "olá computador_pt_cortexm.ppn",
+    },
+    ru={
+        "name": "привет компьютер",
+        "file": "привет компьютер_ru_cortexm.ppn",
+    },
+    nl={
+        "name": "hallo computer",
+        "file": "hallo computer_nl_cortexm.ppn",
+    },
+    hi={
+        "name": "नमस्ते कंप्यूटर",
+        "file": "नमस्ते कंप्यूटर_hi_cortexm.ppn",
+    },
+    ar={
+        "name": "مرحبا الكمبيوتر",
+        "file": "مرحبا الكمبيوتر_ar_cortexm.ppn",
+    },
+    pl={
+        "name": "cześć komputer",
+        "file": "cześć komputer_pl_cortexm.ppn",
+    },
+    sv={
+        "name": "hej dator",
+        "file": "hej dator_sv_cortexm.ppn",
+    },
+    vn={
+        "name": "xin chào máy tính",
+        "file": "xin chào máy tính_vn_cortexm.ppn",
+    },
+    zh={
+        "name": "你好电脑",
+        "file": "你好电脑_zh_cortexm.ppn",
+    },
+)
+
+DEFAULT_RHINO_MODEL = dict(
+    en={
+        "name": "Smart Lighting [en]",
+        "file": "smart_lighting_cortexm.rhn",
+    },
+    de={
+        "name": "Beleuchtung [de]",
+        "file": "beleuchtung_cortexm.rhn",
+    },
+    es={
+        "name": "Iluminación Inteligente [es]",
+        "file": "iluminación_inteligente_cortexm.rhn",
+    },
+    fr={
+        "name": "éclairage intelligent [fr]",
+        "file": "éclairage_intelligent_cortexm.rhn",
+    },
+    it={
+        "name": "illuminazione [it]",
+        "file": "illuminazione_cortexm.rhn",
+    },
+    ja={
+        "name": "sumāto_shōmei [ja]",
+        "file": "sumāto_shōmei_cortexm.rhn",
+    },
+    ko={
+        "name": "seumateu_jomyeong [ko]",
+        "file": "seumateu_jomyeong_cortexm.rhn",
+    },
+    pt={
+        "name": "luz_inteligente [pt]",
+        "file": "luz_inteligente_cortexm.rhn",
+    },
+    ru={
+        "name": "Simple Context [ru]",
+        "file": "simple_context_ru_cortexm.rhn",
+    },
+    nl={
+        "name": "Simple Context [nl]",
+        "file": "simple_context_nl_cortexm.rhn",
+    },
+    hi={
+        "name": "Simple Context [hi]",
+        "file": "simple_context_hi_cortexm.rhn",
+    },
+    ar={
+        "name": "Simple Context [ar]",
+        "file": "simple_context_ar_cortexm.rhn",
+    },
+    pl={
+        "name": "Simple Context [pl]",
+        "file": "simple_context_pl_cortexm.rhn",
+    },
+    sv={
+        "name": "Simple Context [sv]",
+        "file": "simple_context_sv_cortexm.rhn",
+    },
+    vn={
+        "name": "Simple Context [vn]",
+        "file": "simple_context_vn_cortexm.rhn",
+    },
+    zh={
+        "name": "Simple Context [zh]",
+        "file": "simple_context_zh_cortexm.rhn",
+    },
+    fa={
+        "name": "Simple Context [fa]",
+        "file": "simple_context_fa_cortexm.rhn",
+    },
+)
