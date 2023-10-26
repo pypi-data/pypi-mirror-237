dbf9ac50b78bcb8d043f35d4f411ab97636478e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:07:12 +0000

    Add package ruleminer/0.1.24/ruleminer-0.1.24-py2.py3-none-any.whl

---
+++ packages/ruleminer/ruleminer-0.1.24-py2.py3-none-any.whl/ruleminer/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for ruleminer."""
+
+__author__ = """Willem Jan Willemse"""
+__email__ = "w.j.willemse@dnb.nl"
+__version__ = "0.1.24"
+
+from .ruleminer import *
+from .utils import *
+from .parser import _quoted_string, _column
+from .metrics import *
+from .const import *
