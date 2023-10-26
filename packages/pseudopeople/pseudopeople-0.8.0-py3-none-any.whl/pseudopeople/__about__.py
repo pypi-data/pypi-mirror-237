bb2cdbeafa4902127027e41ecf1849dcbe84c7c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:08:46 +0000

    Add package pseudopeople/0.8.0/pseudopeople-0.8.0-py3-none-any.whl

---
+++ packages/pseudopeople/pseudopeople-0.8.0-py3-none-any.whl/pseudopeople/__about__.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "pseudopeople"
+__summary__ = "pseudopeople is package which adds noise to simulated census-scale data using standard scientific Python tools."
+__uri__ = "https://github.com/ihmeuw/pseudopeople"
+
+__version__ = "0.8.0"
+
+__author__ = "The pseudopeople developers"
+__email__ = "vivarium.dev@gmail.com"
+
+__license__ = "BSD-3-Clause"
+__copyright__ = f"Copyright 2023 {__author__}"
