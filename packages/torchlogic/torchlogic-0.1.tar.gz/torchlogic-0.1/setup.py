821992e2fe157961588b8d77349b37c603933e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 14:06:56 +0000

    Add package torchlogic/0.1/torchlogic-0.1.tar.gz

---
+++ packages/torchlogic/torchlogic-0.1.tar.gz/torchlogic-0.1/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Felix Petersen',
+    author_email='ads0600@felix-petersen.de',
+    description='None',
+    install_requires=['difflogic'],
+    long_description='Use `difflogic <https://pypi.python.org/pypi/difflogic/>`_ instead.',
+    maintainer='Felix Petersen',
+    maintainer_email='ads0740@felix-petersen.de',
+    name='torchlogic',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/difflogic/',
+    version='0.1',
+    zip_safe=False,
+)
