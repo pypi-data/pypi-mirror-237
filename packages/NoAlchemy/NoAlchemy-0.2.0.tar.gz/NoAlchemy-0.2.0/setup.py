fb38248245f57981a5979ae43fc34f7468f2d608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:08:04 +0000

    Add package NoAlchemy/0.2.0/NoAlchemy-0.2.0.tar.gz

---
+++ packages/NoAlchemy/NoAlchemy-0.2.0.tar.gz/NoAlchemy-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+from noalchemy.version import get_version_string 
+
+setup(
+    name="NoAlchemy",
+    version=get_version_string(),
+    description="A MongoDB Object-Document Mapper (ODM) based on SQLAlchemy.",
+    author="grantUser",
+    url="https://github.com/grantUser/NoAlchemy",
+    packages=find_packages(),
+    install_requires=[
+        "pymongo",
+        "mongomock",
+        "pytest"
+    ],
+)
