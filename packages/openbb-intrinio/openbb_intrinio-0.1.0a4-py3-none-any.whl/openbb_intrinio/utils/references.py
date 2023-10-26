9ec401ae7899656c90058a9183220a02c9dbe639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 04:09:01 +0000

    Add package openbb-intrinio/0.1.0a4/openbb_intrinio-0.1.0a4-py3-none-any.whl

---
+++ packages/openbb-intrinio/openbb_intrinio-0.1.0a4-py3-none-any.whl/openbb_intrinio/utils/references.py
@@ -0,0 +1,13 @@
+from typing import Literal
+
+SOURCES = Literal[
+    "iex",
+    "bats",
+    "bats_delayed",
+    "utp_delayed",
+    "cta_a_delayed",
+    "cta_b_delayed",
+    "intrinio_mx",
+    "intrinio_mx_plus",
+    "delayed_sip",
+]
