c25d8d00388ded4aab5477e8929748d9c463473e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 06:06:24 +0000

    Add package foliage/1.7.0/foliage-1.7.0-py3-none-any.whl

---
+++ packages/foliage/foliage-1.7.0-py3-none-any.whl/foliage/__init__.py
@@ -0,0 +1,35 @@
+'''
+__init__.py for foliage
+
+Copyright
+---------
+
+Copyright (c) 2021-2022 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+# Package metadata
+# .............................................................................
+#
+#  ╭────────────────────── Notice ── Notice ── Notice ─────────────────────╮
+#  |    The following values are automatically updated at every release    |
+#  |    by the Makefile. Manual changes to these values will be lost.      |
+#  ╰────────────────────── Notice ── Notice ── Notice ─────────────────────╯
+
+__version__     = '1.7.0'
+__description__ = 'Foliage: a tool to do bulk changes in FOLIO using the OKAPI API'
+__url__         = 'https://github.com/caltechlibrary/foliage'
+__author__      = 'Mike Hucka'
+__email__       = 'helpdesk@library.caltech.edu'
+__license__     = 'BSD 3-clause license'
+
+
+# Miscellaneous utilities.
+# .............................................................................
+
+def print_version():
+    print(f'{__name__} version {__version__}')
+    print(f'Authors: {__author__}')
+    print(f'URL: {__url__}')
+    print(f'License: {__license__}')
