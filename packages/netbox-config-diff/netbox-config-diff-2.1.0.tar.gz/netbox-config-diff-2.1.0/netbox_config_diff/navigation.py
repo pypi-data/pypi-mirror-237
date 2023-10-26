35ac75243b68b6ad60ed30d401d7ff204c90b614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 16:05:50 +0000

    Add package netbox-config-diff/2.1.0/netbox-config-diff-2.1.0.tar.gz

---
+++ packages/netbox-config-diff/netbox-config-diff-2.1.0.tar.gz/netbox-config-diff-2.1.0/netbox_config_diff/navigation.py
@@ -0,0 +1,46 @@
+from extras.plugins import PluginMenuButton, PluginMenuItem
+from utilities.choices import ButtonColorChoices
+
+
+def get_add_button(model: str) -> PluginMenuButton:
+    return PluginMenuButton(
+        link=f"plugins:netbox_config_diff:{model}_add",
+        title="Add",
+        icon_class="mdi mdi-plus-thick",
+        color=ButtonColorChoices.GREEN,
+        permissions=[f"netbox_config_diff.add_{model}"],
+    )
+
+
+menu_items = (
+    PluginMenuItem(
+        link="plugins:netbox_config_diff:platformsetting_list",
+        link_text="Platform Settings",
+        buttons=[get_add_button("platformsetting")],
+        permissions=["netbox_config_diff.view_platformsetting"],
+    ),
+    PluginMenuItem(
+        link="plugins:netbox_config_diff:configcompliance_list",
+        link_text="Config Compliances",
+        buttons=[],
+        permissions=["netbox_config_diff.view_configcompliance"],
+    ),
+    PluginMenuItem(
+        link="plugins:netbox_config_diff:configurationrequest_list",
+        link_text="Configuration Requests",
+        buttons=[get_add_button("configurationrequest")],
+        permissions=["netbox_config_diff.view_configurationrequest"],
+    ),
+    PluginMenuItem(
+        link="plugins:netbox_config_diff:configurationrequest_job_list",
+        link_text="Jobs",
+        buttons=[],
+        permissions=["core.view_job"],
+    ),
+    PluginMenuItem(
+        link="plugins:netbox_config_diff:substitute_list",
+        link_text="Substitutes",
+        buttons=[get_add_button("substitute")],
+        permissions=["netbox_config_diff.view_substitute"],
+    ),
+)
