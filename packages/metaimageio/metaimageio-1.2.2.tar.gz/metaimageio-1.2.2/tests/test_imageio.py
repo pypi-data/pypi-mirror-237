cc7c924e6ba7fe8aca9f2ba3d1a005ef9e32d66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 06:06:24 +0000

    Add package metaimageio/1.2.2/metaimageio-1.2.2.tar.gz

---
+++ packages/metaimageio/metaimageio-1.2.2.tar.gz/metaimageio-1.2.2/tests/test_imageio.py
@@ -0,0 +1,21 @@
+import metaimageio
+import numpy as np
+import pytest
+try:
+    import imageio.v2 as iio
+except ModuleNotFoundError:
+    import imageio as iio
+
+
+@pytest.fixture(scope='module')
+def imageio():
+    yield metaimageio.imageio()
+
+
+def test_imageio(imageio, filepath):
+    assert imageio == 'MetaImageIO'
+    metaimageio.imageio()
+    a = (100 * np.random.random_sample((4, 3, 2)))
+    iio.imwrite(filepath, a, format='MetaImageIO')
+    b = iio.imread(filepath, format='MetaImageIO')
+    np.testing.assert_almost_equal(b, a)
