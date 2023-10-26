4ed5a58988cd5792fd413329c01a5a2abe252e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-26 05:07:53 +0000

    Add package UncertaintyPlayground/0.1.2/UncertaintyPlayground-0.1.2.tar.gz

---
+++ packages/UncertaintyPlayground/UncertaintyPlayground-0.1.2.tar.gz/UncertaintyPlayground-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+# Load requirements from requirements.txt file
+with open('uncertaintyplayground/requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='UncertaintyPlayground',
+    version='0.1.2',
+    packages=find_packages(),
+    author='Ilia Azizi',
+    author_email='ilia.azizi@unil.ch',
+    description='A Python library for uncertainty estimation in supervised learning tasks',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Unco3892/UncertaintyPlayground', 
+    install_requires=requirements,
+    include_package_data=True,
+    tests_require=[
+        'unittest'
+    ],
+    test_suite='uncertaintyplayground.tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # 'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
