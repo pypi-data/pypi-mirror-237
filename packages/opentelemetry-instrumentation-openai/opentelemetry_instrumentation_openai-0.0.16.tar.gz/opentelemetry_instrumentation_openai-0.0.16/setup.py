b17eb30b838973f14c01a269a8ff1aec617ffe40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 16:10:17 +0000

    Add package opentelemetry-instrumentation-openai/0.0.16/opentelemetry_instrumentation_openai-0.0.16.tar.gz

---
+++ packages/opentelemetry-instrumentation-openai/opentelemetry_instrumentation_openai-0.0.16.tar.gz/opentelemetry_instrumentation_openai-0.0.16/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['openai']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['opentelemetry-api>=1.20.0,<2.0.0',
+ 'opentelemetry-instrumentation>=0.41b0,<0.42',
+ 'opentelemetry-semantic-conventions-ai>=0.0.7,<0.0.8']
+
+setup_kwargs = {
+    'name': 'opentelemetry-instrumentation-openai',
+    'version': '0.0.16',
+    'description': 'OpenTelemetry OpenAI instrumentation',
+    'long_description': 'None',
+    'author': 'Gal Kleinman',
+    'author_email': 'gal@traceloop.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.1,<4',
+}
+
+
+setup(**setup_kwargs)
