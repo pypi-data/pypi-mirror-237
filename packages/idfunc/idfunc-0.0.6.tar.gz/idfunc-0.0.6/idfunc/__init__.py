617b1afd19b322d9b61df306e374acfa036e3a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:06:53 +0000

    Add package idfunc/0.0.6/idfunc-0.0.6.tar.gz

---
+++ packages/idfunc/idfunc-0.0.6.tar.gz/idfunc-0.0.6/idfunc/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.0.6"
+from .core import (passable, identity, IdentityWarning, echo, iden, idfn, noop, idfunc, nocall)
+
+__all__ = [
+    'passable', 'identity', 'IdentityWarning', 
+    'echo', 'iden', 'idfn', 'noop', 'idfunc', 'nocall'
+]
