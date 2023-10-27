82aa0b386d83a4612c7fd6c9761b3682683abe08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 05:09:18 +0000

    Add package dtaianomaly/0.1.1/dtaianomaly-0.1.1.tar.gz

---
+++ packages/dtaianomaly/dtaianomaly-0.1.1.tar.gz/dtaianomaly-0.1.1/dtaianomaly/anomaly_detection/__init__.py
@@ -0,0 +1,6 @@
+
+from .TimeSeriesAnomalyDetector import TimeSeriesAnomalyDetector
+from .utility.Windowing import Windowing
+from .utility.TrainType import TrainType
+
+from .pyod.PyODAnomalyDetector import PyODAnomalyDetector
