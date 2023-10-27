11e772600cfc342c0b4bf651302d457329c3bcb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 15:07:51 +0000

    Add package kfsd/0.0.139/kfsd-0.0.139-py3-none-any.whl

---
+++ packages/kfsd/kfsd-0.0.139-py3-none-any.whl/kfsd/apps/core/management/commands/run.py
@@ -0,0 +1,39 @@
+from kfsd.apps.core.management.commands.baserun import Command as BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Utils Pkg Setup"
+
+    def getConfig(self):
+        return {
+            "kubefacets": {
+                "basic": [self.update_utils_pkg, self.clean_db],
+                "migrate": [
+                    "python manage.py makemigrations",
+                    "python manage.py migrate",
+                ],
+                "env": {
+                    "service_id": "{}".format(self.service_id),
+                    "env": "dev",
+                    "settings_id": "SETTING=Kubefacets",
+                    "outbound_policy_id": "POLICY_TYPE=Events,POLICY_NAME=Outbound",
+                    "inbound_policy_id": "POLICY_TYPE=Events,POLICY_NAME=Inbound",
+                },
+                "setup_fixtures": [
+                    "v1/kubefacets/config/settings.json",
+                    "v1/kubefacets/events/base.json",
+                    "v1/kubefacets/events/events_base_inbound.json",
+                    "v1/kubefacets/events/events_base_outbound.json",
+                    "v1/kubefacets/events/events_filter_inbound.json",
+                    "v1/kubefacets/events/events_filter_outbound.json",
+                    "v1/kubefacets/events/events_inbound_clear_inbound_tbl.json",
+                    "v1/kubefacets/events/events_inbound_clear_outbound_tbl.json",
+                    "v1/kubefacets/events/events_inbound_sso_api_user_upsert.json",
+                    "v1/kubefacets/events/events_inbound_sso_api_verify_upsert.json",
+                    "v1/kubefacets/events/events_outbound_tbl_upsert.json",
+                ],
+                "data_fixtures": [],
+                "msmq": "python manage.py basemsmq -s={}".format(self.service_id),
+                "server": "python manage.py runserver 8008",
+            }
+        }
