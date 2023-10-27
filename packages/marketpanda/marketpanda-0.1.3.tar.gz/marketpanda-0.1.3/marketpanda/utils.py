3a7fa06a06c28e8d90ef2cb829d0e3a8c91940c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:06:05 +0000

    Add package marketpanda/0.1.3/marketpanda-0.1.3.tar.gz

---
+++ packages/marketpanda/marketpanda-0.1.3.tar.gz/marketpanda-0.1.3/marketpanda/utils.py
@@ -0,0 +1,4 @@
+# © Copyright 2023 dnbf.tech GmbH
+# Created by fayssalelmofatiche at 10.10.23
+# Description: different functions for helping with data retrieval from sqlite database and transformations
+
