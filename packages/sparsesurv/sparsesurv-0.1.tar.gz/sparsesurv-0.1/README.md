b7e349d8bb22e356fbac6d49f0d16d26270a5b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 05:11:03 +0000

    Add package sparsesurv/0.1/sparsesurv-0.1.tar.gz

---
+++ packages/sparsesurv/sparsesurv-0.1.tar.gz/sparsesurv-0.1/README.md
@@ -0,0 +1,56 @@
+[![License: BSD3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+[![GitHub Pages](https://img.shields.io/badge/docs-sphinx-blue)](https://boevalab.github.io/sparsesurv/)
+![GitHub all releases](https://img.shields.io/github/downloads/niklexical/sparsesurv/total)
+
+# sparsesurv
+`sparsesurv` [1] is a toolbox for high-dimensional survival analysis. Currently, the package is focused exclusively on knowledge distillation for sparse survival analysis, sometimes also called preconditoning [2, 3]. In the future, we plan to also extend `sparsesurv` to other techniques useful for (high-dimensional) survival analysis that are not commonly available in Python.
+
+## Installation
+The easiest way to install `sparsesurv` is currently via PyPi:
+
+```
+pip install sparsesurv
+```
+
+If you want to install directly from Github, you can also install by cloning the repo, or directly piping the repo to pip:
+
+```
+git clone https://github.com/BoevaLab/sparsesurv/
+cd sparsesurv
+pip install .
+```
+
+```
+pip install git+https://github.com/BoevaLab/sparsesurv.git
+```
+
+If there is sufficient interest, we may also provide a conda package in the future.
+
+## Bug reports and feature requests
+If you have a bug report to make or a feature request for something you would like included in `sparsesurv` in the future, please open a [Github issue](https://github.com/BoevaLab/sparsesurv/issues).
+
+## General questions
+If you have general __questions__, meaning you are unsure about the usage of `sparsesurv`, or have other questions about the package that do not seem like a bug or feature request, please use [Github discussions](https://github.com/BoevaLab/sparsesurv/discussions/).
+
+## Documentation and user guides
+Documentation and user guides are available on [Github pages](https://boevalab.github.io/sparsesurv/index.html).
+
+## Contributing
+We always welcome new contributors to `sparsesurv`. If you're interested in contributing, get in touch with us (see Contact) or have a look at the open issues.
+
+## Contact
+[Nikita Janakarajan](jnikita@ethz.ch)
+
+[David Wissel](dwissel@ethz.ch)
+
+## Citation
+If you use any or part of this package, please cite our work.
+[TODO - add bibtext]
+
+
+## References
+[1] David Wissel, Nikita Janakarajan, Daniel Rowson, Julius Schulte, Xintian Yuan, Valentina Boeva. "sparsesurv: Sparse survival models via knowledge distillation." (2023, under review).
+
+[2] Paul, Debashis, et al. "“Preconditioning” for feature selection and regression in high-dimensional problems." (2008): 1595-1618.
+
+[3] Pavone, Federico, et al. "Using reference models in variable selection." Computational Statistics 38.1 (2023): 349-371.
---
+++ packages/sparsesurv/sparsesurv-0.1.tar.gz/sparsesurv-0.1/setup.py
@@ -0,0 +1,94 @@
+"""Install package."""
+import io
+import re
+
+from pkg_resources import parse_requirements
+from setuptools import find_packages, setup
+
+
+def read_version(filepath: str) -> str:
+    """Read the __version__ variable from the file.
+
+    Args:
+        filepath: probably the path to the root __init__.py
+
+    Returns:
+        the version
+    """
+    match = re.search(
+        r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        io.open(filepath, encoding="utf_8_sig").read(),
+    )
+    if match is None:
+        raise SystemExit("Version number not found.")
+    return match.group(1)
+
+
+# ease installation during development
+vcs = re.compile(r"(git|svn|hg|bzr)\+")
+try:
+    with open("requirements.txt") as fp:
+        VCS_REQUIREMENTS = [
+            str(requirement)
+            for requirement in parse_requirements(fp)
+            if vcs.search(str(requirement))
+        ]
+except FileNotFoundError:
+    # requires verbose flags to show
+    print("requirements.txt not found.")
+    VCS_REQUIREMENTS = []
+
+# TODO: Update these values according to the name of the module.
+setup(
+    name="sparsesurv",
+    version=read_version("sparsesurv/__init__.py"),  # single place for version
+    license='BSD-3-Clause',
+    description="Installable sparsesurv package via pip or source. Modify as needed.",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/BoevaLab/sparsesurv",
+    authors="David Wissel, Nikita Janakarajan",
+    author_email="dwissel@inf.ethz.ch, jnikita@inf.ethz.ch",
+    # the following exclusion is to prevent shipping of tests.
+    # if you do include them, add pytest to the required packages.
+    packages=find_packages(".", exclude=["*tests*"]),
+    package_data={"sparsesurv": ["py.typed"]},
+    extras_require={
+        "vcs": VCS_REQUIREMENTS,
+        "test": ["pytest", "pytest-cov"],
+        "dev": [
+            # tests
+            "pytest",
+            "pytest-cov",
+            # checks
+            "black==21.5b0",
+            "flake8",
+            "mypy",
+            # docs
+            "sphinx",
+            "sphinx-autodoc-typehints",
+            "better-apidoc",
+            "six",
+            "sphinx_rtd_theme",
+            "myst-parser",
+        ],
+    },
+    classifiers=[
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+	'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+	'Operating System :: OS Independent',
+	'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    install_requires=[
+        "numpy>=1.23.4",
+	"pandas>=1.5.1",
+	"celer>=0.7.2",
+	"numba>=0.56.4",
+	"scikit-learn>=1.2.1",
+    ],
+    keywords=['Survival', 'Knowledge Distillation',  'Sparse Survival', 'High Dimensional Data']
+)
