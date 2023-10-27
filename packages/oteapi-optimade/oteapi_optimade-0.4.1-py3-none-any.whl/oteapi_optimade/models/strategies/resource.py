320e7bf3a052c9ed70529d21bf594f035ae02000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 03:05:44 +0000

    Add package oteapi-optimade/0.4.1/oteapi_optimade-0.4.1-py3-none-any.whl

---
+++ packages/oteapi-optimade/oteapi_optimade-0.4.1-py3-none-any.whl/oteapi_optimade/models/strategies/resource.py
@@ -0,0 +1,78 @@
+"""Models specific to the resource strategy."""
+from typing import Any, Dict, List, Literal, Optional
+
+from oteapi.models import ResourceConfig, SessionUpdate
+from pydantic import Field
+
+from oteapi_optimade.models.config import OPTIMADEConfig
+from oteapi_optimade.models.custom_types import OPTIMADEUrl
+
+
+class OPTIMADEResourceConfig(ResourceConfig):
+    """OPTIMADE-specific resource strategy config."""
+
+    accessUrl: OPTIMADEUrl = Field(
+        ...,
+        description="Either a base OPTIMADE URL or a full OPTIMADE URL.",
+    )
+    accessService: Literal[
+        "optimade",
+        "OPTIMADE",
+        "OPTiMaDe",
+        "optimade+dlite",
+        "OPTIMADE+dlite",
+        "OPTiMaDe+dlite",
+        "optimade+DLite",
+        "OPTIMADE+DLite",
+        "OPTiMaDe+DLite",
+    ] = Field(
+        ...,
+        description="The registered strategy name for OPTIMADEResourceStrategy.",
+    )
+    configuration: OPTIMADEConfig = Field(
+        OPTIMADEConfig(),
+        description=(
+            "OPTIMADE configuration. Contains relevant information necessary to "
+            "perform OPTIMADE queries."
+        ),
+    )
+
+
+class OPTIMADEResourceSession(SessionUpdate):
+    """OPTIMADE session for the resource strategy."""
+
+    optimade_config: Optional[OPTIMADEConfig] = Field(
+        None,
+        description=(
+            "OPTIMADE configuration. Contains relevant information necessary to "
+            "perform OPTIMADE queries."
+        ),
+    )
+    optimade_resources: List[Dict[str, Any]] = Field(
+        [],
+        description=(
+            "List of OPTIMADE resources (structures, references, errors, ...) returned"
+            " from the OPTIMADE request."
+        ),
+    )
+    optimade_resource_model: str = Field(
+        "",
+        description=(
+            "Importable path to the resource model to be used to parse the OPTIMADE "
+            "resources in `optimade_resource`. The importable path should be a fully "
+            "importable path to a module separated by a colon (`:`) to then define the "
+            "resource model class name. This means one can then do:\n\n```python\n"
+            "from PACKAGE.MODULE import RESOURCE_CLS\n```\nFrom the value "
+            "`PACKAGE.MODULE:RESOURCE_CLS`"
+        ),
+        regex=(
+            r"^([a-zA-Z][a-zA-Z0-9_]*(\.[a-zA-Z][a-zA-Z0-9_]*)*"  # package.module
+            r":[a-zA-Z][a-zA-Z0-9_]*)?$"  # class
+        ),
+    )
+
+    class Config:
+        """Pydantic configuration for `OPTIMADEResourceSession`."""
+
+        validate_assignment = True
+        arbitrary_types_allowed = True
---
+++ packages/oteapi-optimade/oteapi_optimade-0.4.1-py3-none-any.whl/oteapi_optimade/strategies/parse.py
@@ -0,0 +1,209 @@
+"""Demo strategy class for text/json."""
+import json
+import logging
+from typing import TYPE_CHECKING
+
+from optimade.models import ErrorResponse, Success
+from oteapi.datacache import DataCache
+from oteapi.models import SessionUpdate
+from oteapi.plugins import create_strategy
+from oteapi.plugins.entry_points import StrategyType
+from pydantic import ValidationError
+from pydantic.dataclasses import dataclass
+
+from oteapi_optimade.exceptions import OPTIMADEParseError
+from oteapi_optimade.models import OPTIMADEParseConfig, OPTIMADEParseSession
+from oteapi_optimade.utils import model2dict
+
+if TYPE_CHECKING:  # pragma: no cover
+    from typing import Any, Dict, Optional, Union
+
+
+LOGGER = logging.getLogger("oteapi_optimade.strategies")
+
+
+@dataclass
+class OPTIMADEParseStrategy:
+    """Parse strategy for JSON.
+
+    **Implements strategies**:
+
+    - `("mediaType", "application/vnd.optimade+json")`
+    - `("mediaType", "application/vnd.OPTIMADE+json")`
+    - `("mediaType", "application/vnd.OPTiMaDe+json")`
+    - `("mediaType", "application/vnd.optimade+JSON")`
+    - `("mediaType", "application/vnd.OPTIMADE+JSON")`
+    - `("mediaType", "application/vnd.OPTiMaDe+JSON")`
+    - `("mediaType", "application/vnd.optimade")`
+    - `("mediaType", "application/vnd.OPTIMADE")`
+    - `("mediaType", "application/vnd.OPTiMaDe")`
+
+    """
+
+    parse_config: OPTIMADEParseConfig
+
+    def initialize(self, session: "Optional[Dict[str, Any]]" = None) -> SessionUpdate:
+        """Initialize strategy.
+
+        This method will be called through the `/initialize` endpoint of the OTE-API
+        Services.
+
+        Parameters:
+            session: A session-specific dictionary context.
+
+        Returns:
+            An update model of key/value-pairs to be stored in the session-specific
+            context from services.
+
+        """
+        return SessionUpdate()
+
+    def get(
+        self, session: "Optional[Union[SessionUpdate, Dict[str, Any]]]" = None
+    ) -> OPTIMADEParseSession:
+        """Request and parse an OPTIMADE response using OPT.
+
+        This method will be called through the strategy-specific endpoint of the
+        OTE-API Services.
+
+        Configuration values provided in `resource_config.configuration` take
+        precedence over the derived values from `downloadUrl`.
+
+        Workflow:
+
+        1. Request OPTIMADE response.
+        2. Parse as an OPTIMADE Python tools (OPT) pydantic response model.
+
+        Parameters:
+            session: A session-specific dictionary-like context.
+
+        Returns:
+            An update model of key/value-pairs to be stored in the session-specific
+            context from services.
+
+        """
+        if session and isinstance(session, dict):
+            session = OPTIMADEParseSession(**session)
+        elif session and isinstance(session, SessionUpdate):
+            session = OPTIMADEParseSession(
+                **model2dict(session, exclude_defaults=True, exclude_unset=True)
+            )
+        else:
+            session = OPTIMADEParseSession()
+
+        if session.optimade_config:
+            self.parse_config.configuration.update(
+                model2dict(
+                    session.optimade_config, exclude_defaults=True, exclude_unset=True
+                )
+            )
+
+        cache = DataCache(self.parse_config.configuration.datacache_config)
+        if self.parse_config.downloadUrl in cache:
+            response: "Dict[str, Any]" = cache.get(self.parse_config.downloadUrl)
+        elif (
+            self.parse_config.configuration.datacache_config.accessKey
+            and self.parse_config.configuration.datacache_config.accessKey in cache
+        ):
+            response = cache.get(
+                self.parse_config.configuration.datacache_config.accessKey
+            )
+        else:
+            download_config = self.parse_config.copy()
+            session.update(
+                create_strategy(StrategyType.DOWNLOAD, download_config).initialize(
+                    model2dict(session, exclude_defaults=True, exclude_unset=True)
+                )
+            )
+            session.update(
+                create_strategy(StrategyType.DOWNLOAD, download_config).get(
+                    model2dict(session, exclude_defaults=True, exclude_unset=True)
+                )
+            )
+
+            response = {"json": json.loads(cache.get(session.pop("key")))}
+
+        if (
+            not response.get("ok", True)
+            or (
+                200 > response.get("status_code", 200)
+                or response.get("status_code", 200) >= 300
+            )
+            or "errors" in response.get("json", {})
+        ):
+            # Error response
+            try:
+                response_object = ErrorResponse(**response.get("json", {}))
+            except ValidationError as exc:
+                LOGGER.error(
+                    "Could not validate an error response.\nValidationError: "
+                    "%s\nresponse=%r",
+                    exc,
+                    response,
+                )
+                raise OPTIMADEParseError(
+                    "Could not validate an error response."
+                ) from exc
+        else:
+            # Successful response
+            response_model = self.parse_config.downloadUrl.response_model()
+            if response_model:
+                if not isinstance(response_model, tuple):
+                    response_model = (response_model,)
+                for model_cls in response_model:
+                    try:
+                        response_object = model_cls(**response.get("json", {}))
+                    except ValidationError:
+                        pass
+                    else:
+                        break
+                else:
+                    LOGGER.error(
+                        "Could not validate for an expected response model.\nURL=%r\n"
+                        "response_models=%r\nresponse=%s",
+                        self.parse_config.downloadUrl,
+                        response_model,
+                        response,
+                    )
+                    raise OPTIMADEParseError(
+                        "Could not validate for an expected response model."
+                    )
+            else:
+                # No "endpoint" or unknown
+                try:
+                    response_object = Success(**response.get("json", {}))
+                except ValidationError as exc:
+                    LOGGER.error(
+                        "Unknown or unparseable endpoint.\nValidatonError: %s\n"
+                        "URL=%r\nendpoint=%r\nresponse_model=%r\nresponse=%s",
+                        exc,
+                        self.parse_config.downloadUrl,
+                        self.parse_config.downloadUrl.endpoint,
+                        response_model,
+                        response,
+                    )
+                    raise OPTIMADEParseError(
+                        "Unknown or unparseable endpoint."
+                    ) from exc
+
+        if self.parse_config.configuration.return_object:
+            session.optimade_response_object = response_object
+        else:
+            session.optimade_response = model2dict(response_object)
+
+        if session.optimade_config and session.optimade_config.query_parameters:
+            session = session.copy(
+                update={
+                    "optimade_config": session.optimade_config.copy(
+                        update={
+                            "query_parameters": model2dict(
+                                session.optimade_config.query_parameters,
+                                exclude_defaults=True,
+                                exclude_unset=True,
+                            )
+                        }
+                    )
+                }
+            )
+
+        return session
