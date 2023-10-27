53cf82fb83117b2aa59e9671daba2d52c75ad32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 06:08:41 +0000

    Add package reach-commons/0.3.4/reach_commons-0.3.4.tar.gz

---
+++ packages/reach-commons/reach_commons-0.3.4.tar.gz/reach_commons-0.3.4/reach_commons/reach_aws/sqs/client.py
@@ -0,0 +1,118 @@
+import json
+import logging
+from decimal import Decimal
+from functools import cached_property
+
+import boto3
+from botocore.exceptions import ClientError
+
+from reach_commons.reach_aws.sqs.exceptions import (SQSClientPublishError,
+                                                    SQSClientTopicNotFound)
+
+logger = logging.getLogger(__name__)
+
+MESSAGE_ATTRIBUTES_TYPE = {
+    str: "String",
+    int: "Number",
+    Decimal: "Number",
+    float: "Number",
+    bytes: "Binary",
+    list: "String.Array",
+    tuple: "String.Array",
+}
+
+FALLBACK_MESSAGE_ATTRIBUTES_TYPE = "String"
+
+
+# noinspection PyMethodMayBeStatic
+class BaseSQSClient:
+    def __init__(
+        self,
+        topic_name=None,
+        log_level="debug",
+        region_name="us-east-1",
+        profile_name=None,
+    ):
+        self.topic_name = topic_name
+        self.log_level = log_level
+        self.region_name = region_name
+        self.profile_name = profile_name
+
+    @property
+    def log(self):
+        return getattr(logger, self.log_level)
+
+    @staticmethod
+    def _prepare_message_attributes(attributes):
+        message_attributes = {}
+        for key, value in attributes.items():
+            attr_type = MESSAGE_ATTRIBUTES_TYPE.get(
+                type(value), FALLBACK_MESSAGE_ATTRIBUTES_TYPE
+            )
+            value_key = "BinaryValue" if attr_type == "Binary" else "StringValue"
+            if attr_type in ("String.Array", "Number"):
+                value = json.dumps(value)
+            elif attr_type == "String":
+                value = str(value)
+            message_attributes[key] = {
+                "DataType": attr_type,
+                value_key: value,
+            }
+        return message_attributes
+
+    def handle_exception(self, exc, message_data, message_attributes):
+        error_msg = (
+            "error_publishing_message, "
+            "topic_name={}, "
+            "message_data={!r}, "
+            "message_attributes={!r}, "
+            "error={!r}".format(self.topic_name, message_data, message_attributes, exc)
+        )
+        logger.error(error_msg)
+        if exc.response["Error"]["Code"] == "NotFound":
+            raise SQSClientTopicNotFound(error_msg)
+        raise SQSClientPublishError(error_msg)
+
+
+class SQSClient(BaseSQSClient):
+    @cached_property
+    def client(self):
+        session = boto3.Session(
+            region_name=self.region_name, profile_name=self.profile_name
+        )
+        return session.client("sqs")
+
+    def publish(self, message_data, delay_seconds=1, message_attributes=None):
+        message_attributes = message_attributes or {}
+        message_attributes = self._prepare_message_attributes(message_attributes)
+
+        logger.info(
+            "topic_name={}, "
+            "message_data={!r}, "
+            "message_attributes={!r}".format(
+                self.topic_name, message_data, message_attributes
+            )
+        )
+
+        message = json.dumps(message_data)
+
+        try:
+            self.client.send_message(
+                QueueUrl=self.topic_name,
+                MessageBody=message,
+                DelaySeconds=delay_seconds,
+                MessageAttributes=message_attributes,
+            )
+        except ClientError as exc:
+            self.handle_exception(exc, message_data, message_attributes)
+
+        self.log(
+            "published_message, "
+            "topic_name={}, "
+            "message={!r}, "
+            "message_attributes={!r}".format(
+                self.topic_name, message, message_attributes
+            )
+        )
+
+        return True
