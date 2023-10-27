cde5e76b2a1d2af32f5ef73f2d327bc01b1ac43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 03:06:18 +0000

    Add package kurbopy/0.10.40/kurbopy-0.10.40.tar.gz

---
+++ packages/kurbopy/kurbopy-0.10.40.tar.gz/kurbopy-0.10.40/src/pathseg.rs
@@ -0,0 +1,42 @@
+use crate::cubicbez::CubicBez;
+use crate::line::Line;
+use crate::quadbez::QuadBez;
+use kurbo::PathSeg as KPathSeg;
+use pyo3::prelude::*;
+
+#[pyclass(subclass)]
+#[derive(Clone, Debug)]
+pub struct PathSeg(pub KPathSeg);
+
+impl From<KPathSeg> for PathSeg {
+    fn from(p: KPathSeg) -> Self {
+        Self(p)
+    }
+}
+
+#[pymethods]
+impl PathSeg {
+    fn as_line(&self) -> Option<Line> {
+        if let KPathSeg::Line(l) = self.0 {
+            Some(l.into())
+        } else {
+            None
+        }
+    }
+
+    fn as_quad(&self) -> Option<QuadBez> {
+        if let KPathSeg::Quad(l) = self.0 {
+            Some(l.into())
+        } else {
+            None
+        }
+    }
+
+    fn as_cubic(&self) -> Option<CubicBez> {
+        if let KPathSeg::Cubic(l) = self.0 {
+            Some(l.into())
+        } else {
+            None
+        }
+    }
+}
