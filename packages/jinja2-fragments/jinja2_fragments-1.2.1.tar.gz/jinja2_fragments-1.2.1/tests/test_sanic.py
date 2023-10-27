424c4b43113f842cf57d7d2b910ea703d760202f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 14:08:31 +0000

    Add package jinja2-fragments/1.2.1/jinja2_fragments-1.2.1.tar.gz

---
+++ packages/jinja2-fragments/jinja2_fragments-1.2.1.tar.gz/jinja2_fragments-1.2.1/tests/test_sanic.py
@@ -0,0 +1,36 @@
+import pytest
+from conftest import SANIC_ENABLED
+
+if SANIC_ENABLED:
+    from sanic_testing.testing import SanicTestClient
+
+
+@pytest.mark.skipif(not SANIC_ENABLED, reason="Sanic requires python 3.8 or higher")
+class TestSanicRenderBlock:
+    @pytest.mark.parametrize(
+        "only_content, html_name",
+        [
+            (False, "simple_page.html"),
+            (True, "simple_page_content.html"),
+        ],
+    )
+    def test_simple_page(
+        self, sanic_client: "SanicTestClient", get_html, only_content, html_name
+    ):
+        _, response = sanic_client.get(
+            "/simple_page", params={"only_content": only_content}
+        )
+        html = get_html(html_name)
+        assert html == response.text
+
+    @pytest.mark.parametrize(
+        "route, html_name",
+        [
+            ("/nested_content", "nested_blocks_and_variables_content.html"),
+            ("/nested_inner", "nested_blocks_and_variables_inner.html"),
+        ],
+    )
+    def test_nested_page(self, sanic_client, get_html, route, html_name):
+        _, response = sanic_client.get(route)
+        html = get_html(html_name)
+        assert html == response.text
