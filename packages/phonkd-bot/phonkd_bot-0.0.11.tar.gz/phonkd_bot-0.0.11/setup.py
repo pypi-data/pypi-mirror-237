4a560f61920c103d075a5ee463752676780cf394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 17:06:52 +0000

    Add package phonkd-bot/0.0.11/phonkd_bot-0.0.11.tar.gz

---
+++ packages/phonkd-bot/phonkd_bot-0.0.11.tar.gz/phonkd_bot-0.0.11/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.11'
+DESCRIPTION = 'A framework that makes discord bot programming easy'
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# Setting up
+setup(
+    name="phonkd_bot",
+    version=VERSION,
+    author="Phonki",
+    author_email="<phonkibusiness@gmail.com>",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["discord", "asyncio"],
+    keywords=['python', 'discord'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
