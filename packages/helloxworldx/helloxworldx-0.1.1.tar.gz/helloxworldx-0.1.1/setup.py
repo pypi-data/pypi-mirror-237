bb082343adbfc79d8df2b40871a0a5a37689c56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 03:07:40 +0000

    Add package helloxworldx/0.1.1/helloxworldx-0.1.1.tar.gz

---
+++ packages/helloxworldx/helloxworldx-0.1.1.tar.gz/helloxworldx-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['helloxworldx']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'helloxworldx',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': 'wow this is my project wow\n',
+    'author': 'alok',
+    'author_email': 'alokmilenium@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
