d37d44725366955aee406fb1566662ac49ba8a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 15:06:52 +0000

    Add package genomicspy/0.1.1.dev4/genomicspy-0.1.1.dev4.tar.gz

---
+++ packages/genomicspy/genomicspy-0.1.1.dev4.tar.gz/genomicspy-0.1.1.dev4/README.rst
@@ -0,0 +1,37 @@
+==========
+genomicspy
+==========
+
+
+.. image:: https://img.shields.io/pypi/v/genomicspy.svg
+        :target: https://pypi.python.org/pypi/genomicspy
+
+.. image:: https://img.shields.io/travis/vivianleung/genomicspy.svg
+        :target: https://travis-ci.com/vivianleung/genomicspy
+
+.. image:: https://readthedocs.org/projects/genomicspy/badge/?version=latest
+        :target: https://genomicspy.readthedocs.io/en/latest/?version=latest
+        :alt: Documentation Status
+
+
+
+
+VTools for manipulating vcfs doing genomics work
+
+
+* Free software: MIT license
+* Documentation: https://genomicspy.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ packages/genomicspy/genomicspy-0.1.1.dev4.tar.gz/genomicspy-0.1.1.dev4/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+version = '0.1.1dev4'
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+#with open('requirements.txt') as requirements_file:
+#    requirements = [line.strip() for line in requirements_file]
+#test_requirements = [ ]
+
+setup(
+    author="Vivian Leung",
+    author_email='leung.vivian.w@gmail.com',
+    python_requires='>=3.10',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+    description="Tools for manipulating vcfs doing genomics work",
+    entry_points={
+        'console_scripts': [
+            'genomicspy=genomicspy.cli:main',
+        ],
+    },
+ #   install_requires=requirements,
+    license="MIT License",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords=['genomicspy', 'vcf', 'genomics'],
+    name='genomicspy',
+    packages=find_packages(include=['genomicspy', 'genomicspy.*']),
+    test_suite='tests',
+#    tests_require=test_requirements,
+    url='https://github.com/vivianleung/genomicspy',
+    version=version,
+    zip_safe=False,
+)
