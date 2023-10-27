67a9643a3a9b3cfd2678f01f5c5ba1a09cd1e27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 14:07:10 +0000

    Add package lea-cli/0.2.2/lea_cli-0.2.2.tar.gz

---
+++ packages/lea-cli/lea_cli-0.2.2.tar.gz/lea_cli-0.2.2/lea/clients/bigquery.py
@@ -0,0 +1,138 @@
+from __future__ import annotations
+
+import os
+
+import pandas as pd
+import sqlglot
+
+import lea
+
+from .base import Client
+
+
+class BigQuery(Client):
+    def __init__(self, credentials, location, project_id, dataset_name, username):
+        from google.cloud import bigquery
+
+        self.project_id = project_id
+        self.location = location
+        self.client = bigquery.Client(credentials=credentials)
+        self._dataset_name = dataset_name
+        self.username = username
+
+    def prepare(self, console):
+        dataset = self.create_dataset()
+        console.log(f"Created dataset {dataset.dataset_id}")
+
+    @property
+    def sqlglot_dialect(self):
+        return sqlglot.dialects.Dialects.BIGQUERY
+
+    @property
+    def dataset_name(self):
+        return f"{self._dataset_name}_{self.username}" if self.username else self._dataset_name
+
+    def create_dataset(self):
+        from google.cloud import bigquery
+
+        dataset_ref = bigquery.DatasetReference(
+            project=self.project_id, dataset_id=self.dataset_name
+        )
+        dataset = bigquery.Dataset(dataset_ref)
+        dataset.location = self.location
+        dataset = self.client.create_dataset(dataset, exists_ok=True)
+        return dataset
+
+    def teardown(self):
+        from google.cloud import bigquery
+
+        dataset_ref = self.client.dataset(self.dataset_name)
+        dataset = bigquery.Dataset(dataset_ref)
+        dataset.location = self.location
+        self.client.delete_dataset(dataset, delete_contents=True, not_found_ok=True)
+
+    def _make_job(self, view: lea.views.SQLView):
+        query = view.query.replace(f"{self._dataset_name}.", f"{self.dataset_name}.")
+
+        return self.client.create_job(
+            {
+                "query": {
+                    "query": query,
+                    "destinationTable": {
+                        "projectId": self.project_id,
+                        "datasetId": self.dataset_name,
+                        "tableId": f"{self._make_view_path(view).split('.', 1)[1]}",
+                    },
+                    "createDisposition": "CREATE_IF_NEEDED",
+                    "writeDisposition": "WRITE_TRUNCATE",
+                },
+                "labels": {
+                    "job_dataset": self.dataset_name,
+                    "job_schema": view.schema,
+                    "job_table": f"{self._make_view_path(view).split('.', 1)[1]}",
+                    "job_username": self.username,
+                    "job_is_github_actions": "GITHUB_ACTIONS" in os.environ,
+                },
+            }
+        )
+
+    def _create_sql(self, view: lea.views.SQLView):
+        job = self._make_job(view)
+        job.result()
+
+    def _create_python(self, view: lea.views.PythonView):
+        from google.cloud import bigquery
+
+        dataframe = self._load_python(view)
+
+        job_config = bigquery.LoadJobConfig(
+            schema=[],
+            write_disposition="WRITE_TRUNCATE",
+        )
+
+        job = self.client.load_table_from_dataframe(
+            dataframe,
+            f"{self.project_id}.{self._make_view_path(view)}",
+            job_config=job_config,
+        )
+        job.result()
+
+    def _load_sql(self, view: lea.views.SQLView) -> pd.DataFrame:
+        query = view.query
+        if self.username:
+            query = query.replace(f"{self._dataset_name}_{self.username}.", f"{self.dataset_name}.")
+        return pd.read_gbq(query, credentials=self.client._credentials)
+
+    def list_existing_view_names(self):
+        return [
+            table.table_id.split("__", 1) for table in self.client.list_tables(self.dataset_name)
+        ]
+
+    def delete_view(self, view: lea.views.View):
+        self.client.delete_table(f"{self.project_id}.{self._make_view_path(view)}")
+
+    def get_columns(self, schema=None) -> pd.DataFrame:
+        schema = schema or self.dataset_name
+        query = f"""
+        SELECT
+            table_name AS table,
+            column_name AS column,
+            data_type AS type
+        FROM {schema}.INFORMATION_SCHEMA.COLUMNS
+        """
+        return self._load_sql(
+            lea.views.GenericSQLView(
+                schema=None, name=None, query=query, sqlglot_dialect=self.sqlglot_dialect
+            )
+        )
+
+    def _make_view_path(self, view: lea.views.View) -> str:
+        return f"{self.dataset_name}.{view.schema}{lea._SEP}{view.name}"
+
+    def make_test_unique_column(self, view: lea.views.View, column: str) -> str:
+        return f"""
+        SELECT {column}, COUNT(*) AS n
+        FROM {self._make_view_path(view)}
+        GROUP BY {column}
+        HAVING n > 1
+        """
---
+++ packages/lea-cli/lea_cli-0.2.2.tar.gz/lea_cli-0.2.2/lea/clients/duckdb.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+
+import os
+
+import duckdb
+import pandas as pd
+import sqlglot
+
+import lea
+
+from .base import Client
+
+
+class DuckDB(Client):
+    def __init__(self, path: str, username: str | None):
+        self.path = path
+        self.username = username
+        self.con = duckdb.connect(self.path)
+
+    @property
+    def sqlglot_dialect(self):
+        return sqlglot.dialects.Dialects.DUCKDB
+
+    def prepare(self, views, console):
+        schemas = set(
+            f"{view.schema}_{self.username}" if self.username else view.schema for view in views
+        )
+        for schema in schemas:
+            self.con.sql(f"CREATE SCHEMA IF NOT EXISTS {schema}")
+            console.log(f"Created schema {schema}")
+
+    def _create_python(self, view: lea.views.PythonView):
+        dataframe = self._load_python(view)  # noqa: F841
+        self.con.sql(
+            f"CREATE OR REPLACE TABLE {self._make_view_path(view)} AS SELECT * FROM dataframe"
+        )
+
+    def _create_sql(self, view: lea.views.SQLView):
+        query = view.query
+        if self.username:
+            for schema in {schema for schema, *_ in view.dependencies}:
+                query = query.replace(f"{schema}.", f"{schema}_{self.username}.")
+        self.con.sql(f"CREATE OR REPLACE TABLE {self._make_view_path(view)} AS ({query})")
+
+    def _load_sql(self, view: lea.views.SQLView):
+        query = view.query
+        if self.username:
+            for schema in {schema for schema, *_ in view.dependencies}:
+                query = query.replace(f"{schema}.", f"{schema}_{self.username}.")
+        return self.con.cursor().sql(query).df()
+
+    def delete_view(self, view: lea.views.View):
+        self.con.sql(f"DROP TABLE IF EXISTS {self._make_view_path(view)}")
+
+    def teardown(self):
+        os.remove(self.path)
+
+    def list_existing_view_names(self) -> list[tuple[str, str]]:
+        results = duckdb.sql("SELECT table_schema, table_name FROM information_schema.tables").df()
+        return [(r["table_schema"], r["table_name"]) for r in results.to_dict(orient="records")]
+
+    def get_columns(self, schema=None) -> pd.DataFrame:
+        query = """
+        SELECT
+            table_schema || '.' || table_name AS view_name,
+            column_name AS column,
+            data_type AS type
+        FROM information_schema.columns
+        """
+        return self.con.sql(query).df()
+
+    def _make_view_path(self, view: lea.views.View) -> str:
+        schema, *leftover = view.key
+        schema = f"{schema}_{self.username}" if self.username else schema
+        return f"{schema}.{lea._SEP.join(leftover)}"
+
+    def make_test_unique_column(self, view: lea.views.View, column: str) -> str:
+        schema, *leftover = view.key
+        return f"""
+        SELECT {column}, COUNT(*) AS n
+        FROM {f"{schema}.{lea._SEP.join(leftover)}"}
+        GROUP BY {column}
+        HAVING n > 1
+        """
