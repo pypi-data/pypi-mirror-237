a5268597123a8c90554f16faf3058ef5077b2c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:07:43 +0000

    Add package beautiful-rainbow/1.0.0/beautiful_rainbow-1.0.0.tar.gz

---
+++ packages/beautiful-rainbow/beautiful_rainbow-1.0.0.tar.gz/beautiful_rainbow-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='beautiful_rainbow',
+    version='1.0.0',
+    author='ilco',
+    description='A very basic package practice.',
+    packages=find_packages(),
+    install_requires=[],
+)
