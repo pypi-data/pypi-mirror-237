1bda2c23af2781e72820cd5f279aedd12564bbbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 16:09:07 +0000

    Add package DLMS-SPODES/0.47.1/DLMS_SPODES-0.47.1.tar.gz

---
+++ packages/DLMS-SPODES/DLMS_SPODES-0.47.1.tar.gz/DLMS_SPODES-0.47.1/test/test_hdlc.py
@@ -0,0 +1,25 @@
+import unittest
+from src.DLMS_SPODES.hdlc import frame
+
+
+class TestType(unittest.TestCase):
+    def test_Address(self):
+        ad1 = frame.Address(
+            upper_address=0x1,
+            lower_address=None)
+        self.assertEqual(ad1.content, b'\x03', "upper only")
+        ad2 = frame.Address(content=b'\x07')
+        self.assertEqual((ad2.upper, ad2.lower), (3, None), "1length from content")
+        ad3 = frame.Address(
+            upper_address=0x1,
+            lower_address=None,
+            length=2
+        )
+        self.assertEqual(ad3.content, b'\x02\x01', "upper only")
+        self.assertEqual((ad3.upper, ad3.lower), (1, 0), "lower set to 0")
+        ad4 = frame.Address(
+            upper_address=0x1,
+            lower_address=None,
+            length=4
+        )
+        self.assertEqual(ad4.content, b'\x00\x02\x00\x01', "upper only")
