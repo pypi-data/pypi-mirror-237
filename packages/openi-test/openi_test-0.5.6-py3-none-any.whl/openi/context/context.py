7c59d2b0a4780172c4f621c4fa80788833cb780b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 16:10:18 +0000

    Add package openi-test/0.5.6/openi_test-0.5.6-py3-none-any.whl

---
+++ packages/openi-test/openi_test-0.5.6-py3-none-any.whl/openi/context/context.py
@@ -0,0 +1,19 @@
+from ..cloudbrain.download import download_dataset, download_pretrain_model
+from ..cloudbrain.upload import upload_output
+
+class OpeniContext:
+    def __init__(self, dataset_path, pretrain_model_path):
+        self.dataset_path = dataset_path
+        self.pretrain_model_path = pretrain_model_path
+        self.output_path = None
+
+    def upload_openi(self):
+        return upload_output(self.output_path)
+        
+def prepare():
+    # 在prepare函数内部下载数据和模型，并初始化OpeniContext对象
+    dataset_path = download_dataset()
+    pretrain_model_path = download_pretrain_model()
+    
+    # 创建并返回OpeniContext对象
+    return OpeniContext(dataset_path, pretrain_model_path)
