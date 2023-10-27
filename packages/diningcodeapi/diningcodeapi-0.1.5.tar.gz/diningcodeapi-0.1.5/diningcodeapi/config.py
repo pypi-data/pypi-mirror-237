f302763f491aea54dc5ee82bc1e16d13f803b323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 15:07:05 +0000

    Add package diningcodeapi/0.1.5/diningcodeapi-0.1.5.tar.gz

---
+++ packages/diningcodeapi/diningcodeapi-0.1.5.tar.gz/diningcodeapi-0.1.5/diningcodeapi/config.py
@@ -0,0 +1,7 @@
+HEADERS = {
+    'referer' : 'https://www.diningcode.com/',
+    "content-type":"application/x-www-form-urlencoded",
+    'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'
+}
+
+MAX_PAGE = 2000
