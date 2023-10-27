c88c1fb731fab26459c95af17e200c5a8e4a7a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:07:20 +0000

    Add package deltatextsplitter/0.0.1/deltatextsplitter-0.0.1-py3-none-any.whl

---
+++ packages/deltatextsplitter/deltatextsplitter-0.0.1-py3-none-any.whl/deltatextsplitter/__init__.py
@@ -0,0 +1,5 @@
+# import modules and classes you want to be available for
+# installation. Be sure to add them to the relevant child-inits as well
+# and be sure to use relative imports as from this directory!
+from .Source.deltatextsplitter import deltatextsplitter
+
