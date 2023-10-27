35a930262872205c184077a2c4668d1ae7efb968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 06:08:07 +0000

    Add package mhi-cosim/1.1.3/mhi_cosim-1.1.3-cp36-cp36m-win_amd64.whl

---
+++ packages/mhi-cosim/mhi_cosim-1.1.3-cp36-cp36m-win_amd64.whl/mhi/cosim/__main__.py
@@ -0,0 +1,8 @@
+import mhi.cosim
+
+def show_version():
+    print("MHI Cosim Library v{}".format(mhi.cosim.VERSION))
+    print("(c) Manitoba Hydro International Ltd.")
+    print()
+
+show_version()
