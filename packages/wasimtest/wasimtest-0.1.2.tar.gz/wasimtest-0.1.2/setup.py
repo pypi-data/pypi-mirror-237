36a4e6c1f018491aa459c68c4ebb395997b91ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 03:06:46 +0000

    Add package wasimtest/0.1.2/wasimtest-0.1.2.tar.gz

---
+++ packages/wasimtest/wasimtest-0.1.2.tar.gz/wasimtest-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="wasimtest",
+    version="0.1.2",
+    packages=["published",],
+    install_requires=[],
+    license="MIT",
+    url="https://bitbucket.org/northbay/generative_ai/",
+    author="Python Supply",
+    author_email="contact@python.supply",
+    description="Python library that serves as an example/template "+\
+                "for a package publishing guide.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
