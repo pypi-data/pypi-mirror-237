f9707f26de2b269d8f6f3a69f630bfdfa7da8cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 16:09:33 +0000

    Add package seleniumbase/4.20.7/seleniumbase-4.20.7.tar.gz

---
+++ packages/seleniumbase/seleniumbase-4.20.7.tar.gz/seleniumbase-4.20.7/examples/boilerplates/samples/google_test.py
@@ -0,0 +1,16 @@
+"""google.com example test that uses page objects"""
+from seleniumbase import BaseCase
+from .google_objects import HomePage, ResultsPage
+
+
+class GoogleTests(BaseCase):
+    def test_google_dot_com(self):
+        self.open("https://google.com/ncr")
+        self.sleep(0.4)
+        self.save_screenshot_to_logs()  # ("./latest_logs" folder)
+        self.sleep(0.2)
+        self.type(HomePage.search_box, "github.com")
+        self.assert_element(HomePage.search_button)
+        self.assert_element(HomePage.feeling_lucky_button)
+        self.click(HomePage.search_button)
+        self.assert_text("github.com", ResultsPage.search_results)
---
+++ packages/seleniumbase/seleniumbase-4.20.7.tar.gz/seleniumbase-4.20.7/examples/boilerplates/samples/test_page_objects.py
@@ -0,0 +1,37 @@
+"""An example using the Classic Page Object Model."""
+from seleniumbase import BaseCase
+BaseCase.main(__name__, __file__)
+
+
+class GooglePage:
+    def go_to_google(self, sb):
+        sb.open("https://google.com/ncr")
+        sb.sleep(0.1)
+        sb.hide_elements('iframe')  # Hide "Sign in" pop-up
+        sb.sleep(0.2)
+
+    def do_search(self, sb, search_term):
+        sb.click('[title="Search"]')
+        sb.type('[title="Search"]', search_term + "\n")
+
+    def click_search_result(self, sb, content):
+        sb.click('a:contains("%s")' % content)
+
+
+class SeleniumBaseIOPage:
+    def do_search_and_click(self, sb, search_term):
+        sb.sleep(0.05)
+        sb.type('form[name="search"] input', search_term)
+        sb.click("li.md-search-result__item h1:contains(%s)" % search_term)
+
+
+class MyTests(BaseCase):
+    def test_page_objects(self):
+        search_term = "SeleniumBase.io Docs"
+        expected_text = "SeleniumBase"
+        GooglePage().go_to_google(self)
+        GooglePage().do_search(self, search_term)
+        self.assert_text(expected_text, "#search")
+        GooglePage().click_search_result(self, expected_text)
+        SeleniumBaseIOPage().do_search_and_click(self, "Dashboard")
+        self.assert_text("Dashboard", "main h1")
