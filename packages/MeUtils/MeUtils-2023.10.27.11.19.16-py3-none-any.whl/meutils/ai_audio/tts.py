834c2342459be7adbcef3b7b93b7ae7bb1e79405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 15:06:59 +0000

    Add package MeUtils/2023.10.27.11.19.16/MeUtils-2023.10.27.11.19.16-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2023.10.27.11.19.16-py3-none-any.whl/meutils/ai_audio/tts.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : tts
+# @Time         : 2023/10/25 18:21
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : https://www.qianduan.shop/blogs/detail/140
+# https://baijiahao.baidu.com/s?id=1762431177147996323&wfr=spider&for=pc
+# https://github.com/pndurette/gTTS
+
+from meutils.pipe import *
