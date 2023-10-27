23bd44c38fcbd8d7807ef47d807333630811d2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 15:06:38 +0000

    Add package general-sam/0.4.2/general_sam-0.4.2.tar.gz

---
+++ packages/general-sam/general_sam-0.4.2.tar.gz/general_sam-0.4.2/src/lib.rs
@@ -0,0 +1,14 @@
+pub mod sam;
+pub mod trie;
+pub mod utils;
+
+use pyo3::prelude::*;
+
+#[pymodule]
+fn general_sam(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<trie::TrieNode>()?;
+    m.add_class::<trie::Trie>()?;
+    m.add_class::<sam::GeneralSAMState>()?;
+    m.add_class::<sam::GeneralSAM>()?;
+    Ok(())
+}
