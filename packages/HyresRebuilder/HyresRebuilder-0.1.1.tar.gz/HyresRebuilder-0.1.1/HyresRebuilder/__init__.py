dc3f7c3755f0e85664edf38d3bfa3787904117ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 05:09:50 +0000

    Add package HyresRebuilder/0.1.1/HyresRebuilder-0.1.1.tar.gz

---
+++ packages/HyresRebuilder/HyresRebuilder-0.1.1.tar.gz/HyresRebuilder-0.1.1/HyresRebuilder/__init__.py
@@ -0,0 +1,14 @@
+""" HyresRebuilder
+Written by Shanlong Li
+
+Python modules
+----------------
+The package consists of the following Python modules:
+* HyresRebuilder
+* Rotamer
+"""
+__version__ = "0.1.1"
+from .Rebuilder import *
+from .Rotamer import *
+from .base import *
+
