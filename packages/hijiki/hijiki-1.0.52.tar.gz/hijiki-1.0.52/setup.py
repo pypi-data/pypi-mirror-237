96973c098f3ba80c2ec298214bee2c8c4546eaa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:06:06 +0000

    Add package hijiki/1.0.52/hijiki-1.0.52.tar.gz

---
+++ packages/hijiki/hijiki-1.0.52.tar.gz/hijiki-1.0.52/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hijiki",
+    version="1.0.52",
+    description="Python Rabbit wrapper library to simplify to use Exchanges and Queues with decorators",
+    author="Leandro Vilson Battisti",
+    keywords=['Celery', 'Kombu', 'RabbitMQ', 'decorator'],
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
