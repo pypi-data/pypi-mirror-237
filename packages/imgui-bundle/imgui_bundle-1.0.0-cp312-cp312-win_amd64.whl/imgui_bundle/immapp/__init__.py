8ef8fd3281fe9cd78026e39f75e20501d4ae1c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 16:09:54 +0000

    Add package imgui-bundle/1.0.0/imgui_bundle-1.0.0-cp312-cp312-win_amd64.whl

---
+++ packages/imgui-bundle/imgui_bundle-1.0.0-cp312-cp312-win_amd64.whl/imgui_bundle/immapp/__init__.py
@@ -0,0 +1,16 @@
+# Part of ImGui Bundle - MIT License - Copyright (c) 2022-2023 Pascal Thomet - https://github.com/pthom/imgui_bundle
+from imgui_bundle._imgui_bundle import immapp_cpp as immapp_cpp
+from imgui_bundle._imgui_bundle.immapp_cpp import *
+
+from imgui_bundle.immapp import icons_fontawesome
+from imgui_bundle.immapp.immapp_utils import (
+    static as static,
+    run_anon_block as run_anon_block,
+)
+from imgui_bundle.immapp.immapp_notebook import run_nb as run_nb
+
+
+from imgui_bundle._imgui_bundle.hello_imgui import (
+    RunnerParams as RunnerParams,
+    SimpleRunnerParams as SimpleRunnerParams,
+)
