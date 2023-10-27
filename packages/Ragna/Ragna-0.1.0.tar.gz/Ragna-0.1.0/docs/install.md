723fe3d7686e8a00906a7be9b365e2df2a02fbf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 14:08:16 +0000

    Add package Ragna/0.1.0/Ragna-0.1.0.tar.gz

---
+++ packages/Ragna/Ragna-0.1.0.tar.gz/Ragna-0.1.0/docs/install.md
@@ -0,0 +1,33 @@
+# Installation
+
+## Prerequisites
+
+You need Python 3.9 or above in your working environment to use Ragna.
+
+## Install Ragna
+
+You can install Ragna with `pip`:
+
+```bash
+pip install ragna
+```
+
+If you want to install all optional dependencies, append `[builtin]`:
+
+```bash
+pip install ragna[builtin]
+```
+
+And, upgrade to latest versions with:
+
+```bash
+pip install --upgrade ragna
+```
+
+<!-- Add conda and conda-forge if/when available -->
+
+## Verify your installation
+
+```
+ragna --version
+```
---
+++ packages/Ragna/Ragna-0.1.0.tar.gz/Ragna-0.1.0/ragna/_utils.py
@@ -0,0 +1,85 @@
+import functools
+import threading
+from typing import Any, Callable
+from urllib.parse import SplitResult, urlsplit, urlunsplit
+
+
+def fix_module(globals: dict[str, Any]) -> None:
+    """Fix the __module__ attribute on public objects to hide internal structure.
+
+    Put the following snippet at the end of public `__init__.py` files of ragna
+    subpackages. This will hide any internally private structure.
+
+    ```python
+    # isort: split
+
+    from ragna._utils import fix_module
+
+    fix_module(globals())
+    del fix_module
+    ```
+    """
+    for name, obj in globals.items():
+        if name.startswith("_"):
+            continue
+
+        obj.__module__ = globals["__package__"]
+
+
+def _replace_hostname(split_result: SplitResult, hostname: str) -> SplitResult:
+    # This is a separate function, since hostname is not an element of the SplitResult
+    # namedtuple, but only a property. Thus, we need to replace the netloc item, from
+    # which the hostname is generated.
+    if split_result.port is None:
+        netloc = hostname
+    else:
+        netloc = f"{hostname}:{split_result.port}"
+    return split_result._replace(netloc=netloc)
+
+
+def get_origins(url: str) -> list[str]:
+    origins = [url]
+
+    # Since localhost is an alias for 127.0.0.1, we allow both so users and developers
+    # don't need to worry about it.
+    components = urlsplit(url)
+    if components.hostname == "127.0.0.1":
+        origins.append(urlunsplit(_replace_hostname(components, "localhost")))
+    elif components.hostname == "localhost":
+        origins.append(urlunsplit(_replace_hostname(components, "127.0.0.1")))
+
+    return origins
+
+
+def timeout_after(
+    seconds: float = 5, *, message: str = ""
+) -> Callable[[Callable], Callable]:
+    timeout = f"Timeout after {seconds:.1f} seconds"
+    message = timeout if message else f"{timeout}: {message}"
+
+    def decorator(fn: Callable) -> Callable:
+        @functools.wraps(fn)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            result: Any = TimeoutError(message)
+
+            def target() -> None:
+                nonlocal result
+                try:
+                    result = fn(*args, **kwargs)
+                except Exception as exc:
+                    result = exc
+
+            thread = threading.Thread(target=target)
+            thread.daemon = True
+
+            thread.start()
+            thread.join(seconds)
+
+            if isinstance(result, Exception):
+                raise result
+
+            return result
+
+        return wrapper
+
+    return decorator
