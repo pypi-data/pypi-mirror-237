af995cc795ce1972810fb07a4b94fbd63a46121c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 03:05:43 +0000

    Add package testios/0.0.2/testios-0.0.2.tar.gz

---
+++ packages/testios/testios-0.0.2.tar.gz/testios-0.0.2/testios/cli.py
@@ -0,0 +1,14 @@
+import click
+
+from testios import __version__
+from testios.scaffold import create_scaffold
+
+
+@click.command()
+@click.version_option(version=__version__, help="Show version.")
+@click.option("-p", "--projectName", type=str, default=None, help="Create demo by project name")
+# 老是变，等最后定下来再搞，目前也没啥用
+def main(projectName):
+
+    if projectName:
+        create_scaffold(projectName)
