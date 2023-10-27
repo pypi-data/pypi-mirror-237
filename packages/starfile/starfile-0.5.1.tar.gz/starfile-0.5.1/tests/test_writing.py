733b303927ed1d860a55964a1ad8e7b0b0e76a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 03:07:48 +0000

    Add package starfile/0.5.1/starfile-0.5.1.tar.gz

---
+++ packages/starfile/starfile-0.5.1.tar.gz/starfile-0.5.1/tests/test_writing.py
@@ -0,0 +1,70 @@
+from os.path import join as join_path
+from tempfile import TemporaryDirectory
+
+import pandas as pd
+
+from starfile.parser import StarParser
+from starfile.writer import StarWriter
+
+from .constants import loop_simple, postprocess, test_data_directory, test_df
+
+
+def test_write_simple_block():
+    s = StarParser(postprocess)
+    output_file = test_data_directory / 'basic_block.star'
+    StarWriter(s.data_blocks, output_file)
+    assert output_file.exists()
+
+
+def test_write_loop():
+    s = StarParser(loop_simple)
+    output_file = test_data_directory / 'loop_block.star'
+    StarWriter(s.data_blocks, output_file)
+    assert output_file.exists()
+
+
+def test_write_multiblock():
+    s = StarParser(postprocess)
+    output_file = test_data_directory / 'multiblock.star'
+    StarWriter(s.data_blocks, output_file)
+    assert output_file.exists()
+
+
+def test_from_single_dataframe():
+    output_file = test_data_directory / 'from_df.star'
+
+    StarWriter(test_df, output_file)
+    assert output_file.exists()
+
+    s = StarParser(output_file)
+
+
+def test_create_from_dataframes():
+    dfs = [test_df, test_df]
+
+    output_file = test_data_directory / 'from_list.star'
+    StarWriter(dfs, output_file)
+    assert output_file.exists()
+
+    s = StarParser(output_file)
+    assert len(s.data_blocks) == 2
+
+
+def test_can_write_non_zero_indexed_one_row_dataframe():
+    # see PR #13 - https://github.com/alisterburt/starfile/pull/13
+    df = pd.DataFrame([[1, 2, 3]], columns=["A", "B", "C"])
+    df.index += 1
+
+    with TemporaryDirectory() as directory:
+        filename = join_path(directory, "test.star")
+        StarWriter(df, filename)
+        with open(filename) as output_file:
+            output = output_file.read()
+
+    expected = (
+        "_A #1\n"
+        "_B #2\n"
+        "_C #3\n"
+        "1\t2\t3"
+    )
+    assert (expected in output)
