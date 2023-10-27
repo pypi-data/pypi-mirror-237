c1133c3a6e9e5ab99105f94130fe2d280fce25da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 03:07:07 +0000

    Add package moldrug/3.5.0/moldrug-3.5.0.tar.gz

---
+++ packages/moldrug/moldrug-3.5.0.tar.gz/moldrug-3.5.0/src/moldrug/constraintconf.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+For the functions: duplicate_conformers, get_mcs, generate_conformers, constraintconf
+and the class ProteinLigandClashFilter:
+    Code borrowed from Pat Walters
+    https://github.com/PatWalters/fragment_expansion/blob/master/rdkit_eval/rd_gen_restricted_confs.py
+    Which was borrowed from Joshua Meyers
+    https://github.com/JoshuaMeyers/Snippets/blob/master/200405_constrained_conformers.ipynb
+    and that code was adapted from Tim Dudgeon
+    https://github.com/InformaticsMatters/pipelines/blob/master/src/python/pipelines/rdkit/constrained_conf_gen.py
+    All I've done is change the command line wrapper, modify how to remove conformers that clash with the protein, add documentation and
+    handle some possible run time exceptions.
+"""
+from copy import deepcopy
+from rdkit import Chem
+from rdkit.Chem import AllChem, rdFMCS
+import os
+from typing import  Optional
+# import warnings
+from tqdm import tqdm
+import Bio.PDB as PDB
+from moldrug.utils import compressed_pickle
+from moldrug import verbose
+
+def duplicate_conformers(m: Chem.rdchem.Mol, new_conf_idx: int, rms_limit: float = 0.5) -> bool:
+    """
+    Check if a conformer with index new_onf_idx it is duplicated based on rms_limit
+
+    Parameters
+    ----------
+    m : Chem.rdchem.Mol
+        An RDKit molecule with conformers.
+    new_conf_idx : int
+        Index of the conformer to check.
+    rms_limit : float, optional
+        Threshold of rms to consider duplicate structure, by default 0.5.
+
+    Returns
+    -------
+    bool
+        True if the conformer new_conf_idx id is duplicate. false otherwise.
+    """
+    rmslist = []
+    for i in range(m.GetNumConformers()):
+        if i == new_conf_idx:
+            continue
+        rms = AllChem.GetConformerRMS(m, new_conf_idx, i, prealigned=True)
+        rmslist.append(rms)
+    return any(i < rms_limit for i in rmslist)
+
+def get_mcs(mol_one: Chem.rdchem.Mol, mol_two: Chem.rdchem.Mol) -> str:
+    """
+    Code to find the maximum common substructure between two molecules.
+
+    Parameters
+    ----------
+    mol_one : Chem.rdchem.Mol
+        The first molecule.
+    mol_two : Chem.rdchem.Mol
+        The second molecule.
+
+    Returns
+    -------
+    str
+        The SMILES string of the Maximum Common Substructure (MCS).
+    """
+    mcs_smarts = Chem.MolFromSmarts(rdFMCS.FindMCS([mol_one, mol_two], completeRingsOnly=True, matchValences=True).smartsString)
+    mcs_smi = Chem.MolToSmiles(mcs_smarts)
+    # Workaround in case of fails
+    if not Chem.MolFromSmiles(mcs_smi):
+        valid_atoms = mol_one.GetSubstructMatch(mcs_smarts)
+        mcs_smi = Chem.MolFragmentToSmiles(mol_one, atomsToUse=valid_atoms)
+    return mcs_smi
+
+def gen_aligned_conf(mol: Chem.rdchem.Mol, ref_mol: Chem.rdchem.Mol, ref_smi:str):
+    """Generate a conformation of mol aligned to ref_mol
+
+    Parameters
+    ----------
+    mol : Chem.rdchem.Mol
+        Molecule to generate the conformation aligned to ref_mol
+    ref_mol : Chem.rdchem.Mol
+        Reference molecule with a conformation
+    ref_smi : str
+        The SMILES string for which the aligned will take place.
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    AllChem.EmbedMolecule(mol)
+    # AllChem.MMFFOptimizeMolecule(mol)
+    ref_smi_mol = Chem.MolFromSmiles(ref_smi)
+    Chem.rdMolAlign.AlignMol(
+        mol,
+        ref_mol,
+        atomMap = list(zip(mol.GetSubstructMatch(ref_smi_mol), ref_mol.GetSubstructMatch(ref_smi_mol)))
+        )
+    return mol
+
+def generate_conformers(mol: Chem.rdchem.Mol,
+                        ref_mol: Chem.rdchem.Mol,
+                        num_conf: int,
+                        ref_smi: str = None,
+                        minimum_conf_rms: Optional[float] = None,
+                        ) -> Chem.rdchem.Mol:
+    """
+    Generate constrained conformers
+
+    Parameters
+    ----------
+    mol : Chem.rdchem.Mol
+        The molecule for which the conformer will be generated.
+    ref_mol : Chem.rdchem.Mol
+        The reference structure from which the reference coordinates will be taken.
+    num_conf : int
+        Maximum number of conformer to generate.
+    ref_smi : str, optional
+        part of the molecule to keep fix. If ref_smi is not given, assume to the MCS between mol and ref_mol, by default None
+    minimum_conf_rms : Optional[float], optional
+        Threshold of rms to consider duplicate structure, by default None
+
+    Returns
+    -------
+    Chem.rdchem.Mol
+        mol with with generated conformers. In case some Exception ocurred, it returns mol without conformers and write in the
+        current directory generate_conformers_error.log with the nature of the Exception.
+    """
+    # Creating the error directory if needed
+    if not os.path.isdir('error'):
+        os.makedirs('error')
+    # if SMILES to be fixed are not given, assume to the MCS
+    if ref_smi:
+        if not Chem.MolFromSmiles(ref_smi):
+            raise ValueError("The provided ref_smi is not valid.")
+    else:
+        ref_smi = get_mcs(mol, ref_mol)
+        if not Chem.MolFromSmiles(ref_smi):
+            raise ValueError("generate_conformers fails generating ref_smi based on the MCS between mol and ref_mol")
+
+    try:
+        # Creating core of reference ligand #
+        core_with_wildcards = AllChem.ReplaceSidechains(ref_mol, Chem.MolFromSmiles(ref_smi))
+        core1 = AllChem.DeleteSubstructs(core_with_wildcards, Chem.MolFromSmiles('*'))
+        core1.UpdatePropertyCache()
+
+        # Add Hs so that conf gen is improved
+        mol.RemoveAllConformers()
+        outmol = deepcopy(mol)
+        mol_wh = Chem.AddHs(mol)
+
+        # Generate conformers with constrained embed
+        dup_count = 0
+        for i in range(num_conf):
+            temp_mol = Chem.Mol(mol_wh)  # copy to avoid inplace changes
+            try:
+                AllChem.ConstrainedEmbed(temp_mol, core1, randomseed=i)
+            except Exception as e:
+                if verbose:
+                    print(F"AllChem.ConstrainedEmbed fails with: {e}. On the molecules:\n current mol: {Chem.MolToSmiles(temp_mol)}\ncore: {Chem.MolToSmiles(core1)}\nTrying with gen_aligned_conf")
+                temp_mol = gen_aligned_conf(temp_mol, ref_mol, ref_smi)
+            temp_mol = Chem.RemoveHs(temp_mol)
+            conf_idx = outmol.AddConformer(temp_mol.GetConformer(0), assignId=True)
+            if minimum_conf_rms is not None:
+                if duplicate_conformers(outmol, conf_idx, rms_limit=minimum_conf_rms):
+                    dup_count += 1
+                    outmol.RemoveConformer(conf_idx)
+        if dup_count:
+            pass
+            # print(f'removed {dup_count} duplicated conformations')
+        return outmol
+    except Exception as e:
+        # print(e)
+        # cwd = os.getcwd()
+        # warnings.warn(f"generate_conformers failed. Check the file {os.path.join(cwd, 'error/generate_conformers_error.pbz2')}")
+        compressed_pickle('error/generate_conformers_error', e)
+        mol.RemoveAllConformers()
+        return mol
+
+class ProteinLigandClashFilter:
+    """
+    Class used to eliminate clash between a ligand and a protein.
+    """
+    def __init__(self, protein_pdbpath: str, distance: float = 1.5):
+        """
+        This is the constructor of the class
+
+        Parameters
+        ----------
+        protein_pdbpath : str
+            Path top the protein pdb file
+        distance : float, optional
+            Threshold of distance to consider a clash, by default 1.5
+        """
+        parser = PDB.PDBParser(QUIET=True, PERMISSIVE=True)
+        s = parser.get_structure('protein', protein_pdbpath)
+        self.kd = PDB.NeighborSearch(list(s.get_atoms()))
+        self.radius = distance
+
+    def __call__(self, conf: Chem.rdchem.Conformer) -> bool:
+        """
+        Call deffinition
+
+        Parameters
+        ----------
+        conf : Chem.rdchem.Conformer
+            The conformer to be evaluated
+
+        Returns
+        -------
+        bool
+            True if there clash, False otherwise.
+        """
+        for coord in conf.GetPositions():
+            res = self.kd.search(coord, radius=self.radius)
+            if len(res):
+                return True
+        return False
+
+def constraintconf(pdb:str, smi:str, fix:str, out:str, max_conf:int = 25, rms:float = 0.01, bump:float = 1.5):
+    """
+    It generates several conformations in the binding pocket with a specified constraint.
+
+    Parameters
+    ----------
+    pdb : str
+        Protein pdb file
+    smi : str
+        Input SMILES file name
+    fix : str
+        File with fixed piece of the molecule
+    out : str
+        Output file name
+    max_conf : int, optional
+        Maximum number of conformers to generate, by default 25
+    rms : float, optional
+        RMS cutoff, by default 0.01
+    bump : float, optional
+        Bump cutoff, by default 1.5
+    """
+
+    ref = Chem.MolFromMolFile(fix)
+    suppl = Chem.SmilesMolSupplier(smi, titleLine=False)
+    writer = Chem.SDWriter(out)
+
+    clash_filter = ProteinLigandClashFilter(pdb, distance=bump)
+
+    for mol in tqdm(suppl):
+        # generate conformers
+        out_mol = generate_conformers(mol, ref,
+                                      max_conf,
+                                      ref_smi=Chem.MolToSmiles(ref),
+                                      minimum_conf_rms=rms)
+
+        # remove conformers that clash with the protein
+        clashIds = [conf.GetId() for conf in out_mol.GetConformers() if clash_filter(conf)]
+        _ = [out_mol.RemoveConformer(clashId) for clashId in clashIds]
+
+        # write out the surviving conformers
+        for conf in out_mol.GetConformers():
+            writer.write(out_mol, confId=conf.GetId())
+
+
+if __name__ == '__main__':
+    pass
---
+++ packages/moldrug/moldrug-3.5.0.tar.gz/moldrug-3.5.0/tests/test_moldrug.py
@@ -0,0 +1,366 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from rdkit import Chem
+from moldrug import utils, fitness, home
+from moldrug.data import receptor_pdbqt, ligands, boxes, receptor_pdb, constraintref
+import tempfile, os, gzip, shutil, requests, yaml, copy, sys
+# import pytest
+from multiprocessing import cpu_count
+
+
+# Creating a temporal directory
+tmp_path = tempfile.TemporaryDirectory()
+# Creating receptors files
+r_x0161_pdbqt_file = os.path.join(tmp_path.name, 'r_x0161.pdbqt')
+r_x0161_pdb_file = os.path.join(tmp_path.name, 'r_x0161.pdb')
+r_6lu7_pdbqt_file = os.path.join(tmp_path.name, 'r_6lu7.pdbqt')
+with open(r_x0161_pdbqt_file, 'w') as r:
+    r.write(receptor_pdbqt.r_x0161)
+with open(r_x0161_pdb_file, 'w') as r:
+    r.write(receptor_pdb.r_x0161)
+with open(r_6lu7_pdbqt_file, 'w') as r:
+    r.write(receptor_pdbqt.r_6lu7)
+
+# Getting the crem data base
+url = "http://www.qsar4u.com/files/cremdb/replacements02_sc2.db.gz"
+r = requests.get(url, allow_redirects=True)
+crem_dbgz_path = os.path.join(tmp_path.name,'crem.db.gz')
+crem_db_path = os.path.join(tmp_path.name,'crem.db')
+open(crem_dbgz_path, 'wb').write(r.content)
+with gzip.open(crem_dbgz_path, 'rb') as f_in:
+    with open(crem_db_path, 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+def test_single_receptor_command_line():
+    Config = {
+        "01_grow": {
+            "type": "GA",
+            "njobs": 3,
+            "seed_mol": ligands.r_x0161,
+            "AddHs": True,
+            "costfunc": "Cost",
+            "costfunc_kwargs": {
+                "vina_executable": "vina",
+                "receptor_pdbqt_path": r_x0161_pdbqt_file,
+                "boxcenter": boxes.r_x0161["A"]['boxcenter'],
+                "boxsize": boxes.r_x0161["A"]['boxsize'],
+                "exhaustiveness": 4,
+                "ncores": 4,
+                "num_modes": 1
+            },
+            "crem_db_path": crem_db_path,
+            "maxiter": 1,
+            "popsize": 2,
+            "beta": 0.001,
+            "pc": 1,
+            "get_similar": False,
+            "mutate_crem_kwargs": {
+                "radius": 3,
+                "min_size": 0,
+                "max_size": 0,
+                "min_inc": -5,
+                "max_inc": 6,
+                "ncores": 1
+            },
+            "save_pop_every_gen": 10,
+            "deffnm": "01_grow"
+        },
+        "02_allow_grow": {
+            "mutate_crem_kwargs": {
+                "radius": 3,
+                "min_size": 1,
+                "max_size": 0,
+                "min_inc": -5,
+                "max_inc": 3,
+                "ncores": 12
+            },
+            "maxiter": 1,
+            "deffnm": "02_allow_grow"
+        }
+    }
+    cwd = os.getcwd()
+    with open(os.path.join(tmp_path.name, "test_single_receptor.yml"), 'w') as c:
+        yaml.dump(Config, c)
+    os.chdir(tmp_path.name)
+    p = utils.run('moldrug test_single_receptor.yml')
+    print(p.stdout)
+    # Run a second time but with a seed population
+    Config['01_grow']['seed_mol'] = ['02_allow_grow_pop.pbz2', '02_allow_grow_pop.pbz2']
+    with open(os.path.join(tmp_path.name, "test_single_receptor_init_pop.yml"), 'w') as c:
+        yaml.dump(Config, c)
+    p = utils.run('moldrug test_single_receptor_init_pop.yml')
+    print(p.stdout)
+
+    os.chdir(cwd)
+
+def test_multi_receptor(maxiter = 1, popsize = 2, njobs = 3, NumbCalls = 1):
+    out = utils.GA(
+        seed_mol=[Chem.MolFromSmiles(ligands.r_x0161), Chem.MolFromSmiles(ligands.r_x0161)],
+        AddHs= True,
+        maxiter=maxiter,
+        popsize=popsize,
+        crem_db_path = crem_db_path,
+        pc = 1,
+        get_similar = True,
+        mutate_crem_kwargs = {
+            'radius':3,
+            'min_size':0,
+            'min_inc':-5,
+            'max_inc':3,
+            'replace_ids': [3, 4, 5, 7],
+            'protected_ids': [0],
+        },
+        costfunc = fitness.CostMultiReceptors,
+        costfunc_kwargs = {
+            'receptor_pdbqt_path': [r_x0161_pdbqt_file, r_6lu7_pdbqt_file],
+            'boxcenter' : [boxes.r_x0161["A"]['boxcenter'], boxes.r_6lu7["A"]['boxcenter']],
+            'boxsize': [boxes.r_x0161["A"]['boxsize'], boxes.r_6lu7["A"]['boxsize']],
+            'vina_score_type': ['min', 'max'],
+            'exhaustiveness': 4,
+            'ncores': int(cpu_count() / njobs),
+            'num_modes': 1,
+        },
+        save_pop_every_gen = 20,
+        deffnm = os.path.join(tmp_path.name, 'test_multi_receptor')
+        )
+
+    for _ in range(NumbCalls):
+        out(njobs = njobs)
+
+    for o in out.pop:
+        print(o.smiles, o.cost)
+    out.pickle(os.path.join(tmp_path.name, f"result_test_multi_receptor_NumGens_{out.NumGens}_PopSize_{popsize}"), compress=False)
+    print(out.to_dataframe())
+
+    with open(os.path.join(tmp_path.name, 'vina_out.pdbqt'), 'w') as p:
+        p.write(out.pop[0].pdbqt[0])
+
+    vina_out = utils.VINA_OUT(os.path.join(tmp_path.name, 'vina_out.pdbqt'))
+    vina_out.chunks[0].get_atoms()
+    vina_out.chunks[0].write(os.path.join(tmp_path.name, 'chunk.pdbqt'))
+    cwd = os.getcwd()
+    os.chdir(tmp_path.name)
+    vina_out.chunks[0].write()
+    os.chdir(cwd)
+
+def test_local_command_line():
+    Config = {
+        "main": {
+            "type": "Local",
+            "njobs": 1,
+            "pick": 1,
+            "AddHs": True,
+            "seed_mol": Chem.MolToSmiles(Chem.AddHs(Chem.MolFromSmiles(ligands.r_x0161))),
+            "costfunc": "CostOnlyVina",
+            "costfunc_kwargs": {
+                "vina_executable": "vina",
+                "receptor_pdbqt_path": r_x0161_pdbqt_file,
+                "boxcenter": boxes.r_x0161["A"]['boxcenter'],
+                "boxsize": boxes.r_x0161["A"]['boxsize'],
+                "exhaustiveness": 4,
+                "num_modes": 1
+            },
+            "crem_db_path": crem_db_path,
+            "grow_crem_kwargs": {
+                "radius": 3,
+                "min_atoms": 1,
+                "max_atoms": 3
+            }
+        }
+    }
+    cwd = os.getcwd()
+    os.chdir(tmp_path.name)
+    with open("local_config.yml", 'w') as c:
+        yaml.dump(Config, c)
+
+    utils.run(f"moldrug local_config.yml --fitness {os.path.join(home.home(), 'fitness.py')} --outdir results")
+    print(os.listdir())
+    # This problems with the modules are not so convenient
+    os.chdir('results')
+    sys.path.append('.')
+    result = utils.decompress_pickle('local_result.pbz2')
+    result.pickle('local_non_compress', compress=False)
+    print(result.to_dataframe())
+    os.chdir(cwd)
+
+# @pytest.mark.filterwarnings("ignore:\nVina failed")
+def test_fitness_module():
+    individual = utils.Individual(Chem.MolFromSmiles(ligands.r_x0161))
+    individual_corrupted = copy.deepcopy(individual)
+    individual_corrupted.pdbqt = 'This is a corrupted pdbqt'
+    receptor_pdbqt_path = [r_x0161_pdbqt_file,r_6lu7_pdbqt_file]
+    boxcenter = [boxes.r_x0161['A']['boxcenter'], boxes.r_6lu7['A']['boxcenter']]
+    boxsize = [boxes.r_x0161['A']['boxsize'], boxes.r_6lu7['A']['boxsize']]
+    vina_score_type = ['min', 'max']
+
+    fitness.Cost(
+        Individual = copy.deepcopy(individual),
+        wd = tmp_path.name,
+        receptor_pdbqt_path = r_x0161_pdbqt_file,
+        boxcenter = boxes.r_x0161['A']['boxcenter'],
+        boxsize = boxes.r_x0161['A']['boxsize'],
+        exhaustiveness = 4,
+        ncores = 4)
+    fitness.Cost(
+        Individual = copy.deepcopy(individual),
+        wd = tmp_path.name,
+        receptor_pdbqt_path = r_x0161_pdbqt_file,
+        boxcenter = boxes.r_x0161['A']['boxcenter'],
+        boxsize = boxes.r_x0161['A']['boxsize'],
+        exhaustiveness = 4,
+        ncores = 4,
+        constraint=True,
+        constraint_type='local_only',
+        constraint_ref=Chem.MolFromMolBlock(constraintref.r_x0161),
+        constraint_receptor_pdb_path = r_x0161_pdb_file,
+        )
+
+    fitness.Cost(Individual = copy.deepcopy(
+        individual_corrupted),wd = tmp_path.name,receptor_pdbqt_path = r_x0161_pdbqt_file,
+        boxcenter = boxes.r_x0161['A']['boxcenter'],boxsize = boxes.r_x0161['A']['boxsize'],exhaustiveness = 4,ncores = 4)
+    fitness.CostMultiReceptors(
+        Individual = copy.deepcopy(individual_corrupted),wd = tmp_path.name,receptor_pdbqt_path = receptor_pdbqt_path,
+        vina_score_type = vina_score_type, boxcenter = boxcenter,boxsize = boxsize,exhaustiveness = 4,ncores = 4)
+    fitness.CostMultiReceptorsOnlyVina(
+        Individual = copy.deepcopy(individual),wd = tmp_path.name,receptor_pdbqt_path = receptor_pdbqt_path,
+        vina_score_type = vina_score_type,boxcenter = boxcenter,boxsize = boxsize,exhaustiveness = 4,ncores = 4)
+    fitness.CostMultiReceptorsOnlyVina(
+        Individual = copy.deepcopy(individual),wd = tmp_path.name,receptor_pdbqt_path = receptor_pdbqt_path,
+        vina_score_type = vina_score_type,boxcenter = boxcenter,boxsize = boxsize,exhaustiveness = 4,ncores = 4,wt_cutoff=2)
+    fitness.CostMultiReceptorsOnlyVina(Individual = copy.deepcopy(
+        individual_corrupted),wd = tmp_path.name,receptor_pdbqt_path = receptor_pdbqt_path,
+        vina_score_type = vina_score_type,boxcenter = boxcenter,boxsize = boxsize,exhaustiveness = 4,ncores = 4)
+
+
+    fitness.CostOnlyVina(
+        Individual = copy.deepcopy(individual),wd = tmp_path.name,receptor_pdbqt_path = r_x0161_pdbqt_file,
+        boxcenter = boxes.r_x0161['A']['boxcenter'], boxsize = boxes.r_x0161['A']['boxsize'],exhaustiveness = 4,ncores = 4)
+    fitness.CostOnlyVina(
+        Individual = copy.deepcopy(individual),wd = tmp_path.name,receptor_pdbqt_path = r_x0161_pdbqt_file,
+        boxcenter = boxes.r_x0161['A']['boxcenter'], boxsize = boxes.r_x0161['A']['boxsize'],exhaustiveness = 4,ncores = 4, wt_cutoff=2)
+    fitness.CostOnlyVina(
+        Individual = copy.deepcopy(individual_corrupted),wd = tmp_path.name,receptor_pdbqt_path = r_x0161_pdbqt_file,
+        boxcenter = boxes.r_x0161['A']['boxcenter'], boxsize = boxes.r_x0161['A']['boxsize'],exhaustiveness = 4,ncores = 4)
+
+    fitness.__get_mol_cost(
+        mol = Chem.MolFromMolBlock(constraintref.r_x0161),wd = tmp_path.name,receptor_pdbqt_path = r_x0161_pdbqt_file,
+        boxcenter = boxes.r_x0161['A']['boxcenter'], boxsize = boxes.r_x0161['A']['boxsize'],)
+
+    # Clean
+    utils.tar_errors()
+    os.remove('error.tar.gz')
+
+def test_home():
+    home.home(dataDir='data')
+
+def test_get_sim_utils():
+    from rdkit.Chem import AllChem
+    mols = []
+    ref_fps = []
+    for s in ['CC', 'CCO']:
+        mol = Chem.MolFromSmiles(s)
+        mols.append(mol)
+        ref_fps.append(AllChem.GetMorganFingerprintAsBitVect(mol, 2))
+    utils.get_sim(mols, ref_fps)
+
+def test_lipinski():
+    mol = Chem.MolFromSmiles('CCCO')
+    utils.lipinski_filter(Chem.MolFromSmiles('BrCC(COCN)CC(Br)CC(Cl)CCc1ccccc1CCCC(NCCCO)'))
+    utils.lipinski_filter(mol)
+    utils.lipinski_profile(mol)
+
+def test_Individual():
+    I1 = utils.Individual(Chem.MolFromSmiles('CC'), cost=10)
+    I2 = utils.Individual(Chem.MolFromSmiles('CCO'), cost=2)
+    I3 = copy.copy(I1)
+    I4 = copy.deepcopy(I2)
+    I5 = utils.Individual(Chem.MolFromSmiles('CC'), cost = 10, pdbqt=I1.pdbqt)
+    assert I3 + I4 == 12
+    assert I5 - I2 == 8
+    assert I1 > I2
+    assert I1 >= I2
+    assert I2 < I1
+    assert I2 <= I1
+    assert -I1 == -10
+    assert abs(I1) == 10
+    assert I1 * I2 == 20
+    assert I1 / I2 == 5
+    assert I1 // I2 == 5
+    assert I1 % I2 == 0
+    assert divmod(I1, I2) == (5,0)
+    assert I1**I2 == 100
+
+def test_miscellanea():
+    obj0 = []
+    for i in range(0,50):
+        obj0.append(utils.NominalTheBest(Value=i, LowerLimit=10, Target=20, UpperLimit=30))
+
+    utils.full_pickle(os.path.join(tmp_path.name,'test_desirability'), obj0)
+    utils.compressed_pickle(os.path.join(tmp_path.name,'test_desirability'), obj0)
+    obj1 = utils.loosen(os.path.join(tmp_path.name,'test_desirability.pkl'))
+    obj2 = utils.decompress_pickle(os.path.join(tmp_path.name,'test_desirability.pbz2'))
+
+    assert obj0 == obj1
+    assert obj0 == obj2
+
+# def test_local():
+#     os.chdir(tmp_path.name)
+#     local = utils.Local(
+#         seed_mol = Chem.AddHs(Chem.MolFromSmiles(ligands.r_x0161)),
+#         crem_db_path = crem_db_path,
+#         costfunc=fitness.CostOnlyVina,
+#         costfunc_kwargs = {
+#             "vina_executable": "vina",
+#             "receptor_pdbqt_path": r_x0161_pdbqt_file,
+#             "boxcenter": boxes.r_x0161["A"]['boxcenter'],
+#             "boxsize": boxes.r_x0161["A"]['boxsize'],
+#             "exhaustiveness": 4,
+#             "num_modes": 1
+#         },
+#         grow_crem_kwargs = {
+#             "radius": 3,
+#             "min_atoms": 1,
+#             "max_atoms": 3
+#         },
+#         AddHs=True,
+#     )
+#     local(njobs=1,pick=1)
+
+#     utils.make_sdf(local.pop, sdf_name = os.path.join(tmp_path.name,"local_pop"))
+#     print(os.listdir(tmp_path.name))
+
+#     print(local.to_dataframe())
+
+def test_constraintconf():
+    from moldrug.constraintconf import constraintconf
+    with Chem.SDWriter(os.path.join(tmp_path.name, 'fix.sdf')) as w:
+        mol = Chem.MolFromMolBlock(constraintref.r_x0161)
+        w.write(mol)
+    with open(os.path.join(tmp_path.name, 'mol.smi'), 'w') as f:
+        f.write(ligands.r_x0161)
+
+    constraintconf(
+        pdb=r_x0161_pdb_file,
+        smi = os.path.join(tmp_path.name, 'mol.smi'),
+        fix= os.path.join(tmp_path.name, 'fix.sdf'),
+        out = os.path.join(tmp_path.name, 'conf.sdf')
+    )
+    # Clean
+    utils.tar_errors()
+
+def test_generate_conformers():
+    from moldrug.constraintconf import generate_conformers
+    from rdkit.Chem import AllChem
+    ref = Chem.MolFromSmiles('O=S(=O)(Nc1ccc(Cl)cc1)c1ccsc1C(O)O')
+    mol = Chem.MolFromSmiles('CN(C)S(=O)(=O)c1cc(NS(=O)(=O)c2ccsc2C(O)O)ccc1Cl')
+
+    AllChem.EmbedMolecule(ref)
+    AllChem.MMFFOptimizeMolecule(ref)
+    generate_conformers(Chem.RemoveHs(mol), Chem.RemoveHs(ref), 50)
+
+    # Clean
+    utils.tar_errors()
+
+
+if __name__ == '__main__':
+    pass
