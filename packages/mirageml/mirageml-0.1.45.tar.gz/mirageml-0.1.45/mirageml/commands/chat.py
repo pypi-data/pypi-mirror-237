1d17a8e7fa7b6f6cf987bb4f628153067aba1309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 06:07:32 +0000

    Add package mirageml/0.1.45/mirageml-0.1.45.tar.gz

---
+++ packages/mirageml/mirageml-0.1.45.tar.gz/mirageml-0.1.45/mirageml/commands/chat.py
@@ -0,0 +1,181 @@
+import os
+
+import typer
+from rich.box import HORIZONTALS
+from rich.console import Console
+from rich.live import Live
+from rich.markdown import Markdown
+from rich.panel import Panel
+
+from .add_source import add_local_source
+from .config import load_config
+from .list_sources import get_sources
+from .rag import rag_chat
+from .utils.codeblocks import (
+    add_indices_to_code_blocks,
+    copy_code_to_clipboard,
+    extract_code_from_markdown,
+)
+from .utils.custom_inputs import multiline_input
+from .utils.llm import llm_call
+
+console = Console()
+config = load_config()
+
+
+def chat(files: list[str] = [], urls: list[str] = [], sources: list[str] = []):
+    # Beginning of the chat sequence
+    transient_sources = []
+    if files or urls or sources:
+        index_local = False
+        if "local" in sources:
+            index_local = True
+            sources.remove("local")
+
+        for file in files:
+            if os.path.isdir(file):
+                sources.append(add_local_source(file))
+                files.remove(file)
+
+        local_sources, remote_sources = get_sources()
+        all_sources = list(set(local_sources + remote_sources))
+        for source in sources:
+            if source not in all_sources:
+                typer.secho(
+                    f"Source: {source} does not exist. Please add it with `mirageml add source`",
+                    fg=typer.colors.RED,
+                    bold=True,
+                )
+                sources.remove(source)
+
+        if index_local:
+            sources.append(add_local_source("local"))
+
+        if files or urls:
+            from .utils.local_source import crawl_files
+            from .utils.web_source import extract_from_url
+
+            with Live(
+                Panel(
+                    "Scraping Files and Urls",
+                    title="[bold green]Assistant[/bold green]",
+                    border_style="green",
+                ),
+                console=console,
+                transient=True,
+            ) as live:
+                for source, extractor in zip(files + urls, [crawl_files] * len(files) + [extract_from_url] * len(urls)):
+                    live.update(
+                        Panel(
+                            f"Extracting data from {source}",
+                            title="[bold green]Assistant[/bold green]",
+                            border_style="green",
+                        )
+                    )
+                    data, metadata = extractor(source)
+                    if data:
+                        transient_sources.append((data, metadata))
+
+    while True:
+        chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
+        ai_response = ""
+        if sources or transient_sources:
+            chat_history, ai_response = rag_chat(sources, transient_sources)
+
+        while True:
+            # Loop for follow-up questions
+            try:
+                if ai_response:
+                    chat_history.append({"role": "assistant", "content": ai_response})
+                    indexed_ai_response = add_indices_to_code_blocks(ai_response)
+                    console.print(
+                        Panel(
+                            Markdown(indexed_ai_response),
+                            title="[bold blue]Assistant[/bold blue]",
+                            box=HORIZONTALS,
+                            border_style="blue",
+                        )
+                    )
+                    code_blocks = extract_code_from_markdown(ai_response)
+                    if code_blocks:
+                        selected_indices = multiline_input(
+                            "Enter the numbers of the code blocks you want to copy, separated by commas (e.g. '1,3,4') or ask a follow-up. Type reset to search again"
+                        )
+                        try:
+                            selected_indices = list(map(int, selected_indices.split(",")))
+                            copy_code_to_clipboard(code_blocks, selected_indices)
+                            ai_response = ""
+                            continue
+                        except ValueError:
+                            user_input = selected_indices
+                    else:
+                        user_input = multiline_input("Ask a follow-up. Type reset to search again. Ctrl+C to interrupt")
+                else:
+                    if len(chat_history) == 1:
+                        user_input = multiline_input("Chat with Mirage")
+                    else:
+                        user_input = multiline_input("Ask a follow-up. Type reset to search again. Ctrl+C to interrupt")
+                if user_input.lower().strip() == "exit":
+                    typer.secho("Ending chat. Goodbye!", fg=typer.colors.BRIGHT_GREEN, bold=True)
+                    return
+                elif (
+                    user_input.lower().strip() == "reset"
+                    or user_input.lower().strip() == "restart"
+                    or user_input.lower().strip() == "start over"
+                    or user_input.lower().strip() == "clear"
+                ):
+                    break
+
+            except KeyboardInterrupt:
+                typer.secho("Ending chat. Goodbye!", fg=typer.colors.BRIGHT_GREEN, bold=True)
+                return
+
+            chat_history.append({"role": "user", "content": user_input})
+
+            try:
+                with Live(
+                    Panel(
+                        "Assistant is thinking...",
+                        title="[bold blue]Assistant[/bold blue]",
+                        box=HORIZONTALS,
+                        border_style="blue",
+                    ),
+                    console=console,
+                    transient=True,
+                    auto_refresh=True,
+                    refresh_per_second=8,
+                ) as live:
+                    response = llm_call(
+                        chat_history,
+                        model=config["model"],
+                        stream=True,
+                        local=config["local_mode"],
+                    )
+
+                    ai_response = ""
+                    if config["local_mode"]:
+                        for chunk in response:
+                            ai_response += chunk
+                            live.update(
+                                Panel(
+                                    Markdown(ai_response),
+                                    title="[bold blue]Assistant[/bold blue]",
+                                    box=HORIZONTALS,
+                                    border_style="blue",
+                                )
+                            )
+                    else:
+                        for chunk in response.iter_content(chunk_size=512):
+                            if chunk:
+                                decoded_chunk = chunk.decode("utf-8")
+                                ai_response += decoded_chunk
+                                live.update(
+                                    Panel(
+                                        Markdown(ai_response),
+                                        title="[bold blue]Assistant[/bold blue]",
+                                        box=HORIZONTALS,
+                                        border_style="blue",
+                                    )
+                                )
+            except KeyboardInterrupt:
+                pass
---
+++ packages/mirageml/mirageml-0.1.45.tar.gz/mirageml-0.1.45/mirageml/commands/config.py
@@ -0,0 +1,73 @@
+import json
+import os
+
+import typer
+
+
+def show_config():
+    config = load_config()
+    from rich import print
+
+    print(json.dumps(config, indent=4))
+
+
+def load_config():
+    config_path = os.path.expanduser("~/.mirageml.json")
+    curr_config = {}
+    if os.path.exists(config_path):
+        with open(config_path, "r") as f:
+            curr_config = json.load(f)
+    curr_config = {
+        "local_mode": curr_config.get("local_mode", False),
+        "model": curr_config.get("model", "gpt-4"),
+        "keep_files_local": curr_config.get("keep_files_local", True),
+        "local": curr_config.get("local", []),
+        "remote": curr_config.get("remote", []),
+    }
+    save_config(curr_config)
+    return curr_config
+
+
+def save_config(config):
+    config_path = os.path.expanduser("~/.mirageml.json")
+    with open(config_path, "w") as f:
+        json.dump(config, f, indent=4)
+
+
+def set_config():
+    config = load_config()
+
+    valid = {
+        "local_mode": (("True (not recommended without GPU)", "False"), json.loads),
+        "model": (("gpt-3.5-turbo", "gpt-4"), str),
+        "keep_files_local": (("True", "False"), json.loads),
+    }
+
+    for key, value in config.items():
+        if key not in valid:
+            continue
+        curvalue = value
+        while True:
+            question = f"Enter the value for '{key}' [{', '.join(valid[key][0])}] (current value: {curvalue}): "
+            value = input(question)
+            if value == "":
+                break
+            if value not in [x.split()[0] for x in valid[key][0]]:
+                typer.secho(
+                    f"Invalid value for '{key}'. Please use one of these options {[x.split()[0] for x in valid[key][0]]}",
+                    fg=typer.colors.BRIGHT_RED,
+                )
+                continue
+            config[key] = valid[key][1](value.lower())
+            save_config(config)
+            break
+
+    typer.secho("MirageML Config updated!", fg=typer.colors.BRIGHT_GREEN)
+    show_config()
+
+
+def set_var_config(json_data):
+    config = load_config()
+    for key, value in json_data.items():
+        config[key] = value
+    save_config(config)
