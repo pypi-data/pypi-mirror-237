9ad537ad8dcf0d7d8b38c6903d6d6b6d3959b0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:08:23 +0000

    Add package dycw-utilities/0.14.19/dycw_utilities-0.14.19.tar.gz

---
+++ packages/dycw-utilities/dycw_utilities-0.14.19.tar.gz/dycw_utilities-0.14.19/tests/test_json.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import datetime as dt
+from pathlib import Path
+from typing import Any
+
+from pytest import mark, param, raises
+
+from utilities.datetime import UTC
+from utilities.json import serialize
+
+
+class TestSerialize:
+    @mark.parametrize(
+        ("x", "expected"),
+        [
+            param(dt.date(2000, 1, 1), '"2000-01-01"'),
+            param(
+                dt.datetime(2000, 1, 1, 12, tzinfo=UTC),
+                '"2000-01-01T12:00:00+00:00"',
+            ),
+            param(Path("a/b/c"), '"a/b/c"'),
+            param({1, 2, 3}, '"set([1, 2, 3])"'),
+            param({"a", "b", "c"}, '"set([\\"a\\", \\"b\\", \\"c\\"])"'),
+            param(frozenset([1, 2, 3]), '"frozenset([1, 2, 3])"'),
+            param(
+                frozenset(["a", "b", "c"]),
+                '"frozenset([\\"a\\", \\"b\\", \\"c\\"])"',
+            ),
+        ],
+    )
+    def test_main(self, x: Any, expected: str) -> None:
+        assert serialize(x) == expected
+
+    def test_error(self) -> None:
+        class Example:
+            pass
+
+        with raises(TypeError, match="Invalid type"):
+            _ = serialize(Example())
