2a3d18c8761bb5daea008d2f22258ce9cc39b858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 05:08:09 +0000

    Add package solar-data-tools/1.0.3/solar_data_tools-1.0.3-py3-none-any.whl

---
+++ packages/solar-data-tools/solar_data_tools-1.0.3-py3-none-any.whl/solardatatools/utilities.py
@@ -0,0 +1,80 @@
+""" Utilities Module
+
+This module contains utility functions used by other modules.
+
+"""
+
+import sys
+import numpy as np
+import cvxpy as cvx
+
+
+def basic_outlier_filter(x, outlier_constant=1.5):
+    """
+    Applies an outlier filter based on the interquartile range definition:
+        any data point more than 1.5 interquartile ranges (IQRs) below the
+        first quartile or above the third quartile
+
+    Function returns a boolean mask for entries in the input array that are
+    not outliers.
+
+    :param x: ndarray
+    :param outlier_constant: float, multiplier constant on IQR
+    :return: boolean mask
+    """
+    a = np.array(x)
+    upper_quartile = np.percentile(a, 75)
+    lower_quartile = np.percentile(a, 25)
+    iqr = (upper_quartile - lower_quartile) * outlier_constant
+    quartile_set = (lower_quartile - iqr, upper_quartile + iqr)
+    mask = np.logical_and(a >= quartile_set[0], a <= quartile_set[1])
+    return mask
+
+
+def progress(count, total, status="", bar_length=60):
+    """
+    Python command line progress bar in less than 10 lines of code. · GitHub
+    https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
+    :param count: the current count, int
+    :param total: to total count, int
+    :param status: a message to display
+    :return:
+    """
+    bar_len = bar_length
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = "=" * filled_len + "-" * (bar_len - filled_len)
+
+    sys.stdout.write("[%s] %s%s ...%s\r" % (bar, percents, "%", status))
+    sys.stdout.flush()
+
+
+def find_runs(x):
+    """Find runs of consecutive items in an array.
+    https://gist.github.com/alimanfoo/c5977e87111abe8127453b21204c1065"""
+
+    # ensure array
+    x = np.asanyarray(x)
+    if x.ndim != 1:
+        raise ValueError("only 1D array supported")
+    n = x.shape[0]
+
+    # handle empty array
+    if n == 0:
+        return np.array([]), np.array([]), np.array([])
+
+    else:
+        # find run starts
+        loc_run_start = np.empty(n, dtype=bool)
+        loc_run_start[0] = True
+        np.not_equal(x[:-1], x[1:], out=loc_run_start[1:])
+        run_starts = np.nonzero(loc_run_start)[0]
+
+        # find run values
+        run_values = x[loc_run_start]
+
+        # find run lengths
+        run_lengths = np.diff(np.append(run_starts, n))
+
+        return run_values, run_starts, run_lengths
