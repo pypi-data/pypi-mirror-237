d83b868e0d5ee4cdcd9149e2c1567637c6ddf099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 15:07:02 +0000

    Add package fastapi-init/0.1.5/fastapi_init-0.1.5.tar.gz

---
+++ packages/fastapi-init/fastapi_init-0.1.5.tar.gz/fastapi_init-0.1.5/fastapi_init/main.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+
+import requests
+import git
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command", help="Set command process")
+    parser.add_argument("project_name", default="fastapi_init", help="Lệnh để thực thi")
+    parser.add_argument("version", default="main", help="Lệnh để thực thi")
+    parser.add_argument("package_key", default=None)
+    args = parser.parse_args()
+
+    if args.command == "init_project":
+        git.repo.Repo.clone_from(
+            "git@git.rabiloo.net:tech/base/fastapi-init.git", f"./{args.project_name}",
+            branch=args.version
+        )
+        if args.package_key:
+            os.system(f"export PACKAGE_REGISTRY_TOKEN={args.package_key}")
+        os.system(f"pip install -r {args.project_name}/cicd/requirements/requirements.txt")
+        os.system(f"pip install -r {args.project_name}/cicd/requirements/requirements-style.txt")
+    elif args.command == "get_weather":
+        response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=Hà Nội&appid=YOUR_API_KEY")
+        data = response.json()
+        print(data["weather"][0]["main"])
+
+
+if __name__ == "__main__":
+    main()
