e1e50adaf7d65d84b20eb3391cb52459dfdd8d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 05:08:34 +0000

    Add package open-groceries/0.1.4/open_groceries-0.1.4-py3-none-any.whl

---
+++ packages/open-groceries/open_groceries-0.1.4-py3-none-any.whl/open_groceries/models/groceries.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import Any, Optional
+
+@dataclass
+class Ratings:
+    average: int
+    count: int
+
+@dataclass
+class GroceryItem:
+    type: str
+    id: str
+    name: str
+    location: Optional[str]
+    images: list[str]
+    tags: list[str]
+    price: float
+    ratings: Ratings
+    metadata: dict[str, Any]
