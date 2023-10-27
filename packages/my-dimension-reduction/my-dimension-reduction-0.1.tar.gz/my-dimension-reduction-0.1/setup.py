4d36d7cb7caf115d6eeaf2aa9cdda631abaaf2a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:05:59 +0000

    Add package my-dimension-reduction/0.1/my-dimension-reduction-0.1.tar.gz

---
+++ packages/my-dimension-reduction/my-dimension-reduction-0.1.tar.gz/my-dimension-reduction-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my-dimension-reduction",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+        "pandas"
+    ],
+)
