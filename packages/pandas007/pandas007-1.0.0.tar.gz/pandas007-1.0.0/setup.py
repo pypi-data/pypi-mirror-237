5e00db6b6b9d0db71514710f529ca44864197a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 05:11:14 +0000

    Add package pandas007/1.0.0/pandas007-1.0.0.tar.gz

---
+++ packages/pandas007/pandas007-1.0.0.tar.gz/pandas007-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pandas007",
+    version="1.0.0",
+    description="A simple Python package",
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires=">=3.6",
+)
