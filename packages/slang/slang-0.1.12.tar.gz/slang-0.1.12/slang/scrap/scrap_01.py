497f7853ed9cee6eeda092180a3905a8500cf47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:07:14 +0000

    Add package slang/0.1.12/slang-0.1.12.tar.gz

---
+++ packages/slang/slang-0.1.12.tar.gz/slang-0.1.12/slang/scrap/scrap_01.py
@@ -0,0 +1,14 @@
+from matplotlib.pylab import *
+from numpy import *
+from collections import Counter
+import pandas as pd
+from itertools import chain
+from slang.core import DFLT_CHUNKER
+
+# from agen.sound.audio import Sound
+# from slang.featurizers import _rms_and_crossing_levels, _zero_crossing_ratio
+# import seaborn as sns
+
+# import i_do_not_exist.here
+# import i_do_not_exist.there
+# from i_also_do_not_exit import something
