78d28c4ec0ae0a15535f2a98fb02ee5d5d200b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:07:23 +0000

    Add package jetnet/0.2.4/jetnet-0.2.4-py3-none-any.whl

---
+++ packages/jetnet/jetnet-0.2.4-py3-none-any.whl/jetnet/__init__.py
@@ -0,0 +1,10 @@
+# __init__
+
+import jetnet.datasets  # noqa: F401
+import jetnet.datasets.normalisations
+import jetnet.datasets.utils
+import jetnet.evaluation
+import jetnet.losses
+import jetnet.utils  # noqa: F401
+
+__version__ = "0.2.4"
---
+++ packages/jetnet/jetnet-0.2.4-py3-none-any.whl/jetnet/losses/__init__.py
@@ -0,0 +1,1 @@
+from .losses import *  # noqa: F401, F403
