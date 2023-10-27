c8d134a22cad1b0a7e039a6fa28507d1cc576946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 06:08:37 +0000

    Add package djangorestframework-filters-mini/2023.0.0/djangorestframework-filters-mini-2023.0.0.tar.gz

---
+++ packages/djangorestframework-filters-mini/djangorestframework-filters-mini-2023.0.0.tar.gz/djangorestframework-filters-mini-2023.0.0/tests/test_utils.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+
+from rest_framework_filters.filterset import lookups_for_field
+
+from .testapp.models import Note, Person
+
+
+class LookupsForFieldTests(TestCase):
+    def test_standard_field(self):
+        model_field = Person._meta.get_field('name')
+        lookups = lookups_for_field(model_field)
+
+        self.assertIn('exact', lookups)
+        self.assertNotIn('year', lookups)
+        self.assertNotIn('date', lookups)
+
+    def test_transformed_field(self):
+        model_field = Person._meta.get_field('datetime_joined')
+        lookups = lookups_for_field(model_field)
+
+        self.assertIn('exact', lookups)
+        self.assertIn('year__exact', lookups)
+        self.assertIn('date__year__exact', lookups)
+
+    def test_relation_field(self):
+        # ForeignObject relations are special cased currently
+        model_field = Note._meta.get_field('author')
+        lookups = lookups_for_field(model_field)
+
+        self.assertIn('exact', lookups)
+        self.assertIn('in', lookups)
+        self.assertNotIn('regex', lookups)
+
+
+class LookupsForTransformTests(TestCase):
+    def test_recursion_prevention(self):
+        model_field = Person._meta.get_field('name')
+        lookups = lookups_for_field(model_field)
+
+        self.assertIn('unaccent__exact', lookups)
+        self.assertNotIn('unaccent__unaccent__exact', lookups)
