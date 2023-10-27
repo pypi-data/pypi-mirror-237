24abaa6a38665e5649e97050aa69fea8750819bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 05:11:29 +0000

    Add package fasteasySD/0.0.2/fasteasySD-0.0.2.tar.gz

---
+++ packages/fasteasySD/fasteasySD-0.0.2.tar.gz/fasteasySD-0.0.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="fasteasySD",
+    version="0.0.2",
+    license_files = ('LICENSE.txt',),
+    author="WaGaNaWa",
+    author_email="m4a1carbin4@gmail.com",
+    description="simple_fast_easy_sd",
+    long_description=open('README.md').read(),
+    url="https://github.com/m4a1carbin4/fasteasysd",
+    packages=setuptools.find_packages(),
+)
