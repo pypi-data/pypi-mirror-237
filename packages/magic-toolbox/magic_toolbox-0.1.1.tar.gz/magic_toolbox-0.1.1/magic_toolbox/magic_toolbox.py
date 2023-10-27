4287a7c48850c23910dbb1eb3b4021e7322fe859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 14:08:33 +0000

    Add package magic-toolbox/0.1.1/magic_toolbox-0.1.1.tar.gz

---
+++ packages/magic-toolbox/magic_toolbox-0.1.1.tar.gz/magic_toolbox-0.1.1/magic_toolbox/magic_toolbox.py
@@ -0,0 +1,30 @@
+from plac import Interpreter
+from typing import Callable
+from importlib import import_module
+from inspect import getmembers, isfunction
+
+
+def get_tools() -> list[tuple[str,Callable]]:
+    tools = import_module('toolbox.tools')
+    return [ (n,tool) 
+            for n,tool in getmembers(tools) 
+            if isfunction(tool)]
+    
+class MagicToolBox(object):
+    
+    """
+    A CLI framework
+
+    Raises:
+        plac.Interpreter.Exit: [signals to exit interactive interpreter]
+    """
+    
+ 
+    
+    commands = tuple(n for n,_ in get_tools()) 
+   
+for name,tool in get_tools():
+    setattr(MagicToolBox,name,tool) 
+        
+if __name__ == '__main__':
+    Interpreter.call(MagicToolBox)
