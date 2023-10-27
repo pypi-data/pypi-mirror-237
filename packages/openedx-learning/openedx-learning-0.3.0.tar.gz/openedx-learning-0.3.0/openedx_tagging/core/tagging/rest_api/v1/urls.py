3b32fb50f8aba6dd501ad586021778cc95e96d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 05:08:05 +0000

    Add package openedx-learning/0.3.0/openedx-learning-0.3.0.tar.gz

---
+++ packages/openedx-learning/openedx-learning-0.3.0.tar.gz/openedx-learning-0.3.0/openedx_tagging/core/tagging/rest_api/v1/urls.py
@@ -0,0 +1,26 @@
+"""
+Taxonomies API v1 URLs.
+"""
+
+from django.urls.conf import include, path
+from rest_framework.routers import DefaultRouter
+
+from . import views, views_import
+
+router = DefaultRouter()
+router.register("taxonomies", views.TaxonomyView, basename="taxonomy")
+router.register("object_tags", views.ObjectTagView, basename="object_tag")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "taxonomies/<str:pk>/tags/",
+        views.TaxonomyTagsView.as_view(),
+        name="taxonomy-tags",
+    ),
+    path(
+        "import/template.<str:file_ext>",
+        views_import.TemplateView.as_view(),
+        name="taxonomy-import-template",
+    ),
+]
