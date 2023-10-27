8ce12c7844c7f45bdddefaf9b08975c0563ba93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 16:10:07 +0000

    Add package testios/0.0.5/testios-0.0.5.tar.gz

---
+++ packages/testios/testios-0.0.5.tar.gz/testios-0.0.5/testios/__init__.py
@@ -0,0 +1,12 @@
+from .case import TestCase
+from .page import Page
+from .core.ios import *
+from .running.runner import main
+from .utils.config import config
+from .utils.pytest_util import *
+from .utils.allure_util import *
+from .utils.log import logger
+
+
+__version__ = "0.0.5"
+__description__ = "IOS平台自动化测试框架"
