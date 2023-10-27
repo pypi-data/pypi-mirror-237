01e853877ba96b4bd514303a05d3abe68b175ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 03:07:48 +0000

    Add package neofox/1.1.0/neofox-1.1.0.tar.gz

---
+++ packages/neofox/neofox-1.1.0.tar.gz/neofox-1.1.0/neofox/references/install_r_dependencies.R
@@ -0,0 +1,3 @@
+install.packages("caret", repo="http://cran.rstudio.com/")
+install.packages("Peptides", repo="http://cran.rstudio.com/")
+install.packages("doParallel", repo="http://cran.rstudio.com/")
