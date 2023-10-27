0ed711376a40041e5a2703fe8c9fe89f2a3f2908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 03:08:26 +0000

    Add package fairpredictor/0.0.23/fairpredictor-0.0.23.tar.gz

---
+++ packages/fairpredictor/fairpredictor-0.0.23.tar.gz/fairpredictor-0.0.23/predictor/__init__.py
@@ -0,0 +1,6 @@
+from .app import predict
+from .downloader import download
+from .georeferencer import georeference
+from .prediction import run_prediction
+from .raster2polygon import polygonizer
+from .vectorizer import vectorize
