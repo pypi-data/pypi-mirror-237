f7c0247260c8d6c5908f6755117730e4f180f627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 14:07:08 +0000

    Add package smartsolve/0.0.3/smartsolve-0.0.3.tar.gz

---
+++ packages/smartsolve/smartsolve-0.0.3.tar.gz/smartsolve-0.0.3/__init__.py
@@ -0,0 +1,40 @@
+from .preprocessing import AnalyseData, SplitData, MissingValue, SelectFeature, CategoricalData, FeatureScaling
+
+from .models import (LinearRegression, LogisticRegression, DecisionTree, DecisionTreeNode, RandomForest,
+                     KMeansClustering, KNearestNeighbors, NaiveBayes, PrincipalComponentAnalysis,
+                     SupportVectorMachines, GradientBoosting, GaussianMixtureModel, SingularValueDecomposition)
+
+from .evaluation import Validation
+
+
+PACKAGE_VERSION = "0.0.1"
+DEFAULT_CONFIG = {
+    "verbose": False,
+    "debug": False,
+}
+
+__all__ = [
+    "AnalyseData",
+    "SplitData",
+    "MissingValue",
+    "SelectFeature",
+    "CategoricalData",
+    "FeatureScaling",
+    "LinearRegression",
+    "LogisticRegression",
+    "DecisionTree",
+    "DecisionTreeNode",
+    "RandomForest",
+    "KMeansClustering",
+    "KNearestNeighbors",
+    "NaiveBayes",
+    "PrincipalComponentAnalysis",
+    "SupportVectorMachines",
+    "GradientBoosting",
+    "GaussianMixtureModel",
+    "SingularValueDecomposition",
+    "Validation",
+    "PACKAGE_VERSION",
+    "DEFAULT_CONFIG",
+]
+
