d45e0e2ab650532f82609277eb4e34db8e6b950d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 16:09:32 +0000

    Add package LanyData/0.1.9/LanyData-0.1.9.tar.gz

---
+++ packages/LanyData/LanyData-0.1.9.tar.gz/LanyData-0.1.9/setup.py
@@ -0,0 +1,31 @@
+# from distutils.core import setup
+# from Cython.Build import cythonize
+
+# setup(ext_modules=cythonize(['LanyData/future_data.py', 'LanyData/option_data.py', 'LanyData/lyData.py']))
+
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.rst", "r") as f:
+  long_description = f.read()
+
+setup(name='LanyData',  # 包名
+      version='0.1.9',  # 版本号
+      description='A small package',
+      long_description=long_description,
+      author='weif',
+      author_email='2658133323@qq.com',
+      url='https://mp.weixin.qq.com/s/9FQ-Tun5FbpBepBAsdY62w',
+      install_requires=[],
+      license='MIT License',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.11',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
