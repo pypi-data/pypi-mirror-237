6b39cd34538d4aa4470aa9287804ac8db3f84c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 14:08:14 +0000

    Add package formant/1.143.31/formant-1.143.31.tar.gz

---
+++ packages/formant/formant-1.143.31.tar.gz/formant-1.143.31/formant/sdk/cloud/v2/src/resources/commands/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import CommandTemplate, CommandDeliveryEvent, CommandTemplateTags, CommandParameterMeta, CommandResponseStreamType, CommandRequestListResponse, Command, CommandQuery, CommandDeliveryEventTags, CommandTemplateParameterMeta, CommandListResponse, FileInfo, CommandTemplateListResponse, CommandResponseTags, CommandStreamType, CommandParameter, CommandRequest, CommandDeliveryEventType, ScopeFilter, CommandResponse, ScopeFilterTypesItem, CommandDeliveryEventMetadata, CommandDeliveryEventStreamType, CommandProgress, CommandTags
+from formant.sdk.cloud.v2.formant_admin_api_client.api.command import command_controller_post, command_controller_query
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
