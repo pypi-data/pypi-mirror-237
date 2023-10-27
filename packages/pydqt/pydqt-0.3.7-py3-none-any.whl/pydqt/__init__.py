24d3c3e43667adad7b7b0ddd547fde9b1b7719f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:05:50 +0000

    Add package pydqt/0.3.7/pydqt-0.3.7-py3-none-any.whl

---
+++ packages/pydqt/pydqt-0.3.7-py3-none-any.whl/pydqt/__init__.py
@@ -0,0 +1,33 @@
+from pydqt.pydqt import (
+    get_global_template_dir,
+    create_test_data,
+    test_data_exists,
+    test_data_file_full_path,
+    set_workspace,
+    env_reload,
+    env_edit,
+    QueryTemplateParams,
+    QueryParams,
+    Query,
+    Sql,
+    Workspace,
+)
+
+# from pydqt import pydqt
+
+
+__all__ = [
+    "get_global_template_dir",
+    "create_test_data",
+    "test_data_exists",    
+    "test_data_file_full_path",
+    "set_workspace",
+    "env_reload",
+    "env_edit",
+    "QueryTemplateParams",
+    "QueryParams",
+    "Query",
+    "Sql",
+    "Workspace",
+    "pydqt"
+]
