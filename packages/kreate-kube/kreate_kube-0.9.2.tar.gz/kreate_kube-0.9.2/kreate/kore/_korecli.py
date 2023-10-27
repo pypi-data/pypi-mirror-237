533c20104a5f0d35c6fa7bf7e55ffab831e27165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 14:08:25 +0000

    Add package kreate-kube/0.9.2/kreate_kube-0.9.2.tar.gz

---
+++ packages/kreate-kube/kreate_kube-0.9.2.tar.gz/kreate_kube-0.9.2/kreate/kore/_korecli.py
@@ -0,0 +1,354 @@
+import os
+import sys
+import shutil
+import argparse
+import logging
+import traceback
+import inspect
+
+from ._core import pprint_map, wrap
+from ._repo import clear_cache
+
+from . import _jinyaml
+from ._app import App, Konfig
+from ._jinja_app import JinjaApp
+from pathlib import Path
+import importlib.metadata
+import kreate.kore.dotenv as dotenv
+
+logger = logging.getLogger(__name__)
+
+
+def argument(*name_or_flags, **kwargs):
+    """Convenience function to properly format arguments to pass to the
+    subcommand decorator.
+    """
+    return (list(name_or_flags), kwargs)
+
+
+class KoreCli:
+    def __init__(self):
+        self.load_dotenv()
+        self._konfig = None
+        self._app = None
+        self.epilog = "subcommands:\n"
+        self.cli = argparse.ArgumentParser(
+            prog="kreate",
+            usage=(
+                "kreate [optional arguments] <konfig> [<subcommand>] [subcommand options]"
+            ),
+            description=("kreates files for deploying applications on kubernetes"),
+            formatter_class=argparse.RawTextHelpFormatter,
+        )
+
+        self.subparsers = self.cli.add_subparsers(
+            # title="subcmd",
+            # description="valid subcommands",
+            dest="subcommand",
+            metavar="see subcommands",
+        )
+        self.add_subcommands()
+
+    def get_packages(self):
+        """a list of packages that are shown in the version command"""
+        return []
+
+    def calc_dict(self):
+        result = {}
+        for d in self.args.define:
+            k, v = d.split("=", 1)
+            wrap(result).set(k, v)
+        return result
+
+    def konfig(self):
+        if not self._konfig:
+            self._konfig = self.kreate_konfig(self.konfig_filename)
+            if not self.args.skip_version_check:
+                self._konfig.check_kreate_version()
+        return self._konfig
+
+    def app(self):
+        if not self._app:
+            self._app = self.kreate_app()
+        return self._app
+
+    def kreate_konfig(self, filename: str) -> Konfig:
+        return Konfig(filename, dict_=self.calc_dict(), inkludes=self.args.inklude)
+
+    def kreate_app(self) -> App:
+        return App(self.konfig())
+
+    def add_subcommands(self):
+        self.add_subcommand(clear_repo_cache, [], aliases=["cc"])
+        # subcommand: version
+        self.add_subcommand(version, [], aliases=["vr"])
+        # subcommand: view
+        cmd = self.add_subcommand(view, [], aliases=["v"])
+        cmd.add_argument("key", help="key(s) to show", action="store", nargs="*")
+        # self.add_output_options(cmd)
+
+    def dist_package_version(self, package_name: str):
+        return importlib.metadata.version(package_name)
+
+    def add_subcommand(self, func, args=[], aliases=[], parent=None):
+        parent = parent or self.subparsers
+        alias0 = aliases[0] if aliases else ""
+        self.epilog += f"  {func.__name__:17} {alias0 :3} {func.__doc__ or ''} \n"
+        parser = parent.add_parser(
+            func.__name__, aliases=aliases, description=func.__doc__
+        )
+        for arg in args:
+            parser.add_argument(*arg[0], **arg[1])
+        parser.set_defaults(func=func)
+        return parser
+
+    def load_dotenv(self) -> None:
+        # Primitive way to check if to load ENV vars before parsing vars
+        # .env needs to be loaded before arg parsing, since it may
+        # contain KREATE_OPTIONS
+        load_dot_env = True
+        load_kreate_env = True
+        for arg in sys.argv:
+            if arg == "--no-dotenv":
+                load_dot_env = False
+            if arg == "--no-kreate-env":
+                load_kreate_env = False
+        try:
+            if load_dot_env:
+                dotenv.load_env(Path.cwd() / ".env")
+            if load_kreate_env:
+                dotenv.load_env(Path.home() / ".config/kreate/kreate.env")
+        except Exception as e:
+            logger.error(
+                f"ERROR loading .env file, " f"remove .env file or specify --no-dotenv"
+            )
+            raise
+
+    def get_argv(self):
+        options = os.getenv("KREATE_OPTIONS")
+        if options:
+            result = [*options.split(), *sys.argv[1:]]
+            # options are not parsed yet, so use simple count to
+            # determine if it is debug level
+            vcount = 0
+            for opt in result:
+                vcount += 2 if opt.startswith("-vv") else 0
+                vcount += 1 if opt == "-v" else 0
+            if vcount >= 2:
+                print(
+                    f"DEBUG:prepending KREATE_OPTIONS to get {result}", file=sys.stderr
+                )
+            return result
+        return sys.argv[1:]
+
+    def run(self):
+        self.cli.epilog = self.epilog + "\n"
+        self.add_main_options()
+        self.args = self.cli.parse_args(self.get_argv())
+        self.process_main_options(self.args)
+        try:
+            if self.args.subcommand is None:
+                self.default_command()
+            else:
+                self.args.func(self)
+        except Exception as e:
+            if self.args.verbose:
+                traceback.print_exc()
+            else:
+                print(f"{type(e).__name__}: {e}")
+        finally:
+            if not self.args.keep_secrets:
+                if self._app:
+                    # app was kreated so secrets might need to be cleaned
+                    dir = self._app.target_path
+                    secrets_dir = f"{dir}/secrets"
+                    if os.path.exists(secrets_dir):
+                        logger.info(
+                            f"removing {secrets_dir}, "
+                            "use --keep-secrets or -K option to keep it"
+                        )
+                        shutil.rmtree(secrets_dir)
+
+    def default_command(self):
+        version(self)
+
+    def add_konfig_options(self, cmd):
+        cmd.add_argument(
+            "-k",
+            "--konfig",
+            metavar='file',
+            action="store",
+            default=None,
+            help="konfig file or directory to use (default=KREATE_MAIN_KONFIG_PATH or .)",
+        )
+        cmd.add_argument(
+            "-d",
+            "--define",
+            metavar='yaml-setting',
+            action="append",
+            default=[],
+            help="add yaml (toplevel) element to konfig file",
+        )
+        cmd.add_argument(
+            "-i",
+            "--inklude",
+            metavar='path',
+            action="append",
+            default=[],
+            help="inklude extra files before parsing main konfig",
+        )
+
+    def add_output_options(self, cmd):
+        cmd.add_argument(
+            "-v",
+            "--verbose",
+            action="count",
+            default=0,
+            help="output more details (inluding stacktrace) -vv even more",
+        )
+        cmd.add_argument(
+            "-w", "--warn", action="store_true", help="only output warnings"
+        )
+        cmd.add_argument(
+            "-q",
+            "--quiet",
+            action="store_true",
+            help="do not output any info, just essential output",
+        )
+
+    def add_main_options(self):
+        self.add_konfig_options(self.cli)
+        self.add_output_options(self.cli)
+        self.cli.add_argument(
+            "-K",
+            "--keep-secrets",
+            action="store_true",
+            help="do not remove secrets dirs",
+        )
+        self.cli.add_argument(
+            "-C",
+            "--skip-version-check",
+            action="store_true",
+            help="do not check if required version of kreate is used",
+        )
+        self.cli.add_argument(
+            "--no-dotenv",
+            action="store_true",
+            help="do not load .env file from working dir",
+        )
+        self.cli.add_argument(
+            "--no-kreate-env",
+            action="store_true",
+            help="do not load kreate.env file from user home .config dir",
+        )
+
+    def process_main_options(self, args):
+        if args.verbose >= 3:
+            logging.basicConfig(level=logging.DEBUG)
+        elif args.verbose == 2:
+            logging.basicConfig(level=logging.DEBUG)
+            _jinyaml.logger.setLevel(logging.INFO)
+        elif args.verbose == 1:
+            logging.basicConfig(format="%(message)s", level=logging.INFO)
+        elif args.quiet:
+            logging.basicConfig(format="%(message)s", level=logging.ERROR)
+        else:
+            logging.basicConfig(format="%(message)s", level=logging.WARN)
+        self.konfig_filename = args.konfig
+
+
+def clear_repo_cache(cli: KoreCli):
+    """clear the repo cache"""
+    clear_cache()
+
+
+def view_template(cli, template):
+    app: JinjaApp = cli.kreate_app()
+    if template not in app.kind_templates or template not in app.kind_classes:
+        logger.warn(f"Unknown template kind {template}")
+        return
+    if not cli.args.quiet:
+        print("==========================")
+        print(
+            f"{template} "
+            f"{app.kind_classes[template].__name__}: "
+            f"{app.kind_templates[template]}"
+        )
+        print("==========================")
+        if app.kind_classes[template].__doc__:
+            print(inspect.cleandoc(app.kind_classes[template].__doc__))
+            print("==========================")
+    if app.kind_templates[template] != "NoTemplate":
+        tmpl = app.kind_templates[template]
+        tmpl_text = app.konfig.file_getter.get_data(tmpl)
+        print(tmpl_text)
+
+
+def view_templates(cli, templates):
+    """view the template for a specific kind"""
+    # we call the kreate_app method and not the convenience app()
+    # method, because aktivating the app, will do stuff that might break
+    # template = cli.args.template
+    if templates:
+        for t in templates:
+            view_template(cli, t)
+    else:
+        app: JinjaApp = cli.kreate_app()
+        for template in app.kind_templates:
+            if template in app.kind_templates and template in app.kind_classes:
+                print(
+                    f"{template:24} "
+                    f"{app.kind_classes[template].__name__:20} "
+                    f"{app.kind_templates[template]}"
+                )
+            else:
+                logger.warn(f"skipping template {template}")
+
+
+def view_aliases():
+    return {
+        "i": "inklude",
+        "s": "strukt",
+        "a": "app",
+        "t": "template",
+        "tmpl": "template",
+        "ink": "inklude",
+        "sys": "system",
+        "ver": "version",
+        "kust": "strukt.Kustomization",
+        "depl": "strukt.Deployment",
+        "cron": "strukt.CronJob",
+        "ingr": "strukt.Ingress",
+        "egr": "strukt.Egress",
+        "svc": "strukt.Service",
+        "cm": "strukt.ConfigMap",
+    }
+
+
+def view(cli: KoreCli):
+    """view the entire konfig or subkey(s)"""
+    konfig: Konfig = cli.konfig()
+    if cli.args.key:
+        for idx, k in enumerate(cli.args.key):
+            k = view_aliases().get(k, k)
+            if k == "template":
+                view_templates(cli, cli.args.key[idx + 1 :])
+                break
+            result = konfig.get_path(k)
+            if isinstance(result, str):
+                print(f"{k}: {result}")
+            else:
+                print(k + ":")
+                pprint_map(result, indent="  ")
+    else:
+        pprint_map(konfig.yaml)
+
+
+def version(cli: KoreCli):
+    """view the version"""
+    for pckg in cli.get_packages():
+        try:
+            version = cli.dist_package_version(pckg)
+        except importlib.metadata.PackageNotFoundError:
+            version = "Unknown"
+        print(f"{pckg}: {version}")
---
+++ packages/kreate-kube/kreate_kube-0.9.2.tar.gz/kreate_kube-0.9.2/kreate/kore/_repo.py
@@ -0,0 +1,365 @@
+from typing import Mapping, Protocol, Union
+import requests
+import requests.auth
+import zipfile
+import hashlib
+import io
+import os
+import re
+import pkgutil
+import shutil
+import logging
+import importlib
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+def cache_dir():
+    cache_dir = os.getenv("KREATE_REPO_CACHE_DIR")
+    if not cache_dir:
+        cache_dir = Path.home() / ".cache/kreate/repo"
+    return Path(cache_dir)
+
+
+def clear_cache():
+    logger.warn(f"removing repo cache dir {cache_dir()}")
+    if cache_dir().is_dir():
+        shutil.rmtree(cache_dir())
+
+
+class FileGetter:
+    def __init__(self, konfig, dir: Path):
+        self.konfig = konfig
+        self.dir = dir  # Path(dir)
+        self.repo_prefixes = {
+            "main_konfig:": FixedDirRepo(dir),
+            "cwd:": FixedDirRepo(Path.cwd()),
+            "home:": FixedDirRepo(Path.home()),
+            "py:": PythonPackageRepo(konfig, None),
+        }
+        self.repo_types = {
+            "url-zip:": UrlZipRepo,
+            "local-dir:": LocalKonfigRepo,
+            "local-zip:": LocalZipRepo,
+            "bitbucket-zip:": BitbucketZipRepo,
+            "bitbucket-file:": BitbucketFileRepo,
+        }
+
+    def konfig_repos(self):
+        for repo in self.konfig.get_path("system.repo", []):
+            self.repo_prefixes[repo + ":"] = self.get_repo(repo)
+
+    def get_prefix(self, filename: str) -> str:
+        if filename.startswith("optional:"):
+            filename = filename[9:]
+        if filename.startswith("dekrypt:"):
+            filename = filename[8:]
+        match = re.match("^[a-zA-Z0-9_-]*:", filename)
+        if match:
+            return match.group()[:-1]
+        return None
+
+    def get_data(self, file: str) -> str:
+        orig_file = file
+        dekrypt = False
+        optional = False
+        if file.startswith("optional:"):
+            optional = True
+            file = file[9:]
+        if file.startswith("dekrypt:"):
+            dekrypt = True
+            file = file[8:]
+        repo = None
+        for prefix in self.repo_prefixes:
+            if file.startswith(prefix):
+                file = file[len(prefix) :]
+                repo = self.repo_prefixes[prefix]
+        if repo:
+            data = repo.get_data(file, optional=optional)
+        else:
+            data = self.load_file_data(file)
+        if data is None:
+            if optional:
+                logger.debug(f"ignoring optional file {orig_file}")
+                return ""
+            else:
+                raise FileNotFoundError(f"non-optional file {orig_file} does not exist")
+        if dekrypt:
+            logger.debug(f"dekrypting {file}")
+            data = self.konfig.dekrypt_bytes(data)
+        return data
+
+    def kopy_file(self, loc: str, target: Path) -> None:
+        logger.info(f"kopying file {loc} to {target}")
+        data = self.get_data(loc)
+        dir = target.parent
+        dir.mkdir(parents=True, exist_ok=True)
+        if isinstance(data, bytes):
+            data = data.decode()
+        target.write_text(data)
+
+    def load_file_data(self, filename: str) -> str:
+        logger.debug(f"loading file {filename} ")
+        path = self.dir / filename
+        if not path.exists():
+            return None
+        return path.read_text()
+
+    def get_repo(self, repo_name: str):
+        type = self.konfig.get_path(f"system.repo.{repo_name}.type", None)
+        if type == "url-zip":
+            return UrlZipRepo(self.konfig, repo_name)
+        elif type == "local-dir":
+            return LocalKonfigRepo(self.konfig, repo_name)
+        elif type == "local-zip":
+            return LocalZipRepo(self.konfig, repo_name)
+        elif type == "bitbucket-zip":
+            return BitbucketZipRepo(self.konfig, repo_name)
+        elif type == "bitbucket-file":
+            return BitbucketFileRepo(self.konfig, repo_name)
+        else:
+            raise ValueError(f"Unknow repo type {type} for repo {repo_name}")
+
+
+class Repo(Protocol):
+    def get_data(self, filename: str, optional: bool = False) -> str:
+        ...
+
+
+class PythonPackageRepo(Repo):
+    def get_data(self, filename: str, optional: bool = False) -> str:
+        package_name = filename.split(":")[0]
+        filename = filename[len(package_name) + 1 :]
+        package = importlib.import_module(package_name)
+        logger.debug(f"loading file {filename} from package {package_name}")
+        data = pkgutil.get_data(package.__package__, filename)
+        return data.decode("utf-8")
+
+
+class FixedDirRepo(Repo):
+    def __init__(self, dir: Union[Path, str]):
+        if isinstance(dir, str):
+            self.dir = Path(dir)
+        elif isinstance(dir, Path):
+            self.dir = dir
+        else:
+            raise TypeError(f"Unsupported type {type(dir)}")
+
+    def get_data(self, filename: str, optional: bool = False):
+        print(self.dir, filename)
+        path = self.dir / filename
+        if not path.exists():
+            raise FileNotFoundError(f"could not find file {filename} in {dir}")
+        return path.read_text()
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.dir})"
+
+
+class KonfigRepo(Repo):
+    def __init__(self, konfig, repo_name: str):
+        self.konfig = konfig
+        self.repo_name = repo_name
+        self.repo_konf = konfig.get_path("system.repo." + repo_name, {})
+        self.version = self.get("version")
+
+    def get(self, attr):
+        return self.konfig.get_path(f"system.repo.{self.repo_name}.{attr}", {})
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.repo_name}, version={self.version})"
+
+    def download(self, filename: str) -> bool:
+        raise NotImplementedError(f"Could not download {self.repo_name}")
+
+    def get_data(self, filename: str, optional: bool = False):
+        if optional and self.repo_konf.get("disabled", False):
+            logger.info(
+                f"skipping optional {filename} in disable repo {self.repo_name}"
+            )
+            return ""
+        dir = Path(self.calc_dir())
+        if not dir.exists():
+            self.download(filename)
+        if not dir.is_dir():
+            # add other assertions, or better error message?
+            raise FileExistsError(f"repo dir {dir} exists, but is not a directory")
+        p = dir / filename
+        if not p.exists():
+            if not self.download_extra_file(filename):
+                if optional:
+                    return ""
+                raise FileNotFoundError(f"could not find file {filename} in {dir}")
+        return p.read_text()
+
+    def download_extra_file(self, filename: str) -> bool:
+        return False
+
+    def calc_hash(self, extra: str = "") -> str:
+        return hashlib.md5(
+            (
+                self.repo_name
+                + self.repo_konf.get("version", "")
+                + self.calc_url("...", no_warn=True)
+                + extra
+            ).encode()
+        ).hexdigest()[:10]
+
+    def calc_dir(self) -> Path:
+        hash = self.calc_hash()
+        dir = self.repo_konf.get("cache_name")
+        if dir:
+            if self.version:
+                return cache_dir() / f"{dir}/{self.version}-{hash}"
+            return cache_dir() / f"{dir}-{hash}"
+        if self.version:
+            return (
+                cache_dir() / f"{self.repo_name}/{self.version.replace('/','-')}-{hash}"
+            )
+        else:
+            return cache_dir() / f"{self.repo_name}-{hash}"
+
+    def calc_url(self, filename: str, no_warn=False) -> str:
+        url = self.repo_konf.get("url", "")
+        if self.version:
+            url = url.replace("{version}", self.version)
+        return url
+
+    def unzip_data(self, data) -> None:
+        z = zipfile.ZipFile(io.BytesIO(data))
+        skip_levels = self.repo_konf.get("skip_levels", 0)
+        regexp = self.repo_konf.get("select_regexp", "")
+        self.calc_dir().mkdir(parents=True)
+        unzip(z, self.calc_dir(), skip_levels=skip_levels, select_regexp=regexp)
+
+    def url_response(self, filename: str, raise_error=True):
+        auth = None
+        if self.repo_konf.get("basic_auth", {}):
+            usr_env_var = self.repo_konf["basic_auth"]["usr_env_var"]
+            psw_env_var = self.repo_konf["basic_auth"]["psw_env_var"]
+            usr = os.getenv(usr_env_var)
+            psw = os.getenv(psw_env_var)
+            auth = requests.auth.HTTPBasicAuth(usr, psw)
+        url = self.calc_url(filename)
+        logger.info(f"downloading {self.calc_dir()} from {url}")
+        response = requests.get(url, auth=auth)
+        if response.status_code >= 300 and raise_error:
+            raise IOError(
+                f"status {response.status_code} while downloading {url} with message {response.content}"
+            )
+        return response
+
+
+class LocalKonfigRepo(KonfigRepo):
+    def calc_dir(self):
+        dir: str = self.repo_konf.get("dir")
+        if self.version:
+            dir = dir.replace("{version}", self.version)
+        return Path(dir)
+
+
+class LocalZipRepo(KonfigRepo):
+    def download(self, filename: str) -> bool:
+        path: str = self.repo_konf.get("path")
+        if self.version:
+            path = path.replace("{version}", self.version)
+        logger.info(f"unzipping {self.calc_dir()} from {path}")
+        data = Path(path).read_bytes()
+        self.unzip_data(data)
+        return True
+
+
+class UrlZipRepo(KonfigRepo):
+    def download(self, filename: str) -> bool:
+        data = self.url_response(filename).content
+        self.unzip_data(data)
+        return True
+
+
+class BitbucketZipRepo(KonfigRepo):
+    def download(self, filename: str) -> bool:
+        data = self.url_response(filename).content
+        self.unzip_data(data)
+        return True
+
+    def calc_url(self, filename: str, no_warn=False) -> str:
+        return self._calc_url("archive", "&format=zip", no_warn=no_warn)
+
+    def _calc_url(self, ext: str, format: str = "", no_warn=False) -> str:
+        url = self.repo_konf.get("url", None)
+        url += f"/{ext}"
+        if self.version.startswith("branch.") and not no_warn:
+            version = self.version[7:]
+            logger.warning(
+                f"Using branch {version} for repo {self.repo_name} is not recommended, use a tag instead"
+            )
+            url += f"?at=refs/heads/{version}" + format
+        else:
+            url += f"?at=refs/tags/{self.version}" + format
+        bitbucket_project = self.repo_konf.get("bitbucket_project")
+        bitbucket_repo = self.repo_konf.get("bitbucket_repo")
+        if bitbucket_project:
+            url = url.replace("{project}", bitbucket_project)
+        if bitbucket_repo:
+            url = url.replace("{repo}", bitbucket_repo)
+        return url
+
+
+class BitbucketFileRepo(BitbucketZipRepo):
+    def download(self, filename: str, raise_error=True) -> bool:
+        response = self.url_response(filename, raise_error=raise_error)
+        if response.status_code > 300:
+            return False
+        path = self.calc_dir() / filename
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.write_bytes(response.content)
+        return True
+
+    def calc_dir(self) -> Path:
+        hash = self.calc_hash()
+        bitb_project = self.repo_konf.get("bitbucket_project")
+        bitb_repo = self.repo_konf.get("bitbucket_repo")
+        version = self.version.replace("/", "-")
+        return (
+            cache_dir()
+            / f"{self.repo_name}-{bitb_project}-{bitb_repo}/{version}-{hash}"
+        )
+
+    def calc_url(self, filename: str, no_warn=False) -> str:
+        return self._calc_url(f"raw/{filename}", no_warn=no_warn)
+
+    def download_extra_file(self, filename: str) -> bool:
+        """check if it was previous attempted to download this file"""
+        marker_path = self.calc_dir() / f"{filename}.does-not-exist"
+        if marker_path.exists():
+            return False
+        if self.download(filename, raise_error=False):
+            return True
+        logger.info(f"could not find {self.calc_url(filename)}, marking it to prevent future attempts")
+        marker_path.touch()
+        return False
+
+
+def unzip(
+    zfile: zipfile.ZipFile,
+    dir: Path,
+    skip_levels: int = 0,
+    select_regexp: str = None,
+):
+    if skip_levels == 0 and not select_regexp:
+        zfile.extractall(dir)
+        return
+    for fname in zfile.namelist():
+        newname = "/".join(fname.split("/")[skip_levels:])
+        if newname and re.match(select_regexp, newname):
+            newpath = dir / newname
+            if fname.endswith("/"):
+                logger.info(f"extracting dir  {newname}")
+                newpath.mkdir(parents=True, exist_ok=True)
+            else:
+                logger.info(f"extracting file {newname}")
+                newpath.parent.mkdir(parents=True, exist_ok=True)
+                newpath.write_bytes(zfile.read(fname))
+        else:
+            logger.debug(f"skipping not selected {fname} ")
