5ee2a7abe36ede6086eb3a2c3ca21dfb7b86be0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 15:06:43 +0000

    Add package shipyard-hightouch/0.1.1a1/shipyard_hightouch-0.1.1a1-py3-none-any.whl

---
+++ packages/shipyard-hightouch/shipyard_hightouch-0.1.1a1-py3-none-any.whl/shipyard_hightouch/cli/authtest.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from shipyard_blueprints import HightouchClient
+
+
+def main():
+    client = HightouchClient(access_token=os.environ.get("HIGHTOUCH_API_KEY"))
+    conn = client.connect()
+    if conn == 200:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
