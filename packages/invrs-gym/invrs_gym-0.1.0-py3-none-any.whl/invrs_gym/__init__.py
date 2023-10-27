8f6083eb8d5da89ecf449a56a644175f1c85431f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 06:08:42 +0000

    Add package invrs-gym/0.1.0/invrs_gym-0.1.0-py3-none-any.whl

---
+++ packages/invrs-gym/invrs_gym-0.1.0-py3-none-any.whl/invrs_gym/__init__.py
@@ -0,0 +1,9 @@
+"""invrs_gym - A collection of inverse design challenges.
+
+Copyright (c) 2023 The INVRS-IO authors.
+"""
+
+__version__ = "v0.1.0"
+__author__ = "Martin F. Schubert <mfschubert@gmail.com>"
+
+from invrs_gym import challenges as challenges
