2a84d9fca5caba9f09d68b79408636e92ebf5351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 14:08:32 +0000

    Add package loggi/0.0.0/loggi-0.0.0-py3-none-any.whl

---
+++ packages/loggi/loggi-0.0.0-py3-none-any.whl/loggi/logger.py
@@ -0,0 +1,39 @@
+import logging
+
+from pathier import Pathier, Pathish
+
+from loggi import models
+
+root = Pathier(__file__).parent
+
+
+def getLogger(name: str, path: Pathish = Pathier.cwd()) -> logging.Logger:
+    """Get a configured `logging.Logger` instance for `name` with a file handler.
+
+    The log file will be located in `path`.
+
+    Default level is `INFO`.
+
+    Logs are in the format: `{levelname}|-|{asctime}|-|{message}
+
+    asctime is formatted as `%x %X`"""
+    path = Pathier(path)
+    logger = logging.getLogger(name)
+    # TODO: Add option for a stream handler
+    handler = logging.FileHandler((path / name).with_suffix(".log"), encoding="utf-8")
+    handler.setFormatter(
+        logging.Formatter(
+            "{levelname}|-|{asctime}|-|{message}",
+            style="{",
+            datefmt="%x %X",
+        )
+    )
+    if handler not in logger.handlers:
+        logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+def load_log(logpath: Pathish) -> models.Log:
+    """Return a `Log` object for the log file at `logpath`."""
+    return models.Log.load_log(Pathier(logpath))
