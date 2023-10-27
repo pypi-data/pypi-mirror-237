70bf12972e66a81915cefb72fbf270da5f12ab24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:07:08 +0000

    Add package fsai-shared-funcs/0.0.201/fsai_shared_funcs-0.0.201.tar.gz

---
+++ packages/fsai-shared-funcs/fsai_shared_funcs-0.0.201.tar.gz/fsai_shared_funcs-0.0.201/fsai_shared_funcs/database_helpers/contextmanagers.py
@@ -0,0 +1,17 @@
+from contextlib import asynccontextmanager
+
+import asyncpg
+from loguru import logger
+
+
+@asynccontextmanager
+async def DatabaseAsyncContextManager(db_pool: asyncpg.pool.Pool) -> asyncpg.Connection:
+    # Perform setup that would go in __aenter__
+
+    logger.debug("Acquiring a connection from the db_pool")
+    async with db_pool.acquire() as connection:
+        async with connection.transaction():
+            logger.debug("Acquired a connection from the db_pool")
+            yield connection  # obj should be the object that will be bound in the as clause
+
+    # Perform teardown that would go in __aexit__
