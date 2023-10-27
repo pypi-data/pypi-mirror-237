198a8acbd472fd74d23f0e90e673403026b40d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 03:08:19 +0000

    Add package doc-controller/0.0.3/doc_controller-0.0.3-py3-none-any.whl

---
+++ packages/doc-controller/doc_controller-0.0.3-py3-none-any.whl/documentation/__init__.py
@@ -0,0 +1,7 @@
+from .doc_dao import *
+from .doc_dto import *
+import logging
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+log.info("Initializing documentation pkg")
---
+++ packages/doc-controller/doc_controller-0.0.3-py3-none-any.whl/tutorial/__init__.py
@@ -0,0 +1,7 @@
+from .step_dto import *
+from .tut_dto import *
+import logging
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+log.info("Initializing tutorial pkg")
