3c12d4b1595d874fef3b6b27f8e3811885380968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 04:08:03 +0000

    Add package devopstestor/1.9.3/devopstestor-1.9.3.tar.gz

---
+++ packages/devopstestor/devopstestor-1.9.3.tar.gz/devopstestor-1.9.3/devopstestor/presets/saltstack/build4docker.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+set -o pipefail
+# If devopstestor is in a container, dockercontroller must be used without volume
+sed -i "s/preset_name: saltstack_dockerincontainer/preset_name: saltstack_dockerincontainer_novolume/g" "$DEVOPSTESTOR_PATH/presets/saltstack/config/machine.yml"
+
+# to avoid redundancy disable pillar and salt source accessor because, they can be mount directly with -v en /srv/salt and in /srv/pillar
+
+sed -i "s/undefined: please use --salt_files to set value/srv\/salt/g" "$DEVOPSTESTOR_PATH/presets/saltstack/config/machine.yml"
+sed -i "s/undefined: please use --pillar_files to set value/srv\/pillar/g" "$DEVOPSTESTOR_PATH/presets/saltstack/config/machine.yml"
+rm -f "$DEVOPSTESTOR_PATH/presets/saltstack/config/arguments.yml"
+
+exit $?
