fff33d7e4b75f79dc2823dacacd386f8e1d46b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 05:10:00 +0000

    Add package synapseml/0.11.4/synapseml-0.11.4-py2.py3-none-any.whl

---
+++ packages/synapseml/synapseml-0.11.4-py2.py3-none-any.whl/synapse/ml/cognitive/__init__.py
@@ -0,0 +1,35 @@
+# Copyright (C) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in project root for information.
+
+
+"""
+SynapseML is an ecosystem of tools aimed towards expanding the distributed computing framework
+Apache Spark in several new directions. SynapseML adds many deep learning and data science tools to the Spark
+ecosystem, including seamless integration of Spark Machine Learning pipelines with
+Microsoft Cognitive Toolkit (CNTK), LightGBM and OpenCV. These tools enable powerful and
+highly-scalable predictive and analytical models for a variety of datasources.
+
+SynapseML also brings new networking capabilities to the Spark Ecosystem. With the HTTP on Spark project,
+users can embed any web service into their SparkML models. In this vein, SynapseML provides easy to use SparkML
+transformers for a wide variety of Microsoft Cognitive Services. For production grade deployment,
+the Spark Serving project enables high throughput, sub-millisecond latency web services,
+backed by your Spark cluster.
+
+SynapseML requires Scala 2.12, Spark 3.0+, and Python 3.6+.
+"""
+
+__version__ = "0.11.4"
+__spark_package_version__ = "0.11.4"
+
+from synapse.ml.cognitive.anomaly import *
+from synapse.ml.cognitive.bing import *
+from synapse.ml.cognitive.face import *
+from synapse.ml.cognitive.form import *
+from synapse.ml.cognitive.language import *
+from synapse.ml.cognitive.openai import *
+from synapse.ml.cognitive.search import *
+from synapse.ml.cognitive.speech import *
+from synapse.ml.cognitive.text import *
+from synapse.ml.cognitive.translate import *
+from synapse.ml.cognitive.vision import *
+
---
+++ packages/synapseml/synapseml-0.11.4-py2.py3-none-any.whl/synapse/ml/cognitive/translate/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (C) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in project root for information.
+
+
+"""
+SynapseML is an ecosystem of tools aimed towards expanding the distributed computing framework
+Apache Spark in several new directions. SynapseML adds many deep learning and data science tools to the Spark
+ecosystem, including seamless integration of Spark Machine Learning pipelines with
+Microsoft Cognitive Toolkit (CNTK), LightGBM and OpenCV. These tools enable powerful and
+highly-scalable predictive and analytical models for a variety of datasources.
+
+SynapseML also brings new networking capabilities to the Spark Ecosystem. With the HTTP on Spark project,
+users can embed any web service into their SparkML models. In this vein, SynapseML provides easy to use SparkML
+transformers for a wide variety of Microsoft Cognitive Services. For production grade deployment,
+the Spark Serving project enables high throughput, sub-millisecond latency web services,
+backed by your Spark cluster.
+
+SynapseML requires Scala 2.12, Spark 3.0+, and Python 3.6+.
+"""
+
+__version__ = "0.11.4"
+__spark_package_version__ = "0.11.4"
+
+from synapse.ml.cognitive.translate.BreakSentence import *
+from synapse.ml.cognitive.translate.Detect import *
+from synapse.ml.cognitive.translate.DictionaryExamples import *
+from synapse.ml.cognitive.translate.DictionaryLookup import *
+from synapse.ml.cognitive.translate.DocumentTranslator import *
+from synapse.ml.cognitive.translate.Translate import *
+from synapse.ml.cognitive.translate.Transliterate import *
+
