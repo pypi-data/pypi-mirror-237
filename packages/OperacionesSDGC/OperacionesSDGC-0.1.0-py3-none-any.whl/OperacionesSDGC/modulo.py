0c9eb32fd8c47d5a14bd880bddb5d2b0e49d59f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 06:07:28 +0000

    Add package OperacionesSDGC/0.1.0/OperacionesSDGC-0.1.0-py3-none-any.whl

---
+++ packages/OperacionesSDGC/OperacionesSDGC-0.1.0-py3-none-any.whl/OperacionesSDGC/modulo.py
@@ -0,0 +1,18 @@
+class operaciones:
+    """
+    asasas 
+    """
+    def __init__(self, a ,b):
+        """
+        asas
+        """
+        self.a = a 
+        self.b = b
+    
+    def suma(self):
+        """ 
+        Esto es un metodo para sumas
+        """
+        return self.a +self.b
+    
+    
