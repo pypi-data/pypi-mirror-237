c487a99d46d417a824bb2f15e3b0fe163a531400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 03:08:07 +0000

    Add package service-Rabbit-thread/0.0.5.post1/service-Rabbit-thread-0.0.5.post1.tar.gz

---
+++ packages/service-Rabbit-thread/service-Rabbit-thread-0.0.5.post1.tar.gz/service-Rabbit-thread-0.0.5.post1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='service-Rabbit-thread',
+    version='0.0.5.post1',
+    license='MIT License',
+    author='Eliézer Schwartz',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='eliezer.mail090@gmail.com',
+    keywords='rabbit',
+    description=u'Wrapper não oficial do Rabbit',
+    packages=['rabbit_library'],
+    install_requires=['pika'],)
