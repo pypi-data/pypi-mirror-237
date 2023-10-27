88fc1d71c961b46fca5c7cf7a0a84e891ddc0ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 17:07:43 +0000

    Add package reptor/0.7/reptor-0.7.tar.gz

---
+++ packages/reptor/reptor-0.7.tar.gz/reptor-0.7/reptor/subcommands.py
@@ -0,0 +1,13 @@
+from collections import OrderedDict
+
+SUBCOMMANDS_GROUPS = OrderedDict(
+    {
+        "core": ("Core", list()),
+        "projects": ("Projects & Templates", list()),
+        "uploads": ("Uploads", list()),
+        "tools": ("Tools", list()),
+        "importers": ("Importers", list()),
+        "other": ("Other", list()),
+        "plugins": ("User Plugins", list()),
+    }
+)
