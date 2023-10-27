727de31a48354aacda80bd3b9b122699f9bf801e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-27 14:08:15 +0000

    Add package tentacletk/0.8.8/tentacletk-0.8.8-py3-none-any.whl

---
+++ packages/tentacletk/tentacletk-0.8.8-py3-none-any.whl/tentacle/slots/maya/__init__.py
@@ -0,0 +1,24 @@
+# !/usr/bin/python
+# coding=utf-8
+try:
+    import pymel.core as pm
+except ImportError as error:
+    print(__file__, error)
+from tentacle.slots import Slots
+
+
+class SlotsMaya(Slots):
+    """App specific methods inherited by all other app specific slot classes."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+# --------------------------------------------------------------------------------------------
+
+
+# module name
+# print (__name__)
+# --------------------------------------------------------------------------------------------
+# Notes
+# --------------------------------------------------------------------------------------------
