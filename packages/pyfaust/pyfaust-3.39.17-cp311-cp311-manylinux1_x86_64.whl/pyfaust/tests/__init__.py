047bac3f168c29aa10c9e553604c8e9b73618baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 03:13:00 +0000

    Add package pyfaust/3.39.17/pyfaust-3.39.17-cp311-cp311-manylinux1_x86_64.whl

---
+++ packages/pyfaust/pyfaust-3.39.17-cp311-cp311-manylinux1_x86_64.whl/pyfaust/tests/__init__.py
@@ -0,0 +1,19 @@
+import unittest
+from pyfaust.tests.TestFaust import TestFaust
+from pyfaust.tests.TestPoly import TestPoly
+
+
+def run_tests(dev, dtype):
+    """
+    Runs all available tests using device dev ('cpu' or 'gpu') and scalar type
+    dtype ('real' or 'complex') when it applies.
+    """
+    runner = unittest.TextTestRunner()
+    suite = unittest.TestSuite()
+    for class_name in ['TestFaust', 'TestPoly']:
+        testloader = unittest.TestLoader()
+        test_names = eval("testloader.getTestCaseNames("+class_name+")")
+        for meth_name in test_names:
+            test = eval(""+class_name+"('"+meth_name+"', dev=dev, dtype=dtype)")
+            suite.addTest(test)
+    runner.run(suite)
