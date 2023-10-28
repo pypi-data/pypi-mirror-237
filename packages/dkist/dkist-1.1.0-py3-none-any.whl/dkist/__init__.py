d9095da4e3b5f676ec9605ed00b625dfaec80313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 04:07:52 +0000

    Add package dkist/1.1.0/dkist-1.1.0-py3-none-any.whl

---
+++ packages/dkist/dkist-1.1.0-py3-none-any.whl/dkist/__init__.py
@@ -0,0 +1,28 @@
+"""
+The DKIST package aims to help you search, obtain and use DKIST data as part of your Python software.
+"""
+from pkg_resources import DistributionNotFound, get_distribution
+
+import astropy.config as _config
+
+from .dataset import Dataset, TiledDataset, load_dataset  # noqa
+from .utils.sysinfo import system_info  # noqa
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "unknown"
+
+__all__ = ['TiledDataset', 'Dataset', 'load_dataset', 'system_info']
+
+
+def write_default_config(overwrite=False):
+    """
+    Writes out the template configuration file for this version of dkist.
+
+    This function will save a template config file for manual editing, if a
+    config file already exits this will write a config file appended with the
+    version number, to facilitate comparison of changes.
+    """
+    return _config.create_config_file("dkist", "dkist", overwrite=overwrite)
