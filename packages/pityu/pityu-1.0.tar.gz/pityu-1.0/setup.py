4baa3e15a831e694d2a3d88011fc9af715ec3854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 05:05:52 +0000

    Add package pityu/1.0/pityu-1.0.tar.gz

---
+++ packages/pityu/pityu-1.0.tar.gz/pityu-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pityu",
+    version="1.0",
+    author="Pityu",
+    author_email="pista1125@gmail.com",
+    description="A description of your package",
+    license="MIT"
+)
