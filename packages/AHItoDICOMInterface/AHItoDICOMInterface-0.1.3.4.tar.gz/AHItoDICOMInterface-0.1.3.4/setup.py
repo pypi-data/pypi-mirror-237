124d2a089740300de565e278bb004bc23b40d62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 15:07:44 +0000

    Add package AHItoDICOMInterface/0.1.3.4/AHItoDICOMInterface-0.1.3.4.tar.gz

---
+++ packages/AHItoDICOMInterface/AHItoDICOMInterface-0.1.3.4.tar.gz/AHItoDICOMInterface-0.1.3.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='AHItoDICOMInterface',
+    version='0.1.3.4',    
+    description='A package to simply export DICOM data from AWS HealthImaging in your application memory or the file system.',
+    url='https://github.com/aws-samples/healthlake-imaging-to-dicom-python-module',
+    long_description='More details about the project and features can be found on the project\'s GitHub page.',
+    author='JP Leger',
+    author_email='jpleger@amazon.com',
+    license='MIT-0',
+    packages=['AHItoDICOMInterface'],
+    install_requires=[  'boto3',
+                        'botocore',
+                        'pydicom',
+                        'pylibjpeg-openjpeg>=1.3.0',
+                        'numpy',
+                        'pillow ',                 
+                      ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT No Attribution License (MIT-0)',  
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',        
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
+            
+)
