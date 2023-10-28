8220694a8dd3dc598f56bd9d9a620b3338a45119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 03:11:48 +0000

    Add package kt2/0.0.4a0/kt2-0.0.4a0.tar.gz

---
+++ packages/kt2/kt2-0.0.4a0.tar.gz/kt2-0.0.4a0/kt2/errors.py
@@ -0,0 +1,10 @@
+class KoppeltaalApiError(Exception):
+    """Base exception class for Koppeltaal errors."""
+
+    ...
+
+
+class OAuthError(Exception):
+    """Base exception class for oauth errors."""
+
+    ...
