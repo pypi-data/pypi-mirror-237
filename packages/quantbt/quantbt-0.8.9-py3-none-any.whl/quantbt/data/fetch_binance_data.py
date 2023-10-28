56482d74a9e9e09fcf29164b8f925353b655624e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 05:06:13 +0000

    Add package quantbt/0.8.9/quantbt-0.8.9-py3-none-any.whl

---
+++ packages/quantbt/quantbt-0.8.9-py3-none-any.whl/quantbt/data/fetch_binance_data.py
@@ -0,0 +1,29 @@
+from binance.client import Client
+import pandas as pd
+import mplfinance as mpf
+from .create_binance_dataframe import create_binance_dataframe
+
+
+def get_data(
+    asset, tf=Client.KLINE_INTERVAL_1HOUR, days="3000 day ago UTC", save_location="data"
+):
+    # asset = asset[0]
+    print(f"Getting data for {asset} on {tf}")
+    client = Client()
+    klines = client.get_historical_klines(asset, tf, days)
+
+    df = create_binance_dataframe(klines)
+    df.to_parquet(f"{save_location}/binance-{asset}-{tf}.parquet")
+
+
+def fetch_binance_data(
+    assets=None,
+    tf=Client.KLINE_INTERVAL_5MINUTE,
+    days="3 day ago UTC",
+    save_location="data",
+):
+    if assets is None:
+        print("Please provide at least one asset item")
+    else:
+        for asset in assets:
+            get_data(asset, tf, days, save_location)
