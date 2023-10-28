2d0eef76178f6f2016995e08d4a664692e8da938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 15:09:43 +0000

    Add package yak-server/0.36.0/yak_server-0.36.0.tar.gz

---
+++ packages/yak-server/yak_server-0.36.0.tar.gz/yak_server-0.36.0/yak_server/helpers/settings.py
@@ -0,0 +1,32 @@
+import sys
+from functools import lru_cache
+
+import pendulum
+
+if sys.version_info >= (3, 9):
+    from typing import Annotated
+else:
+    from typing_extensions import Annotated
+
+from pydantic import PlainValidator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from .rules import Rules
+
+PendulumDateTime = Annotated[pendulum.DateTime, PlainValidator(lambda x: pendulum.parse(x))]
+
+
+class Settings(BaseSettings):
+    jwt_secret_key: str
+    jwt_expiration_time: int
+    competition: str
+    lock_datetime: PendulumDateTime
+    data_folder: str
+    rules: Rules
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="allow")
+
+
+@lru_cache(maxsize=None)
+def get_settings() -> Settings:
+    return Settings()
