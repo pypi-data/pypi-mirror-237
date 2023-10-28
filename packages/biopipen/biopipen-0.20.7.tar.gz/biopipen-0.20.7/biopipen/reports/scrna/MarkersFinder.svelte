4e3db69b8988c817d14894e83c730a0c03fe636b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 05:07:12 +0000

    Add package biopipen/0.20.7/biopipen-0.20.7.tar.gz

---
+++ packages/biopipen/biopipen-0.20.7.tar.gz/biopipen-0.20.7/biopipen/reports/scrna/MarkersFinder.svelte
@@ -0,0 +1,92 @@
+{% from "utils/misc.liq" import report_jobs -%}
+{% from "utils/gsea.liq" import enrichr_report -%}
+<script>
+    import { Image, DataTable } from "$libs";
+    import { Tabs, Tab, TabContent, InlineNotification } from "$ccs";
+</script>
+
+
+{%- macro report_job(job, h=1) -%}
+    {%- set secdirs = job.out.outdir | glob: "*" -%}
+    {%- if len(secdirs) == 1 -%}
+        {%- set secname = secdirs | first | basename -%}
+        {%- for casedir in secdirs[0] | glob: "*" -%}
+            {%- if secname == "DEFAULT" -%}
+                <h{{h}}>{{casedir | basename | escape}}</h{{h}}>
+            {%- else -%}
+                <h{{h}}>{{secname | escape}} - {{casedir | basename | escape}}</h{{h}}>
+            {%- endif -%}
+            {%- if casedir | joinpaths: "error.txt" | exists -%}
+                <InlineNotification
+                    hideCloseButton
+                    lowContrast
+                    kind="warning"
+                    subtitle={{ casedir | joinpaths: "error.txt" | read | quote }}
+                    />
+            {%- else -%}
+                <h{{h+1}}>Markers</h{{h+1}}>
+                <Tabs>
+                    <Tab label="Markers" />
+                    <Tab label="Volcano Plot" />
+                    <svelte:fragment slot="content">
+                        <TabContent>
+                            <DataTable
+                                src={{ casedir | joinpaths: "markers.txt" | quote }}
+                                data={ {{ casedir | joinpaths: "markers.txt" | datatable: sep="\t", nrows=100 }} }
+                                />
+                        </TabContent>
+                        <TabContent>
+                            <Image src={{ casedir | joinpaths: "volcano.png" | quote }} />
+                        </TabContent>
+                    </svelte:fragment>
+                </Tabs>
+
+                <h{{h+1}}>Enrichment analysis</h{{h+1}}>
+                {{ enrichr_report(casedir) }}
+            {%- endif -%}
+        {%- endfor -%}
+    {%- else -%}
+        {%- for secdir in secdirs -%}
+            {%- set sec = secdir | basename -%}
+            <h{{h}}>{{sec | escape}}</h{{h}}>
+            {%- for casedir in secdir | glob: "*" -%}
+                <h{{h+1}}>{{casedir | basename | escape}}</h{{h+1}}>
+                {%- if casedir | joinpaths: "error.txt" | exists -%}
+                    <InlineNotification
+                        hideCloseButton
+                        lowContrast
+                        kind="warning"
+                        subtitle={{ casedir | joinpaths: "error.txt" | read | quote }}
+                        />
+                {%- else -%}
+                    <h{{h+2}}>Markers</h{{h+2}}>
+                    <Tabs>
+                        <Tab label="Markers" />
+                        <Tab label="Volcano Plot" />
+                        <svelte:fragment slot="content">
+                            <TabContent>
+                                <DataTable
+                                    src={{ casedir | joinpaths: "markers.txt" | quote }}
+                                    data={ {{ casedir | joinpaths: "markers.txt" | datatable: sep="\t", nrows=100 }} }
+                                    />
+                            </TabContent>
+                            <TabContent>
+                                <Image src={{ casedir | joinpaths: "volcano.png" | quote }} />
+                            </TabContent>
+                        </svelte:fragment>
+                    </Tabs>
+
+                    <h{{h+2}}>Enrichment analysis</h{{h+2}}>
+                    {{ enrichr_report(casedir) }}
+                {%- endif -%}
+            {%- endfor -%}
+        {%- endfor -%}
+    {%- endif -%}
+{%- endmacro -%}
+
+
+{%- macro head_job(job) -%}
+  <h1>{{job.in.srtobj | stem | escape}}</h1>
+{%- endmacro -%}
+
+{{ report_jobs(jobs, head_job, report_job) }}
