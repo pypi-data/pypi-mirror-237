e17d5357dc910908934a170aa88091f15f742949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 05:07:58 +0000

    Add package basextools/0.0.1/basextools-0.0.1.tar.gz

---
+++ packages/basextools/basextools-0.0.1.tar.gz/basextools-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='basextools',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy~=1.24.3',
+        'pandas~=2.0.2',
+        'scipy~=1.10.1',
+        'matplotlib~=3.7.1',
+        'matplotlib_venn~=0.11.9'
+    ],
+    author='pin-people',
+    author_email='rodrigo.toledo@pinpeople.com.br',
+    description='Tools for EX Analysis',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    extras_require={
+        "dev":[
+            "pytest>=3.7"
+        ],
+    },
+    classifiers=[]
+)
---
+++ packages/basextools/basextools-0.0.1.tar.gz/basextools-0.0.1/src/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["ada", "bender", "lygia", "tables"]
