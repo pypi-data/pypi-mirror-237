e66e1c987d314dca87e633ed91dee5e1d5570065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 05:06:53 +0000

    Add package code-structure-tools/1.0.0/code_structure_tools-1.0.0-2-py3-none-any.whl

---
+++ packages/code-structure-tools/code_structure_tools-1.0.0-2-py3-none-any.whl/code_structure_tools/class_utils.py
@@ -0,0 +1,13 @@
+def example_class_inside_class():
+    """
+    Purpose: If you define a class inside a class, it's just like
+    putting that class variable in the persistent namespace of the outer class
+    --> but again, that namespace isn't accessible from inside function like in module,
+        can only access it through the self
+    """
+    class Outer:
+        class Inner:
+            pass
+
+        def __init__(self):
+            self.inner_obj = self.Inner()
