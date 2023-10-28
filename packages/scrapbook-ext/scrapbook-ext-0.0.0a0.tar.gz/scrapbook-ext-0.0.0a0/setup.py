baf9502f13882c1fcc02b2df555b34787bbcf82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 15:09:21 +0000

    Add package scrapbook-ext/0.0.0a0/scrapbook-ext-0.0.0a0.tar.gz

---
+++ packages/scrapbook-ext/scrapbook-ext-0.0.0a0.tar.gz/scrapbook-ext-0.0.0a0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import setuptools
+import pathlib
+
+this_directory = pathlib.Path(__file__).parent
+
+setuptools.setup(
+    name='scrapbook-ext',
+    version='0.0.0a',
+    description='Extensions for scrapbook',
+    long_description=(this_directory / 'README.md').read_text(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    package_dir={
+        '': 'src'
+    },
+    packages=['scrapbook_ext'],
+    install_requires=[
+        'scrapbook'
+    ],
+    extras_require={
+        'dev': []
+    }
+)
