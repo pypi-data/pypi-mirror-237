725b04fa75802f7d0a09dff10b49abe57c727237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 04:12:41 +0000

    Add package tensorboard-wrapper/0.2.1/tensorboard-wrapper-0.2.1.tar.gz

---
+++ packages/tensorboard-wrapper/tensorboard-wrapper-0.2.1.tar.gz/tensorboard-wrapper-0.2.1/src/tensorboard_wrapper/__init__.py
@@ -0,0 +1,12 @@
+"""
+Tensorboard Wrapper
+
+This is a wrapper class for the Tensorflow's Tensorboard application.
+"""
+
+from .tensorboard import Tensorboard
+from .exceptions import BoardAlreadyExistsException
+
+
+__author__ = "Nathan Gavenski"
+__credits__ = "Nathan Gavenski"
