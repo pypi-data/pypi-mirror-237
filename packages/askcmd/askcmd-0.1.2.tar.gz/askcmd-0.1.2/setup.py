373ef83d5c42530cf69824264637a201254b3a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 15:09:45 +0000

    Add package askcmd/0.1.2/askcmd-0.1.2.tar.gz

---
+++ packages/askcmd/askcmd-0.1.2.tar.gz/askcmd-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="askcmd",
+    version="0.1.2",
+    author='lhan',
+    author_email='lhan12345@hotmail.com',
+    description='A command line tool to generate any commands from natural language',
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    entry_points={
+        'console_scripts': [
+            'askcmd = askcmd.__main__:main',
+        ],
+    }
+)
