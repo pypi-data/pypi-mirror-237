5e714fa7136e5984bf2e36e7528e12cfe12c02c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 03:12:59 +0000

    Add package balance-mange-trade-ex-forex-next3-ex-forex-next3/1.51/balance_mange_trade_ex_forex_next3_ex_forex_next3-1.51.tar.gz

---
+++ packages/balance-mange-trade-ex-forex-next3-ex-forex-next3/balance_mange_trade_ex_forex_next3_ex_forex_next3-1.51.tar.gz/balance_mange_trade_ex_forex_next3_ex_forex_next3-1.51/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup (name='balance_mange_trade_ex_forex_next3_ex_forex_next3' ,
+       version = '1.51' ,
+       description = 'balance_mange_trade_ex_forex_next3_ex_forex_next3' ,
+       author = 'omid' ,
+       packages =['balance_mange_trade_ex_forex_next3_ex_forex_next3'] ,
+       zip_safe = False
+       )
