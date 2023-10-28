02e756954a1c12827fca9f44a5407ed143ba1eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 03:11:59 +0000

    Add package refractfs/1.0.0/refractfs-1.0.0.tar.gz

---
+++ packages/refractfs/refractfs-1.0.0.tar.gz/refractfs-1.0.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+setup(
+    name="refractfs",
+    version="1.0.0",
+    description="Feature store recipe",
+    author="Amit Boke",
+    classifiers=["Programming Language :: Python :: 3.8"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["feast==0.34.1"],
+    python_requires=">=3.8,<3.10"
+)
