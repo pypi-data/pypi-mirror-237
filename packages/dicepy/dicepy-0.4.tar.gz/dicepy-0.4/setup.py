5236a8243fc0064f47046c3cbd1debe338de0006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 03:10:51 +0000

    Add package dicepy/0.4/dicepy-0.4.tar.gz

---
+++ packages/dicepy/dicepy-0.4.tar.gz/dicepy-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dicepy',
+    version='0.4',
+    author='James J. Evans',
+    author_email='joesaysahoy@gmail.com',
+    description='A dice rolling library for Python.',
+    readme='README.md',
+    url='https://github.com/primal-coder/dicepy',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    keywords='dice rolling die d20 dnd d&d rpg role playing game',
+    python_requires='>=3.6',
+    install_requires=['pyglet']
+)
