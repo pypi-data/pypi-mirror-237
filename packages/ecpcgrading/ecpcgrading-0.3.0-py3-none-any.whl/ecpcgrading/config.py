33c41e1b38aaefb04e2ff3840ec7df01ff1f095c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 05:07:10 +0000

    Add package ecpcgrading/0.3.0/ecpcgrading-0.3.0-py3-none-any.whl

---
+++ packages/ecpcgrading/ecpcgrading-0.3.0-py3-none-any.whl/ecpcgrading/config.py
@@ -0,0 +1,25 @@
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+from pathlib import Path
+
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    root_path: Path
+    submissions_path: Path = Path("submissions")
+    code_path: Path = Path("code")
+    env_prefix: str = "ECPC_"
+    course_alias: str
+    assignment_group: str
+    groupset: str | None = None
+    group: str | None = None
+
+
+def read_config(folder: Path):
+    defaults = {"root_path": folder}
+    with open(folder / "grading.toml", "rb") as f:
+        data = tomllib.load(f)
+    return Config.model_validate(defaults | data)
