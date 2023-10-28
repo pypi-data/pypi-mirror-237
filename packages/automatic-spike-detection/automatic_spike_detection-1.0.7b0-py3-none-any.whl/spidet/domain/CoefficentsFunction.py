fc41b58af4594532e04f3fde8119bf002f6a8de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 03:12:14 +0000

    Add package automatic-spike-detection/1.0.7b0/automatic_spike_detection-1.0.7b0-py3-none-any.whl

---
+++ packages/automatic-spike-detection/automatic_spike_detection-1.0.7b0-py3-none-any.whl/spidet/domain/CoefficentsFunction.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import Any, Dict
+import numpy as np
+
+from spidet.domain.SpikeDetectionFunction import SpikeDetectionFunction
+
+
+@dataclass
+class CoefficientsFunction(SpikeDetectionFunction):
+    spikes_on_indices: np.ndarray[Any, np.dtype[int]]
+    spikes_off_indices: np.ndarray[Any, np.dtype[int]]
+    spike_threshold: float
