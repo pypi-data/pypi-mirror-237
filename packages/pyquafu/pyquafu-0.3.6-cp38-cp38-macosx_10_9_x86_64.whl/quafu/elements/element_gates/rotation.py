ad1ac7cf0f765ff22c357ffaed8e6431c01509e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 15:09:23 +0000

    Add package pyquafu/0.3.6/pyquafu-0.3.6-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/pyquafu/pyquafu-0.3.6-cp38-cp38-macosx_10_9_x86_64.whl/quafu/elements/element_gates/rotation.py
@@ -0,0 +1,92 @@
+from typing import Dict
+
+from quafu.elements.matrices import rx_mat, ry_mat, rz_mat, rxx_mat, ryy_mat, rzz_mat
+from ..quantum_gate import QuantumGate, SingleQubitGate, ParametricGate
+
+__all__ = ['RXGate', 'RYGate', 'RZGate', 'RXXGate', 'RYYGate', 'RZZGate']
+
+
+class RXGate(ParametricGate, SingleQubitGate):
+    name = "RX"
+    
+    def __init__(self, pos: int, theta: float = 0.):
+        ParametricGate.__init__(self, pos, paras=theta)
+
+    @property
+    def matrix(self):
+        return rx_mat(self.paras)
+
+
+class RYGate(ParametricGate, SingleQubitGate):
+    name = "RY"
+    
+    def __init__(self, pos: int, theta: float = 0.):
+        ParametricGate.__init__(self, pos, paras=theta)
+
+    @property
+    def matrix(self):
+        return ry_mat(self.paras)
+
+
+class RZGate(ParametricGate, SingleQubitGate):
+    name = "RZ"
+    
+    def __init__(self, pos: int, theta: float = 0.):
+        ParametricGate.__init__(self, pos, paras=theta)
+        
+    @property
+    def matrix(self):
+        return rz_mat(self.paras)
+
+
+class RXXGate(ParametricGate):
+    name = "RXX"
+
+    def __init__(self, q1: int, q2: int, theta: float = 0.):
+        ParametricGate.__init__(self, [q1, q2], paras=theta)
+
+    @property
+    def matrix(self):
+        return rxx_mat(self.paras)
+    
+    @property
+    def named_pos(self) -> Dict:
+        return {'pos': self.pos}
+
+
+class RYYGate(ParametricGate):
+    name = "RYY"
+
+    def __init__(self, q1: int, q2: int, theta: float = 0.):
+        ParametricGate.__init__(self, [q1, q2], paras=theta)
+
+    @property
+    def matrix(self):
+        return ryy_mat(self.paras)
+
+    @property
+    def named_pos(self) -> Dict:
+        return {'pos': self.pos}
+
+
+class RZZGate(ParametricGate):
+    name = "RZZ"
+
+    def __init__(self, q1: int, q2: int, theta: float = 0.):
+        ParametricGate.__init__(self, [q1, q2], paras=theta)
+
+    @property
+    def matrix(self):
+        return rzz_mat(self.paras)
+
+    @property
+    def named_pos(self) -> Dict:
+        return {'pos': self.pos}
+
+
+QuantumGate.register_gate(RXGate)
+QuantumGate.register_gate(RYGate)
+QuantumGate.register_gate(RZGate)
+QuantumGate.register_gate(RXXGate)
+QuantumGate.register_gate(RYYGate)
+QuantumGate.register_gate(RZZGate)
