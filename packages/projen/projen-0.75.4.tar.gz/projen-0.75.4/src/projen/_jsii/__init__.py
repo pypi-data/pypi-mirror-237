1922e10ebd639b9ac8f800e8b7cadbc1ed700d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 04:10:31 +0000

    Add package projen/0.75.4/projen-0.75.4.tar.gz

---
+++ packages/projen/projen-0.75.4.tar.gz/projen-0.75.4/src/projen/_jsii/__init__.py
@@ -0,0 +1,21 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "projen", "0.75.4", __name__[0:-6], "projen@0.75.4.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
