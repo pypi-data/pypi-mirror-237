b1e66d221b4c14e57bd15f1ae15bd54063e802d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 04:07:50 +0000

    Add package pygame-simple-core/0.1.3/pygame_simple_core-0.1.3.tar.gz

---
+++ packages/pygame-simple-core/pygame_simple_core-0.1.3.tar.gz/pygame_simple_core-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as file:
+    readme = file.read()
+
+setup(
+    name='pygame_simple_core',
+    version='0.1.3',
+    license='MIT license',
+    author='Vinicius Putti Morais',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author_email='viniputtim@gmail.com',
+    keywords='pygame',
+    description='A Python library for building Pygame applications with reusable components and utilities.',
+    packages=find_packages(),
+    install_requires=['pygame']
+)
