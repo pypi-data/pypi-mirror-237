6e84fa8a781954aa72754b805b83b2a14476d092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 03:10:48 +0000

    Add package ibm-generative-ai/0.4.1/ibm-generative-ai-0.4.1.tar.gz

---
+++ packages/ibm-generative-ai/ibm-generative-ai-0.4.1.tar.gz/ibm-generative-ai-0.4.1/src/genai/credentials.py
@@ -0,0 +1,37 @@
+import re
+from warnings import warn
+
+
+class Credentials:
+    DEFAULT_API = "https://workbench-api.res.ibm.com"
+
+    def __init__(
+        self,
+        api_key: str,
+        api_endpoint: str = DEFAULT_API,
+    ):
+        """
+        Instantiate the credentials object
+
+        Args:
+            api_key (str): The GENAI API Key
+            api_endpoint (str, optional): GENAI API Endpoint. Defaults to DEFAULT_API.
+        """
+        if api_key is None:
+            raise ValueError("api_key must be provided")
+        self.api_key = api_key
+        if api_endpoint is None:
+            raise ValueError("api_endpoint must be provided")
+        self.api_endpoint = api_endpoint.rstrip("/")
+        self._remove_api_endpoint_version()
+
+    def _remove_api_endpoint_version(self) -> None:
+        [api, *version] = re.split(r"(/v\d+$)", self.api_endpoint, maxsplit=1)
+        if version:
+            warn(
+                DeprecationWarning(
+                    f"The 'api_endpoint' property should not contain any explicit API version"
+                    f"(rename it from '{self.api_endpoint}' to just '{api}')"
+                )
+            )
+            self.api_endpoint = api
---
+++ packages/ibm-generative-ai/ibm-generative-ai-0.4.1.tar.gz/ibm-generative-ai-0.4.1/tests/test_service_interface.py
@@ -0,0 +1,46 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from genai.schemas import GenerateParams, ReturnOptions
+from genai.services import ServiceInterface
+from genai.utils.request_utils import sanitize_params
+from tests.assets.response_helper import SimpleResponse
+
+
+@pytest.mark.unit
+class TestServiceInterface:
+    def setup_method(self):
+        # mock object for the API call
+        self.service = ServiceInterface(service_url="SERVICE_URL", api_key="API_KEY")
+        self.model = "google/ul2"
+        self.inputs = ["Write a tagline for an alumni association: Together we"]
+
+    @pytest.fixture
+    def params(seld):
+        return GenerateParams(decoding_method="greedy", return_options=ReturnOptions(input_text=True))
+
+    @patch("genai.services.RequestHandler.patch")
+    def test_tou(self, mocked_post_request):
+        expected_resp = SimpleResponse.terms_of_use()
+        expected = MagicMock(status_code=200, json=expected_resp)
+        mocked_post_request.return_value = expected
+
+        resp = self.service.terms_of_use(True)
+
+        assert resp == expected
+        assert resp.status_code == 200
+
+    @patch(
+        "genai.services.RequestHandler.patch",
+        side_effect=Exception("some general error"),
+    )
+    def test_tou_exception(self, mock):
+        with pytest.raises(BaseException, match="some general error"):
+            self.service.terms_of_use(True)
+
+    def test_sanitize_params(self, params):
+        new_dict = sanitize_params(params=params)
+
+        assert isinstance(new_dict, dict)
+        assert "min_new_tokens" not in new_dict
