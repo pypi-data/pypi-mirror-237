29191d051970adad8e2e3ffe544631d9d6bce106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 03:11:07 +0000

    Add package datamaestro-text/2023.10.27/datamaestro-text-2023.10.27.tar.gz

---
+++ packages/datamaestro-text/datamaestro-text-2023.10.27.tar.gz/datamaestro-text-2023.10.27/setup.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from setuptools import setup
+
+basepath = Path(__file__).parent
+
+
+setup(
+    install_requires=(basepath / "requirements.txt").read_text(), use_scm_version=True,
+)
