4647ebd9fc49caa1b8a41746ee14c81cb07f786b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 04:07:58 +0000

    Add package mockallan/0.0.7/mockallan-0.0.7-py3-none-any.whl

---
+++ packages/mockallan/mockallan-0.0.7-py3-none-any.whl/request.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass, field
+
+
+
+class ContentType:
+	TEXT_PLAIN = {'Content-Type': 'text/plain'}
+	APPLICATION_JSON = {'Content-Type': 'application/json'}
+	APPLICATION_JSON_ERROR = {'Content-Type': 'application/json+error'}
+	APPLICATION_XML = {'Content-Type': 'application/xml'}
+	APPLICATION_OCTET_STREAM = {'Content-Type': 'application/octet-stream'}
+
+
+@dataclass
+class HTTPRequest:
+	method: str
+	path: str
+	query: dict = field(default_factory=dict)
+	headers: dict = field(default_factory=dict)
+	body: dict | str | bytes = ''
+
+	@staticmethod
+	def endpoint(request: 'HTTPRequest') -> tuple[str, str]:
+		return (request.method.upper(), request.path)
+
+
+@dataclass
+class HTTPResponse:
+	code: int
+	headers: dict = field(default_factory=dict)
+	body: dict | str | bytes = ''
