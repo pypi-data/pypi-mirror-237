74914ea74b1681e7875fe09b7c2ba98437bd72b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 16:07:40 +0000

    Add package MDIC3/1.0.2/MDIC3-1.0.2.tar.gz

---
+++ packages/MDIC3/MDIC3-1.0.2.tar.gz/MDIC3-1.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name="MDIC3",
+    version="1.0.2",
+    author="Lyxiaotai",
+    author_email="liuyi54894xiaotai@163.com",  
+    description="A Lucky Python Package",  
+    url="https://github.com/LYxiaotai/MDIC3",     
+   
+)
+
+
