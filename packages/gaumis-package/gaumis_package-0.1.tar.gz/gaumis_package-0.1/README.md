b6a8157974553dce235474a71aec79cb5c2beff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 15:08:56 +0000

    Add package gaumis-package/0.1/gaumis_package-0.1.tar.gz

---
+++ packages/gaumis-package/gaumis_package-0.1.tar.gz/gaumis_package-0.1/README.md
@@ -0,0 +1,2 @@
+# gaumis_package
+This is demo project to push package to pypi
---
+++ packages/gaumis-package/gaumis_package-0.1.tar.gz/gaumis_package-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gaumis_package',
+    version='0.1',
+    description='A package for doing cool things',
+    author='Kumar Gaurav',
+    author_email='kumargaurav1527@gmail.com',
+    url='https://github.com/Gaumis/gaumis_package.git',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=[
+        # Add any dependencies your package requires here
+    ],
+)
