890e92fe84501c11116a2f04ea9deb160188b36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 06:06:27 +0000

    Add package fair-mobil/1.1.4/fair-mobil-1.1.4.tar.gz

---
+++ packages/fair-mobil/fair-mobil-1.1.4.tar.gz/fair-mobil-1.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fair-mobil",
+    version="1.1.4",
+    description="My package description",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy>=1.20.0",
+        "scipy>=1.6.0",
+        "pandas>=1.2.0",
+        "geopandas>=0.9.0",
+        "plotly>=5.0.0",
+    ],
+)
