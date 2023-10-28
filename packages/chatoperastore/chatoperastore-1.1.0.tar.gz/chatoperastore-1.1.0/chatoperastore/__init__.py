b1ba0de9324662d6d178dbfafb2f881b0cb47b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 15:08:14 +0000

    Add package chatoperastore/1.1.0/chatoperastore-1.1.0.tar.gz

---
+++ packages/chatoperastore/chatoperastore-1.1.0.tar.gz/chatoperastore-1.1.0/chatoperastore/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["download_licensedfile", "extract_licensedfile", "LicensedfileDownloadException"]
+__version__ = "1.1.0"
+
+from .chatoperastore import download_licensedfile
+from .chatoperastore import extract_licensedfile
+from .exceptions import LicensedfileDownloadException
