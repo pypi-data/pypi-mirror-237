2c8e6329b8a694c4ee0e5cb5ad8b9c2a46053d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-28 04:07:57 +0000

    Add package genice-core/0.5.1/genice_core-0.5.1.tar.gz

---
+++ packages/genice-core/genice_core-0.5.1.tar.gz/genice_core-0.5.1/test_pbc.py
@@ -0,0 +1,28 @@
+from logging import DEBUG, INFO, basicConfig, getLogger
+
+import networkx as nx
+
+# import py3Dmol
+from genice2.genice import GenIce
+from genice2.plugin import Format, Lattice
+
+from genice_core import ice_graph
+
+logger = getLogger()
+basicConfig(level=DEBUG)
+
+lattice = Lattice("1h")
+formatter = Format("raw", stage=(1, 2))
+raw = GenIce(lattice, signature="Ice Ih", rep=(3, 3, 3)).generate_ice(formatter)
+
+# graph is the topology of the hydrogen-bond network
+g = nx.Graph(raw["graph"])
+# reppositions contains the positions of CoM of water in fractional coordinate
+layout = raw["reppositions"]
+# repcell is the cell matrix (transposed)
+cell = raw["repcell"]
+
+# set orientations of the hydrogen bonds.
+dg = ice_graph(
+    g, vertexPositions=layout, isPeriodicBoundary=True, dipoleOptimizationCycles=100
+)
