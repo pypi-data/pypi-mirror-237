4a737c5dfc7438a901d98c5efc219c0f68cf26a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 04:10:53 +0000

    Add package cricinfo/0.1/cricinfo-0.1.tar.gz

---
+++ packages/cricinfo/cricinfo-0.1.tar.gz/cricinfo-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cricinfo",
+    version="0.1",
+    packages=["cricinfo"],
+    install_requires=[
+        # List your library's dependencies here
+        
+    ],
+)
