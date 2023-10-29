d6489ddd74d6779afd3abcee9e67b3a32f55e4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 03:10:09 +0000

    Add package drf-serializer-prefetch/1.1.5/drf_serializer_prefetch-1.1.5-py3-none-any.whl

---
+++ packages/drf-serializer-prefetch/drf_serializer_prefetch-1.1.5-py3-none-any.whl/tests/models.py
@@ -0,0 +1,26 @@
+# Django
+from django.db import models
+
+
+class Continent(models.Model):
+    label = models.CharField(max_length=50)
+
+
+class Country(models.Model):
+    label = models.CharField(max_length=50)
+    continent = models.ForeignKey(Continent, on_delete=models.CASCADE, null=True)
+
+
+class Pizza(models.Model):
+    label = models.CharField(max_length=50)
+    provenance = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    extra_data = models.JSONField(null=True)
+
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE, related_name="toppings")
+    label = models.CharField(max_length=50)
+    origin = models.ForeignKey(
+        Country, on_delete=models.CASCADE, related_name="toppings"
+    )
