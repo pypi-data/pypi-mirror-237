cb8d084d4c6da06484ebfe42b4da901d9ae0e32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 14:07:58 +0000

    Add package adiosiker/1.0/adiosiker-1.0.tar.gz

---
+++ packages/adiosiker/adiosiker-1.0.tar.gz/adiosiker-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adiosiker",
+    version="1.0",
+    description="solo importa",
+    author="Tu Nombre",
+    author_email="tu@email.com",
+    packages=find_packages(),
+    install_requires=[""
+                      ],
+)
