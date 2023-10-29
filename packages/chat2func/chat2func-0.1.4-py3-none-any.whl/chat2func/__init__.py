98965cd1f792d5bc3f5c2979f1e4069936ba861c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 04:08:59 +0000

    Add package chat2func/0.1.4/chat2func-0.1.4-py3-none-any.whl

---
+++ packages/chat2func/chat2func-0.1.4-py3-none-any.whl/chat2func/__init__.py
@@ -0,0 +1,9 @@
+from .schema import FunctionCallError, collect_functions, function_call, json_schema, type_to_schema
+
+__all__ = [
+    "collect_functions",
+    "function_call",
+    "FunctionCallError",
+    "json_schema",
+    "type_to_schema",
+]
