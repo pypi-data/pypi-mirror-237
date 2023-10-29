d5647a8d1dafd6fa5e785f5311fdb78372c72838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 04:10:53 +0000

    Add package pythics/0.0.1/pythics-0.0.1.tar.gz

---
+++ packages/pythics/pythics-0.0.1.tar.gz/pythics-0.0.1/README.md
@@ -0,0 +1,10 @@
+### What is pythics?
+
+Pythics is a simple 2D physics simulator developed alongside instruction in AP Physics 1. While mostly being an application of the concepts learned in class to further my understanding, it will be implemented to coexist with another project using pygame for game development. 
+
+## Units
+* Mass &#8594; $grams$ 
+* Positions &#8594; $meters$ 
+* Velocity &#8594; $\frac{meters}{second}$
+* Acceleration &#8594; $\frac{meters}{second^2}$
+* Forces &#8594; $Newtons$
---
+++ packages/pythics/pythics-0.0.1.tar.gz/pythics-0.0.1/pythics/vector.py
@@ -0,0 +1,68 @@
+import math
+
+class Vector:
+    # Quick directional vectors
+    @property
+    def down() -> 'Vector':
+        return Vector(0, 1)
+
+    @property
+    def up() -> 'Vector':
+        return Vector(0, -1)
+
+    @property
+    def left() -> 'Vector':
+        return Vector(-1, 0)
+
+    @property
+    def right() -> 'Vector':
+        return Vector(1, 0)
+
+    @property
+    def zero() -> 'Vector':
+        return Vector(0, 0)
+
+    # Initializing function
+    def __init__(self, x: float, y: float) -> None:
+        self.x: float = x
+        self.y: float = y
+
+
+    # Quality of life functions
+    @property
+    def as_tup(self):
+        return (self.x, self.y)
+
+    def __repr__(self) -> str:
+        return f'<{self.x}, {self.y}>'
+
+    # Vector functions
+    @property
+    def magnitude(self) -> float:
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def normalize(self) -> 'Vector':
+        mag = self.magnitude
+        return Vector(self.x / mag, self.y / mag)
+
+    def dot(self, other: 'Vector') -> float:
+        return self.x * other.x + self.y * other.y
+
+    # Operations
+    def __sub__(self, other: 'Vector') -> 'Vector':
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __add__(self, other: 'Vector') -> 'Vector':
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, scalar: float) -> 'Vector':
+        return Vector(self.x * scalar, self.y * scalar)
+
+    def __truediv__(self, scalar: float) -> 'Vector':
+        return Vector(self.x / scalar, self.y / scalar)
+
+    def __mul__(self, scalar: int) -> 'Vector':
+        return Vector(self.x * scalar, self.y * scalar)
+
+    def __truediv__(self, scalar: int) -> 'Vector':
+        return Vector(self.x / scalar, self.y / scalar)
