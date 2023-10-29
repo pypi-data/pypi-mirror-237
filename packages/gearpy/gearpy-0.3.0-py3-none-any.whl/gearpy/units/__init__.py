1097edc0030e6085cfacdffc516294f36d967e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 04:11:14 +0000

    Add package gearpy/0.3.0/gearpy-0.3.0-py3-none-any.whl

---
+++ packages/gearpy/gearpy-0.3.0-py3-none-any.whl/gearpy/units/__init__.py
@@ -0,0 +1,8 @@
+from .units import AngularPosition, \
+                   AngularSpeed, \
+                   AngularAcceleration, \
+                   InertiaMoment, \
+                   Torque, \
+                   Time, \
+                   TimeInterval
+from .unit_base import UnitBase
