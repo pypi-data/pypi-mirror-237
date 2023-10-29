5672230c6e76ac1fcb0ff237cb5a1fc7687a0538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 04:09:25 +0000

    Add package datallia/0.1.2/datallia-0.1.2.tar.gz

---
+++ packages/datallia/datallia-0.1.2.tar.gz/datallia-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datallia',
+    version='0.1.2',
+    packages=find_packages(),
+    url='',
+    license='MIT',
+    author='Pablo Florindo',
+    author_email='pabloflogon@gmail.com',
+    description='Simple data structure library with suitable __repr__ methods',
+    install_requires=[
+        # List your library dependencies here
+    ],
+)
