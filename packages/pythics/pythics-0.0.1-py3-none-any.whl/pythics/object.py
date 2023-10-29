f30a0609f8eb3ee25c15662904095c27f13249d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 04:10:38 +0000

    Add package pythics/0.0.1/pythics-0.0.1-py3-none-any.whl

---
+++ packages/pythics/pythics-0.0.1-py3-none-any.whl/pythics/object.py
@@ -0,0 +1,22 @@
+from pythics.vector import Vector
+
+class Object:
+    def __init__(self, position: Vector = Vector(0, 0), mass: int = 1, velocity: Vector = Vector(0, 0), acceleration: Vector = Vector(0, 0), normalForce: bool = False):
+        self.position: Vector = position
+        self.mass: int = mass
+        self.velocity: Vector = velocity
+        self.acceleration: Vector = acceleration
+        self.normalForce = normalForce
+
+    def addVelocity(self, velocity: Vector):
+        self.velocity += velocity
+
+    def update(self, dt):
+        if not self.normalForce:
+            self.velocity += (self.acceleration * dt) + (Vector(0, 100) * dt)
+        else:
+            self.velocity += (self.acceleration * dt)
+
+        print(f'velocity {self.velocity}')
+
+        self.position += self.velocity * dt
