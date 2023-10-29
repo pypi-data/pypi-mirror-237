2e7606d9f3b501d73379eaf9e5d18caf33708c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 03:08:54 +0000

    Add package user-MTM/1.0.1/user_MTM-1.0.1.tar.gz

---
+++ packages/user-MTM/user_MTM-1.0.1.tar.gz/user_MTM-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="user_MTM",
+    version="1.0.1",
+    description="Modulo di test utente per l'accesso al database mysql",
+    author="MTM_group",
+    packages=find_packages(),
+    install_requires=[
+        "mysql-connector-python",
+    ],
+)
