5c5ffc2281a5af347888297dd46b1d1645a29114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 04:10:59 +0000

    Add package biobabel/0.5.1/biobabel-0.5.1.tar.gz

---
+++ packages/biobabel/biobabel-0.5.1.tar.gz/biobabel-0.5.1/README.md
@@ -0,0 +1,89 @@
+
+
+# Biobabel : a unified interface for reading a plethora of physiology file formats
+
+Many different file formats exist for physiology signals such as cardiac (ECG, PPG) or respiratory data. Many packages exist in Python to read these formats but they load into different data structures. This package is a wrapper of sorts for already existing tools providing a unified easy-to-use interface.
+
+
+
+# Installation
+
+```
+python3 -m pip install biobabel
+```
+
+# Usage
+
+
+## Command line
+To preview a file, use the provided script from the command line:
+
+```
+biobabel <FILENAME>
+```
+
+You can leave `<FILENAME>` blank in which case you will be prompted to select a file you want to preview.
+
+
+## Python
+Simple usage example in Python:
+
+```
+import biobabel
+bio = biobabel.load('filename.txt')
+bio.print()
+bio.plot()
+bio.save('new_filename.hdf5')
+```
+
+More complete overview of functionality in [Jupyter notebook](https://github.com/florisvanvugt/biobabel/blob/main/tests/Usage.ipynb).
+
+
+# Requirements
+
+
+# Physiology dialects
+
+Currently supported dialects are:
+* hdphysio5 thanks to [hdphysio5](https://github.com/florisvanvugt/hdphysio5)
+* labstreaminglayer (LSL) XDF (rudimentary) thanks to [pyxdf](https://pypi.org/project/pyxdf/)
+* BioPAC Acknowledge (acq) thanks to [bioread](https://pypi.org/project/bioread/)
+* opensignals ("OpenSignals (r)evolution" thanks to [opensignalsreader](https://github.com/PGomes92/opensignalsreader)
+* Brams-Bio-Box (custom)
+* TeensyECG (custom)
+
+Wish-list:
+* EDF
+* Biosemi BDF https://pypi.org/project/pybdf/
+
+
+# Development
+
+Install from Github:
+
+```
+python -m pip install "biobabel @ git+https://github.com/florisvanvugt/biobabel"
+```
+
+
+Install locally:
+
+```
+pip install .
+```
+
+and editable:
+
+```
+pip install -e .
+```
+
+
+
+Create wheels with:
+
+```
+python3 -m build
+```
+
+
---
+++ packages/biobabel/biobabel-0.5.1.tar.gz/biobabel-0.5.1/src/biobabel/io.py
@@ -0,0 +1,79 @@
+
+import biobabel.load_teensyecg
+import biobabel.load_hdphysio5
+import biobabel.load_lsl
+import biobabel.load_acq
+import biobabel.load_opensignals
+import biobabel.load_bramsbiobox
+
+import os
+
+
+def load(fname,dialect=None):
+    """
+    Load physiology signal file.
+
+    fname : filename of the file to be read
+    dialect : the file format. If None, the format is guessed
+    """
+    
+    if not os.path.exists(fname):
+        print("File '{}' does not seem to exist.".format(fname))
+        return
+    
+    if dialect==None:
+        # Try to guess
+        dialect = guess_dialect(fname)
+        if dialect:
+            print("Guessed that this is {} format.".format(dialect))
+        
+    if dialect=="teensyecg":
+        return biobabel.load_teensyecg.load(fname)
+
+    if dialect=="hdphysio5":
+        return biobabel.load_hdphysio5.load(fname)
+
+    if dialect=="lsl":
+        return biobabel.load_lsl.load(fname)
+
+    if dialect=="acq":
+        return biobabel.load_acq.load(fname)
+
+    if dialect=="opensignals":
+        return biobabel.load_opensignals.load(fname)
+
+    if dialect=="bramsbiobox":
+        return biobabel.load_bramsbiobox.load(fname)
+    
+    return None # did not manage to load
+
+
+
+
+
+def guess_dialect(fname):
+
+    if fname.lower().endswith('.hdf5'):
+        return "hdphysio5" # guess
+    if fname.lower().endswith('.xdf'):
+        return "lsl" # guess
+    if fname.lower().endswith('.acq'):
+        return "acq" # guess
+
+    if fname.lower().endswith('.txt'):
+
+        # To guess the dialect, we have to actually probe the file itself
+        with open(fname) as f:
+            ln = f.readline()
+
+        if ln.find('OpenSignals')>-1:
+            return 'opensignals'
+  
+        
+        return "teensyecg" # guess
+
+
+    if fname.lower().endswith('.csv'):
+        #ln.find('Time(ms)')>-1 and ln.find('Xaccel')>-1 and ln.find('Gauge')>-1:
+        return 'bramsbiobox'
+
