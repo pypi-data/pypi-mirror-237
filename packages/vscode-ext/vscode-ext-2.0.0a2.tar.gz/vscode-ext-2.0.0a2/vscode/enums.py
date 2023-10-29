a8708b94ed852e8e33d0631bb4cc44fcb7bf5082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 03:10:20 +0000

    Add package vscode-ext/2.0.0a2/vscode-ext-2.0.0a2.tar.gz

---
+++ packages/vscode-ext/vscode-ext-2.0.0a2.tar.gz/vscode-ext-2.0.0a2/vscode/enums.py
@@ -0,0 +1,17 @@
+from enum import IntEnum
+
+__all__ = ("ViewColumn",)
+
+
+class ViewColumn(IntEnum):
+    Beside = -2
+    Active = -1
+    One = 1
+    Two = 2
+    Three = 3
+    Four = 4
+    Five = 5
+    Six = 6
+    Seven = 7
+    Eight = 8
+    Nine = 9
