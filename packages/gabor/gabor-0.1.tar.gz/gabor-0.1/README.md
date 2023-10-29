91cd0ea368d50fff15f5ae846eddfac9a4f8db99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-29 03:08:06 +0000

    Add package gabor/0.1/gabor-0.1.tar.gz

---
+++ packages/gabor/gabor-0.1.tar.gz/gabor-0.1/README.md
@@ -0,0 +1,1 @@
+Ciao, sono Luca Gabor
---
+++ packages/gabor/gabor-0.1.tar.gz/gabor-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='gabor',
+  version='0.1',
+  description='Libreria Python per trovare il numero primo più vicino ad una variabile (escludendo il caso in cui la variabile sia già un numero primo)',
+  author='Luca Gabor',
+  author_email='luca.gabor1010@gmail.com',
+  license='MIT',
+  packages=['C:\\Users\\lucag\\OneDrive\\Desktop\\gabor'],
+  install_requires=[],
+  python_requires='>=3.6',
+)
